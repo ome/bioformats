5a03b7900be8c6dfa406982683f397405e56fa0f
Curtis Rueden <ctrueden@wisc.edu>
2005-02-24 22:58:46 +0000

    Update version number for release with arbitrary slicing.

--- loci/visbio/VisBio.java
+++ loci/visbio/VisBio.java
@@ -44,7 +44,7 @@ public class VisBio {
   public static final String TITLE = "VisBio";
 
   /** Application version. */
-  public static final String VERSION = "v3.00 beta3d";
+  public static final String VERSION = "v3.00 beta3e";
 
   /** Application author. */
   public static final String AUTHOR = "Curtis Rueden, LOCI";
