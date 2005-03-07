26a0f7dce2f15113bc6ad1a011089378acf119d5
Curtis Rueden <ctrueden@wisc.edu>
2005-03-07 23:11:34 +0000

    Update version number, add build date.

--- loci/visbio/VisBio.java
+++ loci/visbio/VisBio.java
@@ -44,10 +44,13 @@ public class VisBio {
   public static final String TITLE = "VisBio";
 
   /** Application version. */
-  public static final String VERSION = "v3.00 beta3g";
+  public static final String VERSION = "v3.00 beta3h";
 
   /** Application author. */
   public static final String AUTHOR = "Curtis Rueden, LOCI";
+
+  /** Application build date. */
+  public static final String DATE = "7 March 2005";
 
 
   // -- Constructor --
