b976e67ccc8bc7e1c3d458ddc0a63363d1b86213
Curtis Rueden <ctrueden@wisc.edu>
2005-03-27 07:31:39 +0000

    Add Windows batch files for convenience.

---
+++ loci/visbio/bio.bat
@@ -0,0 +1,1 @@
+@java -mx896m -Dswing.defaultlaf=com.jgoodies.plaf.windows.ExtWindowsLookAndFeel loci.visbio.VisBio %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ loci/visbio/vbdebug.bat
@@ -0,0 +1,1 @@
+@java -mx1024m -Dvisbio.debug=true -Dswing.defaultlaf=com.jgoodies.plaf.plastic.Plastic3DLookAndFeel -verbose loci.visbio.VisBio %1 %2 %3 %4 %5 %6 %7 %8 %9 > debug.txt
