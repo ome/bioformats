f7169601ef14a587dc6b59adf376a64055e3abbd
Curtis Rueden <ctrueden@wisc.edu>
2005-04-05 18:49:17 +0000

    Update logo to 64x64, update version number, update what's new list.

--- loci/visbio/VisBio.java
+++ loci/visbio/VisBio.java
@@ -45,13 +45,13 @@ public class VisBio {
   public static final String TITLE = "VisBio";
 
   /** Application version. */
-  public static final String VERSION = "v3.00 beta4e";
+  public static final String VERSION = "v3.00 beta4f";
 
   /** Application author. */
   public static final String AUTHOR = "Curtis Rueden, LOCI";
 
   /** Application build date. */
-  public static final String DATE = "31 March 2005";
+  public static final String DATE = "5 April 2005";
 
 
   // -- Constructor --
