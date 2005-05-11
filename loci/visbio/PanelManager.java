192fb730bce8b7274716e0873a420c2a7726705e
Curtis Rueden <ctrueden@wisc.edu>
2005-05-11 18:42:06 +0000

    Remove floating control window logic.

--- loci/visbio/PanelManager.java
+++ loci/visbio/PanelManager.java
@@ -23,46 +23,21 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 package loci.visbio;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Vector;
 import javax.swing.*;
 import loci.visbio.help.HelpManager;
-import loci.visbio.state.BooleanOption;
-import loci.visbio.state.OptionManager;
 import loci.visbio.util.SwingUtil;
 
 /** PanelManager is the manager encapsulating VisBio's control panel logic. */
-public class PanelManager extends LogicManager implements ActionListener {
-
-  // -- Constants --
-
-  /** String for floating control windows option. */
-  public static final String FLOATING = "Floating control windows";
-
+public class PanelManager extends LogicManager {
 
   // -- GUI components --
 
   /** Control panels. */
   private Vector panels;
 
-  /** Floating control panel windows. */
-  private Vector floatWindows;
-
   /** Tabbed pane containing control panels. */
   private JTabbedPane tabs;
-
-  /** Shortcut buttons for bringing up floating control panel windows. */
-  private Vector buttons;
-
-  /** Pane containing shortcut buttons. */
-  private JPanel buttonPane;
-
-
-  // -- Other fields --
-
-  /** Whether control panels are located in separate, floating windows. */
-  private boolean floating;
 
 
   // -- Constructor --
@@ -82,20 +57,8 @@ public class PanelManager extends LogicManager implements ActionListener {
     WindowManager wm = (WindowManager) bio.getManager(WindowManager.class);
     wm.addWindow(w);
     panels.add(cpl);
-    floatWindows.add(w);
 
-    // create shortcut button
-    JButton b = new JButton(name);
-    b.addActionListener(this);
-    String tip = "Shows " + name + " panel";
-    b.setToolTipText(tip);
-    buttons.add(b);
-    buttonPane.add(b);
-
-    if (!floating) tabs.addTab(name, null, cpl, cpl.getTip());
-
-    bio.addMenuItem("Window", name,
-      "loci.visbio.PanelManager.windowShow(" + name + ")", name.charAt(0));
+    tabs.addTab(name, null, cpl, cpl.getTip());
 
     bio.generateEvent(this, "add " + name + " panel", false);
   }
@@ -107,8 +70,7 @@ public class PanelManager extends LogicManager implements ActionListener {
   public void repack(ControlPanel cpl) {
     int ndx = panels.indexOf(cpl);
     if (ndx < 0) return;
-    SwingUtil.repack((JFrame) floatWindows.elementAt(ndx));
-    if (!floating) SwingUtil.repack(bio);
+    SwingUtil.repack(bio);
   }
 
   /** Gets the control panel with the given name. */
@@ -130,27 +92,10 @@ public class PanelManager extends LogicManager implements ActionListener {
       Object src = evt.getSource();
       if (src == this) doGUI();
     }
-    else if (eventType == VisBioEvent.STATE_CHANGED) {
-      if (evt.getMessage().equals("add option")) return;
-      Object src = evt.getSource();
-      if (src instanceof OptionManager) {
-        OptionManager om = (OptionManager) src;
-        BooleanOption option = (BooleanOption) om.getOption(FLOATING);
-        setFloating(option.getValue());
-      }
-    }
   }
 
   /** Gets the number of tasks required to initialize this logic manager. */
   public int getTasks() { return 3; }
-
-
-  // -- ActionListener API methods --
-
-  /** Handles shortcut button presses. */
-  public void actionPerformed(ActionEvent e) {
-    windowShow(((JButton) e.getSource()).getText());
-  }
 
 
   // -- Helper methods --
@@ -159,69 +104,15 @@ public class PanelManager extends LogicManager implements ActionListener {
   private void doGUI() {
     bio.setSplashStatus("Initializing control panel logic");
     panels = new Vector();
-    floatWindows = new Vector();
 
     // control panel containers
     tabs = new JTabbedPane();
     bio.getContentPane().add(tabs);
 
-    // shortcut buttons for floating control panel windows
-    buttons = new Vector();
-    buttonPane = new JPanel();
-
-    // options menu
-    bio.setSplashStatus(null);
-    OptionManager om = (OptionManager) bio.getManager(OptionManager.class);
-    om.addBooleanOption("General", FLOATING, 'f',
-      "Toggles whether each control panel has its own window", floating);
-
     // help window
     bio.setSplashStatus(null);
     HelpManager hm = (HelpManager) bio.getManager(HelpManager.class);
     hm.addHelpTopic("Control panels", "control_panels.html");
-  }
-
-  /** Sets whether control panels are separate, floating windows. */
-  private void setFloating(boolean floating) {
-    if (this.floating == floating) return;
-    this.floating = floating;
-
-    JPanel pane = (JPanel) bio.getContentPane();
-    if (floating) {
-      pane.remove(tabs);
-      tabs.removeAll();
-      for (int i=0; i<panels.size(); i++) {
-        ControlPanel cpl = (ControlPanel) panels.elementAt(i);
-        JFrame w = (JFrame) floatWindows.elementAt(i);
-        w.setContentPane(cpl);
-      }
-      pane.add(buttonPane);
-    }
-    else {
-      pane.remove(buttonPane);
-      for (int i=0; i<panels.size(); i++) {
-        ControlPanel cpl = (ControlPanel) panels.elementAt(i);
-        JFrame w = (JFrame) floatWindows.elementAt(i);
-        w.hide();
-        tabs.addTab(cpl.getName(), null, cpl, cpl.getTip());
-      }
-      pane.add(tabs);
-    }
-    bio.pack();
-  }
-
-
-  // -- Menu commands --
-
-  /** Displays or switches to the given control panel. */
-  public void windowShow(String name) {
-    ControlPanel cpl = (ControlPanel) getPanel(name);
-    int ndx = panels.indexOf(cpl);
-    if (floating) {
-      WindowManager wm = (WindowManager) bio.getManager(WindowManager.class);
-      wm.showWindow((JFrame) floatWindows.elementAt(ndx));
-    }
-    else tabs.setSelectedIndex(ndx);
   }
 
 }
