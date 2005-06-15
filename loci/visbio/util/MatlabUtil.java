816728bebb4700bf9d4fdceab4630dea2f15ad1c
Curtis Rueden <ctrueden@wisc.edu>
2005-06-15 19:04:32 +0000

    Remove unnecessary import statement.

--- loci/visbio/util/MatlabUtil.java
+++ loci/visbio/util/MatlabUtil.java
@@ -24,7 +24,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 package loci.visbio.util;
 
 import loci.visbio.math.*;
-import visad.*;
 
 /** MatlabUtil contains useful MATLAB and Octave functions. */
 public abstract class MatlabUtil {
