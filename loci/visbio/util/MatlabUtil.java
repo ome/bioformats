733653a13d2c903a81eb28c90549e524a247797a
Curtis Rueden <ctrueden@wisc.edu>
2005-06-17 20:27:32 +0000

    Initial implementation of MATLAB/Octave function transform.

--- loci/visbio/util/MatlabUtil.java
+++ loci/visbio/util/MatlabUtil.java
@@ -23,10 +23,20 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 package loci.visbio.util;
 
+import java.rmi.RemoteException;
+import java.util.StringTokenizer;
 import loci.visbio.math.*;
+import visad.*;
 
 /** MatlabUtil contains useful MATLAB and Octave functions. */
 public abstract class MatlabUtil {
+
+  // -- Constants --
+
+  /** Variable naming scheme for MATLAB return values. */
+  protected static final String VAR = "visbioValue";
+
+
 
   // -- MATLAB methods --
 
@@ -73,40 +83,160 @@ public abstract class MatlabUtil {
   // -- FlatField methods --
 
   /**
-   * Converts FlatField samples to a series of
-   * 2D image planes (one per range component).
+   * Executes the given MATLAB/Octave function, converting the specified VisAD
+   * FlatField into a series of 2D double arrays. Returns results as a new
+   * FlatField.
    */
-  public static double[][][] samplesToDoubles(double[][] samples,
-    int xLen, int yLen)
-  {
-    double[][][] d = new double[samples.length][][];
-    for (int i=0; i<samples.length; i++) {
-      d[i] = unrasterize(samples[i], xLen, yLen);
+  public static FlatField exec(String function, FlatField ff) {
+    Set set = ff.getDomainSet();
+    if (!(set instanceof GriddedSet)) return null;
+    GriddedSet gset = (GriddedSet) set;
+    int[] len = gset.getLengths();
+    double[][] samples = null;
+    try { samples = ff.getValues(false); }
+    catch (VisADException exc) {
+      exc.printStackTrace();
+      return null;
     }
-    return d;
-  }
 
-  /** Converts a series of 2D image planes to a FlatField samples array. */
-  public static double[][] doublesToSamples(double[][][] d) {
-    double[][] samples = new double[d.length][];
-    for (int i=0; i<d.length; i++) samples[i] = rasterize(d[i]);
-    return samples;
-  }
+    int num = samples.length;
+    int cols = len[0];
+    int rows = len[1];
+    int size = cols * rows;
 
-  /** Converts a rasterized 1D array into a properly dimensioned 2D array. */
-  public static double[][] unrasterize(double[] arr, int xLen, int yLen) {
-    double[][] d = new double[yLen][xLen];
-    for (int y=0; y<yLen; y++) System.arraycopy(arr, y * xLen, d[y], 0, xLen);
-    return d;
-  }
+    // convert arguments into command string
+    StringBuffer sb = new StringBuffer();
+    sb.append(function);
+    sb.append("(");
+    for (int i=0; i<num; i++) {
+      if (i > 0) sb.append(",");
+      sb.append("[");
+      for (int j=0; j<size; j++) {
+        if (j > 0 && j % len[0] == 0) sb.append(";");
+        sb.append(" ");
+        sb.append(samples[i][j]);
+      }
+      sb.append("]");
+    }
+    sb.append(")");
+    String command = sb.toString();
+    /*TEMP*/System.out.println("Octave/MATLAB command = " + command);
 
-  /** Converts a 2D array into a rasterized 1D array. */
-  public static double[] rasterize(double[][] arr) {
-    int xLen = arr[0].length;
-    int yLen = arr.length;
-    double[] d = new double[xLen * yLen];
-    for (int y=0; y<yLen; y++) System.arraycopy(arr[y], 0, d, y * xLen, xLen);
-    return d;
+    double[][] samps = null;
+
+    OctaveContext octave = getOctave();
+    if (octave != null) { // try Octave
+      // execute command
+      OctaveValueList ovl = octave.evalString(command);
+
+      // convert result to samples array
+      samps = new double[ovl.size()][];
+      for (int i=0; i<samps.length; i++) {
+        OctaveMatrix m = ovl.get(i).matrixValue();
+        samps[i] = m.toArray(); // samples are already rasterized
+      }
+    }
+
+    MatlabControl matlab = getMatlab();
+    if (samps == null && matlab != null) { // try MATLAB
+      // prepend return value variables
+      sb = new StringBuffer();
+      sb.append("[");
+      for (int i=0; i<num; i++) {
+        sb.append(" ");
+        sb.append(VAR);
+        sb.append(i);
+      }
+      sb.append("] = ");
+      sb.append(command);
+      command = sb.toString();
+
+      // execute command
+      String ans = null;
+      try { ans = matlab.blockingEval(command); }
+      catch (InterruptedException exc) {
+        exc.printStackTrace();
+        return null;
+      }
+
+      // convert result to samples array
+      samps = new double[num][size];
+      StringTokenizer st = new StringTokenizer(ans);
+      String matlabError = "Invalid MATLAB output: expected ";
+      int v = -1, r = -1, c = -1, clo = -1, chi = -1;
+      while (st.hasMoreTokens()) {
+        String token = st.nextToken();
+        if (token.startsWith(VAR)) {
+          // parse variable name
+          token = token.substring(VAR.length());
+          int n = -1;
+          try { n = Integer.parseInt(token); }
+          catch (NumberFormatException exc) { }
+
+          String eq = st.hasMoreTokens() ? st.nextToken() : "";
+
+          if (n != v + 2 || !eq.equals("=")) {
+            System.err.println(matlabError + "variable assignment");
+            return null;
+          }
+
+          v++;
+          r = 0;
+          c = 0;
+        }
+        else if (token.equals("Columns")) {
+          // parse columns range
+          String first = st.hasMoreTokens() ? st.nextToken() : "";
+          String through = st.hasMoreTokens() ? st.nextToken() : "";
+          String last = st.hasMoreTokens() ? st.nextToken() : "";
+
+          clo = -1;
+          try { clo = Integer.parseInt(first); }
+          catch (NumberFormatException exc) { }
+
+          chi = -1;
+          try { chi = Integer.parseInt(last); }
+          catch (NumberFormatException exc) { }
+
+          if (clo != c + 1 || clo < chi || !through.equals("through")) {
+            System.err.println(matlabError + "columns range");
+            return null;
+          }
+        }
+        else if (v < 0) {
+          System.err.println(matlabError + "variable assignment");
+          return null;
+        }
+        else {
+          // parse matrix entry
+          double value = Double.NaN;
+          try { value = Double.parseDouble(token); }
+          catch (NumberFormatException exc) { }
+
+          if (value != value) {
+            System.err.println(matlabError + "matrix entry");
+            return null;
+          }
+
+          samps[v][r * cols + c] = value;
+
+          c++;
+          if (c > chi) {
+            r++;
+            c = clo;
+          }
+        }
+      }
+    }
+
+    try {
+      FlatField result = new FlatField((FunctionType) ff.getType(), set);
+      result.setSamples(samps, false);
+      return result;
+    }
+    catch (VisADException exc) { exc.printStackTrace(); }
+    catch (RemoteException exc) { exc.printStackTrace(); }
+    return null;
   }
 
 }
