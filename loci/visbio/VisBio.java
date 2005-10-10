ccbcfd438cdbd072a25a4e5387a8b09000c41fe2
Curtis Rueden <ctrueden@wisc.edu>
2005-10-10 20:52:53 +0000

    Update to v3.11 with list of bugfixes.

--- loci/visbio/VisBio.java
+++ loci/visbio/VisBio.java
@@ -45,13 +45,13 @@ public class VisBio {
   public static final String TITLE = "VisBio";
 
   /** Application version. */
-  public static final String VERSION = "v3.10";
+  public static final String VERSION = "v3.11";
 
   /** Application author. */
   public static final String AUTHOR = "Curtis Rueden, LOCI";
 
   /** Application build date. */
-  public static final String DATE = "3 October 2005";
+  public static final String DATE = "11 October 2005";
 
   /** Port to use for communicating between application instances. */
   public static final int INSTANCE_PORT = 0xabcd;
