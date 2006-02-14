2d04f5a56982b520566f765d9e7edc3800790d9f
Melissa Linkert <melissa@glencoesoftware.com>
2006-02-14 15:10:22 +0000

    added TiffWriter and file listing available format writers

--- NativeQTReader.java
+++ NativeQTReader.java
@@ -34,7 +34,7 @@ import java.util.Vector;
  *
  * Currently, compressed movies are not supported, although video codecs will
  * be added as time permits.
- * 
+ *
  * @author Melissa Linkert linkert at cs.wisc.edu
  */
 public class NativeQTReader extends FormatReader {
@@ -43,12 +43,12 @@ public class NativeQTReader extends FormatReader {
 
   /** List of identifiers for each container atom. */
   private static final String[] CONTAINER_TYPES = {
-    "moov", "trak", "udta", "tref", "imap", "mdia", "minf", "stbl", "edts", 
+    "moov", "trak", "udta", "tref", "imap", "mdia", "minf", "stbl", "edts",
     "mdra", "rmra", "imag", "vnrp", "dinf"
-  };	    
-       
+  };
+
   // -- Fields --
- 
+
   /** Current file. */
   private RandomAccessFile in;
 
@@ -57,10 +57,10 @@ public class NativeQTReader extends FormatReader {
 
   /** Number of images in the file. */
   private int numImages;
-  
+
   /** Pixel data stored in "mdat" atom. */
   private byte[] pixels;
-  
+
   /** Width of a single plane. */
   private int width;
 
@@ -69,27 +69,27 @@ public class NativeQTReader extends FormatReader {
 
   /** Pixel depth. */
   private int bitsPerPixel;
- 
+
   /** Raw plane size, in bytes. */
   private int rawSize;
-  
+
   /** Offsets to each plane's pixel data. */
   private Vector offsets;
-  
+
   /** Pixel data for the previous image plane. */
   private byte[] prevPixels;
- 
+
   /** Video codec used by this movie. */
   private String codec;
-  
+
   // -- Constructor --
 
   /** Constructs a new QuickTime reader. */
   public NativeQTReader() { super("Native QuickTime", "mov"); }
-  
-  
+
+
   // -- FormatReader API methods --
- 
+
   /** Checks if the given block is a valid header for a QuickTime file. */
   public boolean isThisType(byte[] block) {
     return false;
@@ -99,30 +99,30 @@ public class NativeQTReader extends FormatReader {
   public int getImageCount(String id) throws FormatException, IOException {
     if(!id.equals(currentId)) initFile(id);
     return numImages;
-  }        
+  }
 
   /** Obtains the specified image from the given QuickTime file. */
   public Image open(String id, int no) throws FormatException, IOException {
     if (!id.equals(currentId)) initFile(id);
     if (no < 0 || no >= getImageCount(id)) {
       throw new FormatException("Invalid image number: " + no);
-    }       
+    }
     int offset = ((Integer) offsets.get(no)).intValue();
     int nextOffset = pixels.length;
     if (no < offsets.size() - 1) {
       nextOffset = ((Integer) offsets.get(no+1)).intValue();
-    }        
+    }
     byte[] pixs = new byte[nextOffset - offset];
 
     for (int i=0; i<pixs.length; i++) {
       pixs[i] = pixels[offset + i];
-    }        
-    
+    }
+
     byte[] bytes = uncompress(pixs, codec);
     prevPixels = bytes;
-    
-    if (bitsPerPixel == 8) {
-      return DataTools.makeImage(bytes, width, height, 1, false);   
+
+    if (bitsPerPixel <= 8) {
+      return DataTools.makeImage(bytes, width, height, 1, false);
     }
     else if (bitsPerPixel == 16) {
       short[] newPix = new short[bytes.length / 2 + 1];
@@ -135,20 +135,20 @@ public class NativeQTReader extends FormatReader {
       int[] newPix = new int[bytes.length / 4];
       for (int i=0; i<bytes.length; i+=4) {
         newPix[i/4] = DataTools.bytesToInt(bytes, i, little);
-      }        
+      }
       return DataTools.makeImage(newPix, width, height, 1, false);
     }
     else {
       throw new FormatException("Sorry, " + bitsPerPixel + " bits per pixel" +
         " not supported.");
-    }  
-  }        
- 
+    }
+  }
+
   /** Closes any open files. */
   public void close() throws FormatException, IOException {
     if (in != null) in.close();
     in = null;
-    currentId = null;      
+    currentId = null;
   }
 
   /** Initializes the given QuickTime file. */
@@ -158,18 +158,18 @@ public class NativeQTReader extends FormatReader {
     offsets = new Vector();
     parse(0, 0, in.length());
     numImages = offsets.size();
-  }        
- 
+  }
+
 
   // -- Helper methods --
-  
+
   /** Parse all of the atoms in the file. */
   public void parse(int depth, long offset, long length) throws IOException {
-    while (offset < length) {   
+    while (offset < length) {
       in.seek(offset);
       // first 4 bytes are the atom size
       long atomSize = DataTools.read4UnsignedBytes(in, little);
-      
+
       // read the atom type
       byte[] four = new byte[4];
       in.read(four);
@@ -178,30 +178,30 @@ public class NativeQTReader extends FormatReader {
       // if atomSize is 1, then there is an 8 byte extended size
       if (atomSize == 1) {
         atomSize = DataTools.read8SignedBytes(in, little);
-      }	      
+      }
 
       if (atomSize < 0) {
-        System.out.println("Invalid atom size : " + atomSize);	      
+        System.out.println("Invalid atom size : " + atomSize);
       }
-      
+
       byte[] data = new byte[0];
-    
+
       // if this is a container atom, parse the children
       if (isContainer(atomType)) {
-        parse(depth++, in.getFilePointer(), offset + atomSize);  
-      }	     
+        parse(depth++, in.getFilePointer(), offset + atomSize);
+      }
       else {
-        if (atomSize == 0) atomSize = in.length();      
+        if (atomSize == 0) atomSize = in.length();
         data = new byte[(int) atomSize];
-	in.read(data);
-   
+        in.read(data);
+
         if (atomType.equals("mdat")) {
           // we've found the pixel data
           pixels = data;
         }
         else if (atomType.equals("tkhd")) {
-          // we've found the dimensions     
-          
+          // we've found the dimensions
+
           int off = 74;
           width = DataTools.bytesToInt(data, off, little);
           off += 4;
@@ -210,21 +210,21 @@ public class NativeQTReader extends FormatReader {
         else if (atomType.equals("stco")) {
           // we've found the plane offsets
 
-          int numPlanes = DataTools.bytesToInt(data, 4, little); 
+          int numPlanes = DataTools.bytesToInt(data, 4, little);
           if (numPlanes != numImages) {
             int off = DataTools.bytesToInt(data, 8, little);
             offsets.add(new Integer(off));
             for (int i=1; i<numImages; i++) {
               offsets.add(new Integer(off + i*rawSize));
-            }      
+            }
           }
           else {
             int j = 8;
             for (int i=0; i<numPlanes; i++) {
               offsets.add(new Integer(DataTools.bytesToInt(data, j, little)));
               j += 4;
-            } 
-          }  
+            }
+          }
         }
         else if (atomType.equals("stsd")) {
           // found video codec and pixel depth information
@@ -233,25 +233,25 @@ public class NativeQTReader extends FormatReader {
         }
         else if (atomType.equals("stsz")) {
           // found the number of planes
-          rawSize = DataTools.bytesToInt(data, 4, 4, little); 
+          rawSize = DataTools.bytesToInt(data, 4, 4, little);
           numImages = DataTools.bytesToInt(data, 8, 4, little);
         }
-      }	      
-      
+      }
+
       if (atomSize == 0) offset = in.length();
       else offset += atomSize;
 
       // if a 'udta' atom, skip ahead 4 bytes
       if (atomType.equals("udta")) offset += 4;
       //print(depth, atomSize, atomType, data);
-    }	  
+    }
   }
 
   /** Checks if the given String is a container atom type. */
   public boolean isContainer(String type) {
     for(int i=0; i<CONTAINER_TYPES.length; i++) {
       if(type.equals(CONTAINER_TYPES[i])) return true;
-    }	    
+    }
     return false;
   }
 
@@ -264,19 +264,29 @@ public class NativeQTReader extends FormatReader {
   /** Uncompresses an image plane according to the the codec identifier. */
   public byte[] uncompress(byte[] pixs, String code) throws FormatException {
     if (code.equals("raw ")) return pixs;
-    else if (code.equals("rle ")) return rleUncompress(pixs);        
+    else if (code.equals("rle ")) return rleUncompress(pixs);
     else {
       throw new FormatException("Sorry, " + codec + " codec is not supported");
-    }        
+    }
   }
- 
+
   /** Uncompresses a QT RLE compressed image plane. */
   public byte[] rleUncompress(byte[] pixs) throws FormatException {
-    int pt = 0;
+    // -- TODO -- broken
+
+    /* debug */
+    // print out the first 30 bytes
+    System.out.println();
+    for (int i=0; i<30; i++) {
+      System.out.print(pixs[i] + " ");
+    }
+    System.out.println();
+
+    int pt = 8;
     long size = DataTools.bytesToInt(pixs, pt, 4, little);
     pt += 4;
     if (size < 0) size += 4294967296L;
-    
+
     // this frame is the same as the one before it
     if (size < 8) return prevPixels;
 
@@ -284,43 +294,43 @@ public class NativeQTReader extends FormatReader {
     pt += 2;
 
     Vector output = new Vector();
-    
+
     if (header == 0) {
-      int skip = 1;  // always one more than the number of bytes to skip      
-      while (skip != 0) {      
+      int skip = 1;  // always one more than the number of bytes to skip
+      while (skip != 0) {
         skip = pixs[pt];
         pt++;
-        int rleCode = 0;  
-      
+        int rleCode = 0;
+
         while (rleCode != -1) {
           if (skip == 0) break;  // finished decoding
           pt += skip;
           rleCode = pixs[pt];
           pt++;
-        
+
           if (rleCode > 0) {
-            // copy the pixels directly to output buffer      
+            // copy the pixels directly to output buffer
             for (int i=0; i<rleCode; i++) {
               output.add(new Byte(pixs[pt]));
               pt++;
-            }      
+            }
           }
           else if (rleCode < -1) {
             // grab next byte and repeat it -(rleCode) times
             for (int i=0; i<-1*rleCode; i++) {
-              output.add(new Byte(pixs[pt]));        
-            }        
+              output.add(new Byte(pixs[pt]));
+            }
           }
           else if (rleCode == 0) {
             skip = pixs[pt];
             pt++;
           }
         }
-      }  
+      }
     }
     else {
       throw new FormatException("Sorry, unsupported header : " + header);
-    }         
+    }
 
     byte[] toRtn = new byte[output.size()];
     for (int i=0; i<toRtn.length; i++) {
@@ -328,10 +338,10 @@ public class NativeQTReader extends FormatReader {
     }
     return toRtn;
   }
-  
+
   // -- Main method --
-  
+
   public static void main(String[] args) throws FormatException, IOException {
-    new NativeQTReader().testRead(args);  
+    new NativeQTReader().testRead(args);
   }
 }
