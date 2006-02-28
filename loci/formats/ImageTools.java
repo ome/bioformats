751c73a04a80a14809532cb8d9cbed91219598bd
Curtis Rueden <ctrueden@wisc.edu>
2006-02-28 20:20:05 +0000

    Add a bunch of utility methods for image scaling and related functions.

--- loci/formats/ImageTools.java
+++ loci/formats/ImageTools.java
@@ -25,10 +25,14 @@ package loci.formats;
 
 import java.awt.*;
 import java.awt.color.ColorSpace;
+import java.awt.geom.AffineTransform;
 import java.awt.image.*;
 
 /**
  * A utility class with convenience methods for manipulating images.
+ *
+ * Much code was stolen and adapted from DrLaszloJamf's posts at:
+ *   http://forum.java.sun.com/thread.jspa?threadID=522483
  *
  * @author Curtis Rueden ctrueden at wisc.edu
  */
@@ -40,7 +44,115 @@ public abstract class ImageTools {
   protected static final Component OBS = new Container();
 
 
+  // -- Image scaling --
+
+  /** Copies the source image into the target, applying scaling. */
+  public static BufferedImage copy(BufferedImage source, BufferedImage target,
+    Object hint)
+  {
+    Graphics2D g2 = target.createGraphics();
+    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+      RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+    double scalex = (double) target.getWidth() / source.getWidth();
+    double scaley = (double) target.getHeight() / source.getHeight();
+    AffineTransform xform = AffineTransform.getScaleInstance(scalex, scaley);
+    g2.drawRenderedImage(source, xform);
+    g2.dispose();
+    return target;
+  }
+
+  /**
+   * Scales the image using the Java2D API, with the resultant
+   * image optimized for the given graphics configuration.
+   */
+  public static BufferedImage scale2D(BufferedImage image,
+    int width, int height, Object hint, GraphicsConfiguration gc)
+  {
+    if (gc == null) gc = getDefaultConfiguration();
+    int trans = image.getColorModel().getTransparency();
+    return copy(image, gc.createCompatibleImage(width, height, trans), hint);
+  }
+
+  /**
+   * Scales the image using the Java2D API, with the
+   * resultant image having the given color model.
+   */
+  public static BufferedImage scale2D(BufferedImage image,
+    int width, int height, Object hint, ColorModel cm)
+  {
+    WritableRaster raster = cm.createCompatibleWritableRaster(width, height);
+    boolean isRasterPremultiplied = cm.isAlphaPremultiplied();
+    return copy(image, new BufferedImage(cm,
+      raster, isRasterPremultiplied, null), hint);
+  }
+
+  /** Scales the image using the AWT Image API. */
+  public static Image scaleAWT(BufferedImage source, int width,
+    int height, int hint)
+  {
+    return source.getScaledInstance(width, height, hint);
+  }
+
+  /**
+   * Scales the image using the most appropriate API, with the resultant image
+   * having the same color model as the original image.
+   */
+  public static BufferedImage scale(BufferedImage source,
+    int width, int height)
+  {
+    int w = source.getWidth();
+    int h = source.getHeight();
+    if (w == width && h == height) return source;
+    if ((width * height) / (w * h) > 0) {
+      // use Java2D to enlarge
+      return scale2D(source, width, height,
+        RenderingHints.VALUE_INTERPOLATION_BICUBIC, source.getColorModel());
+    }
+    else {
+      // use AWT to shrink
+      return makeImage(scaleAWT(source, width, height,
+        Image.SCALE_AREA_AVERAGING), source.getColorModel());
+    }
+  }
+
+
   // -- Image utilities --
+
+  /** Gets the default graphics configuration for the environment. */
+  public static GraphicsConfiguration getDefaultConfiguration() {
+    GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    GraphicsDevice gd = ge.getDefaultScreenDevice();
+    return gd.getDefaultConfiguration();
+  }
+
+  /**
+   * Creates a copy of the given buffered image that is
+   * optimized for the given graphics configuration.
+   */
+  public static BufferedImage makeCompatible(BufferedImage image,
+    GraphicsConfiguration gc)
+  {
+    if (gc == null) gc = getDefaultConfiguration();
+    int w = image.getWidth();
+    int h = image.getHeight();
+    int transparency = image.getColorModel().getTransparency();
+    BufferedImage result = gc.createCompatibleImage(w, h, transparency);
+    Graphics2D g2 = result.createGraphics();
+    g2.drawRenderedImage(image, null);
+    g2.dispose();
+    return result;
+  }
+
+  /** Ensures the given AWT image is fully loaded. */
+  public static boolean loadImage(Image image) {
+    if (image instanceof BufferedImage) return true;
+    MediaTracker tracker = new MediaTracker(OBS);
+    tracker.addImage(image, 0);
+    try { tracker.waitForID(0); }
+    catch (InterruptedException exc) { return false; }
+    if (MediaTracker.COMPLETE != tracker.statusID(0, false)) return false;
+    return true;
+  }
 
   /**
    * Gets the width and height of the given AWT image,
@@ -52,17 +164,11 @@ public abstract class ImageTools {
       BufferedImage bi = (BufferedImage) image;
       return new Dimension(bi.getWidth(), bi.getHeight());
     }
-    MediaTracker tracker = new MediaTracker(OBS);
-    tracker.addImage(image, 0);
-    try { tracker.waitForID(0); }
-    catch (InterruptedException exc) { exc.printStackTrace(); }
+    loadImage(image);
     return new Dimension(image.getWidth(OBS), image.getHeight(OBS));
   }
 
-  /**
-   * Gets the pixel data from the given AWT image
-   * as an int array.
-   */
+  /** Gets the pixel data from the given AWT image as an int array. */
   public static int[] getIntPixels(Image image, int xDim, int yDim) {
     BufferedImage img = makeImage(image);
 
@@ -110,10 +216,7 @@ public abstract class ImageTools {
     return values[0];
   }
 
-  /**
-   * Gets the pixel data from the given AWT image
-   * as a byte array.
-   */
+  /** Gets the pixel data from the given AWT image as a byte array. */
   public static byte[] getPixels(Image image, int xDim, int yDim) {
     int[] values = getIntPixels(image, xDim, yDim);
     byte[] buf = new byte[xDim * yDim];
@@ -140,11 +243,27 @@ public abstract class ImageTools {
    */
   public static BufferedImage makeImage(Image image) {
     if (image instanceof BufferedImage) return (BufferedImage) image;
-    Dimension dim = getSize(image);
-    int w = dim.width, h = dim.height;
-    BufferedImage result = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
-    Graphics g = result.createGraphics();
-    g.drawImage(image, w, h, OBS);
+    return makeImage(image, makeColorModel(3, DataBuffer.TYPE_INT));
+  }
+
+  /**
+   * Creates a buffered image possessing the given color model,
+   * from the specified AWT image object. If the AWT image is already a
+   * buffered image with the given color model, no new object is created.
+   */
+  public static BufferedImage makeImage(Image image, ColorModel cm) {
+    if (image instanceof BufferedImage) {
+      BufferedImage bi = (BufferedImage) image;
+      if (cm.equals(bi.getColorModel())) return bi;
+    }
+    loadImage(image);
+    int w = image.getWidth(OBS), h = image.getHeight(OBS);
+    boolean alphaPremultiplied = cm.isAlphaPremultiplied();
+    WritableRaster raster = cm.createCompatibleWritableRaster(w, h);
+    BufferedImage result = new BufferedImage(cm,
+      raster, alphaPremultiplied, null);
+    Graphics2D g = result.createGraphics();
+    g.drawImage(image, 0, 0, OBS);
     g.dispose();
     return result;
   }
