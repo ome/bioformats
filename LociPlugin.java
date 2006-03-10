478e131633e572b2a88a275d8e1bd09975133608
Curtis Rueden <ctrueden@wisc.edu>
2006-03-10 19:57:09 +0000

    Make LociPlugin part of loci.formats package.

--- LociPlugin.java
+++ LociPlugin.java
@@ -21,6 +21,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+package loci.formats;
+
 import ij.*;
 import ij.io.OpenDialog;
 import ij.plugin.PlugIn;
