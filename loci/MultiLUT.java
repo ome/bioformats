d3560cf43f33592c964cabbb057bed37fea39a07
Curtis Rueden <ctrueden@wisc.edu>
2006-05-18 20:20:15 +0000

    Fix variable names.

--- loci/MultiLUT.java
+++ loci/MultiLUT.java
@@ -32,9 +32,6 @@ import visad.java3d.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.event.*;
-import javax.swing.text.*;
-import javax.swing.border.*;
 import java.io.IOException;
 import java.rmi.RemoteException;
 
@@ -44,18 +41,18 @@ public class MultiLUT extends Object implements ActionListener {
 
   private TextType text = null;
   private RealType element = null, line = null, value = null;
-  private TupleType text_tuple = null;
+  private TupleType textTuple = null;
 
-  private float[][] data_values = null;
+  private float[][] dataValues = null;
   private float[][] values = null;
-  private float[][] wedge_samples = null;
+  private float[][] wedgeSamples = null;
 
   private FlatField data = null;
-  private FieldImpl text_field = null;
+  private FieldImpl textField = null;
   private FlatField wedge = null;
 
-  private DataReferenceImpl[] value_refs = null;
-  private DataReferenceImpl[] hue_refs = null;
+  private DataReferenceImpl[] valueRefs = null;
+  private DataReferenceImpl[] hueRefs = null;
 
   private int npixels = 0;
 
@@ -67,7 +64,7 @@ public class MultiLUT extends Object implements ActionListener {
   ScalarMap hmap = null;
   ScalarMap huexmap = null;
 
-  private DataReferenceImpl line_ref = null;
+  private DataReferenceImpl lineRef = null;
   /**
       run with 'java -mx256m MultiLUT'
       in directory with SPB1.PIC, SPB2.PIC, ..., SPB17.PIC
@@ -93,7 +90,7 @@ public class MultiLUT extends Object implements ActionListener {
     Unit unit = null;
     String name = null;
     Set set = null;
-    RealType[] value_types = new RealType[NFILES];
+    RealType[] valueTypes = new RealType[NFILES];
     values = new float[NFILES][];
 
     DefaultFamily loader = new DefaultFamily("loader");
@@ -118,56 +115,56 @@ set = Linear2DSet: Length = 393216
         unit = value.getDefaultUnit();
         name = value.getName();
       }
-      value_types[i] = RealType.getRealType(name + (i+1), unit);
+      valueTypes[i] = RealType.getRealType(name + (i+1), unit);
       float[][] temps = ff.getFloats(false);
       values[i] = temps[0];
-      // System.out.println("data " + i + " type: " + value_types[i]);
+      // System.out.println("data " + i + " type: " + valueTypes[i]);
     }
 
     npixels = values[0].length;
 
-    RealTupleType range = new RealTupleType(value_types);
-    FunctionType big_func = new FunctionType(domain, range);
-    final FlatField big_data = new FlatField(big_func, set);
-    big_data.setSamples(values, false);
+    RealTupleType range = new RealTupleType(valueTypes);
+    FunctionType bigFunc = new FunctionType(domain, range);
+    final FlatField bigData = new FlatField(bigFunc, set);
+    bigData.setSamples(values, false);
 
     // RealType value = RealType.getRealType("value");
     RealType hue = RealType.getRealType("hue");
-    RealType HUE = RealType.getRealType("HUE");
-    RealTupleType new_range = new RealTupleType(value, hue);
-    FunctionType new_func = new FunctionType(domain, new_range);
-    data = new FlatField(new_func, set);
-    data_values = new float[2][npixels];
+    RealType bigHue = RealType.getRealType("HUE");
+    RealTupleType newRange = new RealTupleType(value, hue);
+    FunctionType newFunc = new FunctionType(domain, newRange);
+    data = new FlatField(newFunc, set);
+    dataValues = new float[2][npixels];
     DataReferenceImpl ref1 = new DataReferenceImpl("ref1");
     ref1.setData(data);
 
     text = TextType.getTextType("text");
     RealType[] time = {RealType.Time};
-    RealTupleType time_type = new RealTupleType(time);
+    RealTupleType timeType = new RealTupleType(time);
     MathType[] mtypes = {element, line, text};
-    text_tuple = new TupleType(mtypes);
-    FunctionType text_function = new FunctionType(RealType.Time, text_tuple);
-    Set time_set = new Linear1DSet(time_type, 0.0, 1.0, 2);
-    text_field = new FieldImpl(text_function, time_set);
-    DataReferenceImpl text_ref = new DataReferenceImpl("text_ref");
-    text_ref.setData(text_field);
+    textTuple = new TupleType(mtypes);
+    FunctionType textFunction = new FunctionType(RealType.Time, textTuple);
+    Set timeSet = new Linear1DSet(timeType, 0.0, 1.0, 2);
+    textField = new FieldImpl(textFunction, timeSet);
+    DataReferenceImpl textRef = new DataReferenceImpl("textRef");
+    textRef.setData(textField);
 
-    Linear2DSet wedge_set =
+    Linear2DSet wedgeSet =
       new Linear2DSet(domain, 0.0, 767.0, 768, 550.0, 570.0, 21);
-    wedge = new FlatField(new_func, wedge_set);
-    wedge_samples = new float[2][768 * 21];
-    DataReferenceImpl wedge_ref = new DataReferenceImpl("wedge_ref");
-    wedge_ref.setData(wedge);
+    wedge = new FlatField(newFunc, wedgeSet);
+    wedgeSamples = new float[2][768 * 21];
+    DataReferenceImpl wedgeRef = new DataReferenceImpl("wedgeRef");
+    wedgeRef.setData(wedge);
 
     final DataReferenceImpl xref = new DataReferenceImpl("xref");
 
-    // System.out.println("data type: " + new_func);
+    // System.out.println("data type: " + newFunc);
 
     display1 =
       new DisplayImplJ3D("display1", new TwoDDisplayRendererJ3D());
     ScalarMap xmap = new ScalarMap(element, Display.XAxis);
     display1.addMap(xmap);
-    huexmap = new ScalarMap(HUE, Display.XAxis);
+    huexmap = new ScalarMap(bigHue, Display.XAxis);
     display1.addMap(huexmap);
     ScalarMap ymap = new ScalarMap(line, Display.YAxis);
     display1.addMap(ymap);
@@ -182,10 +179,10 @@ set = Linear2DSet: Length = 393216
 
     Control ctrl = textmap.getControl();
     if (ctrl != null && ctrl instanceof TextControl) {
-      TextControl text_control = (TextControl) ctrl;
-      text_control.setSize(1.0);
-      text_control.setJustification(TextControl.Justification.CENTER);
-      text_control.setAutoSize(true);
+      TextControl textControl = (TextControl) ctrl;
+      textControl.setSize(1.0);
+      textControl.setJustification(TextControl.Justification.CENTER);
+      textControl.setAutoSize(true);
     }
 
     // display1.getGraphicsModeControl().setScaleEnable(true);
@@ -196,27 +193,27 @@ set = Linear2DSet: Length = 393216
     display1.addReferences(renderer, xref);
     renderer.suppressExceptions(true);
 
-    DefaultRendererJ3D text_renderer = new DefaultRendererJ3D();
-    display1.addReferences(text_renderer, text_ref);
-    text_renderer.suppressExceptions(true);
+    DefaultRendererJ3D textRenderer = new DefaultRendererJ3D();
+    display1.addReferences(textRenderer, textRef);
+    textRenderer.suppressExceptions(true);
 
-    DefaultRendererJ3D wedge_renderer = new DefaultRendererJ3D();
-    display1.addReferences(wedge_renderer, wedge_ref);
-    wedge_renderer.suppressExceptions(true);
+    DefaultRendererJ3D wedgeRenderer = new DefaultRendererJ3D();
+    display1.addReferences(wedgeRenderer, wedgeRef);
+    wedgeRenderer.suppressExceptions(true);
 
-    line_ref = new DataReferenceImpl("line");
-    Gridded2DSet dummy_set = new Gridded2DSet(domain, null, 1);
-    line_ref.setData(dummy_set);
+    lineRef = new DataReferenceImpl("line");
+    Gridded2DSet dummySet = new Gridded2DSet(domain, null, 1);
+    lineRef.setData(dummySet);
     display1.addReferences(
-      new RubberBandLineRendererJ3D(element, line), line_ref);
+      new RubberBandLineRendererJ3D(element, line), lineRef);
 
     final RealType channel = RealType.getRealType("channel");
     final RealType point = RealType.getRealType("point");
     RealType intensity = RealType.getRealType("intensity");
-    final FunctionType spectrum_type = new FunctionType(channel, intensity);
-    final FunctionType spectra_type = new FunctionType(point, spectrum_type);
-    final FunctionType line_type = new FunctionType(point, intensity);
-    final FunctionType lines_type = new FunctionType(channel, line_type);
+    final FunctionType spectrumType = new FunctionType(channel, intensity);
+    final FunctionType spectraType = new FunctionType(point, spectrumType);
+    final FunctionType lineType = new FunctionType(point, intensity);
+    final FunctionType linesType = new FunctionType(channel, lineType);
 
     final DataReferenceImpl ref2 = new DataReferenceImpl("ref2");
 
@@ -238,9 +235,9 @@ set = Linear2DSet: Length = 393216
     final RealTupleType fdomain = domain;
     CellImpl cell = new CellImpl() {
       public void doAction() throws VisADException, RemoteException {
-        Set cell_set = (Set) line_ref.getData();
-        if (cell_set == null) return;
-        float[][] samples = cell_set.getSamples();
+        Set cellSet = (Set) lineRef.getData();
+        if (cellSet == null) return;
+        float[][] samples = cellSet.getSamples();
         if (samples == null) return;
         // System.out.println("box (" + samples[0][0] + ", " + samples[1][0] +
         //   ") to (" + samples[0][1] + ", " + samples[1][1] + ")");
@@ -260,29 +257,29 @@ set = Linear2DSet: Length = 393216
         }
         Gridded2DSet line = new Gridded2DSet(fdomain, ss, nsamp);
         xref.setData(line);
-        FlatField line_field = (FlatField)
-        //   big_data.resample(line, Data.WEIGHTED_AVERAGE, Data.NO_ERRORS);
-          big_data.resample(line, Data.NEAREST_NEIGHBOR, Data.NO_ERRORS);
-        float[][] line_samples =
-          line_field.getFloats(false); // [NFILES][nsamp]
-        Linear1DSet point_set = new Linear1DSet(point, 0.0, 1.0, nsamp);
-        Integer1DSet channel_set = new Integer1DSet(channel, NFILES);
-        FieldImpl spectra = new FieldImpl(spectra_type, point_set);
+        FlatField lineField = (FlatField)
+        //   bigData.resample(line, Data.WEIGHTED_AVERAGE, Data.NO_ERRORS);
+          bigData.resample(line, Data.NEAREST_NEIGHBOR, Data.NO_ERRORS);
+        float[][] lineSamples =
+          lineField.getFloats(false); // [NFILES][nsamp]
+        Linear1DSet pointSet = new Linear1DSet(point, 0.0, 1.0, nsamp);
+        Integer1DSet channelSet = new Integer1DSet(channel, NFILES);
+        FieldImpl spectra = new FieldImpl(spectraType, pointSet);
         for (int i=0; i<nsamp; i++) {
-          FlatField spectrum = new FlatField(spectrum_type, channel_set);
+          FlatField spectrum = new FlatField(spectrumType, channelSet);
           float[][] temp = new float[1][NFILES];
           for (int j=0; j<NFILES; j++) {
-            temp[0][j] = line_samples[j][i];
+            temp[0][j] = lineSamples[j][i];
           }
           spectrum.setSamples(temp, false);
           spectra.setSample(i, spectrum);
         }
-        FieldImpl lines = new FieldImpl(lines_type, channel_set);
+        FieldImpl lines = new FieldImpl(linesType, channelSet);
         for (int j=0; j<NFILES; j++) {
-          FlatField linex = new FlatField(line_type, point_set);
+          FlatField linex = new FlatField(lineType, pointSet);
           float[][] temp = new float[1][nsamp];
           for (int i=0; i<nsamp; i++) {
-            temp[0][i] = line_samples[j][i];
+            temp[0][i] = lineSamples[j][i];
           }
           linex.setSamples(temp, false);
           lines.setSample(j, linex);
@@ -291,12 +288,12 @@ set = Linear2DSet: Length = 393216
         ref2.setData(new Tuple(new Data[] {spectra, lines}));
       }
     };
-    cell.addReference(line_ref);
+    cell.addReference(lineRef);
 
-    VisADSlider[] value_sliders = new VisADSlider[NFILES];
-    VisADSlider[] hue_sliders = new VisADSlider[NFILES];
-    value_refs = new DataReferenceImpl[NFILES];
-    hue_refs = new DataReferenceImpl[NFILES];
+    VisADSlider[] valueSliders = new VisADSlider[NFILES];
+    VisADSlider[] hueSliders = new VisADSlider[NFILES];
+    valueRefs = new DataReferenceImpl[NFILES];
+    hueRefs = new DataReferenceImpl[NFILES];
 
     JFrame frame = new JFrame("VisAD MultiLUT");
     frame.addWindowListener(new WindowAdapter() {
@@ -334,43 +331,43 @@ set = Linear2DSet: Length = 393216
     center.setMaximumSize(d);
 
     for (int i=0; i<NFILES; i++) {
-      value_refs[i] = new DataReferenceImpl("value" + i);
-      value_refs[i].setData(new Real(1.0));
-      value_sliders[i] = new VisADSlider("value" + i, -100, 100, 100, 0.01,
-                                         value_refs[i], RealType.Generic);
-      left.add(value_sliders[i]);
-      hue_refs[i] = new DataReferenceImpl("hue" + i);
-      hue_refs[i].setData(new Real(1.0));
-      hue_sliders[i] = new VisADSlider("hue" + i, -100, 100, 100, 0.01,
-                                       hue_refs[i], RealType.Generic);
-      center.add(hue_sliders[i]);
+      valueRefs[i] = new DataReferenceImpl("value" + i);
+      valueRefs[i].setData(new Real(1.0));
+      valueSliders[i] = new VisADSlider("value" + i, -100, 100, 100, 0.01,
+                                         valueRefs[i], RealType.Generic);
+      left.add(valueSliders[i]);
+      hueRefs[i] = new DataReferenceImpl("hue" + i);
+      hueRefs[i].setData(new Real(1.0));
+      hueSliders[i] = new VisADSlider("hue" + i, -100, 100, 100, 0.01,
+                                       hueRefs[i], RealType.Generic);
+      center.add(hueSliders[i]);
     }
 
     // slider button for setting all value sliders to 0
-    JButton value_clear = new JButton("Zero values");
-    value_clear.addActionListener(this);
-    value_clear.setActionCommand("value_clear");
+    JButton valueClear = new JButton("Zero values");
+    valueClear.addActionListener(this);
+    valueClear.setActionCommand("valueClear");
     left.add(Box.createVerticalStrut(10));
-    left.add(value_clear);
+    left.add(valueClear);
 
     // slider button for setting all value sliders to 1
-    JButton value_set = new JButton("One values");
-    value_set.addActionListener(this);
-    value_set.setActionCommand("value_set");
-    left.add(value_set);
+    JButton valueSet = new JButton("One values");
+    valueSet.addActionListener(this);
+    valueSet.setActionCommand("valueSet");
+    left.add(valueSet);
 
     // slider button for setting all hue sliders to 0
-    JButton hue_clear = new JButton("Zero hues");
-    hue_clear.addActionListener(this);
-    hue_clear.setActionCommand("hue_clear");
+    JButton hueClear = new JButton("Zero hues");
+    hueClear.addActionListener(this);
+    hueClear.setActionCommand("hueClear");
     center.add(Box.createVerticalStrut(10));
-    center.add(hue_clear);
+    center.add(hueClear);
 
     // slider button for setting all hue sliders to 1
-    JButton hue_set = new JButton("One hues");
-    hue_set.addActionListener(this);
-    hue_set.setActionCommand("hue_set");
-    center.add(hue_set);
+    JButton hueSet = new JButton("One hues");
+    hueSet.addActionListener(this);
+    hueSet.setActionCommand("hueSet");
+    center.add(hueSet);
 
     // slider button for setting all hue sliders to 0
     right.add(display1.getComponent());
@@ -388,14 +385,14 @@ set = Linear2DSet: Length = 393216
     left.add(Box.createVerticalStrut(10));
     left.add(compute);
 
-    int WIDTH = 1200;
-    int HEIGHT = 1000;
+    int width = 1200;
+    int height = 1000;
 
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    frame.setLocation(screenSize.width/2 - WIDTH/2,
-                      screenSize.height/2 - HEIGHT/2);
+    frame.setLocation(screenSize.width/2 - width/2,
+                      screenSize.height/2 - height/2);
 
-    frame.setSize(WIDTH, HEIGHT);
+    frame.setSize(width, height);
     frame.setVisible(true);
 
     doit();
@@ -408,37 +405,37 @@ set = Linear2DSet: Length = 393216
     if (cmd.equals("compute")) {
       doit();
     }
-    else if (cmd.equals("value_clear")) {
+    else if (cmd.equals("valueClear")) {
       try {
         for (int i=0; i<NFILES; i++) {
-          value_refs[i].setData(new Real(0.0));
+          valueRefs[i].setData(new Real(0.0));
         }
       }
       catch (VisADException exc) { exc.printStackTrace(); }
       catch (RemoteException exc) { exc.printStackTrace(); }
     }
-    else if (cmd.equals("value_set")) {
+    else if (cmd.equals("valueSet")) {
       try {
         for (int i=0; i<NFILES; i++) {
-          value_refs[i].setData(new Real(1.0));
+          valueRefs[i].setData(new Real(1.0));
         }
       }
       catch (VisADException exc) { exc.printStackTrace(); }
       catch (RemoteException exc) { exc.printStackTrace(); }
     }
-    else if (cmd.equals("hue_clear")) {
+    else if (cmd.equals("hueClear")) {
       try {
         for (int i=0; i<NFILES; i++) {
-          hue_refs[i].setData(new Real(0.0));
+          hueRefs[i].setData(new Real(0.0));
         }
       }
       catch (VisADException exc) { exc.printStackTrace(); }
       catch (RemoteException exc) { exc.printStackTrace(); }
     }
-    else if (cmd.equals("hue_set")) {
+    else if (cmd.equals("hueSet")) {
       try {
         for (int i=0; i<NFILES; i++) {
-          hue_refs[i].setData(new Real(1.0));
+          hueRefs[i].setData(new Real(1.0));
         }
       }
       catch (VisADException exc) { exc.printStackTrace(); }
@@ -448,13 +445,13 @@ set = Linear2DSet: Length = 393216
 
   public void doit() {
     try {
-      float[] value_weights = new float[NFILES];
-      float[] hue_weights = new float[NFILES];
+      float[] valueWeights = new float[NFILES];
+      float[] hueWeights = new float[NFILES];
       for (int i=0; i<NFILES; i++) {
-        Real r = (Real) value_refs[i].getData();
-        value_weights[i] = (float) r.getValue();
-        r = (Real) hue_refs[i].getData();
-        hue_weights[i] = (float) r.getValue();
+        Real r = (Real) valueRefs[i].getData();
+        valueWeights[i] = (float) r.getValue();
+        r = (Real) hueRefs[i].getData();
+        hueWeights[i] = (float) r.getValue();
       }
       float vmin = Float.MAX_VALUE;
       float vmax = Float.MIN_VALUE;
@@ -463,11 +460,11 @@ set = Linear2DSet: Length = 393216
       for (int j=0; j<npixels; j++) {
         float v = 0, h = 0;
         for (int i=0; i<NFILES; i++) {
-          v += value_weights[i] * values[i][j];
-          h += hue_weights[i] * values[i][j];
+          v += valueWeights[i] * values[i][j];
+          h += hueWeights[i] * values[i][j];
         }
-        data_values[0][j] = v;
-        data_values[1][j] = h;
+        dataValues[0][j] = v;
+        dataValues[1][j] = h;
         if (v < vmin) vmin = v;
         if (v > vmax) vmax = v;
         if (h < hmin) hmin = h;
@@ -476,17 +473,17 @@ set = Linear2DSet: Length = 393216
       for (int i=0; i<768; i++) {
         float hue = hmin + (((float) i) / 767.0f) * (hmax - hmin);
         for (int k=i; k<768*21; k+=768) {
-          wedge_samples[0][k] = vmax;
-          wedge_samples[1][k] = hue;
+          wedgeSamples[0][k] = vmax;
+          wedgeSamples[1][k] = hue;
         }
       }
 
       minmax.setText("vmin = " + vmin + "; vmax = " + vmax);
       double x1 = 0.0, x2 = 767.0, y = 525.0;
-      text_field.setSample(0, new Tuple(text_tuple, new Scalar[] {
+      textField.setSample(0, new Tuple(textTuple, new Scalar[] {
         new Real(element, x1), new Real(line, y), new Text(text, "" + hmin)
       }));
-      text_field.setSample(1, new Tuple(text_tuple, new Scalar[] {
+      textField.setSample(1, new Tuple(textTuple, new Scalar[] {
         new Real(element, x2), new Real(line, y), new Text(text, "" + hmax)
       }));
 
@@ -494,8 +491,8 @@ set = Linear2DSet: Length = 393216
       vmap.setRange(vmin, vmax);
       hmap.setRange(hmin, hmax);
       huexmap.setRange(hmin, hmax);
-      data.setSamples(data_values, false);
-      wedge.setSamples(wedge_samples, false);
+      data.setSamples(dataValues, false);
+      wedge.setSamples(wedgeSamples, false);
       display1.enableAction();
     }
     catch (VisADException ex) { ex.printStackTrace(); }
