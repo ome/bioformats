ea21b8342e2d6e1f040c826cf7a4cc8269d14f41
Curtis Rueden <ctrueden@wisc.edu>
2006-06-30 06:00:17 +0000

    Avoid NullPointerExceptions when doing directory listings; output more verbose information about which files are different how.

--- utils/CompareTrees.java
+++ utils/CompareTrees.java
@@ -34,8 +34,21 @@ public class CompareTrees {
   }
 
   public static final void compare(File dir1, File dir2) {
+    if (dir1 == null && dir2 == null) return;
+    if (dir2 == null) {
+      System.out.println("<<< " + dir1 + " (" +
+        dir1.listFiles().length + " files)");
+      return;
+    }
+    if (dir1 == null) {
+      System.out.println(">>> " + dir2 + " (" +
+        dir2.listFiles().length + " files)");
+      return;
+    }
     File[] list1 = dir1.listFiles();
     File[] list2 = dir2.listFiles();
+    if (list1 == null) list1 = new File[0];
+    if (list2 == null) list2 = new File[0];
     Arrays.sort(list1);
     Arrays.sort(list2);
     int ndx1 = 0, ndx2 = 0;
@@ -44,19 +57,25 @@ public class CompareTrees {
       boolean d2 = list2[ndx2].isDirectory();
       int c = list1[ndx1].getName().compareToIgnoreCase(list2[ndx2].getName());
       if (c < 0) {
-        System.out.println("<<< " + list1[ndx1++] + (d1 ? " **" : ""));
+        System.out.print("<<< " + list1[ndx1++] + "\t[missing]");
+        if (d1) System.out.print(" [directory]");
+        System.out.println();
       }
       else if (c > 0) {
-        System.out.println(">>> " + list2[ndx2++] + (d2 ? " **" : ""));
+        System.out.println(">>> " + list2[ndx2++] + "\tmissing");
+        if (d2) System.out.print(" [directory]");
+        System.out.println();
       }
       else {
-        if ((d1 && !d2) || (!d1 && d2)) {
-          System.out.println("!D! " + list1[ndx1]);
-        }
+        if ((d1 && !d2)) System.out.println("!D! " + list2[ndx1]);
+        else if (!d1 && d2) System.out.println("!D! " + list1[ndx1]);
         else if (d1 && d2) compare(list1[ndx1], list2[ndx2]);
         else {
-          if (list1[ndx1].length() != list2[ndx2].length()) {
-            System.out.println("!S! " + list1[ndx1]);
+          long len1 = list1[ndx1].length();
+          long len2 = list2[ndx2].length();
+          if (len1 != len2) {
+            System.out.println("!S! " + list1[ndx1] +
+              "\t[" + len1 + " vs " + len2 + "]");
           }
         }
         ndx1++;
@@ -66,13 +85,17 @@ public class CompareTrees {
     if (ndx1 < list1.length) {
       for (int i=ndx1; i<list1.length; i++) {
         boolean d1 = list1[i].isDirectory();
-        System.out.println("<<< " + list1[i] + (d1 ? " **" : ""));
+        System.out.print("<<< " + list1[i] + "\t[missing]");
+        if (d1) System.out.print(" [directory]");
+        System.out.println();
       }
     }
     if (ndx2 < list2.length) {
       for (int i=ndx2; i<list2.length; i++) {
         boolean d2 = list2[i].isDirectory();
-        System.out.println(">>> " + list2[i] + (d2 ? " **" : ""));
+        System.out.println(">>> " + list2[i] + "\tmissing");
+        if (d2) System.out.print(" [directory]");
+        System.out.println();
       }
     }
   }
