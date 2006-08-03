fbb412a8a110c0c0288981982a20516453501bbf
Curtis Rueden <ctrueden@wisc.edu>
2006-08-03 22:52:07 +0000

    Add command line option to print a warning for each duplicate.

--- utils/SortList.java
+++ utils/SortList.java
@@ -10,7 +10,8 @@ import java.util.Vector;
 public class SortList {
 
   public static void main(String[] args) throws Exception {
-    String file = args[0];
+    boolean showDups = args[0].equals("-v");
+    String file = args[showDups ? 1 : 0];
     Vector v = new Vector();
     BufferedReader fin = new BufferedReader(new FileReader(file));
     while (true) {
@@ -26,6 +27,7 @@ public class SortList {
     PrintWriter fout = new PrintWriter(new FileWriter(file));
     for (int i=0; i<lines.length; i++) {
       if (i == 0 || !lines[i].equals(lines[i-1])) fout.println(lines[i]);
+      else if (showDups) System.out.println("Duplicate line: " + lines[i]);
     }
     fout.close();
   }
