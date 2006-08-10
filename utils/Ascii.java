39c2983c0870755f0f45410e83a3e6291d88c74a
Curtis Rueden <ctrueden@wisc.edu>
2006-08-10 18:57:12 +0000

    Better spacing.

--- utils/Ascii.java
+++ utils/Ascii.java
@@ -8,10 +8,16 @@ public class Ascii {
   public static void main(String[] args) {
     int max = 255;
     if (args.length > 0) max = Integer.parseInt(args[0]);
+    if (max > 99999) max = 99999; // cap at 5 digits
+    int digits = 1;
+    int q = max;
+    while (q >= 10) {
+      q /= 10;
+      digits++;
+    }
     for (int i=32; i<=max; i++) {
       String s = "" + i;
-      if (i < 100) s = " " + s;
-      if (i < 10) s = " " + s;
+      while (s.length() < digits) s = " " + s;
       System.out.print(s + " " + (char) i + "\t");
       if (i % 10 == 1) System.out.println();
     }
