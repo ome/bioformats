42427337eaaa57c6970f8779f7243ea276d31ae8
Curtis Rueden <ctrueden@wisc.edu>
2006-08-15 17:51:34 +0000

    Use placeholder variables for VERSION and DATE.

--- loci/visbio/VisBio.java
+++ loci/visbio/VisBio.java
@@ -45,13 +45,13 @@ public class VisBio extends Thread {
   public static final String TITLE = "VisBio";
 
   /** Application version. */
-  public static final String VERSION = "v3.30";
+  public static final String VERSION = "@visbio.version@";
 
   /** Application author. */
   public static final String AUTHOR = "Curtis Rueden, LOCI";
 
   /** Application build date. */
-  public static final String DATE = "10 Aug 2006";
+  public static final String DATE = "@date@";
 
   /** Port to use for communicating between application instances. */
   public static final int INSTANCE_PORT = 0xabcd;
