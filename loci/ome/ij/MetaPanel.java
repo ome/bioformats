fef54f2d3feb910f3a092d11601ca114ec58aded
Curtis Rueden <ctrueden@wisc.edu>
2006-08-16 16:14:49 +0000

    Add copyright blurb to OME Plugin for ImageJ; eliminate obsolete joint plugins.config; add plugins.config for OME Plugin.

--- loci/ome/ij/MetaPanel.java
+++ loci/ome/ij/MetaPanel.java
@@ -2,6 +2,25 @@
 // MetaPanel.java
 //
 
+/*
+OME Plugin for ImageJ plugin for transferring images to and from an OME
+database. Copyright (C) 2004-2006 Philip Huettl and Melissa Linkert.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Library General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 package loci.ome.ij;
 
 import ij.*;
