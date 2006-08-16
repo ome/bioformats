c9ba86d5cff1258c7ef4deb72184e0fe1d85ff03
Curtis Rueden <ctrueden@wisc.edu>
2006-08-16 20:54:56 +0000

    Finish move of loci.browser to loci.plugins.browser.

--- loci/plugins/browser/ImagePreview.java
+++ loci/plugins/browser/ImagePreview.java
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 // Adapted from FileChooserDemo2
 
-package loci.browser;
+package loci.plugins.browser;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
--- loci/plugins/browser/LociOpener.java
+++ loci/plugins/browser/LociOpener.java
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 // derived from Sun's FileChooserDemo2
 
-package loci.browser;
+package loci.plugins.browser;
 
 import ij.Macro;
 import ij.io.OpenDialog;
