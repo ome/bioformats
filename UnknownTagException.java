47b89cbc10d3024923b91114f8b8f4289f602445
Curtis Rueden <ctrueden@wisc.edu>
2006-08-18 18:28:35 +0000

    Whitespace and formatting.

--- UnknownTagException.java
+++ UnknownTagException.java
@@ -2,7 +2,7 @@ package loci.formats;
 
 /**
  * Thrown when a given entry does not have a particular tag.
- * 
+ *
  * @author Chris Allan callan at blackcat.ca
  */
 public class UnknownTagException extends RuntimeException {
