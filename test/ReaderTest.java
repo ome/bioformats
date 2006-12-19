6dcbd623a8f0ecd5a957bf91e9dfe6c1009a6e39
Curtis Rueden <ctrueden@wisc.edu>
2006-12-19 22:46:48 +0000

    Make reader tester build list more quickly (no checks for used files), then remove used files for each tested file *after* that file has been tested.

--- test/ReaderTest.java
+++ test/ReaderTest.java
@@ -36,11 +36,16 @@ import loci.formats.*;
  */
 public class ReaderTest extends TestCase {
 
+  // -- Constants --
+
+  /** Debugging flag. */
+  private static final boolean DEBUG = false;
+
   // -- Fields --
 
   private static Vector badFiles;
   private static String currentFile;
-  private static IFormatReader reader;
+  private static FileStitcher reader;
   private static FileWriter logFile;
 
   // -- Constructor --
@@ -49,18 +54,7 @@ public class ReaderTest extends TestCase {
     super(s);
   }
 
-  // -- TestCase API methods --
-
-  /** Releases resources after tests have completed. */
-  protected void tearDown() {
-    try {
-      reader.close();
-      badFiles = null;
-      if (logFile != null) logFile.close();
-    }
-    catch (FormatException fe) { }
-    catch (IOException io) { }
-  }
+  // -- Static ReaderTest API methods --
 
   /** Makes our results available to a TestRunner. */
   public static Test suite() {
@@ -71,8 +65,6 @@ public class ReaderTest extends TestCase {
     suite.addTest(new ReaderTest("testOMEXML"));
     return suite;
   }
-
-  // -- ReaderTest API methods --
 
   /** Sets the file to process. */
   public static void setFile(String file) {
@@ -95,7 +87,9 @@ public class ReaderTest extends TestCase {
         }
         in.close();
       }
-      catch (IOException io) { }
+      catch (IOException io) {
+        if (DEBUG) io.printStackTrace();
+      }
     }
     if (badFiles.contains(file)) return true;
     for (int i=0; i<badFiles.size(); i++) {
@@ -106,82 +100,78 @@ public class ReaderTest extends TestCase {
 
   /** Resets the format reader. */
   public static void resetReader() {
-    try {
-      reader.close();
+    if (reader != null) {
+      try {
+        reader.close();
+      }
+      catch (Exception e) {
+        if (DEBUG) e.printStackTrace();
+      }
     }
-    catch (Exception e) { }
     reader = new FileStitcher();
     OMEXMLMetadataStore store = new OMEXMLMetadataStore();
     store.createRoot();
     reader.setMetadataStore(store);
   }
 
-  /** 
-   * Returns true if any of the given file names are in the 
-   * list of files to test. 
-   */
-  public static boolean isStitchedFile(Vector files, String[] related) {
-    for (int i=0; i<related.length; i++) {
-      if (files.contains(related[i])) return true;  
-    }
-    return false;
-  }
-
   /** Recursively generates a list of files to test. */
   public static void getFiles(String root, Vector files) {
-    if (reader == null) {
-      reader = new FileStitcher();
-      OMEXMLMetadataStore store = new OMEXMLMetadataStore();
-      store.createRoot();
-      reader.setMetadataStore(store);
-    
+    resetReader();
+    File f = new File(root);
+    String[] subs = f.list();
+    if (subs == null) {
+      System.out.println("Invalid directory: " + root); 
+      return;
+    }
+    for (int i=0; i<subs.length; i++) {
+      if (DEBUG) debug("Checking file " + subs[i]);
+      if (isBadFile(subs[i])) {
+        if (DEBUG) debug(subs[i] + " is a bad file");
+        continue;
+      }
+      subs[i] = root + File.separator + subs[i]; 
+      if (new File(subs[i]).isDirectory()) getFiles(subs[i], files);
+      else {
+        if (reader.isThisType(subs[i])) {
+          if (DEBUG) debug("Adding " + subs[i]);
+          files.add(subs[i]);
+        }
+        else if (DEBUG) debug(subs[i] + " has invalid type");
+      }
+    }
+  }
+
+  /** Writes the given message to the log file. */
+  public static void writeLog(String s) {
+    if (logFile == null) {
       Date date = new Date();
       try {
         logFile = new FileWriter("bioformats-test-" + date.toString() + ".log");
         logFile.flush();
       }
-      catch (IOException io) { }
-    }
-  
-    if (!root.endsWith(File.separator)) root += File.separator;
-    File f = new File(root);
-    String[] subs = f.list();
-    if (subs != null) {
-      for (int i=0; i<subs.length; i++) {
-        if (!isBadFile(subs[i])) {
-          subs[i] = root + subs[i]; 
-          File tmp = new File(subs[i]);
-          if (!tmp.isDirectory() && reader.isThisType(subs[i])) {
-            try {
-              if (!isStitchedFile(files, reader.getUsedFiles(subs[i]))) {
-                files.add(subs[i]);
-                /* debug */ System.out.println("adding " + subs[i]);
-              }
-            }
-            catch (IOException io) { files.add(subs[i]); }
-            catch (FormatException fe) { files.add(subs[i]); }
-          }
-          else if (tmp.isDirectory()) getFiles(subs[i], files);
-          else /* debug */ System.out.println(subs[i] + " has invalid type");
-          tmp = null;
-          try { reader.close(); }
-          catch (Exception e) { }
-        }
-        else /* debug */ System.out.println(subs[i] + " is a bad file");
+      catch (IOException io) {
+        if (DEBUG) io.printStackTrace();
       }
     }
-    else System.out.println("Invalid directory : " + root); 
-    f = null;
+    try {
+      logFile.write(s + "\n");
+      logFile.flush();
+    }
+    catch (IOException exc) {
+      if (DEBUG) exc.printStackTrace();
+    }
   }
 
-  // -- Testing methods --
+  public static void debug(String s) { System.out.println(s); }
+
+  // -- ReaderTest API methods - tests --
 
   /** 
    * Checks the SizeX and SizeY dimensions against the actual dimensions of
    * the BufferedImages.
    */
   public void testBufferedImageDimensions() {
-   boolean success = true;
+    boolean success = true;
     try {
       for (int i=0; i<reader.getSeriesCount(currentFile); i++) {
         reader.setSeries(currentFile, i);
@@ -190,7 +180,11 @@ public class ReaderTest extends TestCase {
         int sizeY = reader.getSizeY(currentFile);
         for (int j=0; j<imageCount; j++) {
           BufferedImage b = reader.openImage(currentFile, j);
-          if ((b.getWidth() != sizeX) || (b.getHeight() != sizeY)) {
+          boolean failW = b.getWidth() != sizeX;
+          boolean failH = b.getHeight() != sizeY;
+          if (failW) writeLog(currentFile + " failed width test");
+          if (failH) writeLog(currentFile + " failed height test");
+          if (failW || failH) {
             success = false;
             j = imageCount;
             i = reader.getSeriesCount(currentFile);
@@ -200,15 +194,10 @@ public class ReaderTest extends TestCase {
       }
     }
     catch (Exception e) {
+      if (DEBUG) e.printStackTrace();
       success = false; 
     }
-    try {
-      if (!success) {
-        logFile.write(currentFile + " failed BufferedImage test\n");
-        logFile.flush();
-      }
-    }
-    catch (IOException io) { }
+    if (!success) writeLog(currentFile + " failed BufferedImage test");
     assertTrue(success);
   }
 
@@ -243,15 +232,10 @@ public class ReaderTest extends TestCase {
       }
     }
     catch (Exception e) { 
+      if (DEBUG) e.printStackTrace();
       success = false; 
     }
-    try {
-      if (!success) {
-        logFile.write(currentFile + " failed byte array test\n");
-        logFile.flush();
-      }
-    }
-    catch (IOException io) { }
+    if (!success) writeLog(currentFile + " failed byte array test");
     assertTrue(success);
   }
 
@@ -269,23 +253,16 @@ public class ReaderTest extends TestCase {
         int sizeC = reader.getEffectiveSizeC(currentFile);
         int sizeT = reader.getSizeT(currentFile);
         success = imageCount == sizeZ * sizeC * sizeT;
-        try {
-          if (!success) {
-            logFile.write(currentFile + " failed image count test\n");
-            logFile.flush();
-            assertTrue(false);
-          }
+        if (!success) {
+          writeLog(currentFile + " failed image count test");
+          assertTrue(false);
         }
-        catch (IOException io) { }
       }
       assertTrue(success);
     }
     catch (Exception e) { 
-      try {
-        logFile.write(currentFile + " failed image count test\n");
-        logFile.flush();
-      }
-      catch (IOException io) { }
+      if (DEBUG) e.printStackTrace();
+      writeLog(currentFile + " failed image count test");
       assertTrue(false); 
     }
   }
@@ -312,54 +289,103 @@ public class ReaderTest extends TestCase {
           FormatReader.getPixelTypeString(reader.getPixelType(currentFile));
         String dimensionOrder = reader.getDimensionOrder(currentFile);
 
-        success =  
-          (sizeX == store.getSizeX(null).intValue()) &&
-          (sizeY == store.getSizeY(null).intValue()) &&
-          (sizeZ == store.getSizeZ(null).intValue()) &&
-          (sizeC == store.getSizeC(null).intValue()) &&
-          (sizeT == store.getSizeT(null).intValue()) &&
-          (bigEndian == store.getBigEndian(null).booleanValue()) &&
-          type.toLowerCase().equals(store.getPixelType(null).toLowerCase()) &&
-          dimensionOrder.equals(store.getDimensionOrder(null));
-   
-        if (!success) {
-          try {
-            logFile.write(currentFile + " failed OME-XML sanity test\n");
-            logFile.flush();
-            assertTrue(false);
-          }
-          catch (IOException io) { }
+        Integer ii = new Integer(i);
+        boolean failX = sizeX != store.getSizeX(ii).intValue();
+        boolean failY = sizeY != store.getSizeY(ii).intValue();
+        boolean failZ = sizeZ != store.getSizeZ(ii).intValue();
+        boolean failC = sizeC != store.getSizeC(ii).intValue();
+        boolean failT = sizeT != store.getSizeT(ii).intValue();
+        boolean failBE = bigEndian != store.getBigEndian(ii).booleanValue();
+        boolean failType = !type.equalsIgnoreCase(store.getPixelType(ii));
+        boolean failDE = !dimensionOrder.equals(store.getDimensionOrder(ii));
+        if (failX) writeLog(currentFile + " failed OME-XML SizeX test");
+        if (failY) writeLog(currentFile + " failed OME-XML SizeY test");
+        if (failZ) writeLog(currentFile + " failed OME-XML SizeZ test");
+        if (failC) writeLog(currentFile + " failed OME-XML SizeC test");
+        if (failT) writeLog(currentFile + " failed OME-XML SizeT test");
+        if (failBE) writeLog(currentFile + " failed OME-XML BigEndian test");
+        if (failType) writeLog(currentFile + " failed OME-XML PixelType test");
+        if (failDE) {
+          writeLog(currentFile + " failed OME-XML DimensionOrder test");
+        }
+        if (failX || failY || failZ || failC || failT ||
+          failBE || failType || failDE)
+        {
+          assertTrue(false);
         }
       }
       assertTrue(success); 
     }
     catch (Exception e) { 
-      try {
-        logFile.write(currentFile + " failed OME-XML sanity test\n");
-        logFile.flush();
-      }
-      catch (IOException io) { }
+      if (DEBUG) e.printStackTrace();
+      writeLog(currentFile + " failed OME-XML sanity test");
       assertTrue(false); 
+    }
+  }
+
+  // -- TestCase API methods --
+
+  /** Releases resources after tests have completed. */
+  protected void tearDown() {
+    try {
+      reader.close();
+      badFiles = null;
+      if (logFile != null) {
+        logFile.close();
+        logFile = null;
+      }
+    }
+    catch (FormatException fe) {
+      if (DEBUG) fe.printStackTrace();
+    }
+    catch (IOException io) {
+      if (DEBUG) io.printStackTrace();
     }
   }
 
   // -- Main method --
 
   public static void main(String[] args) {
+    if (DEBUG) FormatReader.debug = true;
     Vector files = new Vector();  
-    ReaderTest.getFiles(args[0], files);
-    System.out.println();
-    for (int i=0; i<files.size(); i++) {
-      System.out.println("Testing " + files.get(i));
-      ReaderTest.setFile((String) files.get(i));
+    if (args == null || args.length == 0) {
+      System.out.println(
+        "Please specify root folder to search for data files.");
+      System.exit(1);
+    }
+    System.out.print("Building file list...");
+    if (DEBUG) System.out.println();
+    getFiles(args[0], files);
+    System.out.println(files.size() + " found.");
+    while (files.size() > 0) {
+      String file = (String) files.get(0);
+      try {
+        FilePattern fp = reader.getFilePattern(file);
+        System.out.println("Testing " + fp.getPattern());
+      }
+      catch (FormatException exc) { exc.printStackTrace(); }
+      catch (IOException exc) { exc.printStackTrace(); }
+      setFile(file);
       TestResult result = new TestResult();
-      ReaderTest.suite().run(result);
+      suite().run(result);
       int total = result.runCount();
       int failed = result.failureCount();
       float failPercent = (float) (100 * ((double) failed / (double) total));
-      System.out.println(files.get(i) + " - " + failed + " failures in " +
+      System.out.println(file + " - " + failed + " failures in " +
         total + " tests (" + failPercent + "% failed)");
-      ReaderTest.resetReader();
+
+      // remove relevant files from the list
+      try {
+        String[] used = reader.getUsedFiles(file);
+        for (int i=0; i<used.length; i++) {
+          if (DEBUG) System.out.println("Removing " + used[i]);
+          files.removeElement(used[i]);
+        }
+      }
+      catch (FormatException exc) { exc.printStackTrace(); }
+      catch (IOException exc) { exc.printStackTrace(); }
+
+      resetReader();
     }
   }
 
