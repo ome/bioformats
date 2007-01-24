b9f12727f2d2105860da5800c473111d5eb96e8a
Melissa Linkert <melissa@glencoesoftware.com>
2007-01-24 16:15:23 +0000

    Reworked configuration file structure for testing framework.  Instead of "bad-files.txt", place a ".bioformats" file in any data directory (it doesn't matter if you place one master file in the root data directory, or smaller files in each bottom-level directory).  These configuration files should have the following structure:
    <filename> <number of series> [<x> <y> <z> <c> <t> <dimension order> 
      <interleaved> <rgb> <thumb x> <thumb y> <pixel type string> <little endian>] 
      <average plane access time (in ms)> <file size (in MB)> 
      <flag set to true if this file should be tested> 
    
    Note that:
    - The filename is relative to the directory the config file is in.
    - The parameters between square brackets should be repeated for each series
        (so if the file has 2 series, specify <x0> <y0> <z0> ... <x1> <y1> <z1> ...)
    - Most of these parameters can be copy/pasted from the output of
      "inf -nometa <filename>".

---
+++ loci/formats/test/ConfigurationFiles.java
@@ -0,0 +1,195 @@
+//
+// ConfigurationFiles.java
+//
+
+/*
+LOCI Bio-Formats package for reading and converting biological file formats.
+Copyright (C) 2005-@year@ Melissa Linkert, Curtis Rueden, Chris Allan
+and Eric Kjellman.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Library General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package loci.formats.test;
+
+import java.io.IOException;
+import java.util.*;
+import loci.formats.*;
+
+/** Stores data from a set of config files */
+public class ConfigurationFiles {
+  Hashtable numSeries = new Hashtable();
+  Hashtable dimensions = new Hashtable();
+  Hashtable dimOrders = new Hashtable();
+  Hashtable interleaved = new Hashtable();
+  Hashtable rgb = new Hashtable();
+  Hashtable thumbDims = new Hashtable();
+  Hashtable pixelType = new Hashtable();
+  Hashtable littleEndian = new Hashtable();
+  Hashtable readTime = new Hashtable();
+  Hashtable memUsage = new Hashtable();
+  Hashtable doTest = new Hashtable();
+
+  int currentSeries = 0;
+
+  public static ConfigurationFiles newInstance() {
+    return new ConfigurationFiles();
+  }
+
+  public int numFiles() { return numSeries.size(); }
+
+  public boolean initialized(String id) {
+    return numSeries.containsKey(id) && getNumSeries(id) <= currentSeries;
+  }
+
+  /** 
+   * Parses data from a new config file and stores it for later use.
+   * Does not perform any checks - it is assumed that the file is properly
+   * written.
+   */
+  public void addFile(String id) throws IOException {
+    RandomAccessStream s = new RandomAccessStream(id);
+    Location l = new Location(id);
+    byte[] b = new byte[(int) s.length()];
+    s.read(b);
+    StringTokenizer st = new StringTokenizer(new String(b), "\n");
+    while (st.hasMoreTokens()) {
+      String line = st.nextToken();
+      if (!line.startsWith("#") && line.trim().length() > 0) {
+        StringTokenizer spaces = new StringTokenizer(line, " ");
+        String file = 
+          new Location(l.getParent(), spaces.nextToken()).getAbsolutePath(); 
+        int nSeries = Integer.parseInt(spaces.nextToken());
+        numSeries.put(file, new Integer(nSeries));
+        int[][] dims = new int[nSeries][5];
+        String[] orders = new String[nSeries];
+        boolean[] interleave = new boolean[nSeries];
+        boolean[] isRGB = new boolean[nSeries];
+        int[][] thumbs = new int[nSeries][2];
+        int[] ptype = new int[nSeries];
+        boolean[] little = new boolean[nSeries];
+
+        for (int i=0; i<nSeries; i++) {
+          dims[i][0] = Integer.parseInt(spaces.nextToken());  
+          dims[i][1] = Integer.parseInt(spaces.nextToken());  
+          dims[i][2] = Integer.parseInt(spaces.nextToken());  
+          dims[i][3] = Integer.parseInt(spaces.nextToken());  
+          dims[i][4] = Integer.parseInt(spaces.nextToken());  
+          orders[i] = spaces.nextToken();
+          interleave[i] = spaces.nextToken().equals("true");
+          isRGB[i] = spaces.nextToken().equals("true");
+          thumbs[i][0] = Integer.parseInt(spaces.nextToken());
+          thumbs[i][1] = Integer.parseInt(spaces.nextToken());
+          ptype[i] = FormatReader.pixelTypeFromString(spaces.nextToken());
+          little[i] = spaces.nextToken().equals("true");
+        }
+        dimensions.put(file, dims);
+        dimOrders.put(file, orders);
+        interleaved.put(file, interleave);
+        rgb.put(file, isRGB);
+        thumbDims.put(file, thumbs);
+        pixelType.put(file, ptype);
+        littleEndian.put(file, little);
+        readTime.put(file, new Float(spaces.nextToken()));
+        memUsage.put(file, new Integer(spaces.nextToken()));
+        doTest.put(file, new Boolean(spaces.nextToken()));
+      }
+    }
+  }
+
+  public void setSeries(String id, int no) {
+    if (no < getNumSeries(id)) currentSeries = no; 
+  }
+
+  public int getNumSeries(String id) { 
+    if (!numSeries.contains(id)) return 0;
+    return ((Integer) numSeries.get(id)).intValue();
+  }
+    
+  public int getWidth(String id) {
+    if (!initialized(id)) return 0;
+    return ((int[][]) dimensions.get(id))[currentSeries][0];
+  }
+    
+  public int getHeight(String id) { 
+    if (!initialized(id)) return 0;
+    return ((int[][]) dimensions.get(id))[currentSeries][1];
+  }
+
+  public int getZ(String id) { 
+    if (!initialized(id)) return 0;
+    return ((int[][]) dimensions.get(id))[currentSeries][2];
+  }
+    
+  public int getC(String id) { 
+    if (!initialized(id)) return 0;
+    return ((int[][]) dimensions.get(id))[currentSeries][3];
+  }
+    
+  public int getT(String id) { 
+    if (!initialized(id)) return 0;
+    return ((int[][]) dimensions.get(id))[currentSeries][4];
+  }
+    
+  public String getDimOrder(String id) { 
+    if (!initialized(id)) return null;
+    return ((String[]) dimOrders.get(id))[currentSeries];
+  }
+
+  public boolean isInterleaved(String id) {
+    if (!initialized(id)) return false;
+    return ((boolean[]) interleaved.get(id))[currentSeries]; 
+  }
+
+  public boolean isRGB(String id) {
+    if (!initialized(id)) return false;
+    return ((boolean[]) rgb.get(id))[currentSeries];
+  }
+
+  public int getThumbX(String id) {
+    if (!initialized(id)) return 0;
+    return ((int[][]) thumbDims.get(id))[currentSeries][0];
+  }
+
+  public int getThumbY(String id) {
+    if (!initialized(id)) return 0;
+    return ((int[][]) thumbDims.get(id))[currentSeries][1];
+  }
+
+  public int getPixelType(String id) {
+    if (!initialized(id)) return -1;
+    return ((int[]) pixelType.get(id))[currentSeries]; 
+  }
+
+  public boolean isLittleEndian(String id) {
+    if (!initialized(id)) return false;
+    return ((boolean[]) littleEndian.get(id))[currentSeries];
+  }
+
+  public float getTimePerPlane(String id) {
+    if (!initialized(id)) return 0;
+    return ((float[]) readTime.get(id))[currentSeries];
+  }
+
+  public int getFileSize(String id) {
+    if (!initialized(id)) return 0;
+    return ((int[]) memUsage.get(id))[currentSeries];
+  }
+
+  public boolean testFile(String id) {
+    if (!initialized(id)) return true;
+    return ((Boolean) doTest.get(id)).booleanValue(); 
+  }
+}
--- loci/formats/test/ReaderTest.java
+++ loci/formats/test/ReaderTest.java
@@ -64,9 +64,15 @@ public class ReaderTest extends TestCase {
 
   // -- Static fields --
 
-  private static Vector badFiles;
+  private static Vector configFiles = new Vector();
+  private static ConfigurationFiles config = ConfigurationFiles.newInstance();
   private static FileWriter logFile;
   private String[] used;
+
+  private static float averagePlaneAccess;
+  private static int maxMemory;  // maximum measured memory usage
+  private static int initialMemory;  // memory usage before opening the file
+  private static int finalMemory;  // memory usage after closing file
 
   // -- Fields --
 
@@ -108,8 +114,15 @@ public class ReaderTest extends TestCase {
    */
   public void testBufferedImageDimensions() {
     boolean success = true;
+    Runtime rt = Runtime.getRuntime();
+    initialMemory = (int) ((rt.totalMemory() - rt.freeMemory()) >> 20);
+    maxMemory = initialMemory;
     try {
+      int planesRead = 0;
+      long l1 = System.currentTimeMillis();
       for (int i=0; i<reader.getSeriesCount(id); i++) {
+        int usedMemory = (int) (rt.totalMemory() - rt.freeMemory()) >> 20;
+        if (usedMemory > maxMemory) maxMemory = usedMemory;
         reader.setSeries(id, i);
         int imageCount = reader.getImageCount(id);
         int sizeX = reader.getSizeX(id);
@@ -127,7 +140,10 @@ public class ReaderTest extends TestCase {
             break;
           }
         }
+        planesRead += imageCount;
       }
+      long l2 = System.currentTimeMillis();
+      averagePlaneAccess = ((float) (l2 - l1)) / planesRead;
     }
     catch (Exception e) {
       if (DEBUG) e.printStackTrace();
@@ -136,6 +152,7 @@ public class ReaderTest extends TestCase {
     if (!success) writeLog(id + " failed BufferedImage test");
     try { reader.close(); }
     catch (Exception e) { }
+    finalMemory = (int) (rt.totalMemory() - rt.freeMemory()) >> 20;
     assertTrue(success);
   }
 
@@ -266,6 +283,127 @@ public class ReaderTest extends TestCase {
     assertTrue(success);
   }
 
+  /** 
+   * Checks that the core metadata values match those given in 
+   * the configuration file.  Note that this test will fail if there was
+   * no configuration file.
+   */
+  public void testConsistent() {
+    boolean success = true;
+    int nSeries = 0;
+    try {
+      nSeries = reader.getSeries(id);
+      if (nSeries != config.getNumSeries(id)) {
+        success = false;
+        writeLog(id + " failed consistent series count check");
+      }
+    }
+    catch (Exception e) {
+      if (DEBUG) e.printStackTrace();
+      success = false;
+    }
+    if (success) {
+      try {
+        for (int i=0; i<nSeries; i++) {
+          config.setSeries(id, i);
+          reader.setSeries(id, i);
+          if (config.getWidth(id) != reader.getSizeX(id)) {
+            success = false;
+            writeLog(id + " failed consistent width check in series " + i);
+          }
+          if (config.getHeight(id) != reader.getSizeY(id)) {
+            success = false;
+            writeLog(id + " failed consistent height check in series " + i);
+          }
+          if (config.getZ(id) != reader.getSizeZ(id)) {
+            success = false;
+            writeLog(id + " failed consistent sizeZ check in series " + i);
+          }
+          if (config.getC(id) != reader.getSizeC(id)) {
+            success = false;
+            writeLog(id + " failed consistent sizeC check in series " + i);
+          }
+          if (config.getT(id) != reader.getSizeT(id)) {
+            success = false;
+            writeLog(id + " failed consistent sizeT check in series " + i);
+          }
+          if (!config.getDimOrder(id).equals(reader.getDimensionOrder(id))) {
+            success = false;
+            writeLog(id + 
+              " failed consistent dimension order check in series " + i);
+          }
+          if (config.isInterleaved(id) != reader.isInterleaved(id)) {
+            success = false;
+            writeLog(id + 
+              " failed consistent interleaving flag check in series " + i);
+          }
+          if (config.isRGB(id) != reader.isRGB(id)) {
+            success = false;
+            writeLog(id + " failed consistent RGB flag check in series " + i);
+          }
+          if (config.getThumbX(id) != reader.getThumbSizeX(id)) {
+            success = false;
+            writeLog(id + 
+              " failed consistent thumbnail width check in series " + i);
+          }
+          if (config.getThumbY(id) != reader.getThumbSizeY(id)) {
+            success = false;
+            writeLog(id + 
+              " failed consistent thumbnail height check in series " + i);
+          }
+          if (config.getPixelType(id) != reader.getPixelType(id)) {
+            success = false;
+            writeLog(id + " failed consistent pixel type check in series " + i);
+          }
+          if (config.isLittleEndian(id) != reader.isLittleEndian(id)) {
+            success = false;
+            writeLog(id + 
+              " failed consistent endianness flag check in series " + i);
+          }
+        }
+      }
+      catch (Exception e) {
+        if (DEBUG) e.printStackTrace();
+        success = false;
+      }
+    }
+    assertTrue(success);
+  }
+
+  /** Check that the memory usage is acceptable. */
+  public void testMemoryUsage() {
+    boolean success = true;
+    
+    // we want the maximum usage to be no more than twice the file size
+    if (maxMemory - initialMemory > 2*config.getFileSize(id)) {
+      success = false;
+      writeLog(id + " failed maximum memory usage test (used " + 
+        (maxMemory - initialMemory) + "MB; expected <= " + 
+        2*config.getFileSize(id) + "MB)");
+    }
+  
+    // check that the reader doesn't have any (significant) memory leaks
+    if (finalMemory - initialMemory > 10) {
+      success = false;
+      writeLog(id + " failed memory leak test (" + 
+        (finalMemory - initialMemory) + "MB leaked)");
+    }
+
+    assertTrue(success);
+  }
+
+  /** Check that the average access time per plane is reasonable. */
+  public void testAccessTime() {
+    boolean success = true;
+    if (averagePlaneAccess - config.getTimePerPlane(id) > 10.0) {
+      success = false;
+      writeLog(id + " failed consistent access time test (got " +
+        averagePlaneAccess + " ms, expected " + config.getTimePerPlane(id) +
+        " ms)");
+    }
+    assertTrue(success);
+  }
+
   // -- TestCase API methods --
 
   /** Sets up the fixture. */
@@ -301,6 +439,11 @@ public class ReaderTest extends TestCase {
     suite.addTest(new ReaderTest("testByteArrayDimensions", id));
     suite.addTest(new ReaderTest("testImageCount", id));
     suite.addTest(new ReaderTest("testOMEXML", id));
+    if (config.initialized(id)) {
+      suite.addTest(new ReaderTest("testConsistent", id));
+      suite.addTest(new ReaderTest("testMemoryUsage", id));
+      suite.addTest(new ReaderTest("testAccessTime", id));
+    }
     return suite;
   }
 
@@ -309,36 +452,24 @@ public class ReaderTest extends TestCase {
    * Bad files are skipped rather than tested.
    */
   public static boolean isBadFile(String file) {
-    if (badFiles == null) {
-      try {
-        badFiles = new Vector();
-        BufferedReader in = new BufferedReader(new InputStreamReader(
-          ReaderTest.class.getResourceAsStream("bad-files.txt")));
-        while (true) {
-          String line = in.readLine();
-          if (line == null) break;
-          if (line.trim().equals("")) continue; // blank line
-          if (line.startsWith("#")) continue; // comment
-          badFiles.add(line);
+    if (config.numFiles() == 0) {
+      for (int i=0; i<configFiles.size(); i++) {
+        try {
+          config.addFile((String) configFiles.get(i));  
         }
-        in.close();
-      }
-      catch (IOException io) {
-        if (DEBUG) io.printStackTrace();
+        catch (IOException e) {
+          if (DEBUG) e.printStackTrace();
+        }
       }
     }
-    String absFile = new Location(file).getAbsolutePath();
-    for (int i=0; i<badFiles.size(); i++) {
-      String bad = (String) badFiles.elementAt(i);
-      if (absFile.endsWith(bad)) return true;
-    }
-    return false;
+    return !config.testFile(file) && !file.endsWith(".bioformats");
   }
 
   /** Recursively generates a list of files to test. */
   public static void getFiles(String root, Vector files) {
     Location f = new Location(root);
     String[] subs = f.list();
+    Arrays.sort(subs);
     if (subs == null) {
       System.out.println("Invalid directory: " + root);
       return;
@@ -346,13 +477,17 @@ public class ReaderTest extends TestCase {
     ImageReader ir = new ImageReader();
     for (int i=0; i<subs.length; i++) {
       if (DEBUG) debug("Checking file " + subs[i]);
-      subs[i] = root + File.separator + subs[i];
+      subs[i] = new Location(root, subs[i]).getAbsolutePath();
       if (isBadFile(subs[i])) {
         if (DEBUG) debug(subs[i] + " is a bad file");
         continue;
       }
       Location file = new Location(subs[i]);
       if (file.isDirectory()) getFiles(subs[i], files);
+      else if (file.getName().equals(".bioformats")) {
+        // special config file for the test suite
+        configFiles.add(file.getAbsolutePath());
+      }
       else {
         if (ir.isThisType(subs[i])) {
           if (DEBUG) debug("Adding " + subs[i]);
@@ -431,5 +566,4 @@ public class ReaderTest extends TestCase {
       test.close();
     }
   }
-
 }
