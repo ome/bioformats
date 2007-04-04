a5972c4cfa8beba95fd4bb205e1adbedd6f8130d
Abraham Sorber <acsorber@gmail.com>
2007-04-04 15:24:21 +0000

    Added OverlayStat class.

---
+++ loci/visbio/overlays/OverlayStat.java
@@ -0,0 +1,58 @@
+//
+// OverlayStat.java
+//
+
+/*
+VisBio application for visualization of multidimensional
+biological image data. Copyright (C) 2002-@year@ Curtis Rueden.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package loci.visbio.overlays;
+
+/** OverlayStat is an arrow wedge overlay. */
+public class OverlayStat {
+   
+  // -- Fields --
+
+  /** The name of this statistic (e.g., 'Curve Length (microns)') */
+  protected String name;
+
+  /** The value of this statistic in formatted string form */
+  protected String value;
+
+  // -- Constructors --
+
+  /** Constructs an OverlayStat object */
+  public OverlayStat(String name, String value) {
+	this.name = name;
+    this.value = value;
+  }
+
+  // -- OverlayObject API methods --
+
+  /** Gets the name of this OverlayStat. */
+  public String getName() { return name; }
+
+  /** Gets the value of this OverlayStat. */
+  public String getValue() { return value; }
+  
+  // -- Object API methods --
+
+  /** Gets a short string representation of this overlay arrow. */
+  public String toString() { return name + " [" + value + "]"; }
+
+}
