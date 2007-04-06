1a6ecbe105a41156f4ae3beb063414969a21c45f
Curtis Rueden <ctrueden@wisc.edu>
2007-04-06 19:26:13 +0000

    Remove unnecessary import.

--- loci/formats/in/LeicaReader.java
+++ loci/formats/in/LeicaReader.java
@@ -25,7 +25,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 package loci.formats.in;
 
 import java.awt.image.BufferedImage;
-import java.awt.image.ColorModel;
 import java.io.*;
 import java.text.*;
 import java.util.*;
