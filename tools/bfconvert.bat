e5551e053adc9c1894641d1c7d3f951bdb862325
Curtis Rueden <ctrueden@wisc.edu>
2007-04-26 21:09:24 +0000

    Add script for converting between file formats.

---
+++ tools/bfconvert.bat
@@ -0,0 +1,39 @@
+@echo off
+
+rem bfconvert: a batch file for converting image files between formats
+
+rem Required JARs: loci_tools.jar
+
+rem JAR libraries must be in the same directory as this
+rem command line script for the command to function.  
+
+rem If you are a developer working from source and have
+rem the LOCI classes in your CLASSPATH, you can set the
+rem LOCI_DEVEL environment variable to use them instead.
+
+set PROG=loci.formats.ImageWriter
+
+if "%LOCI_DEVEL%" == "" (
+  rem Developer environment variable unset; look for proper libraries
+  if exist loci_tools.jar goto found
+  if exist bio-formats.jar goto found
+  goto missing
+) else (
+  rem Developer environment variable set; try to launch
+  java -mx512m %PROG% %*
+  goto end
+)
+
+:found
+rem Library found; try to launch
+java -mx512m -cp bio-formats.jar;loci_tools.jar %PROG% %*
+goto end
+
+:missing
+echo Required JAR libraries not found. Please download:
+echo   loci_tools.jar
+echo from:
+echo   http://www.loci.wisc.edu/ome/formats.html
+echo and place in the same directory as the command line tools.
+
+:end
