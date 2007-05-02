8809a6bbc2cf2de8ce9ac746ad1b4b48aca6d168
Curtis Rueden <ctrueden@wisc.edu>
2007-05-02 20:30:50 +0000

    2007 May 02 release of LOCI Tools

---
+++ loci/plugins/LociUploader.java
@@ -0,0 +1,219 @@
+//
+// LociUploader.java
+//
+
+/*
+LOCI Plugins for ImageJ: a collection of ImageJ plugins including
+the 4D Data Browser, OME Plugin and Bio-Formats Exporter.
+Copyright (C) 2006-@year@ Melissa Linkert, Christopher Peterson,
+Curtis Rueden, Philip Huettl and Francis Wong.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Library General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package loci.plugins;
+
+import java.awt.TextField;
+import ij.*;
+import ij.gui.GenericDialog;
+import ij.plugin.PlugIn;
+import ij.process.ColorProcessor;
+import ij.io.FileInfo;
+import java.util.HashSet;
+import loci.formats.FormatTools;
+import loci.formats.ome.OMEXMLMetadataStore;
+import loci.formats.ome.OMEWriter;
+
+/**
+ * ImageJ plugin for uploading images to an OME server.
+ *
+ * @author Melissa Linkert linket at wisc.edu
+ */
+public class LociUploader implements PlugIn {
+
+  // -- Fields --
+
+  private String server;
+  private String user;
+  private String pass;
+
+  // -- PlugIn API methods --
+
+  public void run(String arg) {
+    // check that we can safely execute the plugin
+    if (!Util.checkVersion()) return;
+    HashSet missing = new HashSet();
+    Util.checkLibrary(Util.BIO_FORMATS, missing);
+    Util.checkLibrary(Util.OME_JAVA_XML, missing);
+    Util.checkLibrary(Util.OME_JAVA_DS, missing);
+    if (!Util.checkMissing(missing)) return;
+
+    promptForLogin();
+    uploadStack();
+  }
+
+  // -- Helper methods --
+
+  /** Open a dialog box that prompts for a username, password, and server. */
+  private void promptForLogin() {
+    GenericDialog prompt = new GenericDialog("Login to OME");
+    prompt.addStringField("Server:   ", Prefs.get("uploader.server", ""), 60);
+    prompt.addStringField("Username: ", Prefs.get("uploader.user", ""), 60);
+    prompt.addStringField("Password: ", "", 60);
+
+    ((TextField) prompt.getStringFields().get(2)).setEchoChar('*');
+    prompt.showDialog();
+
+    server = prompt.getNextString();
+    user = prompt.getNextString();
+    pass = prompt.getNextString();
+
+    Prefs.set("uploader.server", server);
+    Prefs.set("uploader.user", user);
+  }
+
+  /** Log in to the OME server and upload the current image stack. */
+  private void uploadStack() {
+    try {
+      IJ.showStatus("Starting upload...");
+      OMEWriter ul = new OMEWriter();
+      String id = server + "?user=" + user + "&password=" + pass;
+      ul.setId(id);
+
+      ImagePlus imp = WindowManager.getCurrentImage();
+      if (imp == null) {
+        IJ.error("No open images!");
+        IJ.showStatus("");
+        return;
+      }
+      ImageStack is = imp.getImageStack();
+      FileInfo fi = imp.getOriginalFileInfo();
+      OMEXMLMetadataStore store = new OMEXMLMetadataStore();
+
+      // if we opened this stack with the Bio-Formats importer, then the
+      // appropriate OME-XML is in fi.description
+      if (fi != null && fi.description != null &&
+        fi.description.endsWith("</OME>"))
+      {
+        store.createRoot(fi.description);
+      }
+      else {
+        store.createRoot();
+        int pixelType = FormatTools.UINT8;
+        switch (imp.getBitDepth()) {
+          case 16: pixelType = FormatTools.UINT16; break;
+          case 32: pixelType = FormatTools.FLOAT; break;
+        }
+
+        store.setPixels(
+          new Integer(imp.getWidth()),
+          new Integer(imp.getHeight()),
+          new Integer(imp.getNSlices()),
+          new Integer(imp.getNChannels()),
+          new Integer(imp.getNFrames()),
+          new Integer(pixelType),
+          fi == null ? Boolean.TRUE : new Boolean(!fi.intelByteOrder),
+          "XYCZT",  // TODO : figure out a way to calculate the dimension order
+          null,
+          null);
+
+        String name = fi == null ? imp.getTitle() : fi.fileName;
+        store.setImage(name, null, fi == null ? null : fi.info, null);
+
+      }
+      if (is.getProcessor(1) instanceof ColorProcessor) {
+        store.setPixels(null, null, null, null, null,
+          new Integer(FormatTools.UINT8), null, null, null, null);
+      }
+      ul.setMetadataStore(store);
+
+      boolean little = !store.getBigEndian(null).booleanValue();
+
+      for (int i=0; i<is.getSize(); i++) {
+        IJ.showStatus("Reading plane " + (i+1) + "/" + is.getSize());
+        Object pix = is.getProcessor(i + 1).getPixels();
+
+        byte[] toUpload = null;
+
+        if (pix instanceof byte[]) {
+          toUpload = (byte[]) pix;
+        }
+        else if (pix instanceof short[]) {
+          short[] s = (short[]) pix;
+          toUpload = new byte[s.length * 2];
+          for (int j=0; j<s.length; j++) {
+            toUpload[j*2] = little ? (byte) (s[j] & 0xff) :
+              (byte) ((s[j] >>> 8) & 0xff);
+            toUpload[j*2 + 1] = little ? (byte) ((s[j] >>> 8) & 0xff):
+              (byte) (s[j] & 0xff);
+          }
+        }
+        else if (pix instanceof int[]) {
+          if (is.getProcessor(i+1) instanceof ColorProcessor) {
+            byte[][] rgb = new byte[3][((int[]) pix).length];
+            ((ColorProcessor) is.getProcessor(i+1)).getRGB(rgb[0],
+              rgb[1], rgb[2]);
+            int channels = store.getSizeC(null).intValue();
+            if (channels > 3) channels = 3;
+            toUpload = new byte[channels * rgb[0].length];
+            for (int j=0; j<channels; j++) {
+              System.arraycopy(rgb[j], 0, toUpload, 0, rgb[j].length);
+            }
+          }
+          else {
+            int[] p = (int[]) pix;
+            toUpload = new byte[4 * p.length];
+            for (int j=0; j<p.length; j++) {
+              toUpload[j*4] = little ? (byte) (p[j] & 0xff) :
+                (byte) ((p[j] >> 24) & 0xff);
+              toUpload[j*4 + 1] = little ? (byte) ((p[j] >> 8) & 0xff) :
+                (byte) ((p[j] >> 16) & 0xff);
+              toUpload[j*4 + 2] = little ? (byte) ((p[j] >> 16) & 0xff) :
+                (byte) ((p[j] >> 8) & 0xff);
+              toUpload[j*4 + 3] = little ? (byte) ((p[j] >> 24) & 0xff) :
+                (byte) (p[j] & 0xff);
+            }
+          }
+        }
+        else if (pix instanceof float[]) {
+          float[] f = (float[]) pix;
+          toUpload = new byte[f.length * 4];
+          for (int j=0; j<f.length; j++) {
+            int k = Float.floatToIntBits(f[j]);
+            toUpload[j*4] = little ? (byte) (k & 0xff) :
+              (byte) ((k >> 24) & 0xff);
+            toUpload[j*4 + 1] = little ? (byte) ((k >> 8) & 0xff) :
+              (byte) ((k >> 16) & 0xff);
+            toUpload[j*4 + 2] = little ? (byte) ((k >> 16) & 0xff) :
+              (byte) ((k >> 8) & 0xff);
+            toUpload[j*4 + 3] = little ? (byte) ((k >> 24) & 0xff) :
+              (byte) (k & 0xff);
+          }
+        }
+
+        ul.saveBytes(toUpload, i == is.getSize() - 1);
+      }
+
+      IJ.showStatus("Sending data to server...");
+      ul.close();
+      IJ.showStatus("Upload finished.");
+    }
+    catch (Exception e) {
+      IJ.error("Upload failed:\n" + e);
+      e.printStackTrace();
+    }
+  }
+
+}
---
+++ loci/plugins/ThumbLoader.java
@@ -0,0 +1,144 @@
+//
+// ThumbLoader.java
+//
+
+/*
+LOCI Plugins for ImageJ: a collection of ImageJ plugins including
+the 4D Data Browser, OME Plugin and Bio-Formats Exporter.
+Copyright (C) 2006-@year@ Melissa Linkert, Christopher Peterson,
+Curtis Rueden, Philip Huettl and Francis Wong.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Library General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package loci.plugins;
+
+import ij.IJ;
+import java.awt.Dialog;
+import java.awt.Panel;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Arrays;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import loci.formats.FormatException;
+import loci.formats.IFormatReader;
+
+/**
+ * Loads thumbnails for Bio-Formats Importer
+ * series chooser in a separate thread.
+ */
+public class ThumbLoader implements Runnable {
+
+  // -- Fields --
+
+  private IFormatReader ir;
+  private Panel[] p;
+  private Dialog dialog;
+  private boolean stop;
+  private Thread loader;
+
+  // -- Constructor --
+
+  /**
+   * Constructs a new thumbnail loader.
+   * @param ir the reader to use for obtaining the thumbnails
+   * @param p the panels upon which to populate the results
+   * @param dialog the dialog containing the panels
+   */
+  public ThumbLoader(IFormatReader ir, Panel[] p, Dialog dialog) {
+    this.ir = ir;
+    this.p = p;
+    this.dialog = dialog;
+    loader = new Thread(this, "BioFormats-ThumbLoader");
+    loader.start();
+  }
+
+  // -- ThumbLoader methods --
+
+  /** Instructs the thumbnail loader to stop loading thumbnails. */
+  public void stop() {
+    if (loader == null) return;
+    stop = true;
+    IJ.showStatus("Canceling thumbnail generation");
+    try {
+      loader.join();
+      loader = null;
+    }
+    catch (InterruptedException exc) {
+      exc.printStackTrace();
+    }
+    IJ.showStatus("");
+  }
+
+  // -- Runnable methods --
+
+  /** Does the work of loading the thumbnails. */
+  public void run() {
+    try {
+      IJ.showStatus("Gathering series information");
+      int seriesCount = ir.getSeriesCount();
+
+      // find image plane for each series and sort by size
+      SeriesInfo[] info = new SeriesInfo[seriesCount];
+      for (int i=0; i<seriesCount && !stop; i++) {
+        ir.setSeries(i);
+        info[i] = new SeriesInfo(i, ir.getSizeX() * ir.getSizeY());
+      }
+      Arrays.sort(info);
+
+      // open each thumbnail, fastest/smallest first
+      for (int i=0; i<seriesCount && !stop; i++) {
+        final int ii = info[i].index;
+        IJ.showStatus("Reading thumbnail for series #" + (ii + 1));
+        ir.setSeries(ii);
+        // open middle image thumbnail
+        int z = ir.getSizeZ() / 2;
+        int t = ir.getSizeT() / 2;
+        int ndx = ir.getIndex(z, 0, t);
+        BufferedImage thumb = ir.openThumbImage(ndx);
+        ImageIcon icon = new ImageIcon(thumb);
+        p[ii].removeAll();
+        p[ii].add(new JLabel(icon));
+        if (dialog != null) dialog.validate();
+      }
+    }
+    catch (IOException exc) {
+      exc.printStackTrace();
+    }
+    catch (FormatException exc) {
+      exc.printStackTrace();
+    }
+  }
+
+  // -- Helper classes --
+
+  public class SeriesInfo implements Comparable {
+
+    private int index, size;
+
+    public SeriesInfo(int index, int size) {
+      this.index = index;
+      this.size = size;
+    }
+
+    public int compareTo(Object o) {
+      SeriesInfo info = (SeriesInfo) o;
+      return size - info.size;
+    }
+
+  }
+
+}
