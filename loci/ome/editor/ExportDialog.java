817a851264cc4cbc9fa8ecb29bb6bb6db17ca60f
Melissa Linkert <melissa@glencoesoftware.com>
2007-05-24 14:53:58 +0000

    2007 May 24 release of LOCI Tools

---
+++ loci/ome/editor/ExportDialog.java
@@ -0,0 +1,190 @@
+//
+// ExportDialog.java
+//
+
+/*
+OME Metadata Editor application for exploration and editing of OME-XML and
+OME-TIFF metadata. Copyright (C) 2006-@year@ Christopher Peterson.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Library General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package loci.ome.editor;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+
+/**
+ * A class that makes a dialog that gives a JList allowing
+ * users to select the notes they want to export to a
+ * text file.
+ * Note that much of this code was found in an example in
+ * the JDK 1.4 API Specs... ListDialog.java .
+ *
+ * @author Christopher Peterson crpeterson2 at wisc.edu
+ */
+public class ExportDialog extends JDialog implements ActionListener {
+
+  // -- Fields --
+
+  /** Holds the dialog. */
+  private static ExportDialog dialog;
+
+  /** The list of values returned as selected in the dialog. */
+  private static Object [] value;
+
+  /** The JList found in this dialog. */
+  private JList list;
+
+  // -- Constructor --
+
+  /** Set up the GUI, etc. Can't directly instatiate this. */
+  private ExportDialog(Frame frame, Component locationComp, String labelText,
+    String title, Object[] data, Object[] initialValue, String longValue)
+  {
+    super(frame, title, true);
+
+    JButton cancelButton = new JButton("Cancel");
+    cancelButton.addActionListener(this);
+    final JButton exportButton = new JButton("Export Selected");
+    exportButton.setActionCommand("Set");
+    exportButton.addActionListener(this);
+    exportButton.setToolTipText("Export the selected note(s).");
+    final JButton exportAllButton = new JButton("Export All");
+    exportAllButton.setActionCommand("SetAll");
+    exportAllButton.addActionListener(this);
+    exportAllButton.setToolTipText("Export all notes.");
+    getRootPane().setDefaultButton(exportAllButton);
+
+    list = new JList(data) {
+      //Subclass JList to workaround bug 4832765, which can cause the
+      //scroll pane to not let the user easily scroll up to the beginning
+      //of the list.  An alternative would be to set the unitIncrement
+      //of the JScrollBar to a fixed value. You wouldn't get the nice
+      //aligned scrolling, but it should work.
+      public int getScrollableUnitIncrement(Rectangle visibleRect,
+                                            int orientation,
+                                            int direction) {
+        int row;
+        if (orientation == SwingConstants.VERTICAL &&
+          direction < 0 && (row = getFirstVisibleIndex()) != -1)
+        {
+          Rectangle r = getCellBounds(row, row);
+          if ((r.y == visibleRect.y) && (row != 0))  {
+            Point loc = r.getLocation();
+            loc.y--;
+            int prevIndex = locationToIndex(loc);
+            Rectangle prevR = getCellBounds(prevIndex, prevIndex);
+
+            if (prevR == null || prevR.y >= r.y) {
+                return 0;
+            }
+            return prevR.height;
+          }
+        }
+        return super.getScrollableUnitIncrement(
+                        visibleRect, orientation, direction);
+      }
+    };
+
+    list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+    if (longValue != null) {
+        list.setPrototypeCellValue(longValue); //get extra space
+    }
+    list.setLayoutOrientation(JList.VERTICAL);
+    list.setVisibleRowCount(-1);
+    JScrollPane listScroller = new JScrollPane(list);
+    listScroller.setPreferredSize(new Dimension(300, 160));
+    listScroller.setAlignmentX(LEFT_ALIGNMENT);
+
+    JPanel listPane = new JPanel();
+
+    FormLayout panelLayout = new FormLayout(
+      "5dlu, pref:grow:right, 5dlu, pref, 5dlu, pref, 5dlu",
+      "5dlu, pref, 5dlu, pref, 5dlu, pref, 5dlu");
+    listPane.setLayout(panelLayout);
+    CellConstraints cc = new CellConstraints();
+
+    listPane.setLayout(panelLayout);
+    JLabel label = new JLabel(labelText);
+    label.setLabelFor(list);
+    listPane.add(label, cc.xyw(2,2,5));
+    listPane.add(listScroller, cc.xyw(2,4,5));
+
+    listPane.add(exportButton, cc.xy(2,6));
+    listPane.add(exportAllButton, cc.xy(4,6));
+    listPane.add(cancelButton, cc.xy(6,6));
+
+    setContentPane(listPane);
+
+    //Initialize values.
+    setValue(initialValue);
+    pack();
+    setLocationRelativeTo(locationComp);
+  }
+
+  // -- ExportDialog API methods --
+
+  /**
+   * Set up and show the dialog.  The first Component argument
+   * determines which frame the dialog depends on; it should be
+   * a component in the dialog's controlling frame. The second
+   * Component argument should be null if you want the dialog
+   * to come up with its left corner in the center of the screen;
+   * otherwise, it should be the component on top of which the
+   * dialog should appear.
+   */
+  public static Object[] showDialog(Component frameComp,
+    Component locationComp, String labelText, String title,
+    Object[] possibleValues, Object[] initialValue, String longValue)
+  {
+    Frame frame = JOptionPane.getFrameForComponent(frameComp);
+    dialog = new ExportDialog(frame, locationComp, labelText, title,
+      possibleValues, initialValue, longValue);
+    dialog.setVisible(true);
+    return value;
+  }
+
+  // -- Helper methods --
+
+  /** Set the selected values to a specified array. */
+  private void setValue(Object [] newValue) {
+    value = newValue;
+    if (newValue != null) {
+      for (int i = 0;i < newValue.length;i++) {
+        list.setSelectedValue(value[i], false);
+      }
+    }
+  }
+
+  // -- ActionListener API methods --
+
+  /** Handle clicks on the Export and ExportAll and cancel buttons. */
+  public void actionPerformed(ActionEvent e) {
+    if ("Set".equals(e.getActionCommand())) {
+      ExportDialog.value = list.getSelectedValues();
+    }
+    if ("SetAll".equals(e.getActionCommand())) {
+      list.setSelectionInterval(0, list.getModel().getSize() - 1);
+      ExportDialog.value = list.getSelectedValues();
+    }
+    ExportDialog.dialog.setVisible(false);
+  }
+
+}
---
+++ loci/ome/util/OMETablePanel.java
@@ -0,0 +1,324 @@
+//
+// OMETablePanel.java
+//
+
+/*
+OME Plugin for ImageJ plugin for transferring images to and from an OME
+database. Copyright (C) 2004-@year@ Philip Huettl and Melissa Linkert.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Library General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package loci.ome.util;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.event.*;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * OMETablePanel is the class that handles the table used
+ * in displaying the images selectable for download.
+ *
+ * @author Philip Huettl pmhuettl at wisc.edu
+ * @author Melissa Linkert, linkert at wisc.edu
+ */
+public class OMETablePanel implements ActionListener {
+
+  // -- Fields --
+
+  private JDialog dialog;
+  private JTable table;
+  private TableSorter sorter;
+  public boolean cancelPlugin;
+  private JPanel paneR, paneThumb;
+  private Object[][] extra;
+  private JTextArea own, typ, c1, t1, x1, y1, z1, des, thumb;
+  private JTextArea[] areas;
+  private String[] columns;
+
+  // -- Constructor --
+
+  public OMETablePanel(Frame frame, Object[][] tableData, String[] columnNames,
+      Object[][] details) {
+
+    columns = columnNames;
+    cancelPlugin = false;
+    //Creates the Dialog Box for getting OME login information
+    dialog = new JDialog(frame, "Search Results", true);
+    JPanel mainpane = new JPanel();
+    mainpane.setMinimumSize(new Dimension(800, 100));
+    //sets up the detail panel
+    paneR = new JPanel();
+    paneR.setLayout(new BoxLayout(paneR, BoxLayout.Y_AXIS));
+    JPanel paneOwner = new JPanel();
+    paneThumb = new JPanel();
+
+    GridBagLayout grid = new GridBagLayout();
+    paneOwner.setLayout(grid);
+    GridBagConstraints e = new GridBagConstraints();
+    GridBagConstraints d = new GridBagConstraints();
+    e.weightx = 0;
+    e.weighty = 0;
+    e.gridheight = 1;
+    e.gridwidth = 1;
+    e.gridx = 0;
+    e.gridy = 0;
+    e.anchor = GridBagConstraints.FIRST_LINE_END;
+    e.insets = new Insets(2,2,2,2);
+    e.fill = GridBagConstraints.NONE;
+    d.gridwidth = GridBagConstraints.REMAINDER;
+    d.gridheight = 1;
+    d.weighty = 0;
+    d.weightx = 1;
+    d.gridx = 1;
+    d.gridy = 0;
+    d.fill = GridBagConstraints.HORIZONTAL;
+    d.insets = new Insets(2,2,2,2);
+    d.anchor = GridBagConstraints.FIRST_LINE_START;
+
+    paneR.add(paneThumb);
+    paneR.add(paneOwner);
+    JLabel owner = new JLabel("Owner: ", JLabel.RIGHT);
+    JLabel type = new JLabel("Image Type: ", JLabel.RIGHT);
+    JLabel x = new JLabel("Size X: ", JLabel.RIGHT);
+    JLabel y = new JLabel("Size Y: ", JLabel.RIGHT);
+    JLabel z = new JLabel("Size Z: ", JLabel.RIGHT);
+    JLabel c = new JLabel("Size C: ", JLabel.RIGHT);
+    JLabel t = new JLabel("Size T: ", JLabel.RIGHT);
+    JLabel descrip = new JLabel("Description: ", JLabel.RIGHT);
+    owner.setMinimumSize(new Dimension(15, 2));
+    type.setMinimumSize(new Dimension(15, 2));
+    c.setMinimumSize(new Dimension(10, 2));
+    t.setMinimumSize(new Dimension(10, 2));
+    x.setMinimumSize(new Dimension(10, 2));
+    y.setMinimumSize(new Dimension(10, 2));
+    z.setMinimumSize(new Dimension(10, 2));
+    descrip.setMinimumSize(new Dimension(20, 2));
+
+    areas = new JTextArea[] {own, typ, x1, y1, z1, c1, t1};
+    for(int i=0; i<areas.length; i++) {
+      areas[i] = new JTextArea("", 1, 6);
+      areas[i].setEditable(false);
+    }
+
+    thumb = new JTextArea(2,15);
+    des = new JTextArea("", 2, 6);
+    des.setEditable(false);
+    des.setLineWrap(true);
+    thumb.setLineWrap(true);
+    des.setWrapStyleWord(true);
+    thumb.setWrapStyleWord(true);
+    thumb.setEditable(false);
+
+    Dimension thsize = new Dimension(300,100);
+    thumb.setMinimumSize(thsize);
+    thumb.setMaximumSize(thsize);
+
+    JScrollPane desScroll = new JScrollPane(des);
+
+    JLabel[] labels = {owner, type, x, y, z, c, t};
+    for(int i=0; i<labels.length; i++) {
+      paneOwner.add(labels[i], e);
+      paneOwner.add(areas[i], d);
+      e.gridy++;
+      d.gridy++;
+    }
+
+    e.gridheight = 2;
+    e.weighty = 2;
+    d.gridheight = 2;
+    d.weighty = 2;
+    e.fill = GridBagConstraints.VERTICAL;
+    d.fill = GridBagConstraints.BOTH;
+    paneOwner.add(descrip,e);
+    paneOwner.add(desScroll,d);
+    paneThumb.add(thumb);
+
+    extra = details;
+    mainpane.setLayout(new BoxLayout(mainpane, BoxLayout.Y_AXIS));
+    JLabel label = new JLabel("Select which image(s) to download to ImageJ.");
+    label.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+    mainpane.add(label);
+
+    //create table
+    //MyTableModel tableModel = new MyTableModel(tableData, columns);
+    DefaultTableModel tableModel = new DefaultTableModel(tableData, columns) {
+      public Class getColumnClass(int c) {
+        return getValueAt(0, c).getClass();
+      }
+      public boolean isCellEditable(int row, int col) {
+        return col == 0;
+      }
+    };
+    sorter = new TableSorter(tableModel);
+
+    // set whether each column is numerically sorted
+    boolean[] isNumeric = new boolean[columns.length];
+    for(int i=0; i<isNumeric.length; i++) {
+      if(columns[i].equals("ID")) isNumeric[i] = true;
+      else isNumeric[i] = false;
+    }
+    sorter.setNumeric(isNumeric);
+
+    table = new JTable(sorter);
+    table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+    //Ask to be notified of selection changes.
+    ListSelectionModel rowSM = table.getSelectionModel();
+    rowSM.addListSelectionListener(new ListSelectionListener() {
+      public void valueChanged(ListSelectionEvent e) {
+        if (e.getValueIsAdjusting()) return;
+
+        ListSelectionModel lsm = (ListSelectionModel)e.getSource();
+        if (lsm.isSelectionEmpty()) {
+          setPane("-1");
+        }
+        else {
+          int selectedRow = lsm.getMinSelectionIndex();
+          String row = "-1";
+          for(int i=0; i<columns.length; i++) {
+            if(columns[i].equals("ID")) {
+              row = (String) sorter.getValueAt(selectedRow, i);
+            }
+          }
+          setPane(row);
+        }
+      }
+    });
+
+    sorter.setTableHeader(table.getTableHeader());
+    JScrollPane scrollPane = new JScrollPane(table);
+    scrollPane.setMinimumSize(new Dimension(500, 300));
+    table.setPreferredScrollableViewportSize(new Dimension(800, 300));
+    //size columns
+    table.getColumnModel().getColumn(0).setMaxWidth(10);
+    table.getColumnModel().getColumn(1).setPreferredWidth(50);
+    table.getColumnModel().getColumn(2).setPreferredWidth(30);
+    table.getColumnModel().getColumn(2).setMaxWidth(40);
+    table.getColumnModel().getColumn(3).setPreferredWidth(50);
+
+    //put panel together
+    JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+      scrollPane, paneR);
+    splitPane.setDividerLocation(.5);
+    splitPane.setResizeWeight(.5);
+    splitPane.setAlignmentX(JSplitPane.CENTER_ALIGNMENT);
+    mainpane.add(splitPane);
+    dialog.setContentPane(mainpane);
+    EmptyBorder bord = new EmptyBorder(5,5,5,5);
+    JButton ok = new JButton("OK"), cancels = new JButton("Cancel");
+    ok.setActionCommand("OK");
+    cancels.setActionCommand("cancels");
+    JPanel paneBut = new JPanel();
+    paneBut.setLayout(new BoxLayout(paneBut,BoxLayout.X_AXIS));
+    paneBut.add(ok);
+    paneBut.add(cancels);
+    paneBut.add(Box.createHorizontalGlue());
+    mainpane.add(paneBut);
+    cancels.addActionListener(this);
+    ok.addActionListener(this);
+    mainpane.setBorder(bord);
+    mainpane.setMinimumSize(new Dimension(800,300));
+    dialog.pack();
+  }
+
+  // -- Methods --
+
+  /** implements the ActionListener actionPerformed method */
+  public void actionPerformed(ActionEvent e) {
+    if ("OK".equals(e.getActionCommand())) {
+      cancelPlugin = false;
+      dialog.setVisible(false);
+    }
+    else {
+      cancelPlugin = true;
+      dialog.dispose();
+    }
+  }
+
+  /** method that builds the details pane for the selected image */
+  private void setPane(String id) {
+    paneThumb.removeAll();
+
+    // calculate the row index
+    // extra[index][9] should match id
+
+    int index = 0;
+    for(int i=0; i<extra.length; i++) {
+      if(extra[i][9].equals(id)) index = i;
+    }
+
+    if (!id.equals("-1")) {
+      if (extra[index][0] != null) {
+        final BufferedImage img = (BufferedImage) extra[index][0];
+          JPanel imagePanel = new JPanel() {
+          public void paint(Graphics g) {
+            g.drawImage(img, 0, 0, this);
+          }
+          public Dimension getMinimumSize() {
+            return new Dimension(img.getWidth(), img.getHeight());
+          }
+          public Dimension getPreferredSize() {
+            return new Dimension(img.getWidth(), img.getHeight());
+          }
+          public Dimension getMaximumSize() {
+            return new Dimension(img.getWidth(), img.getHeight());
+          }
+        };
+        paneThumb.add(imagePanel);
+      }
+      else {
+        paneThumb.add(thumb);
+        thumb.setText("Java 1.4 required to display thumbnail.");
+      }
+      for(int i=0; i<areas.length; i++) {
+        areas[i].setText((String) extra[index][i+1]);
+      }
+      des.setText((String) extra[index][areas.length+1]);
+    }
+    else {
+      paneThumb.add(thumb);
+      thumb.setText("");
+      for(int i=0; i<areas.length; i++) {
+        areas[i].setText("");
+      }
+      des.setText("");
+    }
+  }
+
+  /** Method that gets the images checked in the table to download to ImageJ */
+  public int[] getInput() {
+    cancelPlugin = true;
+    dialog.setVisible(true);
+    if (cancelPlugin) return null;
+    //checks and puts results into an array
+    ArrayList al = new ArrayList();
+    for (int i=0; i<table.getRowCount(); i++) {
+      if (((Boolean) (sorter.getValueAt(i,0))).booleanValue()) {
+        al.add(sorter.getValueAt(i,2));
+      }
+    }
+    int[] results = new int[al.size()];
+    for (int i=0; i<al.size(); i++) {
+      results[i] = Integer.parseInt((String) al.get(i));
+    }
+    return results;
+  }
+}
