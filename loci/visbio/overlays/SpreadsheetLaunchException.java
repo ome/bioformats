a81b385f61a201b053df751cf4a8c996bf15ccd8
Abraham Sorber <acsorber@gmail.com>
2007-05-25 21:07:07 +0000

    Removed unnecessary import.

--- loci/visbio/overlays/SpreadsheetLaunchException.java
+++ loci/visbio/overlays/SpreadsheetLaunchException.java
@@ -24,7 +24,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 package loci.visbio.overlays;
 
 import java.io.File;
-import com.jgoodies.looks.LookUtils;
 
 /** Exception thrown when spreadsheet launch command fails */
 public class SpreadsheetLaunchException extends Exception{
