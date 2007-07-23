31b5d789200bfa6f85b6fab67b2e0d036bd4458d
Melissa Linkert <melissa@glencoesoftware.com>
2007-07-23 17:39:12 +0000

    Make sure RGB flag gets propagated.

--- loci/formats/in/OMETiffReader.java
+++ loci/formats/in/OMETiffReader.java
@@ -135,6 +135,7 @@ public class OMETiffReader extends BaseTiffReader {
         core.imageCount[i] = ifds.length;
         int sc = core.sizeC[i];
         if (rgb) sc /= 3;
+        core.rgb[i] = rgb; 
         core.sizeT[i] = Integer.parseInt(pixels[i].getAttribute("SizeT"));
         core.pixelType[i] = FormatTools.pixelTypeFromString(
           pixels[i].getAttribute("PixelType"));
