7e5b08f3bd6989e2de5f35c2c41b603a9e03d6b1
Curtis Rueden <ctrueden@wisc.edu>
2007-08-30 16:55:28 +0000

    A simple utility to filter out duplicate stack traces.

---
+++ utils/UniqueTraces.java
@@ -0,0 +1,52 @@
+//
+// UniqueTraces.java
+//
+
+import java.io.*;
+import java.util.Vector;
+
+/**
+ * Reads in a bunch of stack traces from standard input,
+ * filters out duplicates and outputs what's left.
+ */
+public class UniqueTraces {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    Vector traces = new Vector();
+    StringBuffer buf = null;
+    while (true) {
+      String line = in.readLine();
+      if (line == null) break;
+      if (!line.startsWith(" ") && !line.startsWith("\t")) {
+        // start of a new stack trace
+        if (buf == null) buf = new StringBuffer(); // first stack trace
+        else {
+          // record previous stack trace
+          traces.add(buf.toString());
+          buf.setLength(0);
+        }
+        // in the middle of a stack trace
+      }
+      buf.append(line);
+      buf.append("\n");
+    }
+    traces.add(buf.toString());
+
+    String[] traceStrings = new String[traces.size()];
+    traces.copyInto(traceStrings);
+
+    for (int i=0; i<traceStrings.length; i++) {
+      String s = traceStrings[i];
+      boolean match = false;
+      for (int j=0; j<traceStrings.length; j++) {
+        if (i != j && s.equals(traceStrings[j])) {
+          match = true;
+          break;
+        }
+      }
+      if (!match) System.out.print(s);
+    }
+  }
+
+}
