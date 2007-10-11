c35884b6b3eb80b395016952a60727945390f1b1
Curtis Rueden <ctrueden@wisc.edu>
2007-10-11 14:26:32 +0000

    TiffComment now has a trivial implementation thanks to TiffTools.getComment.

--- utils/TiffComment.java
+++ utils/TiffComment.java
@@ -2,9 +2,7 @@
 // TiffComment.java
 //
 
-import loci.formats.RandomAccessStream;
 import loci.formats.TiffTools;
-import java.util.Hashtable;
 
 /** Extracts the comment from the first IFD of the given TIFF file(s). */
 public class TiffComment {
@@ -15,28 +13,8 @@ public class TiffComment {
       return;
     }
     for (int i=0; i<args.length; i++) {
-      // read IFDs
-      RandomAccessStream in = new RandomAccessStream(args[i]);
-      Hashtable ifd = TiffTools.getFirstIFD(in);
-      in.close();
-
-      // extract comment
-      Object o = TiffTools.getIFDValue(ifd, TiffTools.IMAGE_DESCRIPTION);
-      String comment = null;
-      if (o instanceof String) comment = (String) o;
-      else if (o instanceof String[]) {
-        String[] s = (String[]) o;
-        if (s.length > 0) comment = s[0];
-      }
-      else if (o != null) comment = o.toString();
-
-      if (comment != null) {
-        // sanitize line feeds
-        comment = comment.replaceAll("\r\n", "\n");
-        comment = comment.replaceAll("\r", "\n");
-
-        System.out.println(comment);
-      }
+      String comment = TiffTools.getComment(args[i]);
+      if (comment != null) System.out.println(comment);
     }
   }
 
