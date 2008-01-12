53f797e68b09e6cb11e34ce05a1a9766b127aba7
Curtis Rueden <ctrueden@wisc.edu>
2008-01-12 01:53:37 +0000

    Include new command line parameters in usage string.

--- loci/formats/tools/ImageInfo.java
+++ loci/formats/tools/ImageInfo.java
@@ -141,10 +141,10 @@ public final class ImageInfo {
       String fmt = reader instanceof ImageReader ? "any" : reader.getFormat();
       String[] s = {
         "To test read a file in " + fmt + " format, run:",
-        "  showinf [-nopix] [-nometa] [-thumbs]",
-        "    [-minmax] [-merge] [-stitch] [-separate] [-omexml]",
-        "    [-normalize] [-fast] [-debug] [-range start end]",
-        "    [-series num] [-swap order] [-map id] file",
+        "  showinf [-nopix] [-nometa] [-thumbs] [-minmax] [-fill]",
+        "    [-merge] [-stitch] [-separate] [-expand] [-omexml]",
+        "    [-normalize] [-fast] [-debug] [-range start end] [-series num]",
+        "    [-swap order] [-map id] [-preload] [-version v] file",
         "",
         "      file: the image file to read",
         "    -nopix: read metadata only, not pixels",
