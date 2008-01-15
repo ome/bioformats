63bcbbe04e6f27106b0ea873235736e9b3494813
Melissa Linkert <melissa@glencoesoftware.com>
2008-01-15 19:19:59 +0000

    Removed '-fill' option, as it is a duplicate of '-expand'.

--- loci/formats/tools/ImageInfo.java
+++ loci/formats/tools/ImageInfo.java
@@ -61,7 +61,6 @@ public final class ImageInfo {
     boolean doMeta = true;
     boolean thumbs = false;
     boolean minmax = false;
-    boolean fill = false;
     boolean merge = false;
     boolean stitch = false;
     boolean separate = false;
@@ -84,7 +83,6 @@ public final class ImageInfo {
           else if (args[i].equals("-nometa")) doMeta = false;
           else if (args[i].equals("-thumbs")) thumbs = true;
           else if (args[i].equals("-minmax")) minmax = true;
-          else if (args[i].equals("-fill")) fill = true;
           else if (args[i].equals("-merge")) merge = true;
           else if (args[i].equals("-stitch")) stitch = true;
           else if (args[i].equals("-separate")) separate = true;
@@ -141,7 +139,7 @@ public final class ImageInfo {
       String fmt = reader instanceof ImageReader ? "any" : reader.getFormat();
       String[] s = {
         "To test read a file in " + fmt + " format, run:",
-        "  showinf [-nopix] [-nometa] [-thumbs] [-minmax] [-fill]",
+        "  showinf [-nopix] [-nometa] [-thumbs] [-minmax] ",
         "    [-merge] [-stitch] [-separate] [-expand] [-omexml]",
         "    [-normalize] [-fast] [-debug] [-range start end] [-series num]",
         "    [-swap order] [-map id] [-preload] [-version v] file",
@@ -151,7 +149,6 @@ public final class ImageInfo {
         "   -nometa: output only core metadata",
         "   -thumbs: read thumbnails instead of normal pixels",
         "   -minmax: compute min/max statistics",
-        "     -fill: convert indexed color images to RGB",
         "    -merge: combine separate channels into RGB image",
         "   -stitch: stitch files with similar names",
         " -separate: split RGB image into separate channels",
@@ -212,10 +209,9 @@ public final class ImageInfo {
       String pat = FilePattern.findPattern(new Location(id));
       if (pat != null) id = pat;
     }
-    if (fill) reader = new ChannelFiller(reader);
+    if (expand) reader = new ChannelFiller(reader);
     if (separate) reader = new ChannelSeparator(reader);
     if (merge) reader = new ChannelMerger(reader);
-    if (expand) reader = new ChannelFiller(reader);
     MinMaxCalculator minMaxCalc = null;
     if (minmax) reader = minMaxCalc = new MinMaxCalculator(reader);
     DimensionSwapper dimSwapper = null;
