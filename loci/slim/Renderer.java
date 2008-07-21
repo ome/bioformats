5a83cdc949fea65a31279757bfa6c07f49ca6e9a
Eric Kjellman <egkjellman@wisc.edu>
2008-07-21 18:49:01 +0000

    base renderer class for SLIM plotter

---
+++ loci/slim/Renderer.java
@@ -0,0 +1,73 @@
+import loci.slim.CurveCollection;
+
+public abstract class Renderer implements Runnable {
+
+  protected boolean alive;
+  protected boolean ready;
+  protected int maxIterations;
+  protected double maxRCSE;
+  protected int totalIterations;
+  protected int currentIterations;
+  protected int currentX;
+  protected int currentY;
+  protected int subsampleLevel;
+  protected int currProgress;
+  protected int maxProgress;
+  protected CurveCollection curveData;
+  
+  public abstract void run();
+
+  public void stop() {
+    // TODO: Check if alive and ready?
+    alive = false;
+  }
+
+  public Renderer(CurveCollection cc) {
+    curveData = cc;
+  }
+
+  public int getCurrentIterations() {
+    return currentIterations;
+  }
+
+  public int getTotalIterations() {
+    return totalIterations;
+  }
+  
+  public int getCurrentX() {
+    return currentX;
+  }
+
+  public int getCurrentY() {
+    return currentY;
+  }
+
+  public int getSubsampleLevel() {
+    return subsampleLevel;
+  }
+
+  public int getCurrentProgress() {
+    return currProgress;
+  }
+
+  public int getMaxProgress() {
+    return maxProgress;
+  }
+  
+  public void setMaxIterations(int mi) {
+    maxIterations = mi;
+  }
+
+  public void setMaxRCSE(double mr) {
+    maxRCSE = mr;
+  }
+
+  public int getMaxIterations() {
+    return maxIterations;
+  }
+
+  public double getMaxRCSE() {
+    return maxRCSE;
+  }
+
+}
