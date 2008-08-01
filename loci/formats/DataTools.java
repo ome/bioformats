32ddd911994720715934dba3ebdee6fcdb9496e8
Melissa Linkert <melissa@glencoesoftware.com>
2008-08-01 05:08:22 +0000

    Added writeLong utility method (fixes TiffTools compile error).

--- loci/formats/DataTools.java
+++ loci/formats/DataTools.java
@@ -164,6 +164,32 @@ public final class DataTools {
     out.write(b);
   }
 
+  /** Writes a long to the given data output destination. */
+  public static void writeLong(DataOutput out, long v, boolean little)
+    throws IOException
+  {
+    if (little) {
+      out.write((int) (v & 0xff));
+      out.write((int) ((v >>> 8) & 0xff));
+      out.write((int) ((v >>> 16) & 0xff));
+      out.write((int) ((v >>> 24) & 0xff));
+      out.write((int) ((v >>> 32) & 0xff));
+      out.write((int) ((v >>> 40) & 0xff));
+      out.write((int) ((v >>> 48) & 0xff));
+      out.write((int) ((v >>> 56) & 0xff));
+    }
+    else {
+      out.write((int) ((v >>> 56) & 0xff));
+      out.write((int) ((v >>> 48) & 0xff));
+      out.write((int) ((v >>> 40) & 0xff));
+      out.write((int) ((v >>> 32) & 0xff));
+      out.write((int) ((v >>> 24) & 0xff));
+      out.write((int) ((v >>> 16) & 0xff));
+      out.write((int) ((v >>> 8) & 0xff));
+      out.write((int) (v & 0xff));
+    }
+  }
+
   /** Writes an integer to the given data output destination. */
   public static void writeInt(DataOutput out, int v, boolean little)
     throws IOException
