936779d9bcfd0d4bc2846af97c863d212fce0082
Melissa Linkert <melissa@glencoesoftware.com>
2008-12-29 19:42:56 +0000

    2008 Dec 29 release of LOCI Tools

---
+++ components/bio-formats-auto/src/Entity.java
@@ -0,0 +1,61 @@
+//
+// Entity.java
+//
+
+/*
+OME Bio-Formats package for reading and converting biological file formats.
+Copyright (C) 2005-@year@ UW-Madison LOCI and Glencoe Software, Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+import java.util.Hashtable;
+
+/**
+ * An entity is an object that contains a bundle of {@link Property} objects.
+ * It has a list of default attributes (key/value pairs),
+ * as well as a list of version overrides for those attributes.
+ *
+ * @see {EntityList}
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="https://skyking.microscopy.wisc.edu/trac/java/browser/trunk/components/bio-formats/auto/Entity.java">Trac</a>,
+ * <a href="https://skyking.microscopy.wisc.edu/svn/java/trunk/components/bio-formats/auto/Entity.java">SVN</a></dd></dl>
+ *
+ * @author Curtis Rueden ctrueden at wisc.edu
+ */
+public class Entity {
+
+  // -- Fields --
+
+  /** The entity's attributes. */
+  protected Hashtable<String, String> attrs;
+
+  /** The entity's properties. */
+  protected Hashtable<String, Property> props =
+    new Hashtable<String, Property>();
+
+  /** The entity's version overrides. */
+  protected Hashtable<String, Hashtable<String, String>> versions =
+    new Hashtable<String, Hashtable<String, String>>();
+
+  // -- Constructor --
+
+  /** Creates a new entity with the given attributes. */
+  public Entity(Hashtable<String, String> attrs) {
+    this.attrs = attrs;
+  }
+
+}
---
+++ components/bio-formats-auto/src/Property.java
@@ -0,0 +1,57 @@
+//
+// Property.java
+//
+
+/*
+OME Bio-Formats package for reading and converting biological file formats.
+Copyright (C) 2005-@year@ UW-Madison LOCI and Glencoe Software, Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+import java.util.Hashtable;
+
+/**
+ * A property is an object that belongs to a particular {@link Entity}.
+ * It has a list of default attributes (key/value pairs),
+ * as well as a list of version overrides for those attributes.
+ *
+ * @see {EntityList}
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="https://skyking.microscopy.wisc.edu/trac/java/browser/trunk/components/bio-formats/auto/Property.java">Trac</a>,
+ * <a href="https://skyking.microscopy.wisc.edu/svn/java/trunk/components/bio-formats/auto/Property.java">SVN</a></dd></dl>
+ *
+ * @author Curtis Rueden ctrueden at wisc.edu
+ */
+public class Property {
+
+  // -- Fields --
+
+  /** The property's attributes. */
+  protected Hashtable<String, String> attrs;
+
+  /** The property's version overrides. */
+  protected Hashtable<String, Hashtable<String, String>> versions =
+    new Hashtable<String, Hashtable<String, String>>();
+
+  // -- Constructor --
+
+  /** Creates a new property with the given attributes. */
+  public Property(Hashtable<String, String> attrs) {
+    this.attrs = attrs;
+  }
+
+}
