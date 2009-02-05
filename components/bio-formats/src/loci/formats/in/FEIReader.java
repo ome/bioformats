c7987c576a40c599463571967ac97b5a3d16d514
Curtis Rueden <ctrueden@wisc.edu>
2009-02-05 00:00:34 +0000

    Fix typo.

--- components/bio-formats/src/loci/formats/in/FEIReader.java
+++ components/bio-formats/src/loci/formats/in/FEIReader.java
@@ -92,7 +92,7 @@ public class FEIReader extends FormatReader {
 
   /* @see loci.formats.FormatReader#initFile(String) */
   protected void initFile(String id) throws FormatException, IOException {
-    if (debug) debug("PCXReader.initFile(" + id + ")");
+    if (debug) debug("FEIReader.initFile(" + id + ")");
     super.initFile(id);
     in = new RandomAccessStream(id);
 
