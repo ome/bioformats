2da358fbd7b27e231a1b22e5bc79f5dbc7647a9d
Melissa Linkert <melissa@glencoesoftware.com>
2009-03-30 16:40:45 +0000

    Switched licenses from GPL v3 or later to GPL v2 or later.

--- components/slim-plotter/src/loci/slim/fit/CurveFitter.java
+++ components/slim-plotter/src/loci/slim/fit/CurveFitter.java
@@ -9,7 +9,7 @@ Copyright (C) 2006-@year@ Curtis Rueden and Eric Kjellman.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
+the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
