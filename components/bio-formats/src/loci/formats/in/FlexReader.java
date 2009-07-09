89382f026b8d34c0194f8cd654677ab83e978b13
Curtis Rueden <ctrueden@wisc.edu>
2009-07-09 18:44:00 +0000

    Change FLEX constant back to public (needed to query Flex IFDs outside of package; see utils/ExtractFlexMetadata.java for an example).

--- components/bio-formats/src/loci/formats/in/FlexReader.java
+++ components/bio-formats/src/loci/formats/in/FlexReader.java
@@ -49,7 +49,7 @@ public class FlexReader extends FormatReader {
   // -- Constants --
 
   /** Custom IFD entry for Flex XML. */
-  protected static final int FLEX = 65200;
+  public static final int FLEX = 65200;
 
   // -- Fields --
 
