7677ef8bac0d367fa839d6d634054b20aebf8cb4
Curtis Rueden <ctrueden@wisc.edu>
2009-08-17 18:49:47 +0000

    Add "-nosymbols" argument for generating alphanumeric passwords.

--- utils/RandomPass.java
+++ utils/RandomPass.java
@@ -11,14 +11,28 @@ public class RandomPass {
 
   public static void main(String[] args) {
     int passLen = PASS_LEN;
-    if (args.length > 0) {
-      passLen = Integer.parseInt(args[0]);
+    boolean alphaNum = false;
+    for (String arg : args) {
+      if (arg.equals("-nosymbols")) {
+        alphaNum = true;
+      }
+      else {
+        // assume argument is password character length
+        passLen = Integer.parseInt(arg);
+      }
     }
     StringBuffer sb = new StringBuffer(passLen + 2);
     int range = MAX_CHAR - MIN_CHAR + 1;
-    for (int i=0; i<passLen; i++) {
+    int i = 0;
+    while (i < passLen) {
       char c = (char) (range * Math.random() + MIN_CHAR);
+      if (alphaNum) {
+        boolean alpha = (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+        boolean num = c >= '0' && c <= '9';
+        if (!alpha && !num) continue;
+      }
       sb.append(c);
+      i++;
     }
     System.out.println(sb);
   }
