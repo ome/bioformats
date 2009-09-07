85ca79d43d1a602074a736cdc90da047d5ae85a6
Melissa Linkert <melissa@glencoesoftware.com>
2009-09-07 17:43:47 +0000

    Added utility to check for unused imports.

---
+++ utils/UnusedImports.java
@@ -0,0 +1,72 @@
+//
+// UnusedImports.java
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Vector;
+
+/** Scans Java source files for unused and package imports. */
+public class UnusedImports {
+  public static void checkFile(String filename) throws IOException {
+    BufferedReader file =
+      new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
+    Vector<String> imports = new Vector<String>();
+    Vector<String> packages = new Vector<String>();
+
+    String line = file.readLine();
+    int lineNumber = 0;
+    while (line != null) {
+      line = line.trim();
+      if (line.startsWith("import ")) {
+        if (line.endsWith(".*;")) {
+          packages.add(line.replace("import ", ""));
+        }
+        else {
+          String className =
+            line.substring(line.lastIndexOf(".") + 1, line.length() - 1);
+          imports.add(className);
+        }
+      }
+      else {
+        for (int i=0; i<imports.size(); i++) {
+          String importedClass = imports.get(i);
+          if (line.indexOf(importedClass) != -1 && !line.startsWith("//") &&
+            !line.startsWith("*") && !line.startsWith("/*"))
+          {
+            imports.remove(importedClass);
+            i--;
+          }
+        }
+      }
+
+      line = file.readLine();
+      lineNumber++;
+    }
+
+    if (imports.size() > 0 || packages.size() > 0) {
+      System.out.println(filename);
+      if (imports.size() > 0) {
+        System.out.println("  Unused imports:");
+        for (String importedClass : imports) {
+          System.out.println("    " + importedClass);
+        }
+      }
+      if (packages.size() > 0) {
+        System.out.println("  Package imports:");
+        for (String packageName : packages) {
+          System.out.println("    " + packageName);
+        }
+      }
+    }
+
+    file.close();
+  }
+
+  public static void main(String[] args) throws IOException {
+    for (String arg : args) {
+      checkFile(arg);
+    }
+  }
+}
