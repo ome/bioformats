d4d5162d5bf9940318a8811b9b2fb3e9539f6e76
Curtis Rueden <ctrueden@wisc.edu>
2009-10-15 18:36:21 +0000

    A little bit of utils cleanup.

--- components/bio-formats/utils/CommentSurgery.java
+++ components/bio-formats/utils/CommentSurgery.java
@@ -12,8 +12,8 @@ import loci.formats.tiff.TiffTools;
  * make desired alterations to the comment.
  *
  * <dl><dt><b>Source code:</b></dt>
- * <dd><a href="https://skyking.microscopy.wisc.edu/trac/java/browser/trunk/utils/CommentSurgery.java">Trac</a>,
- * <a href="https://skyking.microscopy.wisc.edu/svn/java/trunk/utils/CommentSurgery.java">SVN</a></dd></dl>
+ * <dd><a href="https://skyking.microscopy.wisc.edu/trac/java/browser/trunk/components/bio-formats/utils/CommentSurgery.java">Trac</a>,
+ * <a href="https://skyking.microscopy.wisc.edu/svn/java/trunk/components/bio-formats/utils/CommentSurgery.java">SVN</a></dd></dl>
  */
 public class CommentSurgery {
   public static void main(String[] args) throws Exception {
