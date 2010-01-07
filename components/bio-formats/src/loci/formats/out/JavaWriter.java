f4f06adb7288d35c9545ca955d4d8100014e61d9
Curtis Rueden <ctrueden@wisc.edu>
2010-01-07 19:39:36 +0000

    Add writer for saving images as Java source code. Unfortunately, images larger than ~100x100 will not compile due to source file size limits (error is either "Code too large" or an OOM during compilation).

---
+++ components/bio-formats/src/loci/formats/out/JavaWriter.java
@@ -0,0 +1,263 @@
+//
+// JavaWriter.java
+//
+
+/*
+OME Bio-Formats package for reading and converting biological file formats.
+Copyright (C) 2005-@year@ UW-Madison LOCI and Glencoe Software, Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package loci.formats.out;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import loci.common.DataTools;
+import loci.common.RandomAccessOutputStream;
+import loci.formats.FormatException;
+import loci.formats.FormatTools;
+import loci.formats.FormatWriter;
+import loci.formats.MetadataTools;
+import loci.formats.meta.MetadataRetrieve;
+
+/**
+ * JavaWriter is the file format writer for Java source code.
+ * At the moment, this code is just a very simple container for pixel data.
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="https://skyking.microscopy.wisc.edu/trac/java/browser/trunk/components/bio-formats/src/loci/formats/out/JavaWriter.java">Trac</a>,
+ * <a href="https://skyking.microscopy.wisc.edu/svn/java/trunk/components/bio-formats/src/loci/formats/out/JavaWriter.java">SVN</a></dd></dl>
+ */
+public class JavaWriter extends FormatWriter {
+
+  // -- Fields --
+
+  protected PrintWriter out;
+  protected int no = 0;
+
+  // -- Constructor --
+
+  public JavaWriter() { super("Java source code", "java"); }
+
+  // -- IFormatWriter API methods --
+
+  /* @see loci.formats.IFormatWriter#saveBytes(byte[], int, boolean, boolean) */
+  public void saveBytes(byte[] buf, int series, boolean lastInSeries,
+    boolean last) throws FormatException, IOException
+  {
+    if (buf == null) throw new FormatException("Byte array is null");
+
+    // check pixel type
+    MetadataRetrieve meta = getMetadataRetrieve();
+    MetadataTools.verifyMinimumPopulated(meta, series);
+    String pixelType = meta.getPixelsPixelType(series, 0);
+    int type = FormatTools.pixelTypeFromString(pixelType);
+    if (!DataTools.containsValue(getPixelTypes(), type)) {
+      throw new FormatException("Unsupported image type '" + pixelType + "'.");
+    }
+    int bpp = FormatTools.getBytesPerPixel(type);
+    boolean fp = FormatTools.isFloatingPoint(type);
+    boolean little = Boolean.FALSE.equals(meta.getPixelsBigEndian(series, 0));
+
+    if (!initialized) {
+      writeHeader();
+      initialized = true;
+    }
+
+    // write array
+    String varName = "series" + series + "Plane" + no;
+    Object array = DataTools.makeDataArray(buf, bpp, fp, little);
+    int w = meta.getPixelsSizeX(series, 0).intValue();
+    int h = meta.getPixelsSizeY(series, 0).intValue();
+    if (array instanceof byte[]) {
+      writePlane(varName, (byte[]) array, w, h);
+    }
+    else if (array instanceof short[]) {
+      writePlane(varName, (short[]) array, w, h);
+    }
+    else if (array instanceof int[]) {
+      writePlane(varName, (int[]) array, w, h);
+    }
+    else if (array instanceof long[]) {
+      writePlane(varName, (long[]) array, w, h);
+    }
+    else if (array instanceof float[]) {
+      writePlane(varName, (float[]) array, w, h);
+    }
+    else if (array instanceof double[]) {
+      writePlane(varName, (double[]) array, w, h);
+    }
+
+    no++;
+    if (lastInSeries) no = 0;
+    if (last) writeFooter();
+  }
+
+  /* @see loci.formats.IFormatWriter#canDoStacks() */
+  public boolean canDoStacks() { return true; }
+
+  /* @see loci.formats.IFormatWriter#getPixelTypes() */
+  public int[] getPixelTypes() {
+    return new int[] {
+      FormatTools.INT8,
+      FormatTools.UINT8,
+      FormatTools.UINT16,
+      FormatTools.UINT32,
+      FormatTools.INT32,
+      FormatTools.FLOAT,
+      FormatTools.DOUBLE
+    };
+  }
+
+  // -- IFormatHandler API methods --
+
+  /* @see loci.formats.IFormatHandler#close() */
+  public void close() throws IOException {
+    if (out != null) out.close();
+    out = null;
+    currentId = null;
+    initialized = false;
+  }
+
+  // -- Helper methods --
+
+  protected void writeHeader() throws IOException {
+    out = new PrintWriter(new RandomAccessOutputStream(currentId));
+
+    String className = currentId.substring(0, currentId.length() - 5);
+    className = className.substring(className.lastIndexOf(File.separator) + 1);
+
+    out.println("//");
+    out.println("// " + className + ".java");
+    out.println("//");
+    out.println();
+    out.println("// Generated by Bio-Formats v" + FormatTools.VERSION);
+    out.println("// Generated on " + new Date());
+    out.println();
+    out.println("public class " + className + " {");
+    out.println();
+  }
+
+  protected void writePlane(String varName, byte[] array, int w, int h) {
+    int i = 0;
+    out.println("  public byte[][] " + varName + " = {");
+    for (int y=0; y<h; y++) {
+      out.print("    {");
+      for (int x=0; x<w; x++) {
+        out.print(array[i++]);
+        if (x < w - 1) out.print(", ");
+        else out.print("}");
+      }
+      if (y < h - 1) out.println(",");
+      else out.println();
+    }
+    out.println("  };");
+    out.println();
+  }
+
+  protected void writePlane(String varName, short[] array, int w, int h) {
+    int i = 0;
+    out.println("  public short[][] " + varName + " = {");
+    for (int y=0; y<h; y++) {
+      out.print("    {");
+      for (int x=0; x<w; x++) {
+        out.print(array[i++]);
+        if (x < w - 1) out.print(", ");
+        else out.print("}");
+      }
+      if (y < h - 1) out.println(",");
+      else out.println();
+    }
+    out.println("  };");
+    out.println();
+  }
+
+  protected void writePlane(String varName, int[] array, int w, int h) {
+    int i = 0;
+    out.println("  public int[][] " + varName + " = {");
+    for (int y=0; y<h; y++) {
+      out.print("    {");
+      for (int x=0; x<w; x++) {
+        out.print(array[i++]);
+        if (x < w - 1) out.print(", ");
+        else out.print("}");
+      }
+      if (y < h - 1) out.println(",");
+      else out.println();
+    }
+    out.println("  };");
+    out.println();
+  }
+
+  protected void writePlane(String varName, long[] array, int w, int h) {
+    int i = 0;
+    out.println("  public long[][] " + varName + " = {");
+    for (int y=0; y<h; y++) {
+      out.print("    {");
+      for (int x=0; x<w; x++) {
+        out.print(array[i++]);
+        if (x < w - 1) out.print(", ");
+        else out.print("}");
+      }
+      if (y < h - 1) out.println(",");
+      else out.println();
+    }
+    out.println("  };");
+    out.println();
+  }
+
+  protected void writePlane(String varName, float[] array, int w, int h) {
+    int i = 0;
+    out.println("  public float[][] " + varName + " = {");
+    for (int y=0; y<h; y++) {
+      out.print("    {");
+      for (int x=0; x<w; x++) {
+        out.print(array[i++]);
+        if (x < w - 1) out.print(", ");
+        else out.print("}");
+      }
+      if (y < h - 1) out.println(",");
+      else out.println();
+    }
+    out.println("  };");
+    out.println();
+  }
+
+  protected void writePlane(String varName, double[] array, int w, int h) {
+    int i = 0;
+    out.println("  public double[][] " + varName + " = {");
+    for (int y=0; y<h; y++) {
+      out.print("    {");
+      for (int x=0; x<w; x++) {
+        out.print(array[i++]);
+        if (x < w - 1) out.print(", ");
+        else out.print("}");
+      }
+      if (y < h - 1) out.println(",");
+      else out.println();
+    }
+    out.println("  };");
+    out.println();
+  }
+
+  protected void writeFooter() {
+    out.println("}");
+  }
+
+}
