f4f1cce4690222d686dfd5fa827bbf9a4ea95b6c
Melissa Linkert <melissa@glencoesoftware.com>
2010-02-12 19:05:15 +0000

    Added reader for VG SAM .dti files. See #412.

---
+++ components/bio-formats/src/loci/formats/in/VGSAMReader.java
@@ -0,0 +1,126 @@
+//
+// VGSAMReader.java
+//
+
+/*
+OME Bio-Formats package for reading and converting biological file formats.
+Copyright (C) 2005-@year@ UW-Madison LOCI and Glencoe Software, Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package loci.formats.in;
+
+import java.io.IOException;
+
+import loci.common.RandomAccessInputStream;
+import loci.formats.FormatException;
+import loci.formats.FormatReader;
+import loci.formats.FormatTools;
+import loci.formats.MetadataTools;
+import loci.formats.meta.FilterMetadata;
+import loci.formats.meta.MetadataStore;
+
+/**
+ * VGSAMReader is the file format reader for VG SAM .dti files.
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="https://skyking.microscopy.wisc.edu/trac/java/browser/trunk/components/bio-formats/src/loci/formats/in/VGSAMReader.java">Trac</a>,
+ * <a href="https://skyking.microscopy.wisc.edu/svn/java/trunk/components/bio-formats/src/loci/formats/in/VGSAMReader.java">SVN</a></dd></dl>
+ */
+public class VGSAMReader extends FormatReader {
+
+  // -- Constants --
+
+  public static final String VG_MAGIC_STRING = "VGS";
+  public static final int PIXEL_OFFSET = 368;
+
+  // -- Fields --
+
+  // -- Constructor --
+
+  /** Constructs a new VG SAM reader. */
+  public VGSAMReader() {
+    super("VG SAM", "dti");
+    domains = new String[] {FormatTools.SPM_DOMAIN};
+  }
+
+  // -- IFormatReader API methods --
+
+  /* @see loci.formats.IFormatReader#isThisType(RandomAccessInputStream) */
+  public boolean isThisType(RandomAccessInputStream stream) throws IOException {
+    final int blockLen = 3;
+    if (!FormatTools.validStream(stream, blockLen, false)) return false;
+    return (stream.readString(blockLen)).indexOf(VG_MAGIC_STRING) >= 0;
+  }
+
+  /**
+   * @see loci.formats.IFormatReader#openBytes(int, byte[], int, int, int, int)
+   */
+  public byte[] openBytes(int no, byte[] buf, int x, int y, int w, int h)
+    throws FormatException, IOException
+  {
+    FormatTools.checkPlaneParameters(this, no, buf.length, x, y, w, h);
+
+    in.seek(PIXEL_OFFSET);
+    readPlane(in, x, y, w, h, buf);
+    return buf;
+  }
+
+  // -- Internal FormatReader API methods --
+
+  /* @see loci.formats.FormatReader#initFile(String) */
+  protected void initFile(String id) throws FormatException, IOException {
+    debug("VGSAMReader.initFile(" + id + ")");
+    super.initFile(id);
+    in = new RandomAccessInputStream(id);
+
+    in.seek(348);
+    core[0].sizeX = in.readInt();
+    core[0].sizeY = in.readInt();
+    in.skipBytes(4);
+
+    int bpp = in.readInt();
+    switch (bpp) {
+      case 1:
+        core[0].pixelType = FormatTools.UINT8;
+        break;
+      case 2:
+        core[0].pixelType = FormatTools.UINT16;
+        break;
+      case 4:
+        core[0].pixelType = FormatTools.FLOAT;
+        break;
+      default:
+        throw new FormatException("Unsupported bytes per pixel: " + bpp);
+    }
+
+    core[0].littleEndian = false;
+    core[0].sizeZ = 1;
+    core[0].sizeC = 1;
+    core[0].sizeT = 1;
+    core[0].imageCount = 1;
+    core[0].rgb = false;
+    core[0].interleaved = false;
+    core[0].dimensionOrder = "XYZCT";
+
+    MetadataStore store =
+      new FilterMetadata(getMetadataStore(), isMetadataFiltered());
+    MetadataTools.populatePixels(store, this);
+
+    MetadataTools.setDefaultCreationDate(store, id, 0);
+  }
+
+}
