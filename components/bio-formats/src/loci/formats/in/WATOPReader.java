23ccdca04b00db91d0b6315672c0124674dc60b4
Melissa Linkert <melissa@glencoesoftware.com>
2010-03-01 15:20:11 +0000

    Added reader for WA Technology .wat files.  See #412.

---
+++ components/bio-formats/src/loci/formats/in/WATOPReader.java
@@ -0,0 +1,150 @@
+//
+// WATOPReader.java
+//
+
+/*
+OME Bio-Formats package for reading and converting biological file formats.
+Copyright (C) 2005-@year@ UW-Madison LOCI and Glencoe Software, Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package loci.formats.in;
+
+import java.io.IOException;
+
+import loci.common.DateTools;
+import loci.common.RandomAccessInputStream;
+import loci.formats.FormatException;
+import loci.formats.FormatReader;
+import loci.formats.FormatTools;
+import loci.formats.MetadataTools;
+import loci.formats.meta.FilterMetadata;
+import loci.formats.meta.MetadataStore;
+
+/**
+ * WATOPReader is the file format reader for WA Technology .wat files.
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="https://skyking.microscopy.wisc.edu/trac/java/browser/trunk/components/bio-formats/src/loci/formats/in/WATOPReader.java">Trac</a>,
+ * <a href="https://skyking.microscopy.wisc.edu/svn/java/trunk/components/bio-formats/src/loci/formats/in/WATOPReader.java">SVN</a></dd></dl>
+ */
+public class WATOPReader extends FormatReader {
+
+  // -- Constants --
+
+  private static final int HEADER_SIZE = 4864;
+  private static final String WAT_MAGIC_STRING = "0TOPSystem W.A.Technology";
+
+  // -- Fields --
+
+  // -- Constructor --
+
+  /** Constructs a new WA Technology reader. */
+  public WATOPReader() {
+    super("WA Technology TOP", "wat");
+    domains = new String[] {FormatTools.SEM_DOMAIN};
+  }
+
+  // -- IFormatReader API methods --
+
+  /* @see loci.formats.IFormatReader#isThisType(RandomAccessInputStream) */
+  public boolean isThisType(RandomAccessInputStream stream) throws IOException {
+    final int blockLen = 25;
+    if (!FormatTools.validStream(stream, blockLen, false)) return false;
+    return stream.readString(blockLen).equals(WAT_MAGIC_STRING);
+  }
+
+  /**
+   * @see loci.formats.IFormatReader#openBytes(int, byte[], int, int, int, int)
+   */
+  public byte[] openBytes(int no, byte[] buf, int x, int y, int w, int h)
+    throws FormatException, IOException
+  {
+    FormatTools.checkPlaneParameters(this, no, buf.length, x, y, w, h);
+
+    in.seek(HEADER_SIZE);
+    readPlane(in, x, y, w, h, buf);
+    return buf;
+  }
+
+  // -- Internal FormatReader API methods --
+
+  /* @see loci.formats.FormatReader#initFile(String) */
+  protected void initFile(String id) throws FormatException, IOException {
+    debug("WATOPReader.initFile(" + id + ")");
+    super.initFile(id);
+    in = new RandomAccessInputStream(id);
+    core[0].littleEndian = true;
+    in.order(isLittleEndian());
+
+    in.seek(49);
+    String comment = in.readString(33);
+
+    in.seek(211);
+    int year = in.readInt();
+    int month = in.readInt();
+    int day = in.readInt();
+    int hour = in.readInt();
+    int min = in.readInt();
+    String date = year + "-" + month + "-" + day + "T" + hour + ":" + min;
+    date = DateTools.formatDate(date, "yyyy-MM-dd'T'HH:mm");
+
+    in.skipBytes(8);
+
+    double xSize = in.readInt() / 100d;
+    double ySize = in.readInt() / 100d;
+    double zSize = in.readInt() / 100d;
+
+    core[0].sizeX = in.readInt();
+    core[0].sizeY = in.readInt();
+
+    double tunnelCurrent = in.readInt() / 1000d;
+    double sampleVolts = in.readInt() / 1000d;
+
+    in.skipBytes(180);
+
+    int originalZMax = in.readInt();
+    int originalZMin = in.readInt();
+    int zMax = in.readInt();
+    int zMin = in.readInt();
+
+    addGlobalMeta("Comment", comment);
+    addGlobalMeta("X size (in um)", xSize);
+    addGlobalMeta("Y size (in um)", ySize);
+    addGlobalMeta("Z size (in um)", zSize);
+    addGlobalMeta("Tunnel current (in amps)", tunnelCurrent);
+    addGlobalMeta("Sample volts", sampleVolts);
+    addGlobalMeta("Acquisition date", date);
+
+    core[0].pixelType = FormatTools.INT16;
+    core[0].sizeC = 1;
+    core[0].sizeZ = 1;
+    core[0].sizeT = 1;
+    core[0].imageCount = 1;
+    core[0].dimensionOrder = "XYZCT";
+    core[0].rgb = false;
+
+    MetadataStore store =
+      new FilterMetadata(getMetadataStore(), isMetadataFiltered());
+    MetadataTools.populatePixels(store, this);
+
+    store.setImageDescription(comment, 0);
+    store.setImageCreationDate(date, 0);
+    store.setDimensionsPhysicalSizeX(new Double(xSize / getSizeX()), 0, 0);
+    store.setDimensionsPhysicalSizeY(new Double(ySize / getSizeY()), 0, 0);
+  }
+
+}
