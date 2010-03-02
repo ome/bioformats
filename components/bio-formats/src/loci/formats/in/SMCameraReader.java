d23b61c6f0a12676836b96b8629756a4040936b0
Melissa Linkert <melissa@glencoesoftware.com>
2010-03-02 21:11:11 +0000

    Added SM Camera reader.  See #412.

---
+++ components/bio-formats/src/loci/formats/in/SMCameraReader.java
@@ -0,0 +1,128 @@
+//
+// SMCameraReader.java
+//
+
+/*
+OME Bio-Formats package for reading and converting biological file formats.
+Copyright (C) 2005-@year@ UW-Madison LOCI and Glencoe Software, Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package loci.formats.in;
+
+import java.io.IOException;
+
+import loci.common.RandomAccessInputStream;
+import loci.formats.FormatException;
+import loci.formats.FormatReader;
+import loci.formats.FormatTools;
+import loci.formats.MetadataTools;
+import loci.formats.meta.FilterMetadata;
+import loci.formats.meta.MetadataStore;
+
+/**
+ * SMCameraReader is the file format reader for SM camera files.
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="https://skyking.microscopy.wisc.edu/trac/java/browser/trunk/components/bio-formats/src/loci/formats/in/SMCameraReader.java">Trac</a>,
+ * <a href="https://skyking.microscopy.wisc.edu/svn/java/trunk/components/bio-formats/src/loci/formats/in/SMCameraReader.java">SVN</a></dd></dl>
+ */
+public class SMCameraReader extends FormatReader {
+
+  // -- Constants --
+
+  private static final byte[] SMC_MAGIC = new byte[] {
+    0, 0, 0, 0, 2, 0, 0, 5, (byte) 0xc9, (byte) 0x88, 0, 5, (byte) 0xcb,
+    (byte) 0x88, 0, 0
+  };
+
+  private static final int HEADER_SIZE = 548;
+
+  // -- Fields --
+
+  // -- Constructor --
+
+  /** Constructs a new SM camera reader. */
+  public SMCameraReader() {
+    super("SM Camera", "");
+    domains = new String[] {FormatTools.SEM_DOMAIN};
+    suffixSufficient = false;
+    suffixNecessary = false;
+  }
+
+  // -- IFormatReader API methods --
+
+  /* @see loci.formats.IFormatReader#isThisType(RandomAccessInputStream) */
+  public boolean isThisType(RandomAccessInputStream stream) throws IOException {
+    final int blockLen = 16;
+    if (!FormatTools.validStream(stream, blockLen, false)) return false;
+    /* debug */ System.out.println("calling isThisType");
+    byte[] magic = new byte[blockLen];
+    stream.read(magic);
+    for (int i=0; i<magic.length; i++) {
+      if (magic[i] != SMC_MAGIC[i]) return false;
+    }
+    return true;
+  }
+
+  /**
+   * @see loci.formats.IFormatReader#openBytes(int, byte[], int, int, int, int)
+   */
+  public byte[] openBytes(int no, byte[] buf, int x, int y, int w, int h)
+    throws FormatException, IOException
+  {
+    FormatTools.checkPlaneParameters(this, no, buf.length, x, y, w, h);
+
+    in.seek(HEADER_SIZE);
+    readPlane(in, x, y, w, h, buf);
+    return buf;
+  }
+
+  /* @see loci.formats.IFormatReader#close(boolean) */
+  public void close(boolean fileOnly) throws IOException {
+    super.close(fileOnly);
+    if (!fileOnly) {
+    }
+  }
+
+  // -- Internal FormatReader API methods --
+
+  /* @see loci.formats.FormatReader#initFile(String) */
+  protected void initFile(String id) throws FormatException, IOException {
+    debug("SMCameraReader.initFile(" + id + ")");
+    super.initFile(id);
+    in = new RandomAccessInputStream(id);
+
+    in.seek(524);
+
+    core[0].sizeY = in.readShort();
+    in.skipBytes(6);
+    core[0].sizeX = in.readShort();
+    core[0].pixelType = FormatTools.UINT8;
+    core[0].sizeZ = 1;
+    core[0].sizeC = 1;
+    core[0].sizeT = 1;
+    core[0].imageCount = 1;
+    core[0].dimensionOrder = "XYZCT";
+    core[0].littleEndian = false;
+
+    MetadataStore store =
+      new FilterMetadata(getMetadataStore(), isMetadataFiltered());
+    MetadataTools.populatePixels(store, this);
+    MetadataTools.setDefaultCreationDate(store, currentId, 0);
+  }
+
+}
