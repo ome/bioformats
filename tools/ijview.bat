aff7099f6bcc60164a027cabb63d5884ad859bb0
Curtis Rueden <ctrueden@wisc.edu>
2010-03-08 21:14:07 +0000

    Fix bugs in ijview command line tool.

--- tools/ijview.bat
+++ tools/ijview.bat
@@ -12,7 +12,7 @@ rem If you are a developer working from source and have
 rem the LOCI classes in your CLASSPATH, you can set the
 rem LOCI_DEVEL environment variable to use them instead.
 
-set PROG=loci.plugins.Importer
+set PROG=loci.plugins.importer.Importer
 set DIR=%~dp0
 
 if "%LOCI_DEVEL%" == "" (
