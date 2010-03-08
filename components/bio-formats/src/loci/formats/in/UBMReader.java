2565d6f198d67aace5cc98266f3856bdb9660205
Melissa Linkert <melissa@glencoesoftware.com>
2010-03-08 20:43:22 +0000

    Added UBM reader.  See #412.

---
+++ components/bio-formats/src/loci/formats/in/UBMReader.java
@@ -0,0 +1,124 @@
+//
+// UBMReader.java
+//
+
+/*
+OME Bio-Formats package for reading and converting biological file formats.
+Copyright (C) 2005-@year@ UW-Madison LOCI and Glencoe Software, Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package loci.formats.in;
+
+import java.io.IOException;
+
+import loci.common.RandomAccessInputStream;
+import loci.formats.FormatException;
+import loci.formats.FormatReader;
+import loci.formats.FormatTools;
+import loci.formats.MetadataTools;
+import loci.formats.meta.FilterMetadata;
+import loci.formats.meta.MetadataStore;
+
+/**
+ * UBMReader is the file format reader for UBM .pr3 files.
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="https://skyking.microscopy.wisc.edu/trac/java/browser/trunk/components/bio-formats/src/loci/formats/in/UBMReader.java">Trac</a>,
+ * <a href="https://skyking.microscopy.wisc.edu/svn/java/trunk/components/bio-formats/src/loci/formats/in/UBMReader.java">SVN</a></dd></dl>
+ */
+public class UBMReader extends FormatReader {
+
+  // -- Constants --
+
+  public static final int HEADER_SIZE = 128;
+
+  // -- Fields --
+
+  private int padding;
+
+  // -- Constructor --
+
+  /** Constructs a new UBM reader. */
+  public UBMReader() {
+    super("UBM", "pr3");
+    domains = new String[] {FormatTools.SEM_DOMAIN};
+  }
+
+  // -- IFormatReader API methods --
+
+  /* @see loci.formats.IFormatReader#isThisType(RandomAccessInputStream) */
+  public boolean isThisType(RandomAccessInputStream stream) throws IOException {
+    //final int blockLen = 16;
+    //if (!FormatTools.validStream(stream, blockLen, false)) return false;
+    //return (stream.readString(blockLen)).indexOf(AL3D_MAGIC_STRING) >= 0;
+    return false;
+  }
+
+  /**
+   * @see loci.formats.IFormatReader#openBytes(int, byte[], int, int, int, int)
+   */
+  public byte[] openBytes(int no, byte[] buf, int x, int y, int w, int h)
+    throws FormatException, IOException
+  {
+    FormatTools.checkPlaneParameters(this, no, buf.length, x, y, w, h);
+
+    in.seek(HEADER_SIZE);
+    readPlane(in, x, y, w, h, padding, buf);
+    return buf;
+  }
+
+  /* @see loci.formats.IFormatReader#close(boolean) */
+  public void close(boolean fileOnly) throws IOException {
+    super.close(fileOnly);
+    if (!fileOnly) {
+      padding = 0;
+    }
+  }
+
+  // -- Internal FormatReader API methods --
+
+  /* @see loci.formats.FormatReader#initFile(String) */
+  protected void initFile(String id) throws FormatException, IOException {
+    debug("UBMReader.initFile(" + id + ")");
+    super.initFile(id);
+    in = new RandomAccessInputStream(id);
+
+    core[0].littleEndian = true;
+    in.order(isLittleEndian());
+
+    in.seek(44);
+
+    core[0].sizeX = in.readInt();
+    core[0].sizeY = in.readInt();
+
+    core[0].pixelType = FormatTools.UINT32;
+    core[0].sizeZ = 1;
+    core[0].sizeC = 1;
+    core[0].sizeT = 1;
+    core[0].imageCount = 1;
+    core[0].dimensionOrder = "XYZCT";
+
+    padding =
+      (int) ((in.length() - FormatTools.getPlaneSize(this)) / (getSizeY() * 4));
+
+    MetadataStore store =
+      new FilterMetadata(getMetadataStore(), isMetadataFiltered());
+    MetadataTools.populatePixels(store, this);
+    MetadataTools.setDefaultCreationDate(store, id, 0);
+  }
+
+}
