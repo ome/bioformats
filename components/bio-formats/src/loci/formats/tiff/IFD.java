90ff863d8cf8b2d1678281b13deb9727587cf424
Melissa Linkert <melissa@glencoesoftware.com>
2010-03-24 15:31:45 +0000

    Ported r6063 to 4.1 branch.

--- components/bio-formats/src/loci/formats/tiff/IFD.java
+++ components/bio-formats/src/loci/formats/tiff/IFD.java
@@ -546,9 +546,11 @@ public class IFD extends HashMap<Integer, Object> {
 
     int samplesPerPixel = getSamplesPerPixel();
     if (bitsPerSample.length < samplesPerPixel) {
-      throw new FormatException("BitsPerSample length (" +
-        bitsPerSample.length + ") does not match SamplesPerPixel (" +
-        samplesPerPixel + ")");
+      LogTools.debug("BitsPerSample length (" + bitsPerSample.length +
+        ") does not match SamplesPerPixel(" + samplesPerPixel + ")");
+      int bits = bitsPerSample[0];
+      bitsPerSample = new int[samplesPerPixel];
+      Arrays.fill(bitsPerSample, bits);
     }
     int nSamples = (int) Math.min(bitsPerSample.length, samplesPerPixel);
     for (int i=0; i<nSamples; i++) {
