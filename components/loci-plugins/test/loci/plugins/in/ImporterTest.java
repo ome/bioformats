7109eb111dfbfdbfe3ba736b4777577e8403d12e
Barry DeZonia <bdezonia@wisc.edu>
2010-05-07 21:53:52 +0000

    Further implementation

--- components/loci-plugins/test/loci/plugins/in/ImporterTest.java
+++ components/loci-plugins/test/loci/plugins/in/ImporterTest.java
@@ -22,9 +22,6 @@ public class ImporterTest {
 
   private static final boolean debug = true;
 
-  // TODO - in place to switch off tests that interface with gui
-  private static final boolean runAllTests = true;
-  
   private enum Axis {Z,C,T};
   
   // ** Helper methods *******************************************************************
@@ -125,8 +122,8 @@ public class ImporterTest {
     assertNotNull(ip);
     assertEquals(x,ip.getWidth());
     assertEquals(y,ip.getHeight());
-    assertEquals(z,ip.getNSlices());
-    assertEquals(c,ip.getNChannels());
+    assertEquals(z,ip.getNSlices());    // tricky - these last 3 getters have side effects that change their output.
+    assertEquals(c,ip.getNChannels());  // TODO - How to test?
     assertEquals(t,ip.getNFrames());
   }
   
@@ -196,8 +193,9 @@ public class ImporterTest {
         }
   }
   
-  private void datasetSwapDimsTest(int pixType, String order, int x, int y, int z, int c, int t)
+  private void datasetSwapDimsTest(int pixType, int x, int y, int z, int t)
   {
+    int c = 3; String order = "XYZCT";
     String path = constructFakeFilename(order, pixType, x, y, z, c, t, -1);
     ImagePlus[] imps = null;
     try {
@@ -220,11 +218,23 @@ public class ImporterTest {
     int numSlices = st.getSize();
     assertEquals(z*c*t,numSlices);
 
-    System.out.println("Numslices == " + numSlices);
+    System.out.println("datasetSwapDimsTest()");
+    System.out.println("  Numslices == " + numSlices);
+    
+    int maxZ = -1;
+    int maxT = -1;
+    int tmp;
     for (int i = 0; i < numSlices; i++)
-      printVals(st.getProcessor(i+1));
-
-    // TODO - do some tests on order of dims of created images
+    {
+      ImageProcessor proc = st.getProcessor(i+1);
+      printVals(proc);
+      tmp = zIndex(proc)+1;
+      if (maxZ < tmp) maxZ = tmp;
+      tmp = tIndex(proc)+1;
+      if (maxT < tmp) maxT = tmp;
+    }
+    assertEquals(z,maxT);
+    assertEquals(t,maxZ);
   }
 
   private void datasetConcatenateTest(int pixType, String order,
@@ -296,14 +306,14 @@ public class ImporterTest {
   
   private void memorySpecifyZRangeTest()
   { 
-    int pixType = FormatTools.UINT8, x=30, y=30, z=5, c=2, t=4, s=-1;
+    int pixType = FormatTools.UINT8, x=30, y=30, z=6, c=2, t=4, s=-1;
     String path = constructFakeFilename("range", pixType, x, y, z, c, t, s);
     ImagePlus[] imps = null;
     try {
       ImporterOptions options = new ImporterOptions();
       options.setId(path);
-      options.setZBegin(0, 0);
-      options.setZEnd(0, 4);
+      options.setZBegin(0, 1);
+      options.setZEnd(0, 5);
       options.setZStep(0, 2);
       imps = BF.openImagePlus(options);
     }
@@ -314,7 +324,7 @@ public class ImporterTest {
       fail(e.getMessage());
     }
     
-    // should have the data: one series, all t's, all c's, z's from 1 to 3 by 2
+    // should have the data: one series, all t's, all c's, z's from 1 to 5 by 2
     assertNotNull(imps);
     assertTrue(imps.length == 1);
     ImagePlus ip = imps[0];
@@ -323,12 +333,17 @@ public class ImporterTest {
     assertTrue(ip.getHeight() == y);
     ImageStack st = ip.getStack();
     int numSlices = st.getSize();
-    assertEquals(16,numSlices);
+    assertEquals(3*c*t,numSlices);
+
     System.out.println("SpecifyZRangeTest: slices below");
     for (int i = 0; i < numSlices; i++)
-      printVals(st.getProcessor(i));
+      printVals(st.getProcessor(i+1));
     
     //TODO - some assertions
+    
+    // all t's present
+    // all c's present
+    // only specific z's present
     
   }
   
@@ -352,7 +367,7 @@ public class ImporterTest {
       fail(e.getMessage());
     }
     
-    // should have the data: one series, all t's, all c's, z's from 1 to 3 by 2
+    // should have the data: one series, all t's, all z's, c's from 3 to 9 by 3
     assertNotNull(imps);
     assertTrue(imps.length == 1);
     ImagePlus ip = imps[0];
@@ -361,12 +376,16 @@ public class ImporterTest {
     assertTrue(ip.getHeight() == y);
     ImageStack st = ip.getStack();
     int numSlices = st.getSize();
-    assertEquals(48,numSlices);
-    System.out.println("SpecifyCRangeTest: slices below");
-    for (int i = 0; i < numSlices; i++)
-      printVals(st.getProcessor(i));
+    assertEquals(z*3*t,numSlices);
+    //System.out.println("SpecifyCRangeTest: slices below");
+    //for (int i = 0; i < numSlices; i++)
+    //  printVals(st.getProcessor(i+1));
     
     //TODO - some assertions
+    
+    // all z's present
+    // all t's present
+    // only specific c's present
     
   }
   
@@ -378,8 +397,8 @@ public class ImporterTest {
     try {
       ImporterOptions options = new ImporterOptions();
       options.setId(path);
-      options.setTBegin(0, 0);
-      options.setTEnd(0, 3);
+      options.setTBegin(0, 1);
+      options.setTEnd(0, 10);
       options.setTStep(0, 4);
       imps = BF.openImagePlus(options);
     }
@@ -390,7 +409,7 @@ public class ImporterTest {
       fail(e.getMessage());
     }
     
-    // should have the data: one series, all t's, all c's, z's from 1 to 3 by 2
+    // should have the data: one series, all z's, all c's, t's from 1 to 10 by 4
     assertNotNull(imps);
     assertTrue(imps.length == 1);
     ImagePlus ip = imps[0];
@@ -399,13 +418,17 @@ public class ImporterTest {
     assertTrue(ip.getHeight() == y);
     ImageStack st = ip.getStack();
     int numSlices = st.getSize();
-    assertEquals(18,numSlices);
-    System.out.println("SpecifyTRangeTest: slices below");
-    for (int i = 0; i < numSlices; i++)
-      printVals(st.getProcessor(i));
+    assertEquals(z*c*3,numSlices);
+    //System.out.println("SpecifyTRangeTest: slices below");
+    //for (int i = 0; i < numSlices; i++)
+    //  printVals(st.getProcessor(i+1));
     
     //TODO - some assertions
     
+    // all z's present
+    // all c's present
+    // only specific t's present
+
   }
   
   private void memoryCropTest(int pixType, int x, int y, int cx, int cy)
@@ -438,29 +461,23 @@ public class ImporterTest {
   @Test
   public void testDefaultBehavior() {
 
-    if (runAllTests)
-    {
-      defaultBehaviorTest(FormatTools.UINT16, 400, 300, 1, 1, 1);
-      defaultBehaviorTest(FormatTools.INT16, 107, 414, 1, 1, 1);
-      defaultBehaviorTest(FormatTools.UINT32, 323, 206, 3, 2, 1);
-      defaultBehaviorTest(FormatTools.UINT8, 57, 78, 5, 4, 3);
-      defaultBehaviorTest(FormatTools.INT32, 158, 99, 2, 3, 4);
-      defaultBehaviorTest(FormatTools.INT8, 232, 153, 3, 7, 5);
-    }
+    defaultBehaviorTest(FormatTools.UINT16, 400, 300, 1, 1, 1);
+    defaultBehaviorTest(FormatTools.INT16, 107, 414, 1, 1, 1);
+    defaultBehaviorTest(FormatTools.UINT32, 323, 206, 3, 2, 1);  // failure on last val = 1,5,
+    defaultBehaviorTest(FormatTools.UINT8, 57, 78, 5, 4, 3);
+    defaultBehaviorTest(FormatTools.INT32, 158, 99, 2, 3, 4);
+    defaultBehaviorTest(FormatTools.INT8, 232, 153, 3, 7, 5);
   }
 
   @Test
   public void testOutputStackOrder()
   {
-    if (runAllTests)
-    {
-      outputStackOrderTest(FormatTools.UINT8, "XYZCT", 82, 47, 2, 3, 4);
-      outputStackOrderTest(FormatTools.UINT8, "XYZTC", 82, 47, 2, 3, 4);
-      outputStackOrderTest(FormatTools.UINT8, "XYCZT", 82, 47, 2, 3, 4);
-      outputStackOrderTest(FormatTools.UINT8, "XYCTZ", 82, 47, 2, 3, 4);
-      outputStackOrderTest(FormatTools.UINT8, "XYTCZ", 82, 47, 2, 3, 4);
-      outputStackOrderTest(FormatTools.UINT8, "XYTZC", 82, 47, 2, 3, 4);
-    }
+    outputStackOrderTest(FormatTools.UINT8, "XYZCT", 82, 47, 2, 3, 4);
+    outputStackOrderTest(FormatTools.UINT8, "XYZTC", 82, 47, 2, 3, 4);
+    outputStackOrderTest(FormatTools.UINT8, "XYCZT", 82, 47, 2, 3, 4);
+    outputStackOrderTest(FormatTools.UINT8, "XYCTZ", 82, 47, 2, 3, 4);
+    outputStackOrderTest(FormatTools.UINT8, "XYTCZ", 82, 47, 2, 3, 4);
+    outputStackOrderTest(FormatTools.UINT8, "XYTZC", 82, 47, 2, 3, 4);
   }
     
   @Test
@@ -476,24 +493,27 @@ public class ImporterTest {
   public void testDatasetSwapDims() {
     // TODO - can't really test this with fake files. It needs a series of files from grouping
     //   to reorder.
-    if (runAllTests)
-    {
-      datasetSwapDimsTest(FormatTools.UINT8, "XYZCT", 82, 47, 2, 3, 4);
-    }
+    
+    // TODO - Curtis says I should be able to test this without grouping
+    
+    datasetSwapDimsTest(FormatTools.UINT8, 82, 47, 1, 3);
+    datasetSwapDimsTest(FormatTools.UINT16, 82, 47, 3, 1);
+    datasetSwapDimsTest(FormatTools.UINT32, 82, 47, 5, 2);
+    datasetSwapDimsTest(FormatTools.INT8, 44, 109, 1, 4);
+    datasetSwapDimsTest(FormatTools.INT16, 44, 109, 2, 1);
+    datasetSwapDimsTest(FormatTools.INT32, 44, 109, 4, 3);
   }
   
   @Test
   public void testDatasetConcatenate() {
     
     // TODO - Curtis says impl broken right now - will test later
-    if (runAllTests)
-    {
-      // open a dataset that has multiple series
+
+    // open a dataset that has multiple series
       //   and should get back a single series
       datasetConcatenateTest(FormatTools.UINT8, "XYZCT", 82, 47, 1, 1, 1, 1);
       datasetConcatenateTest(FormatTools.UINT8, "XYZCT", 82, 47, 1, 1, 1, 17);
       datasetConcatenateTest(FormatTools.UINT8, "XYZCT", 82, 47, 4, 5, 2, 9);
-    }
   }
   
   @Test
@@ -518,11 +538,8 @@ public class ImporterTest {
   
   @Test
   public void testMemoryVirtualStack() {
-    if (runAllTests)
-    {
-      memoryVirtualStackTest(false);
-      memoryVirtualStackTest(true);
-    }
+    memoryVirtualStackTest(false);
+    memoryVirtualStackTest(true);
   }
   
   @Test
@@ -536,18 +553,14 @@ public class ImporterTest {
     memorySpecifyCRangeTest();
     memorySpecifyTRangeTest();
     //memorySpecifySRangeTest();
+    // TODO : generalize other methods and call here with varying params
   }
   
   @Test
   public void testMemoryCrop() {
     
-    // TODO - Curtis says impl broken right now - will test later
-    
-    if (runAllTests)
-    {
-      memoryCropTest(FormatTools.UINT8, 203, 409, 185, 104);
-      // TODO - crop window too big or too small
-    }
+    //TODO - more tests
+    memoryCropTest(FormatTools.UINT8, 203, 409, 185, 104);
   }
   
   @Test
@@ -594,279 +607,3 @@ public class ImporterTest {
     System.exit(0);
   }
 }
-
-  // ****************************************************************************************
-  // * old code vault : working copies to be deleted once refactored code proven
-  // ****************************************************************************************
-  
-  /*
-  @Test
-  public void testOutputStackOrderXYZCT() {
-    final int x = 82, y = 47, z = 2, c = 3, t = 4;
-    final int pixelType = FormatTools.UINT8;
-    final String path = constructFakeFilename("xyzct", pixelType, x, y, z, c, t, -1);
-    ImagePlus[] imps = null;
-    try {
-      final ImporterOptions options = new ImporterOptions();
-      options.setId(path);
-      options.setStackOrder("XYZCT");
-      imps = BF.openImagePlus(options);
-    }
-    catch (IOException e) {
-      fail(e.getMessage());
-    }
-    catch (FormatException e) {
-      fail(e.getMessage());
-    }
-
-    assertNotNull(imps);
-    assertEquals(1,imps.length);
-    
-    ImageStack st = imps[0].getStack();
-    int numSlices = st.getSize();
-    assertEquals(z*c*t,numSlices);
-    int count = 0;
-    // order differs here from other cases
-    System.out.println("XYZCT");
-    for (int k = 0; k < t; k++)
-      for (int j = 0; j < c; j++)
-        for (int i = 0; i < z; i++)
-        {
-          ImageProcessor proc = st.getProcessor(count+1);
-          printVals(proc);
-          assertNotNull(proc);
-          assertEquals(x,proc.getWidth());
-          assertEquals(y,proc.getHeight());
-          assertEquals(count,iIndex(proc));
-          assertEquals(i,zIndex(proc));
-          assertEquals(j,cIndex(proc));
-          assertEquals(k,tIndex(proc));
-          count++;
-        }
-  }
-  
-  @Test
-  public void testOutputStackOrderXYZTC() {
-    final int x = 82, y = 47, z = 2, c = 3, t = 4;
-    final int pixelType = FormatTools.UINT8;
-    final String path = constructFakeFilename("xyztc", pixelType, x, y, z, c, t, -1);
-    ImagePlus[] imps = null;
-    try {
-      final ImporterOptions options = new ImporterOptions();
-      options.setId(path);
-      options.setStackOrder("XYZTC");
-      imps = BF.openImagePlus(options);
-    }
-    catch (IOException e) {
-      fail(e.getMessage());
-    }
-    catch (FormatException e) {
-      fail(e.getMessage());
-    }
-
-    assertNotNull(imps);
-    assertEquals(1,imps.length);
-    
-    ImageStack st = imps[0].getStack();
-    int numSlices = st.getSize();
-    assertEquals(z*c*t,numSlices);
-    int count = 0;
-    // order differs here from other cases
-    System.out.println("XYZTC");
-    for (int j = 0; j < c; j++)
-      for (int k = 0; k < t; k++)
-        for (int i = 0; i < z; i++)
-        {
-          ImageProcessor proc = st.getProcessor(count+1);
-          printVals(proc);
-          assertNotNull(proc);
-          assertEquals(x,proc.getWidth());
-          assertEquals(y,proc.getHeight());
-          //assertEquals(count,iIndex(proc));
-          assertEquals(i,zIndex(proc));
-          assertEquals(j,cIndex(proc));
-          assertEquals(k,tIndex(proc));
-          count++;
-        }
-  }
-  
-  @Test
-  public void testOutputStackOrderXYCZT() {
-    final int x = 82, y = 47, z = 2, c = 3, t = 4;
-    final int pixelType = FormatTools.UINT8;
-    final String path = constructFakeFilename("xyczt", pixelType, x, y, z, c, t, -1);
-    ImagePlus[] imps = null;
-    try {
-      final ImporterOptions options = new ImporterOptions();
-      options.setId(path);
-      options.setStackOrder("XYCZT");
-      imps = BF.openImagePlus(options);
-    }
-    catch (IOException e) {
-      fail(e.getMessage());
-    }
-    catch (FormatException e) {
-      fail(e.getMessage());
-    }
-
-    assertNotNull(imps);
-    assertEquals(1,imps.length);
-    
-    ImageStack st = imps[0].getStack();
-    int numSlices = st.getSize();
-    assertEquals(z*c*t,numSlices);
-    int count = 0;
-    // order differs here from other cases
-    System.out.println("XYCZT");
-    for (int k = 0; k < t; k++)
-      for (int i = 0; i < z; i++)
-        for (int j = 0; j < c; j++)
-        {
-          ImageProcessor proc = st.getProcessor(count+1);
-          printVals(proc);
-          assertNotNull(proc);
-          assertEquals(x,proc.getWidth());
-          assertEquals(y,proc.getHeight());
-          //assertEquals(count,iIndex(proc));
-          assertEquals(i,zIndex(proc));
-          assertEquals(j,cIndex(proc));
-          assertEquals(k,tIndex(proc));
-          count++;
-        }
-  }
-  
-  @Test
-  public void testOutputStackOrderXYCTZ() {
-    final int x = 82, y = 47, z = 2, c = 3, t = 4;
-    final int pixelType = FormatTools.UINT8;
-    final String path = constructFakeFilename("xyctz", pixelType, x, y, z, c, t, -1);
-    ImagePlus[] imps = null;
-    try {
-      final ImporterOptions options = new ImporterOptions();
-      options.setId(path);
-      options.setStackOrder("XYCTZ");
-      imps = BF.openImagePlus(options);
-    }
-    catch (IOException e) {
-      fail(e.getMessage());
-    }
-    catch (FormatException e) {
-      fail(e.getMessage());
-    }
-
-    assertNotNull(imps);
-    assertEquals(1,imps.length);
-    
-    ImageStack st = imps[0].getStack();
-    int numSlices = st.getSize();
-    assertEquals(z*c*t,numSlices);
-    int count = 0;
-    // order differs here from other cases
-    System.out.println("XYCTZ");
-    for (int i = 0; i < z; i++)
-      for (int k = 0; k < t; k++)
-        for (int j = 0; j < c; j++)
-        {
-          ImageProcessor proc = st.getProcessor(count+1);
-          printVals(proc);
-          assertNotNull(proc);
-          assertEquals(x,proc.getWidth());
-          assertEquals(y,proc.getHeight());
-          //assertEquals(count,iIndex(proc));
-          assertEquals(i,zIndex(proc));
-          assertEquals(j,cIndex(proc));
-          assertEquals(k,tIndex(proc));
-          count++;
-        }
-  }
-  
-  @Test
-  public void testOutputStackOrderXYTZC() {
-    final int x = 82, y = 47, z = 2, c = 3, t = 4;
-    final int pixelType = FormatTools.UINT8;
-    final String path = constructFakeFilename("xytzc", pixelType, x, y, z, c, t, -1);
-    ImagePlus[] imps = null;
-    try {
-      final ImporterOptions options = new ImporterOptions();
-      options.setId(path);
-      options.setStackOrder("XYTZC");
-      imps = BF.openImagePlus(options);
-    }
-    catch (IOException e) {
-      fail(e.getMessage());
-    }
-    catch (FormatException e) {
-      fail(e.getMessage());
-    }
-
-    assertNotNull(imps);
-    assertEquals(1,imps.length);
-    
-    ImageStack st = imps[0].getStack();
-    int numSlices = st.getSize();
-    assertEquals(z*c*t,numSlices);
-    int count = 0;
-    // order differs here from other cases
-    System.out.println("XYTZC");
-    for (int j = 0; j < c; j++)
-      for (int i = 0; i < z; i++)
-        for (int k = 0; k < t; k++)
-        {
-          ImageProcessor proc = st.getProcessor(count+1);
-          printVals(proc);
-          assertNotNull(proc);
-          assertEquals(x,proc.getWidth());
-          assertEquals(y,proc.getHeight());
-          //assertEquals(count,iIndex(proc));
-          assertEquals(i,zIndex(proc));
-          assertEquals(j,cIndex(proc));
-          assertEquals(k,tIndex(proc));
-          count++;
-        }
-  }
-  
-  @Test
-  public void testOutputStackOrderXYTCZ() {
-    final int x = 82, y = 47, z = 2, c = 3, t = 4;
-    final int pixelType = FormatTools.UINT8;
-    final String path = constructFakeFilename("xytcz", pixelType, x, y, z, c, t, -1);
-    ImagePlus[] imps = null;
-    try {
-      final ImporterOptions options = new ImporterOptions();
-      options.setId(path);
-      options.setStackOrder("XYTCZ");
-      imps = BF.openImagePlus(options);
-    }
-    catch (IOException e) {
-      fail(e.getMessage());
-    }
-    catch (FormatException e) {
-      fail(e.getMessage());
-    }
-
-    assertNotNull(imps);
-    assertEquals(1,imps.length);
-    
-    ImageStack st = imps[0].getStack();
-    int numSlices = st.getSize();
-    assertEquals(z*c*t,numSlices);
-    int count = 0;
-    // order differs here from other cases
-    System.out.println("XYTCZ");
-    for (int i = 0; i < z; i++)
-      for (int j = 0; j < c; j++)
-        for (int k = 0; k < t; k++)
-        {
-          ImageProcessor proc = st.getProcessor(count+1);
-          printVals(proc);
-          assertNotNull(proc);
-          assertEquals(x,proc.getWidth());
-          assertEquals(y,proc.getHeight());
-          //assertEquals(count,iIndex(proc));
-          assertEquals(i,zIndex(proc));
-          assertEquals(j,cIndex(proc));
-          assertEquals(k,tIndex(proc));
-          count++;
-        }
-  }
-*/
