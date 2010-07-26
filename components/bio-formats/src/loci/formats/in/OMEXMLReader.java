4825b30f5bacef6e9eb2b636e273c72307e7b264
Melissa Linkert <melissa@glencoesoftware.com>
2010-07-26 16:25:40 +0000

    Switch calls to MetadataTools.populatePixels and OMEXMLService.convertMetadata, so that references are not lost.

--- components/bio-formats/src/loci/formats/in/OMEXMLReader.java
+++ components/bio-formats/src/loci/formats/in/OMEXMLReader.java
@@ -342,8 +342,8 @@ public class OMEXMLReader extends FormatReader {
     // populate assigned metadata store with the
     // contents of the internal OME-XML metadata object
     MetadataStore store = getMetadataStore();
-    service.convertMetadata(omexmlMeta, store);
     MetadataTools.populatePixels(store, this);
+    service.convertMetadata(omexmlMeta, store);
   }
 
   // -- Helper class --
