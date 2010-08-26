097a54a192b6520aaffb5fc83c32a93a924e3649
Curtis Rueden <ctrueden@wisc.edu>
2010-08-26 17:36:25 +0000

    Various improvements to Bio-Formats MATLAB bfopen script:   * Better handling of unsigned data types   * Better case logic for Bio-Formats trunk vs. 4.2 stable   * Better case logic for MATLAB 7.1+ vs. older versions   * Better code organization, including user configuration at the top

--- components/bio-formats/utils/bfopen.m
+++ components/bio-formats/utils/bfopen.m
@@ -6,13 +6,14 @@ A script for opening microscopy images in MATLAB using Bio-Formats.
 The function returns a list of image series; i.e., a cell array of cell
 arrays of (matrix, label) pairs, with each matrix representing a single
 image plane, and each inner list of matrices representing an image
-series. See the bottom of this file below for examples of usage.
+series. See below for examples of usage.
 
 Portions of this code were adapted from:
 http://www.mathworks.com/support/solutions/data/1-2WPAYR.html?solution=1-2WPAYR
 
-This method is ~1.5x-2.5x slower than Bio-Formats's command line showinf
-tool (MATLAB R14 vs. java 1.6.0_20), due to overhead from copying arrays.
+This method is ~1.5x-2.5x slower than Bio-Formats's command line
+showinf tool (MATLAB 7.0.4.365 R14 SP2 vs. java 1.6.0_20),
+due to overhead from copying arrays.
 
 Thanks to all who offered suggestions and improvements:
     * Ville Rantanen
@@ -22,35 +23,107 @@ Thanks to all who offered suggestions and improvements:
     * Cris Luengo
     * Arnon Lieber
 
-Internet Explorer sometimes erroneously renames the Bio-Formats library
-to loci_tools.zip. If this happens, rename it back to loci_tools.jar.
+NB: Internet Explorer sometimes erroneously renames the Bio-Formats library
+    to loci_tools.zip. If this happens, rename it back to loci_tools.jar.
+
+Here are some examples of accessing data using the bfopen function:
+
+    % read the data using Bio-Formats
+    data = bfopen('C:/data/experiment.lif');
+
+    % unwrap some specific image planes from the result
+    numSeries = size(data, 1);
+    series1 = data{1, 1};
+    series2 = data{2, 1};
+    series3 = data{3, 1};
+    metadataList = data{1, 2};
+    % ...etc.
+    series1_numPlanes = size(series1, 1);
+    series1_plane1 = series1{1, 1};
+    series1_label1 = series1{1, 2};
+    series1_plane2 = series1{2, 1};
+    series1_label2 = series1{2, 2};
+    series1_plane3 = series1{3, 1};
+    series1_label3 = series1{3, 2};
+    % ...etc.
+
+    % plot the 1st series's 1st image plane in a new figure
+    series1_colorMaps = data{1, 3};
+    figure('Name', series1_label1);
+    if isempty(series1_colorMaps{1})
+        colormap(gray);
+    else
+        colormap(series1_colorMaps{1});
+    end
+    imagesc(series1_plane1);
+
+    % Or if you have the image processing toolbox, you could use:
+    % imshow(series1_plane1, []);
+
+    % Or animate as a movie (assumes 8-bit unsigned data)
+    v = linspace(0, 1, 256)';
+    cmap = [v v v];
+    for p = 1:series1_numPlanes
+        M(p) = im2frame(uint8(series1{p, 1}), cmap);
+    end
+    movie(M);
+
+    % Query some metadata fields (keys are format-dependent)
+    subject = metadataList.get('Subject');
+    title = metadataList.get('Title');
 %}
+
+% -- Configuration - customize this section to your liking --
+
+% Toggle the autoloadBioFormats flag to control automatic loading
+% of the Bio-Formats library using the javaaddpath command.
+%
+% For static loading, you can add the library to MATLAB's class path:
+%     1. Type "edit classpath.txt" at the MATLAB prompt.
+%     2. Go to the end of the file, and add the path to your JAR file
+%        (e.g., C:/Program Files/MATLAB/work/loci_tools.jar).
+%     3. Save the file and restart MATLAB.
+%
+% There are advantages to using the static approach over javaaddpath:
+%     1. If you use bfopen within a loop, it saves on overhead
+%        to avoid calling the javaaddpath command repeatedly.
+%     2. Calling 'javaaddpath' may erase certain global parameters.
+autoloadBioFormats = 1;
+
+% Toggle the stitchFiles flag to control grouping of similarly
+% named files into a single dataset based on file numbering.
+stitchFiles = 0;
+
+% To work with compressed Evotec Flex, fill in your LuraWave license code.
+%lurawaveLicense = 'xxxxxx-xxxxxxx';
+
+% -- Main function - no need to edit anything past this point --
 
 % load the Bio-Formats library into the MATLAB environment
-javaaddpath(fullfile(fileparts(mfilename('fullpath')),'loci_tools.jar'));
-%{
-Alternately, you can add the library to MATLAB's static class path:
-    1. Type "edit classpath.txt" at the MATLAB prompt.
-    2. Go to the end of the file, and add the path to your JAR file
-       (e.g., C:/Program Files/MATLAB/work/loci_tools.jar).
-    3. Save the file and restart MATLAB.
+if autoloadBioFormats
+    path = fullfile(fileparts(mfilename('fullpath')), 'loci_tools.jar');
+    javaaddpath(path);
+end
 
-There are advantages to using the static approach over javaaddpath:
-    1. If you use bfopen within a loop, it saves on overhead
-       to avoid calling the javaaddpath command repeatedly.
-    2. Calling 'javaaddpath' may erase certain global parameters.
-%}
+% set LuraWave license code, if available
+if exist('lurawaveLicense')
+    path = fullfile(fileparts(mfilename('fullpath')), 'lwf_jsdk2.6.jar');
+    javaaddpath(path);
+    java.lang.System.setProperty('lurawave.license', lurawaveLicense);
+end
 
-% to work with Evotec Flex, fill in your LuraWave license code
-%javaaddpath(fullfile(fileparts(mfilename('fullpath')),'lwf_jsdk2.6.jar'));
-%java.lang.System.setProperty('lurawave.license', 'xxxxxx-xxxxxxx');
+% check MATLAB version, since typecast function requires MATLAB 7.1+
+canTypecast = versionCheck(version, 7, 1);
+
+% check Bio-Formats version, since makeDataArray2D function requires trunk
+bioFormatsVersion = char(loci.formats.FormatTools.VERSION);
+isBioFormatsTrunk = versionCheck(bioFormatsVersion, 5, 0);
 
 r = loci.formats.ChannelFiller();
 r = loci.formats.ChannelSeparator(r);
-
-% uncomment the following line to enable grouping of similarly
-% named files into a single dataset based on file numbering
-%r = loci.formats.FileStitcher(r);
+if stitchFiles
+    r = loci.formats.FileStitcher(r);
+end
 
 tic
 r.setId(id);
@@ -64,11 +137,16 @@ for s = 1:numSeries
     pixelType = r.getPixelType();
     bpp = loci.formats.FormatTools.getBytesPerPixel(pixelType);
     fp = loci.formats.FormatTools.isFloatingPoint(pixelType);
+    sgn = loci.formats.FormatTools.isSigned(pixelType);
+    bppMax = power(2, bpp * 8);
     little = r.isLittleEndian();
     numImages = r.getImageCount();
     imageList = cell(numImages, 2);
     colorMaps = cell(numImages);
     for i = 1:numImages
+        if mod(i, 72) == 1
+            fprintf('\n    ');
+        end
         fprintf('.');
         plane = r.openBytes(i - 1);
 
@@ -78,29 +156,70 @@ for s = 1:numSeries
         else
             colorMaps{s, i} = r.get16BitLookupTable()';
         end
-        rowCount = size(colorMaps{s, i}, 1);
-        colCount = size(colorMaps{s, i}, 2);
-        newMap = zeros(rowCount, colCount);
-        for (row = 1:rowCount)
-            for (col = 1:colCount)
-                newMap(row, col) = colorMaps{s, i}(row, col);
-                pow = power(2, bpp * 8);
-                if newMap(row, col) < 0
-                    newMap(row, col) = newMap(row, col) + pow;
-                end
-                newMap(row, col) = newMap(row, col) / (pow - 1);
-            end
+        if ~isempty(colorMaps{s, i})
+            newMap = colorMaps{s, i};
+            m = newMap(row, col) < 0;
+            newMap(m) = newMap(m) + bppMax;
+            colorMaps{s, i} = newMap / (bppMax - 1);
         end
-        colorMaps{s, i} = newMap;
 
         % convert byte array to MATLAB image
-        arr = loci.common.DataTools.makeDataArray2D(plane, ...
-            bpp, fp, little, height);
-        % if using Bio-Formats 4.2 stable rather than trunk,
-        % use the following commands instead of the above line
-        %pix = loci.common.DataTools.makeDataArray(plane, bpp, fp, little);
-        %shape = [width height];
-        %arr = reshape(pix, shape)';
+        if isBioFormatsTrunk && (sgn || ~canTypecast)
+            % can get the data directly to a matrix
+            arr = loci.common.DataTools.makeDataArray2D(plane, ...
+                bpp, fp, little, height);
+        else
+            % get the data as a vector, either because makeDataArray2D
+            % is not available, or we need a vector for typecast
+            arr = loci.common.DataTools.makeDataArray(plane, ...
+                bpp, fp, little);
+        end
+
+        % Java does not have explicitly unsigned data types;
+        % hence, we must inform MATLAB when the data is unsigned
+        if ~sgn
+            if canTypecast
+                % TYPECAST requires at least MATLAB 7.1
+                % NB: arr will always be a vector here
+                switch class(arr)
+                    case 'int8'
+                        arr = typecast(arr, 'uint8');
+                    case 'int16'
+                        arr = typecast(arr, 'uint16');
+                    case 'int32'
+                        arr = typecast(arr, 'uint32');
+                    case 'int64'
+                        arr = typecast(arr, 'uint64');
+                end
+            else
+                % adjust apparent negative values to actual positive ones
+                % NB: arr might be either a vector or a matrix here
+                mask = arr < 0;
+                adjusted = arr(mask) + bppMax / 2;
+                switch class(arr)
+                    case 'int8'
+                        arr = uint8(arr);
+                        adjusted = uint8(adjusted);
+                    case 'int16'
+                        arr = uint16(arr);
+                        adjusted = uint16(adjusted);
+                    case 'int32'
+                        arr = uint32(arr);
+                        adjusted = uint32(adjusted);
+                    case 'int64'
+                        arr = uint64(arr);
+                        adjusted = uint64(adjusted);
+                end
+                adjusted = adjusted + bppMax / 2;
+                arr(mask) = adjusted;
+            end
+        end
+
+        if isvector(arr)
+            % convert results from vector to matrix
+            shape = [width height];
+            arr = reshape(arr, shape)';
+        end
 
         % build an informative title for our figure
         label = id;
@@ -155,50 +274,13 @@ end
 r.close();
 toc
 
-%{
-Here are some examples of accessing data using the bfopen function:
+% -- Helper functions --
 
-% read the data using Bio-Formats
-data = bfopen('C:/data/experiment.lif');
+function [result] = versionCheck(v, maj, min)
 
-% unwrap some specific image planes from the result
-numSeries = size(data, 1);
-series1 = data{1, 1};
-series2 = data{2, 1};
-series3 = data{3, 1};
-metadataList = data{1, 2};
-% ...etc.
-series1_numPlanes = size(series1, 1);
-series1_plane1 = series1{1, 1};
-series1_label1 = series1{1, 2};
-series1_plane2 = series1{2, 1};
-series1_label2 = series1{2, 2};
-series1_plane3 = series1{3, 1};
-series1_label3 = series1{3, 2};
-% ...etc.
-
-% plot the 1st series's 1st image plane in a new figure
-series1_colorMaps = data{1, 3};
-figure('Name', series1_label1);
-if isempty(series1_colorMaps{1})
-    colormap(gray);
-else
-    colormap(series1_colorMaps{1});
-end
-imagesc(series1_plane1);
-
-% Or if you have the image processing toolbox, you could use:
-% imshow(series1_plane1, []);
-
-% Or animate as a movie (assumes 8-bit unsigned data)
-v = linspace(0, 1, 256)';
-cmap = [v v v];
-for p = 1:series1_numPlanes
-    M(p) = im2frame(uint8(series1{p, 1}), cmap);
-end
-movie(M);
-
-% Query some metadata fields (keys are format-dependent)
-subject = metadataList.get('Subject');
-title = metadataList.get('Title');
-%}
+tokens = regexp(v, '[^\d]*(\d+)[^\d]+(\d+).*', 'tokens');
+majToken = tokens{1}(1);
+minToken = tokens{1}(2);
+major = str2num(majToken{1});
+minor = str2num(minToken{1});
+result = major > maj || (major == maj && minor >= min);
