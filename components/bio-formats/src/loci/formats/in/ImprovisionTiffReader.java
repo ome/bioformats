eae7a731bf326acaf4fcb5d6ab699c8061e79bb8
Melissa Linkert <melissa@glencoesoftware.com>
2010-11-23 16:53:54 +0000

    Override get*BitLookupTable() methods so that files with a different LUT for each plane will be properly handled.

--- components/bio-formats/src/loci/formats/in/ImprovisionTiffReader.java
+++ components/bio-formats/src/loci/formats/in/ImprovisionTiffReader.java
@@ -58,6 +58,8 @@ public class ImprovisionTiffReader extends BaseTiffReader {
   private String[] files;
   private MinimalTiffReader[] readers;
 
+  private int lastFile = 0;
+
   // -- Constructor --
 
   public ImprovisionTiffReader() {
@@ -90,6 +92,7 @@ public class ImprovisionTiffReader extends BaseTiffReader {
       }
       readers = null;
       files = null;
+      lastFile = 0;
     }
   }
 
@@ -97,6 +100,28 @@ public class ImprovisionTiffReader extends BaseTiffReader {
   public String[] getSeriesUsedFiles(boolean noPixels) {
     FormatTools.assertId(currentId, true, 1);
     return noPixels ? null : files;
+  }
+
+  /* @see loci.formats.IFormatReader#get8BitLookupTable() */
+  public byte[][] get8BitLookupTable() throws FormatException, IOException {
+    FormatTools.assertId(currentId, true, 1);
+    if (readers == null || lastFile < 0 || lastFile >= readers.length ||
+      readers[lastFile] == null)
+    {
+      return super.get8BitLookupTable();
+    }
+    return readers[lastFile].get8BitLookupTable();
+  }
+
+  /* @see loci.formats.IFormatReader#get16BitLookupTable() */
+  public short[][] get16BitLookupTable() throws FormatException, IOException {
+    FormatTools.assertId(currentId, true, 1);
+    if (readers == null || lastFile < 0 || lastFile >= readers.length ||
+      readers[lastFile] == null)
+    {
+      return super.get16BitLookupTable();
+    }
+    return readers[lastFile].get16BitLookupTable();
   }
 
   /**
@@ -111,6 +136,7 @@ public class ImprovisionTiffReader extends BaseTiffReader {
     int file = FormatTools.getIndex("XYZCT", getSizeZ(), getEffectiveSizeC(),
       getSizeT(), getImageCount(), zct[0], zct[1], zct[2]) % files.length;
     int plane = no / files.length;
+    lastFile = file;
 
     return readers[file].openBytes(plane, buf, x, y, w, h);
   }
