b60a4e9f340b0dfccf83e6e3cc2e2876aded0561
Curtis Rueden <ctrueden@wisc.edu>
2010-11-25 20:32:34 +0000

    Better parsing of ValidBitCounts.

--- components/bio-formats/src/loci/formats/in/FV1000Reader.java
+++ components/bio-formats/src/loci/formats/in/FV1000Reader.java
@@ -126,6 +126,7 @@ public class FV1000Reader extends FormatReader {
   private Vector<String> previewNames;
 
   private String pixelSizeX, pixelSizeY;
+  private int validBits;
   private Vector<String> illuminations;
   private Vector<Integer> wavelengths;
   private String pinholeSize;
@@ -304,6 +305,7 @@ public class FV1000Reader extends FormatReader {
       pixelSize = null;
       imageDepth = 0;
       pixelSizeX = pixelSizeY = null;
+      validBits = 0;
       pinholeSize = null;
       magnification = lensNA = objectiveName = workingDistance = null;
       creationDate = null;
@@ -447,6 +449,10 @@ public class FV1000Reader extends FormatReader {
     imageDepth = Integer.parseInt(referenceParams.get("ImageDepth"));
     pixelSizeX = referenceParams.get("WidthConvertValue");
     pixelSizeY = referenceParams.get("HeightConvertValue");
+    String ripValidBitCounts = referenceParams.get("ValidBitCounts");
+    if (ripValidBitCounts != null) {
+      validBits = Integer.parseInt(ripValidBitCounts);
+    }
 
     int index = 0;
 
@@ -626,6 +632,7 @@ public class FV1000Reader extends FormatReader {
           }
         }
       }
+      core[0].bitsPerPixel = validBits;
 
       IniTable acquisition = pty.getTable("Acquisition Parameters Common");
       if (acquisition != null) {
