9fcaa4ff0f1363091634dbd81ca1bf81aa1c8a6e
Melissa Linkert <melissa@glencoesoftware.com>
2010-11-29 16:53:32 +0000

    Merged BufferedImage unpacking fixes into 4.2: r7236 and r7254.

--- components/bio-formats/src/loci/formats/in/BIFormatReader.java
+++ components/bio-formats/src/loci/formats/in/BIFormatReader.java
@@ -26,6 +26,7 @@ package loci.formats.in;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
+import loci.common.DataTools;
 import loci.formats.FormatException;
 import loci.formats.FormatReader;
 import loci.formats.FormatTools;
@@ -66,8 +67,22 @@ public abstract class BIFormatReader extends FormatReader {
     FormatTools.checkPlaneParameters(this, no, buf.length, x, y, w, h);
 
     BufferedImage data = (BufferedImage) openPlane(no, x, y, w, h);
-    byte[] t = AWTImageTools.getBytes(data, false);
-    System.arraycopy(t, 0, buf, 0, (int) Math.min(t.length, buf.length));
+    switch (data.getColorModel().getComponentSize(0)) {
+      case 8:
+        byte[] t = AWTImageTools.getBytes(data, false);
+        System.arraycopy(t, 0, buf, 0, (int) Math.min(t.length, buf.length));
+        break;
+      case 16:
+        short[][] ts = AWTImageTools.getShorts(data);
+        for (int c=0; c<ts.length; c++) {
+          int offset = c * ts[c].length * 2;
+          for (int i=0; i<ts[c].length && offset < buf.length; i++) {
+            DataTools.unpackBytes(ts[c][i], buf, offset, 2, isLittleEndian());
+            offset += 2;
+          }
+        }
+        break;
+    }
     return buf;
   }
 
