ome/bioformats

commit ee2b4e6dc08b4040da9ddc4d02e8fd05b96b7a47

Don't unnecessarily open and close files when calling openBytes on single-series datasets.

components/bio-formats/src/loci/formats/in/ZeissLSMReader.java
@@ -309,11 +309,12 @@ public class ZeissLSMReader extends FormatReader {
   {
     FormatTools.checkPlaneParameters(this, no, buf.length, x, y, w, h);
 
-    in.close();
-    in = new RandomAccessInputStream(getLSMFileFromSeries(getSeries()));
-    in.order(!isLittleEndian());
-
-    tiffParser = new TiffParser(in);
+    if (getSeriesCount() > 1) {
+      in.close();
+      in = new RandomAccessInputStream(getLSMFileFromSeries(getSeries()));
+      in.order(!isLittleEndian());
+      tiffParser = new TiffParser(in);
+    }
 
     IFDList ifds = ifdsList.get(getSeries());
 
@@ -340,7 +341,7 @@ public class ZeissLSMReader extends FormatReader {
       tiffParser.getSamples(ifds.get(no), buf, x, y, w, h);
       prevChannel = getZCTCoords(no)[1];
     }
-    in.close();
+    if (getSeriesCount() > 1) in.close();
     return buf;
   }
 

commit 7729332565207373844f77425f92eedf22bbe2ba

Fixed silly file closing bug introduced in ee2b4e6dc08b4040da9ddc4d02e8fd05b96b7a47.

components/bio-formats/src/loci/formats/in/ZeissLSMReader.java
@@ -1160,7 +1160,6 @@ public class ZeissLSMReader extends FormatReader {
       }
     }
     ras.close();
-    in.close();
   }
 
   protected void populateMetadataStore(SubBlock block, MetadataStore store,