a5cd874ee751611341fedfe15503edc701cb15b1
Chris Allan <callan@blackcat.ca>
2011-03-09 11:50:04 +0000

    Fix compile errors for Bio-Formats ITK bridge.

--- components/bio-formats/src/loci/formats/tools/BioFormatsItkBridge.java
+++ components/bio-formats/src/loci/formats/tools/BioFormatsItkBridge.java
@@ -21,9 +21,12 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-package loci.formats;
+package loci.formats.tools;
 
 import java.io.IOException;
+
+import loci.formats.FormatException;
+import loci.formats.FormatTools;
 import loci.formats.ImageReader;
 
 /**
