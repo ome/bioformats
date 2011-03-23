543cbe3dc64fb63d031b719bd40d650fc8b3ca92
Melissa Linkert <melissa@glencoesoftware.com>
2011-03-22 21:27:32 -0500

    Various fixes for (potentially) truncated SimplePCI files.

--- components/forks/poi/src/loci/poi/poifs/property/PropertyTable.java
+++ components/forks/poi/src/loci/poi/poifs/property/PropertyTable.java
@@ -158,7 +158,7 @@ public class PropertyTable
     {
         int index = root.getChildIndex();
 
-        if (!Property.isValidIndex(index))
+        if (!Property.isValidIndex(index) || index >= _properties.size())
         {
 
             // property has no children
@@ -177,12 +177,12 @@ public class PropertyTable
                 populatePropertyTree(( DirectoryProperty ) property);
             }
             index = property.getPreviousChildIndex();
-            if (Property.isValidIndex(index))
+            if (Property.isValidIndex(index) && index < _properties.size())
             {
                 children.push(_properties.get(index));
             }
             index = property.getNextChildIndex();
-            if (Property.isValidIndex(index))
+            if (Property.isValidIndex(index) && index < _properties.size())
             {
                 children.push(_properties.get(index));
             }
