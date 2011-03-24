1496fa5d92c4a938ee050860769080d2cef20f52
Curtis Rueden <ctrueden@wisc.edu>
2011-03-24 14:09:59 -0500

    Updated ITKBridgePipes to use a command loop.
    This allows us to reuse the same process from C++ to avoid initializing
    the same file multiple times with a fresh reader for each command.

--- components/bio-formats/src/loci/formats/itk/ITKBridgePipes.java
+++ components/bio-formats/src/loci/formats/itk/ITKBridgePipes.java
@@ -22,253 +22,262 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 package loci.formats.itk;
-import java.io.IOException;
-import loci.formats.*;
-import loci.formats.meta.MetadataRetrieve;
-import loci.formats.meta.MetadataStore;
-import java.io.FileOutputStream;
 
-//import loci.formats.*;
-import loci.formats.IFormatReader;
-import loci.formats.FormatTools;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Hashtable;
+
+import loci.formats.ChannelSeparator;
 import loci.formats.FormatException;
+import loci.formats.FormatTools;
+import loci.formats.IFormatReader;
 import loci.formats.ImageReader;
+import loci.formats.MetadataTools;
 import loci.formats.in.DefaultMetadataOptions;
 import loci.formats.in.MetadataLevel;
-import java.io.BufferedOutputStream;
+import loci.formats.meta.IMetadata;
+import loci.formats.meta.MetadataStore;
 
 /**
- * ImageConverter is a utility class for converting a file between formats.
+ * ITKBridgePipes is a Java console application that listens for "commands"
+ * on stdin and issues results on stdout. It is used by the pipes version of
+ * the ITK Bio-Formats plugin to read image files.
  *
  * <dl><dt><b>Source code:</b></dt>
- * <dd><a href="https://skyking.microscopy.wisc.edu/trac/java/browser/trunk/loci/formats/itk/ITKBridgePipes.java">Trac</a>,
- * <a href="https://skyking.microscopy.wisc.edu/svn/java/trunk/loci/formats/itk/ITKBridgePipes.java">SVN</a></dd></dl>
+ * <dd><a href="http://trac.openmicroscopy.org.uk/ome/browser/bioformats.git/components/bio-formats/src/loci/formats/itk/ITKBridgePipes.java">Trac</a>,
+ * <a href="http://git.openmicroscopy.org/?p=bioformats.git;a=blob;f=components/bio-formats/src/loci/formats/itk/ITKBridgePipes.java;hb=HEAD">Gitweb</a></dd></dl>
+ *
+ * @author Mark Hiner
+ * @author Curtis Rueden
  */
-public final class ITKBridgePipes {
+public class ITKBridgePipes {
 
-  // -- Constructor --
+  private static final String HASH_PREFIX = "hash:";
 
-  private ITKBridgePipes() { }
+  private Hashtable<Integer, IFormatReader> readers =
+    new Hashtable<Integer, IFormatReader>();
 
-  // -- Utility methods --
+  /** Enters an input loop, waiting for commands, until EOF is reached. */
+  public void waitForInput() throws FormatException, IOException {
+    final BufferedReader in =
+      new BufferedReader(new InputStreamReader(System.in));
+    while (true) {
+      final String line = in.readLine(); // blocks until a line is read
+      if (line == null) break; // eof
+      executeCommand(line);
+    }
+    in.close();
+  }
 
-  /** A utility method for converting a file from the command line. */
-  public static boolean readImageInfo(String[] args)
+  /**
+   * Executes the given command line. The following commands are supported:
+   * <ul>
+   * <li>info</li> - Dumps image metadata
+   * <li>read</li> - Dumps image pixels
+   * <li>canRead</li> - Tests whether the given file path can be parsed
+   * </ul>
+   */
+  public boolean executeCommand(String commandLine)
     throws FormatException, IOException
   {
-    IFormatReader reader = new ImageReader();
-    reader = new ChannelSeparator(reader);
+    final int space = commandLine.indexOf(" ");
+    if (space < 0) {
+      System.err.println("Invalid command line: " + commandLine);
+      return false;
+    }
 
-    reader.setMetadataFiltered(true);
-    reader.setOriginalMetadataPopulated(true);
-    MetadataStore store = MetadataTools.createOMEXMLMetadata();
-    if (store == null) System.err.println("OME-Java library not found.");
-    else reader.setMetadataStore(store);
+    final String command = commandLine.substring(0, space).trim();
+    final String filePath = commandLine.substring(space + 1).trim();
 
-    reader.setGroupFiles(false); // avoid grouping all the .lsm when a .mdb is there
-    String fname = args[0];
-    reader.setId(fname);
-    reader.setSeries(0);
+    if (command.equals("info")) {
+       return readImageInfo(filePath);
+    }
+    else if (command.equals("read")) {
+      return read(filePath);
+    }
+    else if (command.equals("canRead")) {
+      return canRead(filePath);
+    }
+    else {
+      System.err.println("Unknown command: " + command);
+    }
+    return false;
+  }
 
-     store = reader.getMetadataStore();
-    MetadataRetrieve meta = (MetadataRetrieve) store;
+  /**
+   * Reads image metadata from the given file path, dumping the resultant
+   * values to stdout in a specific order (which we have not documented here
+   * because we are lazy).
+   *
+   * @param filePath a path to a file on disk, or a hash token for an
+   *   initialized reader (beginning with "hash:") as given by a call to "info"
+   *   earlier.
+   */
+  public boolean readImageInfo(String filePath)
+    throws FormatException, IOException
+  {
+    final IFormatReader reader = createReader(filePath);
+
+    final MetadataStore store = reader.getMetadataStore();
+    IMetadata meta = (IMetadata) store;
 
    // now print the informations
 
    // little endian?
-    System.out.println( reader.isLittleEndian()? 1:0 );
+    System.out.println(reader.isLittleEndian()? 1:0);
 
     // component type
     // set ITK component type
     int pixelType = reader.getPixelType();
     int itkComponentType;
-    if( pixelType == FormatTools.UINT8 )
-      {
-      itkComponentType = 1;
-      }
-    else if( pixelType == FormatTools.INT8 )
-      {
-      itkComponentType = 2;
-      }
-    else if( pixelType == FormatTools.UINT16 )
-      {
-      itkComponentType = 3;
-      }
-    else if( pixelType == FormatTools.INT16 )
-      {
-      itkComponentType = 4;
-      }
-    else if( pixelType == FormatTools.UINT32 )
-      {
-      itkComponentType = 5;
-      }
-    else if( pixelType == FormatTools.INT32 )
-      {
-      itkComponentType = 6;
-      }
-    else if( pixelType == FormatTools.FLOAT )
-      {
-      itkComponentType = 9;
-      }
-    else if( pixelType == FormatTools.DOUBLE )
-      {
-      itkComponentType = 10;
-      }
-    else
-      {
-      itkComponentType = 0;
-      }
-    System.out.println( itkComponentType );
+    switch (pixelType) {
+      case FormatTools.UINT8:
+        itkComponentType = 1;
+        break;
+      case FormatTools.INT8:
+        itkComponentType = 2;
+        break;
+      case FormatTools.UINT16:
+        itkComponentType = 3;
+        break;
+      case FormatTools.INT16:
+        itkComponentType = 4;
+        break;
+      case FormatTools.UINT32:
+        itkComponentType = 5;
+        break;
+      case FormatTools.INT32:
+        itkComponentType = 6;
+        break;
+      case FormatTools.FLOAT:
+        itkComponentType = 9;
+        break;
+      case FormatTools.DOUBLE:
+        itkComponentType = 10;
+        break;
+      default:
+        itkComponentType = 0;
+    }
+    System.out.println(itkComponentType);
 
     // x, y, z, t, c
-    System.out.println( reader.getSizeX() );
-    System.out.println( reader.getSizeY() );
-    System.out.println( reader.getSizeZ() );
-    System.out.println( reader.getSizeT() );
-    System.out.println( reader.getEffectiveSizeC() ); // reader.getSizeC()
+    System.out.println(reader.getSizeX());
+    System.out.println(reader.getSizeY());
+    System.out.println(reader.getSizeZ());
+    System.out.println(reader.getSizeT());
+    System.out.println(reader.getEffectiveSizeC()); // reader.getSizeC()
 
     // number of components
-    System.out.println( reader.getRGBChannelCount() );
+    System.out.println(reader.getRGBChannelCount());
 
     // spacing
-    System.out.println( meta.getPixelsPhysicalSizeX(0) );
-    System.out.println( meta.getPixelsPhysicalSizeY(0) );
-    System.out.println( meta.getPixelsPhysicalSizeZ(0) );
-    System.out.println( meta.getPixelsTimeIncrement(0) );
-    System.out.println( 1.0 ); // should we give something more useful for this one?
+    System.out.println(meta.getPixelsPhysicalSizeX(0));
+    System.out.println(meta.getPixelsPhysicalSizeY(0));
+    System.out.println(meta.getPixelsPhysicalSizeZ(0));
+    System.out.println(meta.getPixelsTimeIncrement(0));
+    // should we give something more useful for channel spacing?
+    System.out.println(1.0);
 
+    // reader hash code
+    final int hashCode = reader.hashCode();
+    System.out.println(HASH_PREFIX + hashCode);
+    readers.put(hashCode, reader);
+
+    System.out.flush();
     return true;
   }
-  
-  /** A utility method for converting a file from the command line. */
-  public static boolean read(String[] args)
+
+  /**
+   * Reads image pixels from the given file path, dumping the resultant binary
+   * stream to stdout.
+   *
+   * @param filePath a path to a file on disk, or a hash token for an
+   *   initialized reader (beginning with "hash:") as given by a call to "info"
+   *   earlier. Using a hash token eliminates the need to initialize the file a
+   *   second time with a fresh reader object. Regardless, after reading the
+   *   file, the reader closes the file handle, and invalidates its hash token.
+   */
+  public boolean read(String filePath)
     throws FormatException, IOException
   {
-    IFormatReader reader = new ImageReader();
-    reader.setMetadataOptions(new DefaultMetadataOptions(MetadataLevel.MINIMUM));
-    reader.setGroupFiles(false); // avoid grouping all the .lsm when a .mdb is there
-    String fname = args[0];
-    reader.setId(fname);
-    reader.setSeries(0);
-    
-    BufferedOutputStream out = new BufferedOutputStream(System.out);
-    for( int c = 0; c < reader.getSizeC(); c++) {
-    	for (int t = 0; t < reader.getSizeT(); t++) {
-    	    for( int z=0; z < reader.getSizeZ(); z++ ) {
-    	      byte[] image = reader.openBytes( reader.getIndex(z, c, t) );
-    	      out.write(image);
-    	     }
-    	}
-    }
-
-    
-    /* attempts at multidimensional reading
-    int pixelType = reader.getPixelType();
-    int bpp = FormatTools.getBytesPerPixel(pixelType);
-    int rgbChannelCount = reader.getRGBChannelCount();
-    
-    int xStart = Integer.valueOf(args[2]);
-    int xCount = Integer.valueOf(args[3]);
-    int yStart = Integer.valueOf(args[4]);
-    int yCount = Integer.valueOf(args[5]);
-    int zStart = Integer.valueOf(args[6]);
-    int zCount = Integer.valueOf(args[7]);
-    int tStart = Integer.valueOf(args[8]);
-    int tCount = Integer.valueOf(args[9]);
-    int cStart = Integer.valueOf(args[10]);
-    int cCount = Integer.valueOf(args[11]);
-    
-    int imageCount = reader.getImageCount();
-    
-    int bytesPerPlane = xCount * yCount * bpp * rgbChannelCount;
-    boolean isInterleaved = reader.isInterleaved();
-    
-    boolean canDoDirect = false;
-    
-    if( (rgbChannelCount == 1) || isInterleaved )
-    	canDoDirect = true;
-    
-    byte[] tmpData = null;
-    
-    if(!canDoDirect)
-    	tmpData = new byte[bytesPerPlane];
+    final IFormatReader reader = createReader(filePath);
 
     BufferedOutputStream out = new BufferedOutputStream(System.out);
-    
-    byte[] buf = new byte[bytesPerPlane];
-
-    for( int c = cStart; c < (cStart + cCount); c++ )
-    {
-      for (int t = tStart; t < (tStart + tCount); t++ )
-      {
-        for (int z = zStart; z < (zStart + zCount); z++ )
-        {
-        	int no = reader.getIndex(z, c, t);
-        	reader.openBytes(no, buf, xStart, yStart, xCount, yCount);
-        	
-        	if (canDoDirect)
-        	{
-        		out.write(buf);
-        	}
-        	else
-        	{
-        		int pos = 0;
-        		for(int x = 0; x < xCount; x++) {
-        			for(int y = 0; y < yCount; y++) {
-        				for(int i = 0; i < rgbChannelCount; i++) {
-        					for(int b = 0; b < bpp; b++) {
-        						int index = yCount * (xCount * (rgbChannelCount * b + i) + x) + y;
-        						tmpData[pos++] = buf[index];
-        					}
-        				}
-        					
-        			}
-        		}
-        		out.write(tmpData);	
-        	}
+    for (int c = 0; c < reader.getSizeC(); c++) {
+      for (int t = 0; t < reader.getSizeT(); t++) {
+        for (int z=0; z < reader.getSizeZ(); z++) {
+          byte[] image = reader.openBytes(reader.getIndex(z, c, t));
+          out.write(image);
         }
       }
     }
-    */
+
     out.close();
-    System.out.close();
+    System.out.flush();
+
+    // close file handle, and invalidate hash token, if any
+    reader.close();
+    readers.remove(reader.hashCode());
+
     return true;
   }
 
-
-  /** A utility method to determine if a file can be read by BioFormats. */
-  public static boolean canRead(String[] args)
+  /** Tests whether the given file path can be parsed by Bio-Formats. */
+  public boolean canRead(String filePath)
     throws FormatException, IOException
   {
-    boolean h = false;
-    IFormatReader reader = new ImageReader();
+    final IFormatReader reader = createReader(null);
+    final boolean canRead = reader.isThisType(filePath);
+    System.out.println(canRead);
+    System.out.flush();
+    return true;
+  }
 
-    try {
-      h = reader.isThisType(args[0]);
-    } catch (Exception e) {
-      e.printStackTrace();
+  private IFormatReader createReader(final String filePath)
+    throws FormatException, IOException
+  {
+    IFormatReader reader = null;
+    if (filePath.startsWith(HASH_PREFIX)) {
+      // file path is actually a reader hash code; reuse it
+      try {
+        final int hashCode = Integer.parseInt(filePath.substring(5));
+        reader = readers.get(hashCode);
+      }
+      catch (NumberFormatException exc) {
+        // invalid hash code; ignore and continue
+      }
+    }
+    if (reader == null) {
+      // no hash code; initialize a fresh reader
+      reader = new ChannelSeparator();
+
+      reader.setMetadataFiltered(true);
+      reader.setOriginalMetadataPopulated(true);
+      final MetadataStore store = MetadataTools.createOMEXMLMetadata();
+      if (store == null) System.err.println("OME-Java library not found.");
+      else reader.setMetadataStore(store);
+      reader.setMetadataOptions(
+        new DefaultMetadataOptions(MetadataLevel.MINIMUM));
+
+      // avoid grouping all the .lsm when a .mdb is there
+      reader.setGroupFiles(false);
+
+      if (filePath != null) {
+        reader.setId(filePath);
+        reader.setSeries(0);
+      }
     }
 
-    System.out.println(h);
-    System.out.close();
-    return true;
+    return reader;
   }
 
   // -- Main method --
 
   public static void main(String[] args) throws FormatException, IOException {
-	  if(args[1].equals("info"))
-	  {
-	     if (!readImageInfo(args)) System.exit(1);
-	  }
-	  else if(args[1].equals("read"))
-  	{
-  		if (!read(args)) System.exit(1);
-  	}
-    else if(args[1].equals("canRead"))
-    {
-      if (!canRead(args)) System.exit(1);
-    }
+    new ITKBridgePipes().waitForInput();
   }
 
 }
