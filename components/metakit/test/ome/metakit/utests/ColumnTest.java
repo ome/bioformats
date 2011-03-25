588e8d3ead3b351d4c0d36be8a57a664c019a57d
Melissa Linkert <melissa@glencoesoftware.com>
2011-03-25 12:21:34 -0500

    Added initial tests for Metakit reader.

---
+++ components/metakit/test/ome/metakit/utests/ColumnTest.java
@@ -0,0 +1,135 @@
+//
+// ColumnTest.java
+//
+
+/*
+OME Bio-Formats package for reading and converting biological file formats.
+Copyright (C) 2005-@year@ UW-Madison LOCI and Glencoe Software, Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package ome.metakit.utests;
+
+import java.io.IOException;
+
+import ome.metakit.MetakitReader;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertNotNull;
+import static org.testng.AssertJUnit.assertNull;
+import static org.testng.AssertJUnit.assertTrue;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="http://trac.openmicroscopy.org.uk/ome/browser/bioformats.git/componets/metakit/test/ome/metakit/utests/ColumnTest.java">Trac</a>,
+ * <a href="http://git.openmicroscopy.org/?p=bioformats.git;a=blob;f=components/metakit/test/ome/metakit/utests/ColumnTest.java">Gitweb</a></dd></dl>
+ *
+ * @author Melissa Linkert melissa at glencoesoftware.com
+ */
+public class ColumnTest {
+
+  private static final String INVALID_TABLE = "this cannot be a valid table";
+
+  private MetakitReader reader;
+
+  @BeforeMethod
+  public void setUp() throws IOException {
+    reader = new MetakitReader(System.getProperty("filename"));
+  }
+
+  @Test
+  public void testValidColumnNames() {
+    String[] tableNames = reader.getTableNames();
+    for (int i=0; i<tableNames.length; i++) {
+      String[] namesByIndex = reader.getColumnNames(i);
+      String[] namesByName = reader.getColumnNames(tableNames[i]);
+
+      assertNotNull(namesByIndex);
+      assertNotNull(namesByName);
+      assertEquals(namesByIndex.length, namesByName.length);
+      for (int j=0; j<namesByIndex.length; j++) {
+        assertNotNull(namesByIndex[j]);
+        assertEquals(namesByIndex[j], namesByName[j]);
+      }
+    }
+  }
+
+  @Test
+  public void testValidColumnTypes() {
+    String[] tableNames = reader.getTableNames();
+    for (int i=0; i<tableNames.length; i++) {
+      Class[] typesByIndex = reader.getColumnTypes(i);
+      Class[] typesByName = reader.getColumnTypes(tableNames[i]);
+
+      assertNotNull(typesByIndex);
+      assertNotNull(typesByName);
+      assertEquals(typesByIndex.length, typesByName.length);
+      for (int j=0; j<typesByIndex.length; j++) {
+        assertNotNull(typesByIndex[j]);
+        assertEquals(typesByIndex[j], typesByName[j]);
+      }
+    }
+  }
+
+  @Test
+  public void testColumnCounts() {
+    for (int i=0; i<reader.getTableCount(); i++) {
+      String[] names = reader.getColumnNames(i);
+      Class[] types = reader.getColumnTypes(i);
+
+      assertNotNull(names);
+      assertNotNull(types);
+      assertEquals(names.length, types.length);
+    }
+  }
+
+  @Test(expectedExceptions={ArrayIndexOutOfBoundsException.class})
+  public void testColumnNameTableIndexTooSmall() {
+    reader.getColumnNames(-1);
+  }
+
+  @Test(expectedExceptions={ArrayIndexOutOfBoundsException.class})
+  public void testColumnTypeTableIndexTooSmall() {
+    reader.getColumnTypes(-1);
+  }
+
+
+  @Test(expectedExceptions={ArrayIndexOutOfBoundsException.class})
+  public void testColumnNameTableIndexTooLarge() {
+    reader.getColumnNames(reader.getTableCount());
+  }
+
+  @Test(expectedExceptions={ArrayIndexOutOfBoundsException.class})
+  public void testColumnTypeTableIndexTooLarge() {
+    reader.getColumnTypes(reader.getTableCount());
+  }
+
+  @Test
+  public void testColumnNameInvalidTableNames() {
+    assertNull(reader.getColumnNames(null));
+    assertNull(reader.getColumnNames(INVALID_TABLE));
+  }
+
+  @Test
+  public void testColumnTypeInvalidTableNames() {
+    assertNull(reader.getColumnTypes(null));
+    assertNull(reader.getColumnTypes(INVALID_TABLE));
+  }
+
+}
---
+++ components/metakit/test/ome/metakit/utests/TableTest.java
@@ -0,0 +1,69 @@
+//
+// TableTest.java
+//
+
+/*
+OME Bio-Formats package for reading and converting biological file formats.
+Copyright (C) 2005-@year@ UW-Madison LOCI and Glencoe Software, Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package ome.metakit.utests;
+
+import java.io.IOException;
+
+import ome.metakit.MetakitReader;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertNotNull;
+import static org.testng.AssertJUnit.assertTrue;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="http://trac.openmicroscopy.org.uk/ome/browser/bioformats.git/componets/metakit/test/ome/metakit/utests/TableTest.java">Trac</a>,
+ * <a href="http://git.openmicroscopy.org/?p=bioformats.git;a=blob;f=components/metakit/test/ome/metakit/utests/TableTest.java">Gitweb</a></dd></dl>
+ *
+ * @author Melissa Linkert melissa at glencoesoftware.com
+ */
+public class TableTest {
+
+  private MetakitReader reader;
+
+  @BeforeMethod
+  public void setUp() throws IOException {
+    reader = new MetakitReader(System.getProperty("filename"));
+  }
+
+  @Test
+  public void testTableCount() {
+    int tableCount = reader.getTableCount();
+    assertTrue(tableCount >= 0);
+    assertEquals(tableCount, reader.getTableNames().length);
+  }
+
+  @Test
+  public void testValidTableNames() {
+    String[] tables = reader.getTableNames();
+    assertNotNull(tables);
+    for (String tableName : tables) {
+      assertNotNull(tableName);
+    }
+  }
+
+}
