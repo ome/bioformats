fd9cc7cd4182bc044a4d0df271e0e3485debddc0
Melissa Linkert <melissa@glencoesoftware.com>
2011-04-06 19:31:27 -0500

    Cleaned up documentation.

--- components/metakit/src/ome/metakit/MetakitTools.java
+++ components/metakit/src/ome/metakit/MetakitTools.java
@@ -1,3 +1,26 @@
+//
+// MetakitTools.java
+//
+
+/*
+OME Metakit package for reading Metakit database files.
+Copyright (C) 2011-@year@ UW-Madison LOCI and Glencoe Software, Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 package ome.metakit;
 
 import java.io.IOException;
@@ -5,8 +28,18 @@ import java.util.ArrayList;
 
 import loci.common.RandomAccessInputStream;
 
+/**
+ * Utility methods for working with Metakit database files.
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="http://trac.openmicroscopy.org.uk/ome/browser/bioformats.git/components/metakit/src/ome/metakit/MetakitTools.java">Trac</a>,
+ * <a href="http://git.openmicroscopy.org/?p=bioformats.git;a=blob;f=components/metakit/src/ome/metakit/MetakitTools.java;hb=HEAD">Gitweb</a></dd></dl>
+ */
 public class MetakitTools {
 
+  /**
+   * Read a Pascal-style string from the given RandomAccessInputStream.
+   */
   public static String readPString(RandomAccessInputStream stream)
     throws IOException
   {
@@ -14,6 +47,12 @@ public class MetakitTools {
     return stream.readString(stringLength);
   }
 
+  /**
+   * Read a byte-packed integer from the given RandomAccessInputStream.
+   *
+   * See http://equi4.com/metakit/metakit-ff.html#formatgrammar for a
+   * description of byte-packed integers.
+   */
   public static int readBpInt(RandomAccessInputStream stream) throws IOException
   {
     int signByte = stream.read();
