59f93885f04af4a2c2ff586ec9c38b72330a2868
Melissa Linkert <melissa@glencoesoftware.com>
2011-05-06 08:07:24 -0500

    One more XML file searching fix.

--- components/bio-formats/src/loci/formats/in/ScanrReader.java
+++ components/bio-formats/src/loci/formats/in/ScanrReader.java
@@ -251,7 +251,8 @@ public class ScanrReader extends FormatReader {
     // make sure we have the .xml file
     if (!checkSuffix(id, "xml") && isGroupFiles()) {
       Location parent = new Location(id).getAbsoluteFile().getParentFile();
-      if (checkSuffix(id, "tif") && parent.getName().equals("Data")) {
+      if (checkSuffix(id, "tif") && parent.getName().equalsIgnoreCase("Data"))
+      {
         parent = parent.getParentFile();
       }
       String[] list = parent.list();
