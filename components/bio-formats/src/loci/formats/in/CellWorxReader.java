abb0761ef47425dedf0955f6cb8103b7ff4e5dd2
Melissa Linkert <melissa@glencoesoftware.com>
2011-05-10 15:48:52 -0500

    Do not populate Screen attributes. (cherry picked from commit 67e44b0513f4c2277d7e0b15844fa2051626cf52)

--- components/bio-formats/src/loci/formats/in/CellWorxReader.java
+++ components/bio-formats/src/loci/formats/in/CellWorxReader.java
@@ -284,16 +284,10 @@ public class CellWorxReader extends FormatReader {
     MetadataTools.populatePixels(store, this);
 
     String plateID = MetadataTools.createLSID("Plate", 0);
-    String screenID = MetadataTools.createLSID("Screen", 0);
 
-    store.setScreenID(screenID, 0);
     Location plate = new Location(id).getAbsoluteFile().getParentFile();
-    Location screen = plate.getParentFile();
-    store.setScreenName(screen.getName(), 0);
-    store.setScreenPlateRef(plateID, 0, 0);
 
     store.setPlateID(plateID, 0);
-    store.setPlateScreenRef(screenID, 0, 0);
     store.setPlateName(plate.getName(), 0);
     for (int i=0; i<core.length; i++) {
       store.setImageID(MetadataTools.createLSID("Image", i), i);
