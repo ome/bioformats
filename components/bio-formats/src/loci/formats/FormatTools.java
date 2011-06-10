a072d223c9439e0a45575637dcc7a8c5a36ffda7
Melissa Linkert <melissa@glencoesoftware.com>
2011-06-10 09:03:05 -0500

    Update version numbers for 4.3.0 release.

--- components/bio-formats/src/loci/formats/FormatTools.java
+++ components/bio-formats/src/loci/formats/FormatTools.java
@@ -153,7 +153,7 @@ public final class FormatTools {
   public static final String DATE = "@date@";
 
   /** Version number of this release. */
-  public static final String VERSION = "4.3-DEV";
+  public static final String VERSION = "4.3.0";
 
   // -- Constants - domains --
 
