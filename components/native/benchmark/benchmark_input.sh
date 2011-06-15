e7543fa349badfedcafd16d1d07ce414696c44da
Curtis Rueden <ctrueden@wisc.edu>
2011-06-15 16:45:19 +0200

    Clean up benchmark scripts. (cherry picked from commit e98cecc050220352f6f5db8bf47d5deff6cbc460)

--- components/native/benchmark/benchmark_input.sh
+++ components/native/benchmark/benchmark_input.sh
@@ -1,15 +1,20 @@
-#!bin/bash
+#!/bin/bash
 
-#This program runs benchmarks for a series of ITK plugins using
-#ImageHistogram1-3 ITK examples against a series of images.
-#The time for each application is printed to an output file.
+# This program runs benchmarks for a series of ITK plugins using
+# ImageHistogram1-3 ITK examples against a series of images.
+# The time for each application is printed to an output file.
 
-#this version takes two arguments: a list of programs and a list of images, and runs the "cross product"
-# of their applications
-# usage: bash benchmark_input.sh "program1 program2 ... programN" "image1 image2 ... imageN"
-#image and program names should be fully qualified paths.
-#The DYLD_LIBRARY_PATH should be set to whatever is needed to run the various programs in the program list
-# but should not contain any of the ITK_AUTOLOAD_PATHS for the JNI, JACE or PIPE BFITK plugin implementations.
+# This version takes two arguments: a list of programs and a list of images,
+# and runs the "cross product" of their applications
+
+# Usage: bash benchmark_input.sh "program1 program2 ... programN"
+#                                "image1 image2 ... imageN"
+
+# Image and program names should be fully qualified paths.
+
+# The DYLD_LIBRARY_PATH should be set to whatever is needed to run the various
+# programs in the program list but should not contain any of the
+# ITK_AUTOLOAD_PATHs for the JNI, JACE or PIPE BFITK plugin implementations.
 
 LIB_PATH=$DYLD_LIBRARY_PATH
 BF_PATH=/Users/hinerm/loci/bioformats/components/native
@@ -18,7 +23,6 @@ PIPE_PATH=$BF_PATH/bf-itk-pipe/build/dist/bf-itk
 JACE_PATH=$BF_PATH/bf-itk-jace/build/dist/bf-itk
 EXPECTED_ARGS=2
 E_BARDARGS=65
-
 
 if [ $# -ne $EXPECTED_ARGS ]
   then
@@ -31,39 +35,39 @@ echo "Time Analysis" > benchmark_out.txt
 echo >> benchmark_out.txt
 
 for program in $1
+do
+  for image in $2
   do
-    for image in $2
-      do
-         echo "Program: " $program >> benchmark_out.txt
-         echo "Image: " $image >> benchmark_out.txt
-         echo >> benchmark_out.txt
+    echo "Program: " $program >> benchmark_out.txt
+    echo "Image: " $image >> benchmark_out.txt
+    echo >> benchmark_out.txt
 
-         echo "***** JNI IMPLEMENTATION *****" >> benchmark_out.txt
-         echo >> benchmark_out.txt
-         export ITK_AUTOLOAD_PATH=$JNI_PATH
-         export DYLD_LIBRARY_PATH=$ITK_AUTOLOAD_PATH:$LIB_PATH
-         bash bmark_apply.sh $program $image
-         echo >> benchmark_out.txt
-         echo >> benchmark_out.txt
-         echo >> benchmark_out.txt
+    echo "***** JNI IMPLEMENTATION *****" >> benchmark_out.txt
+    echo >> benchmark_out.txt
+    export ITK_AUTOLOAD_PATH=$JNI_PATH
+    export DYLD_LIBRARY_PATH=$ITK_AUTOLOAD_PATH:$LIB_PATH
+    bash bmark_apply.sh $program $image
+    echo >> benchmark_out.txt
+    echo >> benchmark_out.txt
+    echo >> benchmark_out.txt
 
-         echo "***** PIPE IMPLEMENTATION *****" >> benchmark_out.txt
-         echo >> benchmark_out.txt
-         export ITK_AUTOLOAD_PATH=$PIPE_PATH
-         export DYLD_LIBRARY_PATH=$ITK_AUTOLOAD_PATH:$LIB_PATH
-         bash bmark_apply.sh $program $image
-         echo >> benchmark_out.txt
-         echo >> benchmark_out.txt
-         echo >> benchmark_out.txt
+    echo "***** PIPE IMPLEMENTATION *****" >> benchmark_out.txt
+    echo >> benchmark_out.txt
+    export ITK_AUTOLOAD_PATH=$PIPE_PATH
+    export DYLD_LIBRARY_PATH=$ITK_AUTOLOAD_PATH:$LIB_PATH
+    bash bmark_apply.sh $program $image
+    echo >> benchmark_out.txt
+    echo >> benchmark_out.txt
+    echo >> benchmark_out.txt
 
-         echo "***** JACE IMPLEMENTATION *****" >> benchmark_out.txt
-         echo >> benchmark_out.txt
-         export ITK_AUTOLOAD_PATH=$JACE_PATH
-         export DYLD_LIBRARY_PATH=$ITK_AUTOLOAD_PATH:$LIB_PATH
-         bash bmark_apply.sh $program $image
-         echo >> benchmark_out.txt
-         echo "*************************" >> benchmark_out.txt
-         echo >> benchmark_out.txt
-         echo >> benchmark_out.txt
-    done
+    echo "***** JACE IMPLEMENTATION *****" >> benchmark_out.txt
+    echo >> benchmark_out.txt
+    export ITK_AUTOLOAD_PATH=$JACE_PATH
+    export DYLD_LIBRARY_PATH=$ITK_AUTOLOAD_PATH:$LIB_PATH
+    bash bmark_apply.sh $program $image
+    echo >> benchmark_out.txt
+    echo "*************************" >> benchmark_out.txt
+    echo >> benchmark_out.txt
+    echo >> benchmark_out.txt
   done
+done
--- components/native/benchmark/bmark_apply.sh
+++ components/native/benchmark/bmark_apply.sh
@@ -1,8 +1,10 @@
-#!bin/bash
+#!/bin/bash
 
-#This script takes two arguments: a command-line program which takes in a single image, and an image.
-#The program is then applied to the image three times and outputs the time each run took, via the time command. 
-#Usage: bash bmrk_apply.sh <program> <image>
+# This script takes two arguments: a command-line program which takes in a
+# single image, and an image. The program is then applied to the image three
+# times and outputs the time each run took, via the time command.
+
+# Usage: bash bmrk_apply.sh <program> <image>
 
 #echo "Program: " $1 >> benchmark_out.txt
 #echo "File: " $2 >> benchmark_out.txt
