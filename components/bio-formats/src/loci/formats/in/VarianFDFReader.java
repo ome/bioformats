6b06ca49b063a241aaa6b45edf01ebb7bac60068
Melissa Linkert <melissa@glencoesoftware.com>
2011-12-05 09:07:43 -0500

    Fix off-by-one errors

--- components/bio-formats/src/loci/formats/in/VarianFDFReader.java
+++ components/bio-formats/src/loci/formats/in/VarianFDFReader.java
@@ -89,7 +89,7 @@ public class VarianFDFReader extends FormatReader {
   {
     FormatTools.checkPlaneParameters(this, no, buf.length, x, y, w, h);
 
-    if (files.size() > 0) {
+    if (files.size() > 1) {
       in = new RandomAccessInputStream(files.get(no));
       in.order(isLittleEndian());
     }
@@ -105,7 +105,7 @@ public class VarianFDFReader extends FormatReader {
       System.arraycopy(buf, dest, buf, src, rowBuf.length);
       System.arraycopy(rowBuf, 0, buf, dest, rowBuf.length);
     }
-    if (files.size() > 0) {
+    if (files.size() > 1) {
       in.close();
     }
 
@@ -146,7 +146,7 @@ public class VarianFDFReader extends FormatReader {
     pixelOffsets = new long[getImageCount()];
     int planeSize = FormatTools.getPlaneSize(this);
     for (int i=0; i<pixelOffsets.length; i++) {
-      if (files.size() > 0) {
+      if (files.size() > 1) {
         in.close();
         in = new RandomAccessInputStream(files.get(i));
         pixelOffsets[i] = in.length() - planeSize;
