ome/bioformats

commit f8c4b7683e9c0e34069d6ea1f7d0ea6486f84afc

Only reset the metadata when initializing a new file
Previously, the MetadataStore associated with a ChannelFiller would be
reset with every call to setId, even if the file name passed to setId
was equivalent to the currently initialized file name.

This caused the ImageJ import plugin to show incorrect image acquisition
dates, since MetadataTools.populatePixels now sets a default image
acquisition date and the plugin generates multiple setId calls per file.

See #7319.

components/scifio/src/loci/formats/ChannelFiller.java
@@ -246,10 +246,12 @@ public class ChannelFiller extends ReaderWrapper {
   /* @see IFormatHandler#setId(String) */
   @Override
   public void setId(String id) throws FormatException, IOException {
-    super.setId(id);
-    lutLength = getLookupTableComponentCount();
-    MetadataStore store = getMetadataStore();
-    MetadataTools.populatePixels(store, this, false, false);
+    if (!id.equals(getCurrentFile())) {
+      super.setId(id);
+      lutLength = getLookupTableComponentCount();
+      MetadataStore store = getMetadataStore();
+      MetadataTools.populatePixels(store, this, false, false);
+    }
   }
 
   // -- Helper methods --

commit 3db5e861843257ef49ecc30c68f00dddb76b5a14

Fix channel count bug introduced in f8c4b76
The lookup table component count needs to be reset on every call to
setId.  Since we only really care about not calling
MetadataTools.populatePixels(...) on files that have already been
initialized, there is no reason not to let everything else run on every
call to setId.

This should allow the tests to pass again.

components/scifio/src/loci/formats/ChannelFiller.java
@@ -246,9 +246,9 @@ public class ChannelFiller extends ReaderWrapper {
   /* @see IFormatHandler#setId(String) */
   @Override
   public void setId(String id) throws FormatException, IOException {
+    super.setId(id);
+    lutLength = getLookupTableComponentCount();
     if (!id.equals(getCurrentFile())) {
-      super.setId(id);
-      lutLength = getLookupTableComponentCount();
       MetadataStore store = getMetadataStore();
       MetadataTools.populatePixels(store, this, false, false);
     }