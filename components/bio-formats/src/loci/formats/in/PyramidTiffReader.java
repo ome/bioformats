fac173dd5596a9cc5b66dd265390aa296fa43bfe
Melissa Linkert <melissa@glencoesoftware.com>
2012-03-18 21:02:48 -0400

    Initial version of pyramid TIFF reader

---
+++ components/bio-formats/src/loci/formats/in/PyramidTiffReader.java
@@ -0,0 +1,195 @@
+//
+// PyramidTiffReader.java
+//
+
+/*
+OME Bio-Formats package for reading and converting biological file formats.
+Copyright (C) 2005-@year@ UW-Madison LOCI and Glencoe Software, Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package loci.formats.in;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import loci.common.DataTools;
+import loci.common.Location;
+import loci.common.RandomAccessInputStream;
+import loci.formats.CoreMetadata;
+import loci.formats.FormatException;
+import loci.formats.FormatTools;
+import loci.formats.ImageReader;
+import loci.formats.MetadataTools;
+import loci.formats.codec.BitWriter;
+import loci.formats.meta.MetadataStore;
+import loci.formats.tiff.IFD;
+import loci.formats.tiff.PhotoInterp;
+import loci.formats.tiff.TiffParser;
+
+/**
+ * PyramidTiffReader is the file format reader for pyramid TIFFs.
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="http://trac.openmicroscopy.org.uk/ome/browser/bioformats.git/components/bio-formats/src/loci/formats/in/PyramidTiffReader.java">Trac</a>,
+ * <a href="http://git.openmicroscopy.org/?p=bioformats.git;a=blob;f=components/bio-formats/src/loci/formats/in/PyramidTiffReader.java;hb=HEAD">Gitweb</a></dd></dl>
+ */
+public class PyramidTiffReader extends BaseTiffReader {
+
+  // -- Constants --
+
+  /** Logger for this class. */
+  private static final Logger LOGGER =
+    LoggerFactory.getLogger(PyramidTiffReader.class);
+
+  // -- Fields --
+
+  // -- Constructor --
+
+  /** Constructs a new pyramid TIFF reader. */
+  public PyramidTiffReader() {
+    super("Pyramid TIFF", new String[] {"tif", "tiff"});
+    domains = new String[] {FormatTools.EM_DOMAIN};
+    suffixSufficient = false;
+    suffixNecessary = false;
+    setFlattenedResolutions(false);
+  }
+
+  // -- IFormatReader API methods --
+
+  /* @see loci.formats.IFormatReader#isThisType(RandomAccessInputStream) */
+  public boolean isThisType(RandomAccessInputStream stream) throws IOException {
+    TiffParser parser = new TiffParser(stream);
+    IFD ifd = parser.getFirstIFD();
+    if (ifd == null) return false;
+    String software = ifd.getIFDTextValue(IFD.SOFTWARE);
+    if (software == null) return false;
+    return software.indexOf("Faas") >= 0;
+  }
+
+  /**
+   * @see loci.formats.IFormatReader#openBytes(int, byte[], int, int, int, int)
+   */
+  public byte[] openBytes(int no, byte[] buf, int x, int y, int w, int h)
+    throws FormatException, IOException
+  {
+    FormatTools.checkPlaneParameters(this, no, buf.length, x, y, w, h);
+    int index = getCoreIndex();
+    tiffParser.getSamples(ifds.get(index), buf, x, y, w, h);
+    return buf;
+  }
+
+  /* @see loci.formats.IFormatReader#close(boolean) */
+  public void close(boolean fileOnly) throws IOException {
+    super.close(fileOnly);
+    if (!fileOnly) {
+    }
+  }
+
+  /* @see loci.formats.IFormatReader#getOptimalTileWidth() */
+  public int getOptimalTileWidth() {
+    FormatTools.assertId(currentId, true, 1);
+    try {
+      return (int) ifds.get(getCoreIndex()).getTileWidth();
+    }
+    catch (FormatException e) {
+      LOGGER.debug("", e);
+    }
+    return super.getOptimalTileWidth();
+  }
+
+  /* @see loci.formats.IFormatReader#getOptimalTileHeight() */
+  public int getOptimalTileHeight() {
+    FormatTools.assertId(currentId, true, 1);
+    try {
+      return (int) ifds.get(getCoreIndex()).getTileLength();
+    }
+    catch (FormatException e) {
+      LOGGER.debug("", e);
+    }
+    return super.getOptimalTileHeight();
+  }
+
+  // -- Internal BaseTiffReader API methods --
+
+  /* @see loci.formats.in.BaseTiffReader#initStandardMetadata() */
+  protected void initStandardMetadata() throws FormatException, IOException {
+    super.initStandardMetadata();
+
+    ifds = tiffParser.getIFDs();
+    for (IFD ifd : ifds) {
+      tiffParser.fillInIFD(ifd);
+    }
+
+    core = new CoreMetadata[ifds.size()];
+
+    for (int i=0; i<core.length; i++) {
+      core[i] = new CoreMetadata();
+
+      if (i == 0) {
+        core[i].resolutionCount = core.length;
+      }
+    }
+
+    // repopulate core metadata
+
+    for (int s=0; s<core.length; s++) {
+      IFD ifd = ifds.get(s);
+      PhotoInterp p = ifd.getPhotometricInterpretation();
+      int samples = ifd.getSamplesPerPixel();
+      core[s].rgb = samples > 1 || p == PhotoInterp.RGB;
+
+      long numTileRows = ifd.getTilesPerColumn() - 1;
+      long numTileCols = ifd.getTilesPerRow() - 1;
+
+      core[s].sizeX = (int) ifd.getImageWidth();
+      core[s].sizeY = (int) ifd.getImageLength();
+      core[s].sizeZ = 1;
+      core[s].sizeT = 1;
+      core[s].sizeC = core[s].rgb ? samples : 1;
+      core[s].littleEndian = ifd.isLittleEndian();
+      core[s].indexed = p == PhotoInterp.RGB_PALETTE &&
+        (get8BitLookupTable() != null || get16BitLookupTable() != null);
+      core[s].imageCount = 1;
+      core[s].pixelType = ifd.getPixelType();
+      core[s].metadataComplete = true;
+      core[s].interleaved = false;
+      core[s].falseColor = false;
+      core[s].dimensionOrder = "XYCZT";
+      core[s].thumbnail = s > 0;
+    }
+  }
+
+  /* @see loci.formats.BaseTiffReader#initMetadataStore() */
+  protected void initMetadataStore() throws FormatException {
+    /* debug */
+    System.out.println("series count = " + getSeriesCount());
+    System.out.println("current series = " + getSeries());
+    /* end debug */
+
+    super.initMetadataStore();
+
+    MetadataStore store = makeFilterMetadata();
+
+    for (int i=0; i<getSeriesCount(); i++) {
+      store.setImageName("Series " + (i + 1), i);
+    }
+  }
+
+}
