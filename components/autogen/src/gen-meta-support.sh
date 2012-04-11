943dc65e440f714cd50949be2a296c41b27dd6ad
Curtis Rueden <ctrueden@wisc.edu>
2012-04-11 09:36:36 -0500

    Update headers: autogen

--- components/autogen/src/gen-meta-support.sh
+++ components/autogen/src/gen-meta-support.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+###
+# #%L
+# Bio-Formats autogen package for programmatically generating source code.
+# %%
+# Copyright (C) 2007 - 2012 Board of Regents of the University of
+# Wisconsin-Madison, Glencoe Software, Inc., and University of Dundee.
+# %%
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 2 of the 
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public 
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/gpl-2.0.html>.
+# #L%
+###
+
 baseDir="$1/components"
 outputFile="$baseDir/autogen/src/meta-support.txt"
 commonClasses="$baseDir/bio-formats/src/loci/formats/MetadataTools.java"
