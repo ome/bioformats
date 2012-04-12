7fc69fbd2974d2e9641f865813dc7e0a7847d307
Curtis Rueden <ctrueden@wisc.edu>
2012-04-12 15:53:30 -0500

    Update headers: forks/jai

--- components/forks/jai/src/jj2000/j2k/codestream/writer/FileCodestreamWriter.java
+++ components/forks/jai/src/jj2000/j2k/codestream/writer/FileCodestreamWriter.java
@@ -1,4 +1,38 @@
 /*
+ * #%L
+ * Fork of JAI Image I/O Tools.
+ * %%
+ * Copyright (C) 2008 - 2012 Board of Regents of the University of
+ * Wisconsin-Madison, Glencoe Software, Inc., and University of Dundee.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+/*
  * $RCSfile: FileCodestreamWriter.java,v $
  * $Revision: 1.1 $
  * $Date: 2005/02/11 05:02:02 $
@@ -12,10 +46,10 @@
  *
  * COPYRIGHT:
  *
- * This software module was originally developed by Raphaël Grosbois and
+ * This software module was originally developed by Raphaï¿½l Grosbois and
  * Diego Santa Cruz (Swiss Federal Institute of Technology-EPFL); Joel
- * Askelöf (Ericsson Radio Systems AB); and Bertrand Berthelot, David
- * Bouchard, Félix Henry, Gerard Mozelle and Patrice Onno (Canon Research
+ * Askelï¿½f (Ericsson Radio Systems AB); and Bertrand Berthelot, David
+ * Bouchard, Fï¿½lix Henry, Gerard Mozelle and Patrice Onno (Canon Research
  * Centre France S.A) in the course of development of the JPEG2000
  * standard as specified by ISO/IEC 15444 (JPEG 2000 Standard). This
  * software module is an implementation of a part of the JPEG 2000
