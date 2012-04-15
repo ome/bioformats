e7d0f204ef4435e66d2e75119c44112b10b9684a
Curtis Rueden <ctrueden@wisc.edu>
2012-04-15 14:07:45 -0500

    Update headers: legacy/ome-editor

--- components/legacy/ome-editor/src/loci/ome/editor/VariableComboEditor.java
+++ components/legacy/ome-editor/src/loci/ome/editor/VariableComboEditor.java
@@ -1,25 +1,26 @@
-//
-// VariableComboEditor.java
-//
-
 /*
-OME Metadata Editor application for exploration and editing of OME-XML and
-OME-TIFF metadata. Copyright (C) 2006-@year@ Christopher Peterson.
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU Library General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Library General Public License for more details.
-
-You should have received a copy of the GNU Library General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+ * #%L
+ * OME Metadata Editor application for exploration and editing of OME-XML and
+ * OME-TIFF metadata.
+ * %%
+ * Copyright (C) 2006 - 2012 Board of Regents of the University of
+ * Wisconsin-Madison, Glencoe Software, Inc., and University of Dundee.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
 
 package loci.ome.editor;
 
