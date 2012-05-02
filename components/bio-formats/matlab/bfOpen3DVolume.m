2d59e1052e25a93cb652046cd203564bad409381
Sebastien Besson <seb.besson@gmail.com>
2012-05-02 11:45:21 +0100

    Improve speed of plane concatenation

--- components/bio-formats/matlab/bfOpen3DVolume.m
+++ components/bio-formats/matlab/bfOpen3DVolume.m
@@ -15,10 +15,7 @@ function volume = bfOpen3DVolume(filename)
 %   volume - 3D array containing all images in the file.
 
     volume = bfopen(filename);
-    vaux{1} = zeros([size(volume{1}, 1), size(volume{1}{1})]);
-    for k = 1:size(vaux{1}, 1)
-        vaux{1}(:,:,k) = volume{1}{k};
-    end
+    vaux{1} = cat(3,volume{1}{:,1});
     vaux{2} = filename;
     volume{1} = vaux;
 end
