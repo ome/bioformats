50e7292191b42ba70c999b9a29a53a673fc833d6
Chris Allan <callan@blackcat.ca>
2012-05-18 12:07:00 +0100

    Add basic new Timestamp primitive. (See #8862)

---
+++ components/ome-xml/src/ome/xml/model/primitives/Timestamp.java
@@ -0,0 +1,48 @@
+//
+// Timestamp.java
+//
+
+/*
+OME Bio-Formats package for reading and converting biological file formats.
+Copyright (C) 2005-@year@ UW-Madison LOCI and Glencoe Software, Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package ome.xml.model.primitives;
+
+/**
+ * Primitive type that represents an ISO 8601 timestamp.
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="http://trac.openmicroscopy.org.uk/ome/browser/bioformats.git/components/ome-xml/src/ome/xml/model/primitives/Timestamp.java">Trac</a>,
+ * <a href="http://git.openmicroscopy.org/?p=bioformats.git;a=blob;f=components/ome-xml/src/ome/xml/model/primitives/Timestamp.java;hb=HEAD">Gitweb</a></dd></dl>
+ */
+public class Timestamp extends PrimitiveType<String> {
+
+  public Timestamp(String value) {
+    super(value);
+  }
+
+  /**
+   * Returns a <code>Timestamp</code> object holding the value of
+   * the specified string.
+   * @param s The string to be parsed.
+   * @return See above.
+   */
+  public static Timestamp valueOf(String value) {
+    return new Timestamp(value);
+  }
+}
