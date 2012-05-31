da4bad6d5edc53367c34b8398c6645e85185f8c7
Melissa Linkert <melissa@glencoesoftware.com>
2012-05-31 10:36:31 -0400

    Fix SizeZ calculation to allow fully 5D data
    Previously, SizeZ would have been set to 1 for datasets that had
    multiple Z sections, timepoints, and channels.
    
    Reported by Dave Knecht.

--- components/bio-formats/src/loci/formats/in/TCSReader.java
+++ components/bio-formats/src/loci/formats/in/TCSReader.java
@@ -426,14 +426,14 @@ public class TCSReader extends FormatReader {
     core[0].indexed = tiffReaders[0].isIndexed();
 
     if (isRGB()) core[0].imageCount /= (getSizeC() / channelCount);
+
     if (getSizeZ() * getSizeT() * getEffectiveSizeC() !=
       (ifds.size() * tiffReaders.length))
     {
-      core[0].sizeZ = 1;
       int c = getEffectiveSizeC();
       if (c == 0) c = 1;
-      core[0].sizeT = (ifds.size() * tiffReaders.length) / c;
-      core[0].imageCount = getSizeT() * c;
+      core[0].sizeT = (ifds.size() * tiffReaders.length) / (c * getSizeZ());
+      core[0].imageCount = getSizeT() * c * getSizeZ();
       if (getSizeT() == 0) {
         core[0].sizeT = 1;
         core[0].imageCount = ifds.size() * tiffReaders.length;
