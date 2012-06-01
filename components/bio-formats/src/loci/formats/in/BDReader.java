506629a81e18d91705bb9217db537d28d8186370
Curtis Rueden <ctrueden@wisc.edu>
2012-06-01 16:58:39 -0500

    Readd Vanderbilt copyright to BDReader

--- components/bio-formats/src/loci/formats/in/BDReader.java
+++ components/bio-formats/src/loci/formats/in/BDReader.java
@@ -2,7 +2,8 @@
  * #%L
  * OME Bio-Formats package for reading and converting biological file formats.
  * %%
- * Copyright (C) 2005 - 2012 Open Microscopy Environment:
+ * Copyright (C) 2005 - 2012 Vanderbilt Integrative Cancer Center, and
+ * Open Microscopy Environment:
  *   - Board of Regents of the University of Wisconsin-Madison
  *   - Glencoe Software, Inc.
  *   - University of Dundee
