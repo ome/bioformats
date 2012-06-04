cbfbbe9fd807ef405ef1d0fbb817c716937b6a91
Curtis Rueden <ctrueden@wisc.edu>
2012-06-03 23:26:46 -0500

    Fix location of copyright headers

--- components/forks/poi/src/loci/poi/hpsf/CustomProperties.java
+++ components/forks/poi/src/loci/poi/hpsf/CustomProperties.java
@@ -1,5 +1,3 @@
-/* ====================================================================
- *
 /*
  * #%L
  * Fork of Apache Jakarta POI.
@@ -23,6 +21,7 @@
  * #L%
  */
 
+/* ====================================================================
    Licensed to the Apache Software Foundation (ASF) under one or more
    contributor license agreements.  See the NOTICE file distributed with
    this work for additional information regarding copyright ownership.
