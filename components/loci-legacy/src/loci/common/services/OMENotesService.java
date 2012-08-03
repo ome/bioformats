8ec653e53d7925dbc5da7e6f79059d57fba46aa3
Mark Hiner <hiner@wisc.edu>
2012-08-03 14:16:35 -0500

    Added OMENotesService extension
    loci.common.services.OMENotesService now extends both
    loci.common.services.Service and ome.scifio.services.OMENotesService to
    ensure that it is valid in both SCIFIO and legacy contexts.

--- components/loci-legacy/src/loci/common/services/OMENotesService.java
+++ components/loci-legacy/src/loci/common/services/OMENotesService.java
@@ -41,7 +41,7 @@ package loci.common.services;
  * <dd><a href="http://trac.openmicroscopy.org.uk/ome/browser/bioformats.git/components/common/src/loci/common/services/OMENotesService.java">Trac</a>,
  * <a href="http://git.openmicroscopy.org/?p=bioformats.git;a=blob;f=components/common/src/loci/common/services/OMENotesService.java;hb=HEAD">Gitweb</a></dd></dl>
  */
-public interface OMENotesService extends Service {
+public interface OMENotesService extends ome.scifio.services.OMENotesService, Service {
 
   /**
    * Creates a new OME Notes instance.
