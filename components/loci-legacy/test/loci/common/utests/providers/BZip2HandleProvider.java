2e1cb45185e76a4f1c8b42d55ef08eb28049245d
Mark Hiner <hiner@wisc.edu>
2012-08-06 14:29:17 -0500

    Duplicate loci-legacy tests from scifio-devel
    As loci-legacy currently mirrors scifio-devel, all the tests from
    scifio-devel are appropriate to run from loci-legacy (under the correct
    packages).
    
    These tests should simply delegate to the scifio-devel classes, and will
    help to ensure backwards compatibility.

---
+++ components/loci-legacy/test/loci/common/utests/providers/BZip2HandleProvider.java
@@ -0,0 +1,78 @@
+/*
+ * #%L
+ * LOCI Common package: utilities for I/O, reflection and miscellaneous tasks.
+ * %%
+ * Copyright (C) 2008 - 2012 Open Microscopy Environment:
+ *   - Board of Regents of the University of Wisconsin-Madison
+ *   - Glencoe Software, Inc.
+ *   - University of Dundee
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package loci.common.utests.providers;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import loci.common.BZip2Handle;
+import loci.common.IRandomAccess;
+
+/**
+ * Implementation of IRandomAccessProvider that produces instances of
+ * loci.common.BZip2Handle.
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="http://trac.openmicroscopy.org.uk/ome/browser/bioformats.git/components/common/test/loci/common/utests/providers/BZip2HandleProvider.java">Trac</a>,
+ * <a href="http://git.openmicroscopy.org/?p=bioformats.git;a=blob;f=components/common/test/loci/common/utests/providers/BZip2HandleProvider.java;hb=HEAD">Gitweb</a></dd></dl>
+ *
+ * @see IRandomAccessProvider
+ * @see loci.common.BZip2Handle
+ */
+class BZip2HandleProvider implements IRandomAccessProvider {
+
+  public IRandomAccess createMock(
+      byte[] page, String mode, int bufferSize) throws IOException {
+    File pageFile = File.createTempFile("page", ".dat");
+    FileOutputStream out = new FileOutputStream(pageFile);
+    out.write(page);
+    out.close();
+
+    Runtime rt = Runtime.getRuntime();
+    Process p = rt.exec(new String[] {"bzip2", pageFile.getAbsolutePath()});
+    try {
+      p.waitFor();
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+
+    pageFile = new File(pageFile.getAbsolutePath() + ".bz2");
+    return new BZip2Handle(pageFile.getAbsolutePath());
+  }
+
+}
---
+++ components/loci-legacy/test/loci/common/utests/providers/URLHandleProvider.java
@@ -0,0 +1,70 @@
+/*
+ * #%L
+ * LOCI Common package: utilities for I/O, reflection and miscellaneous tasks.
+ * %%
+ * Copyright (C) 2008 - 2012 Open Microscopy Environment:
+ *   - Board of Regents of the University of Wisconsin-Madison
+ *   - Glencoe Software, Inc.
+ *   - University of Dundee
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package loci.common.utests.providers;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import loci.common.IRandomAccess;
+import loci.common.URLHandle;
+
+/**
+ * Implementation of IRandomAccessProvider that produces instances of
+ * loci.common.URLHandle.
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="http://trac.openmicroscopy.org.uk/ome/browser/bioformats.git/components/common/test/loci/common/utests/providers/URLHandleProvider.java">Trac</a>,
+ * <a href="http://git.openmicroscopy.org/?p=bioformats.git;a=blob;f=components/common/test/loci/common/utests/providers/URLHandleProvider.java;hb=HEAD">Gitweb</a></dd></dl>
+ *
+ * @see IRandomAccessProvider
+ * @see loci.common.URLHandle
+ */
+class URLHandleProvider implements IRandomAccessProvider {
+
+  public IRandomAccess createMock(
+      byte[] page, String mode, int bufferSize) throws IOException {
+    File f = File.createTempFile("url", ".dat");
+    f.deleteOnExit();
+    FileOutputStream out = new FileOutputStream(f);
+    out.write(page);
+    out.close();
+
+    return new URLHandle(f.toURL().toString());
+  }
+
+}
