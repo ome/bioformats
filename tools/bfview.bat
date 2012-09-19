025cd09b17955278eb0677147e162b58cc63f9d2
Curtis Rueden <ctrueden@wisc.edu>
2012-09-19 14:34:37 -0500

    Fix bug in deprecated bfview command
    When the tools directory is not on the path and the CWD is not
    the tools directory, the delegation to "showinf" does not work;
    the fix is to use the DIR variable to specify showinf's location.

--- tools/bfview.bat
+++ tools/bfview.bat
@@ -2,5 +2,9 @@
 
 rem bfview.bat: a batch file for displaying an image file in the image viewer
 
+set DIR=%~dp0
+if "%DIR:~-1%" == "\" set DIR=%DIR:~0,-1%
+
 echo The 'bfview' command is deprecated. Please use 'showinf' instead.
-showinf %*
+
+"%DIR%\showinf" %*
