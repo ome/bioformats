0232d82c7833f2dae9eea9f46d02f321b164985b
Curtis Rueden <ctrueden@wisc.edu>
2012-09-19 11:29:22 -0500

    Skip the update check if NO_UPDATE_CHECK is set

--- tools/config.bat
+++ tools/config.bat
@@ -8,6 +8,15 @@ rem but you can tweak the settings to your liking.
 rem Set the amount of RAM available to the command line tools.
 set JFLAGS=-Xmx512m
 
+rem Set the NO_UPDATE_CHECK flag to skip the update check.
+rem NO_UPDATE_CHECK=1
+
+rem Skip the update check if the NO_UPDATE_CHECK flag is set.
+if not "%NO_UPDATE_CHECK%" == "" (
+  rem Skip the update check if the NO_UPDATE_CHECK flag is set
+  set JFLAGS=%JFLAGS% -Dbioformats_can_do_upgrade_check=false
+)
+
 rem If you are behind a proxy server, the host name and port must be set.
 set PROXY_HOST=
 set PROXY_PORT=
--- tools/config.sh
+++ tools/config.sh
@@ -8,6 +8,15 @@
 # Set the amount of RAM available to the command line tools.
 JFLAGS="-Xmx512m"
 
+# Set the NO_UPDATE_CHECK flag to skip the update check.
+#NO_UPDATE_CHECK=1
+
+# Skip the update check if the NO_UPDATE_CHECK flag is set.
+if [ -n "$NO_UPDATE_CHECK" ]
+then
+  JFLAGS="$JFLAGS -Dbioformats_can_do_upgrade_check=false"
+fi
+
 # If you are behind a proxy server, the host name and port must be set.
 #PROXY_HOST=
 #PROXY_PORT=
