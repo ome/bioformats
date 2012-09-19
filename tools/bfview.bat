c3ed824fd517ef87e83267364564bf0f790f7fba
Curtis Rueden <ctrueden@wisc.edu>
2012-09-19 10:43:02 -0500

    Simplify deprecated scripts
    There is no need to document the details of their use; interested
    parties can refer to the suggested alternatives for that.

--- tools/bfview.bat
+++ tools/bfview.bat
@@ -2,14 +2,5 @@
 
 rem bfview.bat: a batch file for displaying an image file in the image viewer
 
-rem Required JARs: loci_tools.jar
-
-rem JAR libraries must be in the same directory as this
-rem command line script for the command to function.
-
-rem If you are a developer working from source and have
-rem the LOCI classes in your CLASSPATH, you can set the
-rem LOCI_DEVEL environment variable to use them instead.
-
-echo The 'bfview' command is now deprecated.  Please use the 'showinf' command instead.
+echo The 'bfview' command is deprecated. Please use 'showinf' instead.
 showinf %*
