a62f6529e9862ca79546931a86d10b92eca49a2d
Curtis Rueden <ctrueden@wisc.edu>
2012-09-20 11:08:20 -0500

    Update omeul command to require ome_tools.jar
    Internally, this change also accepts ome-io.jar (and implicitly its
    transitive dependencies) instead of ome_tools.jar, similar to how the
    launch script accepts bio-formats.jar instead of loci_tools.jar.
    
    The script now also warns that omeul is discontinued, and comments that
    the command works only with the OME Perl server.

--- tools/omeul.bat
+++ tools/omeul.bat
@@ -1,8 +1,8 @@
 @echo off
 
-rem omeul.bat: a command-line client-side import tool for OME
+rem omeul.bat: a command-line client-side import tool for the OME Perl server
 
-rem Required JARs: loci_tools.jar
+rem Required JARs: loci_tools.jar, ome_tools.jar
 
 rem JAR libraries must be in the same directory as this
 rem command line script for the command to function.
@@ -10,5 +10,28 @@ rem command line script for the command to function.
 set DIR=%~dp0
 if "%DIR:~-1%" == "\" set DIR=%DIR:~0,-1%
 
+call "%DIR%\config.bat"
+
+if "%SCIFIO_DEVEL%" == "" (
+  rem Developer environment variable unset; look for proper libraries.
+  if exist "%DIR%\ome_tools.jar" goto found
+  if exist "%DIR%\ome-io.jar" goto found
+  goto missing
+)
+
+:found
 set PROG=loci.formats.ome.OMEWriter
+set CPAUX="%DIR%\ome_tools.jar";"%DIR%\ome-io.jar"
 call "%DIR%\launch.bat" %*
+goto end
+
+:missing
+echo Required JAR libraries not found. Please download:
+echo   ome_tools.jar
+echo from:
+echo   http://www.loci.wisc.edu/bio-formats/downloads
+echo.
+echo Please note that omeul is legacy software that
+echo has been discontinued. Use at your own risk.
+
+:end
