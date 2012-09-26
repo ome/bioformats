f8ed5c91eb3e3ff0f6904809e964b31ea06b1f78
Curtis Rueden <ctrueden@wisc.edu>
2012-09-26 15:40:46 -0500

    Add missing set commands to commented-out code

--- tools/config.bat
+++ tools/config.bat
@@ -9,7 +9,7 @@ rem Set the amount of RAM available to the command line tools.
 set JFLAGS=-Xmx512m
 
 rem Set the NO_UPDATE_CHECK flag to skip the update check.
-rem NO_UPDATE_CHECK=1
+rem set NO_UPDATE_CHECK=1
 
 rem If you are behind a proxy server, the host name and port must be set.
 set PROXY_HOST=
@@ -18,4 +18,4 @@ set PROXY_PORT=
 rem If your CLASSPATH already includes the needed classes,
 rem you can set the SCIFIO_DEVEL environment variable to
 rem disable the required JAR library checks.
-rem SCIFIO_DEVEL=1
+rem set SCIFIO_DEVEL=1
