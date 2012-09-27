108b90cfe015fb639d05437c9206a43f4258af22
Melissa Linkert <melissa@glencoesoftware.com>
2012-09-27 16:58:35 -0400

    Add proper attribution

--- components/scifio/src/loci/formats/services/JPEGTurboServiceImpl.java
+++ components/scifio/src/loci/formats/services/JPEGTurboServiceImpl.java
@@ -53,6 +53,10 @@ import org.libjpegturbo.turbojpeg.TJDecompressor;
 import org.scijava.nativelib.NativeLibraryUtil;
 
 /**
+ * Based upon the NDPI to OME-TIFF converter by Matthias Baldauf:
+ *
+ * http://matthias-baldauf.at/software/ndpi_converter/
+ *
  * <dl><dt><b>Source code:</b></dt>
  * <dd><a href="http://trac.openmicroscopy.org.uk/ome/browser/bioformats.git/components/bio-formats/src/loci/formats/services/JPEGTurboServiceImpl.java">Trac</a>,
  * <a href="http://git.openmicroscopy.org/?p=bioformats.git;a=blob;f=components/bio-formats/src/loci/formats/services/JPEGTurboServiceImpl.java;hb=HEAD">Gitweb</a></dd></dl>
