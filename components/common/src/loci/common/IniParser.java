60bf70ee50f625aa3a80a96e1230bec9120072e1
Melissa Linkert <melissa@glencoesoftware.com>
2012-10-17 16:26:45 -0400

    Initial version of autogenerated format pages
    'ant -Dfilelist=src/format-pages.txt gen-format-pages' in components/autogen
    will now generate all of the docs/sphinx/loci/bio-formats-format-*.txt pages.
    The template for these pages is in components/autogen/src/doc/FormatPage.vm,
    and the data is in components/autogen/src/format-pages.txt.
    
    See #9723.
    
    Conflicts:
    
    	components/loci-legacy/src/loci/common/IniParser.java

--- components/common/src/loci/common/IniParser.java
+++ components/common/src/loci/common/IniParser.java
@@ -1,8 +1,8 @@
 /*
  * #%L
- * LOCI Common package: utilities for I/O, reflection and miscellaneous tasks.
+ * OME SCIFIO package for reading and converting scientific file formats.
  * %%
- * Copyright (C) 2008 - 2012 Open Microscopy Environment:
+ * Copyright (C) 2005 - 2012 Open Microscopy Environment:
  *   - Board of Regents of the University of Wisconsin-Madison
  *   - Glencoe Software, Inc.
  *   - University of Dundee
@@ -39,6 +39,7 @@ package loci.common;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -201,9 +202,15 @@ public class IniParser {
 
       // backslash signifies data continues to next line
       boolean slash = slashContinues && line.endsWith("\\");
-      if (slash) line = line.substring(0, line.length() - 1).trim() + " ";
+      boolean blockText = slashContinues && line.endsWith("\\n");
+      if (blockText) {
+        line = line.substring(0, line.length() - 2).trim() + "\n";
+      }
+      else if (slash) {
+        line = line.substring(0, line.length() - 1).trim() + " ";
+      }
       sb.append(line);
-      if (!slash) break;
+      if (!slash && !blockText) break;
     }
     return no;
   }
