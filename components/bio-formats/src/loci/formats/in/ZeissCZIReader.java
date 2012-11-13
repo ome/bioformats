99a0bee6d1879d643599ec294e7ee19ee2033f15
Melissa Linkert <melissa@glencoesoftware.com>
2012-11-13 14:26:13 -0500

    Zeiss CZI: parse Z positions from XML
    See http://fiji.sc/bugzilla/show_bug.cgi?id=523

--- components/bio-formats/src/loci/formats/in/ZeissCZIReader.java
+++ components/bio-formats/src/loci/formats/in/ZeissCZIReader.java
@@ -523,7 +523,10 @@ public class ZeissCZIReader extends FormatReader {
               store.setPlanePositionY(positionsY[i], i, plane);
             }
 
-            if (positionsZ != null && i < positionsZ.length) {
+            if (p.stageZ != null) {
+              store.setPlanePositionZ(p.stageZ, i, plane);
+            }
+            else if (positionsZ != null && i < positionsZ.length) {
               store.setPlanePositionZ(positionsZ[i], i, plane);
             }
 
@@ -1829,7 +1832,7 @@ public class ZeissCZIReader extends FormatReader {
 
     private long dataOffset;
 
-    private Double stageX, stageY, timestamp, exposureTime;
+    private Double stageX, stageY, timestamp, exposureTime, stageZ;
 
     public int x, y;
 
@@ -1930,6 +1933,9 @@ public class ZeissCZIReader extends FormatReader {
                 }
                 else if (tagNode.getNodeName().equals("StageYPosition")) {
                   stageY = new Double(text);
+                }
+                else if (tagNode.getNodeName().equals("FocusPosition")) {
+                  stageZ = new Double(text);
                 }
                 else if (tagNode.getNodeName().equals("AcquisitionTime")) {
                   timestamp = DateTools.getTime(
