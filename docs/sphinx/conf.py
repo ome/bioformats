a583b82a2e7e4305d7f21999ff87bad6dc510e13
Sebastien Besson <seb.besson@gmail.com>
2012-12-07 11:19:35 +0000

    Add Bio-Formats logo to the front page of the PDF

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -274,7 +274,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = "images/bio-formats-logo.pdf"
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
