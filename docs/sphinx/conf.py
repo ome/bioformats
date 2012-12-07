fc51d183a08b004456f89bf8b067482530d1db46
Sebastien Besson <seb.besson@gmail.com>
2012-12-07 11:12:21 +0000

    Add LaTeX options to get rid of extra white pages

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -253,6 +253,8 @@ htmlhelp_basename = 'Bio-Formatsdoc'
 # -- Options for LaTeX output --------------------------------------------------
 
 latex_elements = {
+  'classoptions': ',oneside',
+  'babel': '\\usepackage[english]{babel}'
 # The paper size ('letterpaper' or 'a4paper').
 #'papersize': 'letterpaper',
 
