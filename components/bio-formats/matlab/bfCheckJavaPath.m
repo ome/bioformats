8864ad61f8850cbfc40b0d219576aa29b87ad608
Sebastien Besson <seb.besson@gmail.com>
2012-12-20 16:27:02 +0000

    Return Bio-Formats version in bfCheckJavaPath()

--- components/bio-formats/matlab/bfCheckJavaPath.m
+++ components/bio-formats/matlab/bfCheckJavaPath.m
@@ -1,8 +1,9 @@
-function status = bfCheckJavaPath(varargin)
+function [status, version] = bfCheckJavaPath(varargin)
 % bfCheckJavaPath check Bio-Formats is included in the Java class path
 % 
-% SYNOPSIS  bfCheckJavaPath
+% SYNOPSIS  bfCheckJavaPath()
 %           status = bfCheckJavaPath(autoloadBioFormats)
+%           [status, version] = bfCheckJavaPath()
 %
 % Input 
 %
@@ -12,7 +13,10 @@ function status = bfCheckJavaPath(varargin)
 %
 % Output
 %
-%    status - Boolean. True if loci_tools is part of the Java library.
+%    status - Boolean. True if loci_tools.jar is part of the Java library.
+%
+%    version - String. Current version of Bio-Formats. Empty string if
+%    loci_tools.jar is not part of the Java library
 
 % Input check
 ip = inputParser;
@@ -26,15 +30,22 @@ isLociTools = cellfun(@(x) ~isempty(regexp(x, '.*loci_tools.jar$', 'once')),...
 status = any(isLociTools);
 
 % Return if loci_tools is in the java path or autoloadBioFormats is off
-if status || ~ip.Results.autoloadBioFormats, return; end   
-
-% Assume the jar is in Matlab path or under the same folder as this file
-path = which('loci_tools.jar');
-if isempty(path)
-    path = fullfile(fileparts(mfilename('fullpath')), 'loci_tools.jar');
+if ~status && ip.Results.autoloadBioFormats,
+    % Assume the jar is in Matlab path or under the same folder as this file
+    path = which('loci_tools.jar');
+    if isempty(path)
+        path = fullfile(fileparts(mfilename('fullpath')), 'loci_tools.jar');
+    end
+    assert(exist(path, 'file') == 2, 'Cannot automatically locate loci_tools.jar');
+    
+    % Add loci_tools to dynamic Java class path
+    javaaddpath(path);
+    status = true;
 end
-assert(exist(path, 'file') == 2, 'Cannot automatically locate loci_tools.jar');
 
-% Add loci_tools to dynamic Java class path
-javaaddpath(path);    
-status = true;
+if status
+    % Read Bio-Formats version
+    version = char(loci.formats.FormatTools.VERSION);
+else
+    version = '';
+end
