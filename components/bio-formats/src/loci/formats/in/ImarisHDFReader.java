50c48a40ea4292b0db7ab10c27786c0bcf27fb4f
Melissa Linkert <melissa@glencoesoftware.com>
2013-02-22 11:43:30 -0500

    Imaris HDF: fix channel count parsing
    Fixes QA #4898.
    
    Conflicts:
    
    	components/bio-formats/src/loci/formats/in/ImarisHDFReader.java

--- components/bio-formats/src/loci/formats/in/ImarisHDFReader.java
+++ components/bio-formats/src/loci/formats/in/ImarisHDFReader.java
@@ -516,6 +516,9 @@ public class ImarisHDFReader extends FormatReader {
       else if (name.equals("FileTimePoints")) {
         ms0.sizeT = Integer.parseInt(value);
       }
+      else if (name.equals("NumberOfChannels")) {
+        ms0.sizeC = Integer.parseInt(value);
+      }
       else if (name.equals("RecordingEntrySampleSpacing")) {
         pixelSizeX = Double.parseDouble(value);
       }
@@ -550,7 +553,6 @@ public class ImarisHDFReader extends FormatReader {
         int underscore = attr.indexOf("_") + 1;
         int cIndex = Integer.parseInt(attr.substring(underscore,
           attr.indexOf("/", underscore)));
-        if (cIndex == getSizeC()) ms0.sizeC++;
 
         if (name.equals("Gain")) gain.add(value);
         else if (name.equals("LSMEmissionWavelength")) emWave.add(value);
