7715662663e77177f46cf81f909a0f71ba5cdc82
Melissa Linkert <melissa@glencoesoftware.com>
2013-03-06 17:37:56 -0500

    Slidebook: fix type detection to account for v5.5 files
    Fixes #10473.

--- components/bio-formats/src/loci/formats/in/SlidebookReader.java
+++ components/bio-formats/src/loci/formats/in/SlidebookReader.java
@@ -61,6 +61,7 @@ public class SlidebookReader extends FormatReader {
 
   public static final int SLD_MAGIC_BYTES_1_0 = 0x006c;
   public static final int SLD_MAGIC_BYTES_1_1 = 0x0100;
+  public static final int SLD_MAGIC_BYTES_1_2 = 0x0200;
   public static final int SLD_MAGIC_BYTES_2_0 = 0x01f5;
   public static final int SLD_MAGIC_BYTES_2_1 = 0x0102;
 
@@ -102,7 +103,8 @@ public class SlidebookReader extends FormatReader {
     int magicBytes2 = stream.readShort();
 
 
-    return ((magicBytes2 & 0xff00) == SLD_MAGIC_BYTES_1_1) &&
+    return ((magicBytes2 & 0xff00) == SLD_MAGIC_BYTES_1_1 ||
+      (magicBytes2 & 0xff00) == SLD_MAGIC_BYTES_1_2) &&
       (magicBytes1 == SLD_MAGIC_BYTES_1_0 ||
       magicBytes1 == SLD_MAGIC_BYTES_2_0);
   }
