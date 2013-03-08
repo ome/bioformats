a0030d44b1f06734cd3e9c4e80abcf9307ee63c4
Andrew J Patterson <workonly@qidane.com>
2013-03-08 11:09:38 +0000

    Build fixes for xsd-fu

--- components/xsd-fu/setup.py
+++ components/xsd-fu/setup.py
@@ -15,14 +15,14 @@ import glob
 import sys
 import os
 
-ov = os.environ.get("OMERO_VERSION", "unknown")
+ov = os.environ.get("BF_VERSION", "unknown")
 
-for tools in glob.glob("../../lib/repository/setuptools*.egg"):
+for tools in glob.glob("tempTOFIX/setuptools*.egg"):
     if tools.find(".".join(map(str, sys.version_info[0:2]))) > 0:
        sys.path.insert(0, tools)
 
 from ez_setup import use_setuptools
-use_setuptools(to_dir='../../lib/repository')
+use_setuptools(to_dir='tempTOFIX')
 from setuptools import setup, find_packages
 
 if os.path.exists("target"):
