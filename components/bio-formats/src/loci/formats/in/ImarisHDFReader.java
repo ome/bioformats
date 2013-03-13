c4f778656ae5f7292aad62a829defe849d24d9eb
Melissa Linkert <melissa@glencoesoftware.com>
2013-03-13 10:41:51 -0400

    Imaris: one more channel count fix
    This is what 7c7d5b57a03e17e2c44625e24f09f585dc477dec should have done.

--- components/bio-formats/src/loci/formats/in/ImarisHDFReader.java
+++ components/bio-formats/src/loci/formats/in/ImarisHDFReader.java
@@ -474,7 +474,7 @@ public class ImarisHDFReader extends FormatReader {
       else if (name.equals("FileTimePoints")) {
         core[0].sizeT = Integer.parseInt(value);
       }
-      else if (name.equals("NumberOfChannels")) {
+      else if (name.equals("NumberOfChannels") && getSizeC() == 0) {
         core[0].sizeC = Integer.parseInt(value);
       }
       else if (name.equals("RecordingEntrySampleSpacing")) {
@@ -511,6 +511,7 @@ public class ImarisHDFReader extends FormatReader {
         int underscore = attr.indexOf("_") + 1;
         int cIndex = Integer.parseInt(attr.substring(underscore,
           attr.indexOf("/", underscore)));
+        if (cIndex == getSizeC()) core[0].sizeC++;
 
         if (name.equals("Gain")) gain.add(value);
         else if (name.equals("LSMEmissionWavelength")) emWave.add(value);
