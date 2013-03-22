eb57928052a633146c578230cea61bd6c41e97a8
Melissa Linkert <melissa@glencoesoftware.com>
2013-03-22 10:14:33 -0400

    Initial version of Aperio AFI reader

---
+++ components/bio-formats/src/loci/formats/in/AFIReader.java
@@ -0,0 +1,221 @@
+/*
+ * #%L
+ * OME Bio-Formats package for reading and converting biological file formats.
+ * %%
+ * Copyright (C) 2005 - 2013 Open Microscopy Environment:
+ *   - Board of Regents of the University of Wisconsin-Madison
+ *   - Glencoe Software, Inc.
+ *   - University of Dundee
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the 
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+
+package loci.formats.in;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import loci.common.DataTools;
+import loci.common.Location;
+import loci.common.RandomAccessInputStream;
+import loci.common.xml.BaseHandler;
+import loci.common.xml.XMLTools;
+import loci.formats.ChannelSeparator;
+import loci.formats.CoreMetadata;
+import loci.formats.FormatException;
+import loci.formats.FormatReader;
+import loci.formats.FormatTools;
+import loci.formats.MetadataTools;
+import loci.formats.meta.MetadataStore;
+import ome.xml.model.primitives.PositiveFloat;
+
+import org.xml.sax.Attributes;
+
+/**
+ * AFIReader is the file format reader for Aperio AFI files.
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="http://trac.openmicroscopy.org.uk/ome/browser/bioformats.git/components/bio-formats/src/loci/formats/in/AFIReader.java">Trac</a>,
+ * <a href="http://git.openmicroscopy.org/?p=bioformats.git;a=blob;f=components/bio-formats/src/loci/formats/in/AFIReader.java;hb=HEAD">Gitweb</a></dd></dl>
+ *
+ * @author Melissa Linkert melissa at glencoesoftware.com
+ */
+public class AFIReader extends FormatReader {
+
+  // -- Constants --
+
+  // -- Fields --
+
+  private ArrayList<String> pixels = new ArrayList<String>();
+  private ChannelSeparator reader = new ChannelSeparator(new SVSReader());
+
+  // -- Constructor --
+
+  /** Constructs a new AFI reader. */
+  public AFIReader() {
+    super("Aperio AFI", "afi");
+    domains = new String[] {FormatTools.HISTOLOGY_DOMAIN};
+    hasCompanionFiles = true;
+    datasetDescription = "One .afi file and several similarly-named .svs files";
+  }
+
+  // -- IFormatReader API methods --
+
+  /* @see loci.formats.IFormatReader#isThisType(RandomAccessInputStream) */
+  public boolean isThisType(RandomAccessInputStream stream) throws IOException {
+    final int blockLen = 4;
+    if (!FormatTools.validStream(stream, blockLen, false)) return false;
+    return true;
+  }
+
+  /* @see loci.formats.IFormatReader#isSingleFile(String) */
+  public boolean isSingleFile(String id) throws FormatException, IOException {
+    return false;
+  }
+
+  /**
+   * @see loci.formats.IFormatReader#openBytes(int, byte[], int, int, int, int)
+   */
+  public byte[] openBytes(int no, byte[] buf, int x, int y, int w, int h)
+    throws FormatException, IOException
+  {
+    FormatTools.checkPlaneParameters(this, no, buf.length, x, y, w, h);
+
+    if (getCoreIndex() >= core.size() - 2) {
+      reader.setId(pixels.get(0));
+      reader.setSeries(getSeries());
+      reader.setResolution(getResolution());
+      return reader.openBytes(no, buf, x, y, w, h);
+    }
+
+    int[] coords = getZCTCoords(no);
+    int channel = coords[1];
+    int index = getIndex(coords[0], 0, coords[2]);
+
+    reader.setId(pixels.get(channel));
+    reader.setSeries(getSeries());
+    reader.setResolution(getResolution());
+    return reader.openBytes(index, buf, x, y, w, h);
+  }
+
+  /* @see loci.formats.IFormatReader#getSeriesUsedFiles(boolean) */
+  public String[] getSeriesUsedFiles(boolean noPixels) {
+    FormatTools.assertId(currentId, true, 1);
+
+    if (noPixels) {
+      return new String[] {currentId};
+    }
+
+    String[] files = new String[pixels.size() + 1];
+    files[0] = currentId;
+    for (int i=0; i<pixels.size(); i++) {
+      files[i + 1] = pixels.get(i);
+    }
+    return files;
+  }
+
+  /* @see loci.formats.IFormatReader#fileGroupOption(String) */
+  public int fileGroupOption(String id) throws FormatException, IOException {
+    return FormatTools.MUST_GROUP;
+  }
+
+  /* @see loci.formats.IFormatReader#close(boolean) */
+  public void close(boolean fileOnly) throws IOException {
+    super.close(fileOnly);
+    if (!fileOnly) {
+      reader.close();
+      pixels.clear();
+    }
+  }
+
+  // -- Internal FormatReader API methods --
+
+  /* @see loci.formats.FormatReader#initFile(String) */
+  protected void initFile(String id) throws FormatException, IOException {
+    super.initFile(id);
+
+    // The AFI file is just simple XML that lists the .svs files from
+    // which to read pixel data.  Each .svs corresponds to a single channel;
+    // we assemble the channels in the order in which they are stored in
+    // the XML.
+    //
+    // Note that the last two series are identical across the .svs files,
+    // so we just use the ones from the first listed file.
+
+    String xml = DataTools.readFile(id);
+    XMLTools.parseXML(xml, new AFIHandler());
+
+    String parent = new Location(id).getAbsoluteFile().getParent();
+    String[] channelNames = new String[pixels.size()];
+
+    for (int i=0; i<pixels.size(); i++) {
+      String file = pixels.get(i);
+
+      int underscore = file.indexOf("_");
+      int fullStop = file.indexOf(".");
+      if (underscore >= 0 && fullStop > underscore) {
+        channelNames[i] = file.substring(underscore + 1, fullStop);
+      }
+
+      pixels.set(i, new Location(parent, file).getAbsolutePath());
+    }
+
+    reader.setMetadataStore(getMetadataStore());
+    reader.setId(pixels.get(0));
+
+    core = reader.getCoreMetadataList();
+
+    for (int i=0; i<core.size() - 2; i++) {
+      CoreMetadata c = core.get(i);
+      c.sizeC = pixels.size();
+      c.imageCount = c.sizeC * c.sizeZ * c.sizeT;
+      c.rgb = false;
+      c.cLengths = new int[] {c.sizeC};
+    }
+
+    MetadataStore store = makeFilterMetadata();
+    MetadataTools.populatePixels(store, this, false, false);
+
+    if (getMetadataOptions().getMetadataLevel() != MetadataLevel.MINIMUM) {
+      for (int i=0; i<core.size() - 2; i++) {
+        for (int c=0; c<channelNames.length; c++) {
+          store.setChannelName(channelNames[c], i, c);
+        }
+      }
+    }
+  }
+
+  // -- Helper class --
+
+  class AFIHandler extends BaseHandler {
+    private String currentElement;
+
+    public void characters(char[] ch, int start, int length) {
+      String value = new String(ch, start, length);
+
+      if (currentElement.equals("Path") && value.trim().length() > 0) {
+        pixels.add(value);
+      }
+    }
+
+    public void startElement(String uri, String localName, String qName,
+      Attributes attributes)
+    {
+      currentElement = qName;
+    }
+  }
+
+}
