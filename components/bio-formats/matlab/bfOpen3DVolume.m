cc6352f03c301567c78b850db45cff5c2c10b0e2
Sebastien Besson <seb.besson@gmail.com>
2013-03-22 15:56:45 +0000

    Add license headers to Matlab functions

--- components/bio-formats/matlab/bfOpen3DVolume.m
+++ components/bio-formats/matlab/bfOpen3DVolume.m
@@ -14,6 +14,27 @@ function volume = bfOpen3DVolume(filename)
 %
 %   volume - 3D array containing all images in the file.
 
+% OME Bio-Formats package for reading and converting biological file formats.
+%
+% Copyright (C) 2012 - 2013 Open Microscopy Environment:
+%   - Board of Regents of the University of Wisconsin-Madison
+%   - Glencoe Software, Inc.
+%   - University of Dundee
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as
+% published by the Free Software Foundation, either version 2 of the
+% License, or (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License along
+% with this program; if not, write to the Free Software Foundation, Inc.,
+% 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 % load the Bio-Formats library into the MATLAB environment
 status = bfCheckJavaPath();
 assert(status, ['Missing Bio-Formats library. Either add loci_tools.jar '...
