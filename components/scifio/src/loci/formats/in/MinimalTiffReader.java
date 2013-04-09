ome/bioformats

commit 64f33a89e3277a5c9a6d1bb6cbcd546e6f3a9b95

TIFF: adjust the optimal tile height for large single-tile images
See
http://lists.openmicroscopy.org.uk/pipermail/ome-users/2013-March/003597.html
and
http://lists.openmicroscopy.org.uk/pipermail/ome-users/2013-March/003601.html

components/scifio/src/loci/formats/in/MinimalTiffReader.java
@@ -384,7 +384,15 @@ public class MinimalTiffReader extends FormatReader {
   public int getOptimalTileHeight() {
     FormatTools.assertId(currentId, true, 1);
     try {
-      return (int) ifds.get(0).getTileLength();
+      int height = (int) ifds.get(0).getTileLength();
+      // Some TIFF files only store a single tile, even if the image is
+      // very, very large.  In those cases, we don't want to open the whole
+      // tile if we can avoid it.
+      if (DataTools.safeMultiply32(height, getOptimalTileWidth()) >
+        10 * 1024 * 1024)
+      {
+        return super.getOptimalTileHeight();
+      }
     }
     catch (FormatException e) {
       LOGGER.debug("Could not retrieve tile height", e);

components/scifio/src/loci/formats/tiff/TiffParser.java
@@ -804,8 +804,7 @@ public class TiffParser {
 
     // special case: if we only need one tile, and that tile doesn't need
     // any special handling, then we can just read it directly and return
-    if ((x % tileWidth) == 0 && (y % tileLength) == 0 && width == tileWidth &&
-      height == imageLength && samplesPerPixel == 1 &&
+    if ((x % tileWidth) == 0 && (y % tileLength) == 0 && samplesPerPixel == 1 &&
       (ifd.getBitsPerSample()[0] % 8) == 0 &&
       photoInterp != PhotoInterp.WHITE_IS_ZERO &&
       photoInterp != PhotoInterp.CMYK && photoInterp != PhotoInterp.Y_CB_CR &&
@@ -830,9 +829,25 @@ public class TiffParser {
           }
 
           in.seek(stripOffsets[tile]);
-          int len = (int) Math.min(buf.length - offset, byteCount);
-          in.read(buf, offset, len);
-          offset += len;
+
+          if (width == tileWidth && height == imageLength) {
+            // we want to entire tile, so just read the whole thing directly
+            int len = (int) Math.min(buf.length - offset, byteCount);
+            in.read(buf, offset, len);
+            offset += len;
+          }
+          else {
+            // we only want a piece of the tile, so read each row separately
+            // this is especially necessary for large single-tile images
+            int bpp = ifd.getBitsPerSample()[0] / 8;
+            int len = (int) Math.min(buf.length - offset, width * bpp);
+            for (int row=0; row<height; row++) {
+              in.skipBytes(x * bpp);
+              in.read(buf, offset, len);
+              offset += len;
+              in.skipBytes((int) (bpp * (tileWidth - x - width)));
+            }
+          }
         }
       }
       return buf;

commit b5df7474057117237573a0c0d81d2da581aa75b1

TIFF: actually return the tile height
Caused by 64f33a89e3277a5c9a6d1bb6cbcd546e6f3a9b95.

This may be the most stupid bug that I have ever introduced.

components/scifio/src/loci/formats/in/MinimalTiffReader.java
@@ -388,6 +388,7 @@ public class MinimalTiffReader extends FormatReader {
       if (height <= 0) {
         height = getSizeY();
       }
+
       // Some TIFF files only store a single tile, even if the image is
       // very, very large.  In those cases, we don't want to open the whole
       // tile if we can avoid it.
@@ -396,6 +397,7 @@ public class MinimalTiffReader extends FormatReader {
       {
         return super.getOptimalTileHeight();
       }
+      return height;
     }
     catch (FormatException e) {
       LOGGER.debug("Could not retrieve tile height", e);