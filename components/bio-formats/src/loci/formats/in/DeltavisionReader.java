44a2666d365c473e937b87b074a0d6c682bb2731
Andreas Knab <knabar@gmail.com>
2013-04-10 16:50:37 +0200

    Remove code duplication, add documentation

--- components/bio-formats/src/loci/formats/in/DeltavisionReader.java
+++ components/bio-formats/src/loci/formats/in/DeltavisionReader.java
@@ -174,36 +174,9 @@ public class DeltavisionReader extends FormatReader {
     FormatTools.checkPlaneParameters(this, no, buf.length, x, y, w, h);
 
     int[] coords = getZCTCoords(no);
-
-    int[] newCoords = new int[4];
-    int coordIndex = 0;
-    int dimIndex = 2;
-
-    while (coordIndex < newCoords.length) {
-      char dim = getDimensionOrder().charAt(dimIndex++);
-
-      switch (dim) {
-        case 'Z':
-          newCoords[coordIndex++] = coords[0];
-          break;
-        case 'C':
-          newCoords[coordIndex++] = coords[1];
-          break;
-        case 'T':
-          newCoords[coordIndex++] = getSeries();
-          newCoords[coordIndex++] = coords[2];
-          break;
-      }
-    }
-
-    int planeIndex = FormatTools.positionToRaster(lengths, newCoords);
-
-    // read the image plane's pixel data
-    long planeSize = (long) FormatTools.getPlaneSize(this);
-    long planeOffset = planeSize * planeIndex;
-    long offset = planeOffset + HEADER_LENGTH + extSize;
+    long offset = getPlaneByteOffset(coords[0], coords[1], coords[2]);
     if (offset < in.length()) {
-      in.seek(HEADER_LENGTH + extSize + planeOffset);
+      in.seek(offset);
       readPlane(in, x, getSizeY() - h - y, w, h, buf);
 
       // reverse the order of the rows
@@ -237,6 +210,16 @@ public class DeltavisionReader extends FormatReader {
     }
   }
 
+  /**
+   * This flag indicates if the file is known to be truncated.
+   * If true, the dimension size information is kept
+   * as it is specified in the file header, otherwise the length
+   * of the file is used to override size information.
+   *
+   * This flag must be set before {@link #setId(String)} is called.
+   *
+   * Default is false.
+   */
   public void setTruncatedFileFlag(boolean truncatedFileFlag) {
       this.truncatedFileFlag = truncatedFileFlag;
   }
@@ -872,6 +855,14 @@ public class DeltavisionReader extends FormatReader {
     return (zSize * currentZ) + (wSize * currentW) + (tSize * currentT);
   }
 
+
+  /**
+   * Given any specific Z, W, and T of a plane, determine the absolute
+   * byte offset of the plane in the file.
+   * @param currentZ
+   * @param currentW
+   * @param currentT
+   */
   public long getPlaneByteOffset(int currentZ, int currentW, int currentT) {
       int[] newCoords = new int[4];
       int coordIndex = 0;
@@ -895,8 +886,6 @@ public class DeltavisionReader extends FormatReader {
       }
 
       int planeIndex = FormatTools.positionToRaster(lengths, newCoords);
-
-      // read the image plane's pixel data
       long planeSize = (long) FormatTools.getPlaneSize(this);
       long planeOffset = planeSize * planeIndex;
       long offset = planeOffset + HEADER_LENGTH + extSize;
