a9babbfef07ed29ecded50e717bfe01c8063f86d
Melissa Linkert <melissa@glencoesoftware.com>
2013-04-24 12:19:51 -0400

    Update version numbers to 4.4.7

--- components/scifio/src/loci/formats/UpgradeChecker.java
+++ components/scifio/src/loci/formats/UpgradeChecker.java
@@ -71,7 +71,7 @@ public class UpgradeChecker {
   // -- Constants --
 
   /** Version number of the latest stable release. */
-  public static final String STABLE_VERSION = "4.4.6";
+  public static final String STABLE_VERSION = "4.4.7";
 
   /** Location of the OME continuous integration server. */
   public static final String CI_SERVER = "http://hudson.openmicroscopy.org.uk";
