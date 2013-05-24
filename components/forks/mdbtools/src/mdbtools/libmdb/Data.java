208768f5857576b4d66d5140f39c8e77bea6050d
Melissa Linkert <melissa@glencoesoftware.com>
2013-05-24 12:57:31 -0400

    mdb: fix page swapping bug when reading column text values
    The page buffers are swapped at the beginning of the read, and are
    expected to be swapped back at the end of the read.  This was happening
    if the read was successful and the value was valid, but not if the value
    was invalid.
    
    Fixes #10942.

--- components/forks/mdbtools/src/mdbtools/libmdb/Data.java
+++ components/forks/mdbtools/src/mdbtools/libmdb/Data.java
@@ -772,6 +772,7 @@ public class Data
         if(file.mdb_read_pg(mdb, lval_pg) != fmt.pg_size)
         {
           /* Failed to read */
+      file.mdb_swap_pgbuf(mdb);
           return text;
         }
         if (memo_row != 0)
