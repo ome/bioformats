cb3c80d5f3554511f38a75fdb91cf98a0e048b1c
Blazej Pindelski <bpindelski@dundee.ac.uk>
2013-06-17 11:39:42 +0100

    Move ResourceNamer to loci.formats.

--- components/scifio/src/loci/formats/ResourceNamer.java
+++ components/scifio/src/loci/formats/ResourceNamer.java
@@ -17,7 +17,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-package loci.formats.tools;
+package loci.formats;
 
 import loci.common.Location;
 
--- components/scifio/src/loci/formats/tools/FakeGenerator.java
+++ components/scifio/src/loci/formats/tools/FakeGenerator.java
@@ -42,6 +42,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import loci.common.Location;
+import loci.formats.ResourceNamer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
