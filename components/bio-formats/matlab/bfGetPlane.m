f416b5acad185e593a9fb98ffdd41984fe92835f
Sebastien Besson <seb.besson@gmail.com>
2013-08-07 10:55:00 +0100

    Fix bfGetPlane for signed pixel types
    - Pass the correct input width variable to makeDataArray2D
    - Rename arr variable as I to avoid matrix duplication

--- components/bio-formats/matlab/bfGetPlane.m
+++ components/bio-formats/matlab/bfGetPlane.m
@@ -57,8 +57,8 @@ ip.addRequired('iPlane', @isscalar);
 % openBytes()
 ip.addOptional('x', 1, @isscalar);
 ip.addOptional('y', 1, @isscalar);
-ip.addOptional('w', r.getSizeX(), @isscalar);
-ip.addOptional('h', r.getSizeY(), @isscalar);
+ip.addOptional('width', r.getSizeX(), @isscalar);
+ip.addOptional('height', r.getSizeY(), @isscalar);
 ip.parse(r, iPlane, varargin{:});
 
 % check MATLAB version, since typecast function requires MATLAB 7.1+
@@ -73,20 +73,19 @@ bppMax = power(2, bpp * 8);
 little = r.isLittleEndian();
 
 plane = r.openBytes(iPlane - 1, ip.Results.x - 1, ip.Results.y - 1, ...
-    ip.Results.w, ip.Results.h);
+    ip.Results.width, ip.Results.height);
     
 % convert byte array to MATLAB image
 if sgn || ~canTypecast
     % can get the data directly to a matrix
-    arr = loci.common.DataTools.makeDataArray2D(plane, ...
-        bpp, fp, little, height);
+    I = loci.common.DataTools.makeDataArray2D(plane, ...
+        bpp, fp, little, ip.Results.height);
 else
     % get the data as a vector, either because makeDataArray2D
     % is not available, or we need a vector for typecast
-    arr = loci.common.DataTools.makeDataArray(plane, ...
+    I = loci.common.DataTools.makeDataArray(plane, ...
         bpp, fp, little);
 end
-%     if ~strcmp(class(I),class(arr)), I= cast(I,['u' class(arr)]); end
 
 % Java does not have explicitly unsigned data types;
 % hence, we must inform MATLAB when the data is unsigned
@@ -94,44 +93,44 @@ if ~sgn
     if canTypecast
         % TYPECAST requires at least MATLAB 7.1
         % NB: arr will always be a vector here
-        switch class(arr)
+        switch class(I)
             case 'int8'
-                arr = typecast(arr, 'uint8');
+                I = typecast(I, 'uint8');
             case 'int16'
-                arr = typecast(arr, 'uint16');
+                I = typecast(I, 'uint16');
             case 'int32'
-                arr = typecast(arr, 'uint32');
+                I = typecast(I, 'uint32');
             case 'int64'
-                arr = typecast(arr, 'uint64');
+                I = typecast(I, 'uint64');
         end
     else
         % adjust apparent negative values to actual positive ones
         % NB: arr might be either a vector or a matrix here
-        mask = arr < 0;
-        adjusted = arr(mask) + bppMax / 2;
-        switch class(arr)
+        mask = I < 0;
+        adjusted = I(mask) + bppMax / 2;
+        switch class(I)
             case 'int8'
-                arr = uint8(arr);
+                I = uint8(I);
                 adjusted = uint8(adjusted);
             case 'int16'
-                arr = uint16(arr);
+                I = uint16(I);
                 adjusted = uint16(adjusted);
             case 'int32'
-                arr = uint32(arr);
+                I = uint32(I);
                 adjusted = uint32(adjusted);
             case 'int64'
-                arr = uint64(arr);
+                I = uint64(I);
                 adjusted = uint64(adjusted);
         end
         adjusted = adjusted + bppMax / 2;
-        arr(mask) = adjusted;
+        I(mask) = adjusted;
     end
 end
 
-if isvector(arr)
+if isvector(I)
     % convert results from vector to matrix
-    shape = [ip.Results.w ip.Results.h];
-    I = reshape(arr, shape)';
+    shape = [ip.Results.width ip.Results.height];
+    I = reshape(I, shape)';
 end
 
 
