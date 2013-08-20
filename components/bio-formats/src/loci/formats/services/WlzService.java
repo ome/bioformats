717c42a4bba5c1642fe5dbb0398485cad478d3da
Bill Hill <Bill.Hill@igmm.ed.ac.uk>
2013-08-20 15:49:29 +0100

    First commit.

---
+++ components/bio-formats/src/loci/formats/services/WlzService.java
@@ -0,0 +1,151 @@
+/*!
+* \file         WlzService.java
+* \author       Bill Hill
+* \date         August 2013
+* \version      $Id$
+* \par
+* Address:
+*               MRC Human Genetics Unit,
+*               MRC Institute of Genetics and Molecular Medicine,
+*               University of Edinburgh,
+*               Western General Hospital,
+*               Edinburgh, EH4 2XU, UK.
+* \par
+* Copyright (C), [2013],
+* The University Court of the University of Edinburgh,
+* Old College, Edinburgh, UK.
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be
+* useful but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+* PURPOSE.  See the GNU General Public License for more
+* details.
+*
+* You should have received a copy of the GNU General Public
+* License along with this program; if not, write to the Free
+* Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA  02110-1301, USA.
+* \brief        Woolz service for bioformats.
+*/
+
+package loci.formats.services;
+
+import java.io.IOException;
+
+import loci.formats.FormatException;
+
+import loci.common.services.Service;
+
+public interface WlzService extends Service {
+  
+  /**
+   * Initializes the service for the given file path.
+   * @param 	file 		File path with which to initialize the service.
+   * @throws 	IOException
+   * @throws	 FormatException
+   */
+  public void open(String file, String rw)
+    throws IOException, FormatException;
+
+  /**
+   * Gets width in pixels/voxels.
+   * @return			width.
+   */
+  public int getSizeX();
+
+  /**
+   * Gets height in pixels/voxels.
+   * @return			height.
+   */
+  public int getSizeY();
+
+  /**
+   * Gets depth (number of x-y planes) in voxels.
+   * @return			depth.
+   */
+  public int getSizeZ();
+
+  /**
+   * Gets number of channels.
+   * @return			channels.
+   */
+  public int getSizeC();
+
+  /**
+   * Gets number of time samples.
+   * @return			time.
+   */
+  public int getSizeT();
+
+  /**
+   * Gets boolean for whether image is colour or not.
+   * @return			true if image is colour.
+   */
+  public boolean isRGB();
+
+  /**
+   * Sets up the service, which must have already been opened for writing.
+   * @param	orgX		x origin.
+   * @param	orgY		y origin.
+   * @param	orgZ		z origin (set to 0 for 2D).
+   * @param	pixSzX		width.
+   * @param	pixSzY		height.
+   * @param	pixSzZ		depth (number of planes, set to 1 for 2D).
+   * @param	pixSzC		number of channels.
+   * @param	pixSzT		number of time samples.
+   * @param	voxSzX		pixel/voxel width.
+   * @param	voxSzY		pixel/voxel heigth.
+   * @param 	voxSzZ		voxel deoth.
+   * @param	gType		image value type.
+   * @throws 	FormatException
+   */
+  public void setupWrite(int orgX, int orgY, int orgZ,
+  			 int pixSzX, int pixSzY, int pixSzZ,
+			 int pixSzC, int pixSzT,
+			 int voxSzX, int voxSzY, int voxSzZ,
+			 int gType)
+    throws FormatException;
+
+  /**
+   * Closes the file.
+   */
+  public void close()
+    throws IOException;
+
+  /**
+  * Reads a rectangle of bytes in an x-y plane from the opened Woolz
+  * object.
+  * @return	Buffer of bytes read.
+  * @param	no		plane coordinate (set to 0 for 2D).
+  * @param	buf		buffer for bytes.
+  * @param	x		rectangle first column.
+  * @param	y		rectangle first line.
+  * @param	a		rectangle width (in columns).
+  * @param	h		rectangle heigth (in lines).
+  */
+  public byte[] readBytes(int no, byte[] buf, int x, int y, int w, int h)
+    throws FormatException, IOException;
+
+  /**
+  * Adds a rectangle of bytes in an x-y plane to the opened Woolz object.
+  * @param	no		plane coordinate (set to 0 for 2D).
+  * @param	buf		buffer with bytes.
+  * @param	x		rectangle first column.
+  * @param	y		rectangle first line.
+  * @param	w		rectangle width (in columns).
+  * @param	h		rectangle heigth (in lines).
+  */
+  public void saveBytes(int no, byte[] buf, int x, int y, int w, int h)
+    throws FormatException, IOException;
+
+  /**
+   * Gets the image pixel type.
+   * @return			pixel type.
+   */
+  public int	 getPixelType();
+}
---
+++ components/bio-formats/src/loci/formats/services/WlzServiceImpl.java
@@ -0,0 +1,612 @@
+/*!
+* \file         WlzService.java
+* \author       Bill Hill
+* \date         August 2013
+* \version      $Id$
+* \par
+* Address:
+*               MRC Human Genetics Unit,
+*               MRC Institute of Genetics and Molecular Medicine,
+*               University of Edinburgh,
+*               Western General Hospital,
+*               Edinburgh, EH4 2XU, UK.
+* \par
+* Copyright (C), [2013],
+* The University Court of the University of Edinburgh,
+* Old College, Edinburgh, UK.
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be
+* useful but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+* PURPOSE.  See the GNU General Public License for more
+* details.
+*
+* You should have received a copy of the GNU General Public
+* License along with this program; if not, write to the Free
+* Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA  02110-1301, USA.
+* \brief        Woolz service for bioformats.
+*/
+
+package loci.formats.services;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+import loci.common.Constants;
+import loci.common.Location;
+import loci.common.services.AbstractService;
+import loci.common.services.ServiceException;
+
+import loci.formats.FormatTools;
+import loci.formats.FormatException;
+import loci.formats.MissingLibraryException;
+
+import uk.ac.mrc.hgu.Wlz.WlzException;
+import uk.ac.mrc.hgu.Wlz.WlzGreyType;
+import uk.ac.mrc.hgu.Wlz.WlzObjectType;
+import uk.ac.mrc.hgu.Wlz.WlzFileStream;
+import uk.ac.mrc.hgu.Wlz.WlzFileInputStream;
+import uk.ac.mrc.hgu.Wlz.WlzFileOutputStream;
+import uk.ac.mrc.hgu.Wlz.WlzIBox2;
+import uk.ac.mrc.hgu.Wlz.WlzIBox3;
+import uk.ac.mrc.hgu.Wlz.WlzIVertex2;
+import uk.ac.mrc.hgu.Wlz.WlzIVertex3;
+import uk.ac.mrc.hgu.Wlz.WlzDVertex2;
+import uk.ac.mrc.hgu.Wlz.WlzDVertex3;
+import uk.ac.mrc.hgu.Wlz.WlzObject;
+
+
+public class WlzServiceImpl extends AbstractService
+  implements WlzService {
+
+  // -- Constants --
+
+  public static final String NO_WLZ_MSG =
+    "Woolz is required to read and write Woolz objects.\n" +
+    "Please obtain the necessary JAR and native library files from:\n" +
+    "http://www.emouseatlas.org/emap/analysis_tools_resources/software/woolz.html.\n" +
+    "The source code for these is also available from:\n" +
+    "https://github.com/ma-tech/Woolz.";
+  
+  public static final int WLZ_SERVICE_UNKNOWN = 0;
+  public static final int WLZ_SERVICE_READ    = 1;
+  public static final int WLZ_SERVICE_WRITE   = 2;
+
+  /*
+  * Fields
+  */
+
+  private int	state = WLZ_SERVICE_UNKNOWN;
+  private boolean rgb = true;
+  private int	pixelType = FormatTools.UINT8;
+  private int   objType = WlzObjectType.WLZ_NULL;
+  /* objGType is the Woolz value type, but may be WLZ_GREY_ERROR to indicate
+   * an object with no values, just a domain. */
+  private int   objGType = WlzGreyType.WLZ_GREY_ERROR;
+  private String wlzVersion = new String("unknown");
+  private WlzIBox3 bBox = null;
+  private WlzDVertex3 voxSz = null;
+  private WlzObject wlzObj = null;
+  private WlzFileStream wlzFP = null; 
+
+  /*
+  * Default constructor.
+  */
+  public WlzServiceImpl() {
+  }
+
+  /*
+  * Service methods
+  */
+
+  public void open(String file, String rw)
+    throws FormatException, IOException {
+    try {
+      String v[] = new String[1];
+      WlzObject.WlzGetVersion(v);
+      wlzVersion = new String(v[0]);
+    }
+    catch (UnsatisfiedLinkError e) {
+      throw new MissingLibraryException(NO_WLZ_MSG, e);
+    }
+    if(rw == "r") {
+      openRead(file);
+    }
+    else if(rw == "w") {
+      openWrite(file);
+    }
+    else {
+      throw new IOException("Failed to open file " + file);
+    }
+  }
+
+  public int	getSizeX() {
+    int 	sz;
+    if(bBox == null) {
+      sz = 0;
+    }
+    else {
+      sz = bBox.xMax - bBox.xMin - 1;
+    }
+    return(sz);
+  }
+
+  public int	getSizeY() {
+    int         sz;
+    if(bBox == null) {
+      sz = 0;
+    }
+    else {
+      sz = bBox.yMax - bBox.yMin - 1;
+    }                 
+    return(sz);
+  }
+
+  public int	getSizeZ() {
+    int         sz;
+    if(bBox == null) {
+      sz = 0;
+    }
+    else {
+      sz = bBox.zMax - bBox.zMin - 1;
+    }                 
+    return(sz);
+  }
+
+  public int	getSizeC() {
+    int		sz;
+    if(objGType == WlzGreyType.WLZ_GREY_RGBA) {
+      sz = 4;
+    }
+    else {
+      sz = 1;
+    }
+    return(1);
+  }
+
+  public int	getSizeT() {
+    return(1);
+  }
+
+  public boolean isRGB() {
+    boolean 	rgb;
+    if(objGType == WlzGreyType.WLZ_GREY_RGBA) {
+      rgb = true;
+    }
+    else {
+      rgb = false;
+    }
+    return(rgb);
+  }
+
+  public int	getPixelType() {
+    int		pixType;
+    switch(objGType) {
+      case WlzGreyType.WLZ_GREY_SHORT:
+	pixelType = FormatTools.INT16;
+	break;
+      case WlzGreyType.WLZ_GREY_INT:
+	pixelType = FormatTools.INT32;
+	break;
+      case WlzGreyType.WLZ_GREY_FLOAT:
+	pixelType = FormatTools.FLOAT;
+	break;
+      case WlzGreyType.WLZ_GREY_DOUBLE:
+	pixelType = FormatTools.DOUBLE;
+	break;
+      default:
+	pixelType = FormatTools.UINT8;
+	break;
+    }
+    return(pixelType);
+  }
+
+  public void setupWrite(int orgX, int orgY, int orgZ,
+			 int pixSzX, int pixSzY, int pixSzZ,
+  			 int pixSzC, int pixSzT, 
+			 int voxSzX, int voxSzY, int voxSzZ,
+			 int gType)
+    throws FormatException {
+
+    bBox.xMin = orgX;
+    bBox.yMin = orgY;
+    bBox.zMin = orgZ;
+    bBox.xMax = orgX + pixSzX - 1;
+    bBox.yMax = orgY + pixSzY - 1;
+    bBox.zMax = orgZ + pixSzZ - 1;
+    voxSz.vtX = voxSzX;
+    voxSz.vtY = voxSzY;
+    voxSz.vtZ = voxSzZ;
+    if((bBox.xMax < bBox.xMin) ||
+       (bBox.yMax < bBox.yMin) ||
+       (bBox.zMax < bBox.zMin) ||
+       (pixSzC <= 0) ||
+       (pixSzT <= 0)) {
+      throw new FormatException("Invalid image size (" +
+                                (bBox.xMax - bBox.xMin + 1) + ", " +
+                                (bBox.yMax - bBox.yMin + 1) + ", " +
+                                (bBox.zMax - bBox.zMin + 1) + ", " +
+                                pixSzC + ", " +
+                                pixSzT + ")");
+    }
+    switch(gType) {
+      case FormatTools.UINT8:
+        objGType = WlzGreyType.WLZ_GREY_UBYTE;
+        break;
+      case FormatTools.INT16:
+        objGType = WlzGreyType.WLZ_GREY_SHORT;
+        break;
+      case FormatTools.INT32:
+        objGType = WlzGreyType.WLZ_GREY_INT;
+        break;
+      case FormatTools.FLOAT:
+        objGType = WlzGreyType.WLZ_GREY_FLOAT;
+        break;
+      case FormatTools.DOUBLE:
+        objGType = WlzGreyType.WLZ_GREY_DOUBLE;
+        break;
+      default:
+        throw new FormatException("Invalid image value type");
+    }
+    if(bBox.zMax == bBox.zMin) {
+      objType = WlzObjectType.WLZ_2D_DOMAINOBJ;
+    }
+    else {
+      objType = WlzObjectType.WLZ_3D_DOMAINOBJ;
+    }
+    try {
+      wlzObj = WlzObject.WlzMakeEmpty();
+    }
+    catch (WlzException e) {
+      throw new FormatException("Failed to create Woolz object", e);
+    }
+  }
+
+  public void close()
+    throws IOException {
+    if(wlzObj != null) {
+      if(state == WLZ_SERVICE_WRITE) {
+        try {
+	  if(objType == WlzObjectType.WLZ_3D_DOMAINOBJ) {
+	    WlzObject.WlzSetVoxelSize(wlzObj, voxSz.vtX, voxSz.vtY, voxSz.vtZ);
+	  }
+	  WlzObject.WlzWriteObj(wlzFP, wlzObj);
+	}
+	catch(WlzException e) {
+	  throw new IOException("Failed to write to Woolz object (" + e + ")");
+	}
+      }
+      // Object is freed by garbage collection.
+      wlzObj = null;
+    }
+    if(wlzFP != null) {
+      wlzFP.close();
+      wlzFP = null;
+    }
+  }
+
+  public byte[] readBytes(int no, byte[] buf, int x, int y, int w, int h)
+    throws FormatException, IOException {
+    if((wlzObj == null) || (state != WLZ_SERVICE_READ)) {
+      throw new FormatException("Uninitialised Woolz service");
+    }
+    else {
+      try {
+	switch(objType)
+	{
+	  case WlzObjectType.WLZ_2D_DOMAINOBJ:
+	    buf = readBytes2DDomObj(buf, x, y, w, h);
+	    break;
+	  case WlzObjectType.WLZ_3D_DOMAINOBJ:
+	    buf = readBytes3DDomObj(buf, x, y, no, w, h);
+	    break;
+	  default:
+	    throw new FormatException("Unsupported Woolz object type");
+	}
+      }
+      catch (WlzException e) {
+	throw new FormatException(
+	    "Failed to copy bytes from Woolz object (" + e + ")");
+      }
+    }
+    return(buf);
+  }
+
+  public void saveBytes(int no, byte[] buf, int x, int y, int w, int h)
+    throws FormatException, IOException
+  {
+    if(state == WLZ_SERVICE_WRITE) {
+      WlzIVertex3 og = new WlzIVertex3(x + bBox.xMin, y + bBox.yMin,
+				       no + bBox.zMin);
+      WlzIVertex2 sz = new WlzIVertex2(w, h);
+      try {
+	wlzObj = WlzObject.WlzBuildObj3B(wlzObj, og, sz, objGType,
+					 buf.length, buf);
+      }
+      catch (WlzException e) {
+	throw new FormatException("Failed save bytes to Woolz object", e);
+      }
+    }
+  }
+
+
+  /*
+  * Helper methods.
+  */
+ 
+  private void openRead(String file)
+    throws FormatException, IOException {
+    state = WLZ_SERVICE_READ;
+    try {
+      bBox = new WlzIBox3();
+      voxSz = new WlzDVertex3(1.0, 1.0, 1.0);
+      wlzFP = new WlzFileInputStream(file);
+      wlzObj = WlzObject.WlzReadObj(wlzFP);
+    }
+    catch (WlzException e) {
+      throw new IOException("Failed to read Woolz object (" +
+                            e + ")");
+    }
+    try {
+      objType = WlzObject.WlzGetObjectType(wlzObj);
+    }
+    catch (WlzException e) {
+      throw new FormatException("Unable to find Woolz object type (" +
+                                e + ")");
+    }
+    try {
+      if(WlzObject.WlzObjectValuesIsNull(wlzObj) != 0) {
+	/* Here we use WLZ_GREY_ERROR to indicate that the object has no
+	 * values not an error. */
+	objGType = WlzGreyType.WLZ_GREY_ERROR;
+      }
+      else {
+	objGType = WlzObject.WlzGreyTypeFromObj(wlzObj);
+      }
+    }
+    catch (WlzException e) {
+      throw new FormatException(
+	  "Unable to determine Woolz object value type (" +
+	  e + ")");
+    }
+    switch(objGType) {
+      case WlzGreyType.WLZ_GREY_UBYTE:
+	break;
+      case WlzGreyType.WLZ_GREY_SHORT:
+	break;
+      case WlzGreyType.WLZ_GREY_INT:
+	break;
+      case WlzGreyType.WLZ_GREY_FLOAT:
+	break;
+      case WlzGreyType.WLZ_GREY_DOUBLE:
+	break;
+      case WlzGreyType.WLZ_GREY_RGBA:
+	break;
+      case WlzGreyType.WLZ_GREY_ERROR:
+	break;
+      default:
+	throw new FormatException(
+	    "Inappropriate Woolz object value type (type = " + objGType + ")");
+    }
+  }
+
+  private void openWrite(String file)
+    throws FormatException, IOException {
+    state = WLZ_SERVICE_WRITE;
+    try {
+      wlzFP = new WlzFileOutputStream(file);
+    }
+    catch (IOException e) {
+      throw new IOException("Failed to open " + file + "for writing.");
+    }
+  }
+
+  private byte[] readBytes2DDomObj(byte[] buf, int x, int y, int w, int h)
+    throws WlzException {
+    WlzIVertex2 og = new WlzIVertex2(x + bBox.xMin, y + bBox.yMin);
+    WlzIVertex2 sz = new WlzIVertex2(w, h);
+    WlzIVertex2 dstSz[] = new WlzIVertex2[1];
+    dstSz[0] = null;
+    switch(objGType) {
+      case WlzGreyType.WLZ_GREY_UBYTE:
+	{
+	  byte dstDat[][][] = new byte[1][][];
+	  WlzObject.WlzToUArray2D(dstSz, dstDat, wlzObj, og, sz, 0);
+	  for(int idY = 0; idY < h; ++idY) {
+	    int idYW = idY * w;
+	    for(int idX = 0; idX < w; ++idX) {
+	      buf[idYW + idX] = dstDat[0][idY][idX];
+	    }
+	  }
+	}
+	break;
+      case WlzGreyType.WLZ_GREY_SHORT:
+	{
+	  short m = 0xff;
+	  short dstDat[][][] = new short[1][][];
+	  WlzObject.WlzToSArray2D(dstSz, dstDat, wlzObj, og, sz, 0);
+	  for(int idY = 0; idY < h; ++idY) {
+	    int idYW = idY * w;
+	    for(int idX = 0; idX < w; ++idX) {
+	      short p = dstDat[0][idY][idX];
+	      buf[2 * (idYW + idX)]     = (byte )((p >>> 8) & m);
+	      buf[2 * (idYW + idX) + 1] = (byte )(p & m);
+	    }
+	  }
+	}
+	break;
+      case WlzGreyType.WLZ_GREY_INT:
+	{
+	  int m = 0xff;
+	  int dstDat[][][] = new int[1][][];
+	  WlzObject.WlzToIArray2D(dstSz, dstDat, wlzObj, og, sz, 0);
+	  for(int idY = 0; idY < h; ++idY) {
+	    int idYW = idY * w;
+	    for(int idX = 0; idX < w; ++idX) {
+	      int p = dstDat[0][idY][idX];
+	      buf[idYW + (4 * idX)]     = (byte )((p >> 24) & m);
+	      buf[idYW + (4 * idX) + 1] = (byte )((p >> 16) & m);
+	      buf[idYW + (4 * idX) + 2] = (byte )((p >> 8) & m);
+	      buf[idYW + (4 * idX) + 3] = (byte )(p & m);
+	    }
+	  }
+	}
+	break;
+      case WlzGreyType.WLZ_GREY_RGBA:
+	{
+	  int m = 0xff;
+	  int cOff = h * w;
+	  int dstDat[][][] = new int[1][][];
+	  WlzObject.WlzToRArray2D(dstSz, dstDat, wlzObj, og, sz, 0);
+	  for(int idY = 0; idY < h; ++idY) {
+	    int idYW = idY * w;
+	    for(int idX = 0; idX < w; ++idX) {
+	      int idYWX = idYW + idX;
+	      int p = dstDat[0][idY][idX];
+	      buf[idYWX]              = (byte )((p >>  0) & m);
+	      buf[idYWX + cOff]       = (byte )((p >>  8) & m);
+	      buf[idYWX + (2 * cOff)] = (byte )((p >> 16) & m);
+	      buf[idYWX + (3 * cOff)] = (byte )((p >> 24) & m);
+	    }
+	  }
+	}
+	break;
+      case WlzGreyType.WLZ_GREY_ERROR: /* Indicates no values. */
+        {
+	  int	w8 = (w + 7) / 8;
+	  byte dstDat[][][] = new byte[1][][];
+	  WlzObject.WlzToBArray2D(dstSz, dstDat, wlzObj, og, sz, 0);
+	  for(int idY = 0; idY < h; ++idY) {
+	    int idYW = idY * w;
+	    int idYW8 = idY * w8;
+	    for(int idX = 0; idX < w; ++idX) {
+	      byte p = dstDat[0][idY][idX / 8];
+	      byte b = (byte )(p & (0x01 << (idX % 8)));
+	      if(b == 0){
+	        buf[idYW + idX] = (byte )0x00;
+	      }
+	      else {
+	        buf[idYW + idX] = (byte )0xff;
+	      }
+	    }
+	  }
+	}
+	break;
+      default:
+        throw new WlzException("Unsupported pixel type");
+    }
+    return(buf);
+  }
+
+  private byte[] readBytes3DDomObj(byte[] buf, int x, int y, int z,
+                                      int w, int h)
+    throws WlzException {
+    WlzIVertex3 og = new WlzIVertex3(x + bBox.xMin, y + bBox.yMin,
+				     z + bBox.zMin);
+    WlzIVertex3 sz = new WlzIVertex3(w, h, 1);
+    WlzIVertex3 dstSz[] = new WlzIVertex3[1];
+    dstSz[0] = null;
+    switch(objGType) {
+      case WlzGreyType.WLZ_GREY_UBYTE:
+	{
+	  byte dstDat[][][][] = new byte[1][][][];
+	  WlzObject.WlzToUArray3D(dstSz, dstDat, wlzObj, og, sz, 0);
+	  for(int idY = 0; idY < h; ++idY) {
+	    int idYW = idY * w;
+	    for(int idX = 0; idX < w; ++idX) {
+	      buf[idYW + idX] = dstDat[0][0][idY][idX];
+	    }
+	  }
+	}
+	break;
+      case WlzGreyType.WLZ_GREY_SHORT:
+	{
+	  short m = 0xff;
+	  short dstDat[][][][] = new short[1][][][];
+	  WlzObject.WlzToSArray3D(dstSz, dstDat, wlzObj, og, sz, 0);
+	  for(int idY = 0; idY < h; ++idY) {
+	    int idYW = idY * w;
+	    for(int idX = 0; idX < w; ++idX) {
+	      short p = dstDat[0][0][idY][idX];
+	      buf[2 * (idYW + idX)]     = (byte )((p >>> 8) & m);
+	      buf[2 * (idYW + idX) + 1] = (byte )(p & m);
+	    }
+	  }
+	}
+	break;
+      case WlzGreyType.WLZ_GREY_INT:
+	{
+	  int m = 0xff;
+	  int dstDat[][][][] = new int[1][][][];
+	  WlzObject.WlzToIArray3D(dstSz, dstDat, wlzObj, og, sz, 0);
+	  for(int idY = 0; idY < h; ++idY) {
+	    int idYW = idY * w;
+	    for(int idX = 0; idX < w; ++idX) {
+	      int p = dstDat[0][0][idY][idX];
+	      buf[idYW + (4 * idX)]     = (byte )((p >> 24) & m);
+	      buf[idYW + (4 * idX) + 1] = (byte )((p >> 16) & m);
+	      buf[idYW + (4 * idX) + 2] = (byte )((p >> 8) & m);
+	      buf[idYW + (4 * idX) + 3] = (byte )(p & m);
+	    }
+	  }
+	}
+	break;
+      case WlzGreyType.WLZ_GREY_RGBA:
+	{
+	  int m = 0xff;
+	  int cOff = h * w;
+	  int dstDat[][][][] = new int[1][][][];
+	  WlzObject.WlzToRArray3D(dstSz, dstDat, wlzObj, og, sz, 0);
+	  for(int idY = 0; idY < h; ++idY) {
+	    int idYW = idY * w;
+	    for(int idX = 0; idX < w; ++idX) {
+	      int idYWX = idYW + idX;
+	      int p = dstDat[0][0][idY][idX];
+	      buf[idYWX]              = (byte )((p >>  0) & m);
+	      buf[idYWX + cOff]       = (byte )((p >>  8) & m);
+	      buf[idYWX + (2 * cOff)] = (byte )((p >> 16) & m);
+	      buf[idYWX + (3 * cOff)] = (byte )((p >> 24) & m);
+	    }
+	  }
+	}
+	break;
+      case WlzGreyType.WLZ_GREY_ERROR: /* Indicates no values. */
+        {
+	  int	w8 = (w + 7) / 8;
+	  byte dstDat[][][][] = new byte[1][][][];
+	  WlzObject.WlzToBArray3D(dstSz, dstDat, wlzObj, og, sz, 0);
+	  for(int idY = 0; idY < h; ++idY) {
+	    int idYW = idY * w;
+	    int idYW8 = idY * w8;
+	    for(int idX = 0; idX < w; ++idX) {
+	      byte p = dstDat[0][0][idY][idX / 8];
+	      byte b = (byte )(p & (0x01 << (idX % 8)));
+	      if(b == 0){
+	        buf[idYW + idX] = (byte )0x00;
+	      }
+	      else {
+	        buf[idYW + idX] = (byte )0xff;
+	      }
+	    }
+	  }
+	}
+	break;
+      default:
+        throw new WlzException("Unsupported pixel type");
+    }
+    return(buf);
+  }
+}
+
