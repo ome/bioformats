ed372c08d77cd462b80fd25cf562c7ce35d1760a
Roger Leigh <r.leigh@dundee.ac.uk>
2013-09-06 16:43:09 +0100

    cpp: Add gtest support to test/color

--- cpp/test/color.cpp
+++ cpp/test/color.cpp
@@ -1,122 +1,212 @@
 #include <ome/xml/model/primitives/Color.h>
 
-#include <cassert>
 #include <sstream>
-#include <iostream>
 #include <stdexcept>
+
+#include <gtest/gtest.h>
 
 using ome::xml::model::primitives::Color;
 
-void
-verify (Color::component_type r,
-        Color::component_type g,
-        Color::component_type b,
-        Color::component_type a,
-        Color::composed_type  uval,
-        Color::signed_type    sval,
-        std::string const&    str)
-{
-  Color c1(r, g, b, a);
-  assert(c1 == uval);
-  assert(uval == c1);
-  assert(c1 == sval);
-  assert(sval == c1);
-  assert(c1.getValue() == sval);
-
-  Color::composed_type ct = c1;
-  assert(ct == uval);
-  Color::signed_type st = c1;
-  assert(st == sval);
-
-  assert(c1.getRed() == r);
-  assert(c1.getGreen() == g);
-  assert(c1.getBlue() == b);
-  assert(c1.getAlpha() == a);
-
-  Color c2(str);
-  assert(c1 == c2);
-
-  Color c3(uval);
-  assert(c1 == c3);
-
-  Color c4(sval);
-  assert(c1 == c4);
-
-  std::ostringstream os;
-  os << c1;
-  assert(os.str() == str);
-}
-
-int
-main ()
+TEST(ColorTest, Create1)
 {
   Color c1(0xFE438B12U);
-  assert(c1.getRed() == 0xFE);
-  assert(c1.getGreen() == 0x43);
-  assert(c1.getBlue() == 0x8B);
-  assert(c1.getAlpha() == 0x12);
-  assert(c1 == 0xFE438B12U);
-  assert(0xFE438B12U == c1);
 
+  EXPECT_EQ(c1.getRed(),   0xFE);
+  EXPECT_EQ(c1.getGreen(), 0x43);
+  EXPECT_EQ(c1.getBlue(),  0x8B);
+  EXPECT_EQ(c1.getAlpha(), 0x12);
+  EXPECT_EQ(c1, 0xFE438B12U);
+  EXPECT_EQ(0xFE438B12U, c1);
+}
+
+TEST(ColorTest, Create2)
+{
+  Color c1(0xFE438B12U);
   Color c2(43,23,53,1);
-  assert(c1 != c2);
 
-  try
-    {
-      Color c3("4265839378");
-      assert(0);
-    }
-  catch (const std::invalid_argument& e)
-    {
-      std::cout << "Caught expected exception: " << e.what() << std::endl;
-    }
+  EXPECT_NE(c1, c2);
+}
 
-  try
-    {
-      Color c3("7453894265839378", false);
-      assert(0);
-    }
-  catch (const std::invalid_argument& e)
-    {
-      std::cout << "Caught expected exception: " << e.what() << std::endl;
-    }
-
+TEST(ColorTest, Create3)
+{
+  Color c1(0xFE438B12U);
+  Color c2(43,23,53,1);
   Color c3("4265839378", false);
-  assert(c3 == 0xFE438B12U);
-  assert(c1 == c3);
-  assert(c2 != c3);
 
-  try
-    {
-      Color c4("invalid");
-      assert(0);
-    }
-  catch (const std::invalid_argument& e)
-    {
-      std::cout << "Caught expected exception: " << e.what() << std::endl;
-    }
+  EXPECT_EQ(c3, 0xFE438B12U);
+  EXPECT_EQ(c1, c3);
+  EXPECT_NE(c2, c3);
+}
 
+TEST(ColorTest, CreateFail1)
+{
+  ASSERT_THROW(Color("4265839378"), std::invalid_argument);
+}
+
+TEST(ColorTest, CreateFail2)
+{
+  ASSERT_THROW(Color("7453894265839378"), std::invalid_argument);
+}
+
+TEST(ColorTest, CreateFail3)
+{
+  ASSERT_THROW(Color("invalid"), std::invalid_argument);
+}
+
+TEST(ColorTest, SetComponents)
+{
   Color c5(0x01020304U);
+
   c5.setRed(0x20);
   c5.setGreen(0x21);
   c5.setBlue(0x22);
   c5.setAlpha(0x23);
-  assert(c5 == 0x20212223U);
 
-  Color black(0, 0, 0);
-  Color black2;
-  assert(black == black2);
-
-  verify(255,   0,   0, 255, 0xFF0000FFU,  -16776961,  "-16776961"); // Red
-  verify(  0, 255,   0, 255, 0x00FF00FFU,   16711935,   "16711935"); // Green
-  verify(  0,   0, 255, 255, 0x0000FFFFU,      65535,      "65535"); // Blue
-  verify(  0, 255, 255, 255, 0x00FFFFFFU,   16777215,   "16777215"); // Cyan
-  verify(255,   0, 255, 255, 0xFF00FFFFU,  -16711681,  "-16711681"); // Magenta
-  verify(255, 255,   0, 255, 0xFFFF00FFU,     -65281,     "-65281"); // Yellow
-  verify(  0,   0,   0, 255, 0x000000FFU,        255,        "255"); // Black
-  verify(255, 255, 255, 255, 0xFFFFFFFFU,         -1,         "-1"); // White
-  verify(  0,   0,   0, 127, 0x0000007FU,        127,        "127"); // Transparent black
-  verify(127, 127, 127, 127, 0x7F7F7F7FU, 2139062143, "2139062143"); // Grey
-
-  std::cout << "OK" << std::endl;
+  EXPECT_EQ(c5, 0x20212223U);
 }
+
+TEST(ColorTest, DefaultBlack)
+{
+  Color black(0, 0, 0, 255);
+  Color black2;
+
+  EXPECT_EQ(black, black2);
+}
+
+class ColorTestParameters
+{
+public:
+  Color::component_type r;
+  Color::component_type g;
+  Color::component_type b;
+  Color::component_type a;
+  Color::composed_type  uval;
+  Color::signed_type    sval;
+  std::string           str;
+
+  ColorTestParameters(Color::component_type r,
+                      Color::component_type g,
+                      Color::component_type b,
+                      Color::component_type a,
+                      Color::composed_type  uval,
+                      Color::signed_type    sval,
+                      std::string const&    str):
+    r(r),
+    g(g),
+    b(b),
+    a(a),
+    uval(uval),
+    sval(sval),
+    str(str)
+  {}
+};
+
+class ColorTest : public ::testing::TestWithParam<ColorTestParameters>
+{
+public:
+  Color c;
+
+  virtual void SetUp()
+  {
+    const ColorTestParameters params = GetParam();
+    c = Color(params.r, params.g, params.b, params.a);
+  }
+};
+
+TEST_P(ColorTest, CompareUnsigned)
+{
+  const ColorTestParameters params = GetParam();
+
+  ASSERT_EQ(c, params.uval);
+  ASSERT_EQ(params.uval, c);
+}
+
+TEST_P(ColorTest, CompareSigned)
+{
+  const ColorTestParameters params = GetParam();
+
+  ASSERT_EQ(c, params.sval);
+  ASSERT_EQ(params.sval, c);
+ }
+
+TEST_P(ColorTest, GetValue)
+{
+  const ColorTestParameters params = GetParam();
+
+  ASSERT_EQ(c.getValue(), params.sval);
+}
+
+TEST_P(ColorTest, ComposedCast)
+{
+  const ColorTestParameters params = GetParam();
+
+  Color::composed_type ct = c;
+  ASSERT_EQ(ct, params.uval);
+}
+
+TEST_P(ColorTest, SignedCast)
+{
+  const ColorTestParameters params = GetParam();
+
+  Color::signed_type st = c;
+  ASSERT_EQ(st, params.sval);
+ }
+
+TEST_P(ColorTest, GetComponents)
+{
+  const ColorTestParameters params = GetParam();
+
+  ASSERT_EQ(c.getRed(), params.r);
+  ASSERT_EQ(c.getGreen(), params.g);
+  ASSERT_EQ(c.getBlue(), params.b);
+  ASSERT_EQ(c.getAlpha(), params.a);
+}
+
+TEST_P(ColorTest, ConstructString)
+{
+  const ColorTestParameters params = GetParam();
+
+  Color c2(params.str);
+  ASSERT_EQ(c, c2);
+}
+
+TEST_P(ColorTest, ConstructUnsigned)
+{
+  const ColorTestParameters params = GetParam();
+
+  Color c3(params.uval);
+  ASSERT_EQ(c, c3);
+}
+
+TEST_P(ColorTest, ConstructSigned)
+{
+  const ColorTestParameters params = GetParam();
+
+  Color c4(params.sval);
+  ASSERT_EQ(c, c4);
+}
+
+TEST_P(ColorTest, StreamOutput)
+{
+  const ColorTestParameters params = GetParam();
+
+  std::ostringstream os;
+  os << c;
+  ASSERT_EQ(os.str(), params.str);
+}
+
+ColorTestParameters params[] =
+  {
+    ColorTestParameters(255,   0,   0, 255, 0xFF0000FFU,  -16776961,  "-16776961"), // Red
+    ColorTestParameters(  0, 255,   0, 255, 0x00FF00FFU,   16711935,   "16711935"), // Green
+    ColorTestParameters(  0,   0, 255, 255, 0x0000FFFFU,      65535,      "65535"), // Blue
+    ColorTestParameters(  0, 255, 255, 255, 0x00FFFFFFU,   16777215,   "16777215"), // Cyan
+    ColorTestParameters(255,   0, 255, 255, 0xFF00FFFFU,  -16711681,  "-16711681"), // Magenta
+    ColorTestParameters(255, 255,   0, 255, 0xFFFF00FFU,     -65281,     "-65281"), // Yellow
+    ColorTestParameters(  0,   0,   0, 255, 0x000000FFU,        255,        "255"), // Black
+    ColorTestParameters(255, 255, 255, 255, 0xFFFFFFFFU,         -1,         "-1"), // White
+    ColorTestParameters(  0,   0,   0, 127, 0x0000007FU,        127,        "127"), // Transparent black
+    ColorTestParameters(127, 127, 127, 127, 0x7F7F7F7FU, 2139062143, "2139062143"), // Grey
+  };
+
+INSTANTIATE_TEST_CASE_P(ColorVariants, ColorTest, ::testing::ValuesIn(params));
