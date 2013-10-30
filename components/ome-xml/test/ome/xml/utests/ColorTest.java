5a987f3139ab4fcfc42a680d8a1d93dfaa2d9e13
Roger Leigh <r.leigh@dundee.ac.uk>
2013-10-30 15:54:12 +0000

    ome-xml: Java Color class uses float values internally

--- components/ome-xml/test/ome/xml/utests/ColorTest.java
+++ components/ome-xml/test/ome/xml/utests/ColorTest.java
@@ -54,62 +54,195 @@ public class ColorTest {
 
   @Test
   public void testBlue() {
-    Color blue = new Color(0, 0, 255, 255);
-    assertEquals(blue.getValue().intValue(), 65535);
-    assertEquals(blue.getRed(), 0);
-    assertEquals(blue.getGreen(), 0);
-    assertEquals(blue.getBlue(), 255);
-    assertEquals(blue.getAlpha(), 255);
+    Color blue = new Color(0.0f, 0.0f, 1.0f, 1.0f);
+    assertEquals(blue.getRed(), 0.0f);
+    assertEquals(blue.getGreen(), 0.0f);
+    assertEquals(blue.getBlue(), 1.0f);
+    assertEquals(blue.getAlpha(), 1.0f);
   }
 
   @Test
   public void testRed() {
-    Color red = new Color(255, 0, 0, 255);
-    assertEquals(red.getValue().intValue(), -16776961);
-    assertEquals(red.getRed(), 255);
-    assertEquals(red.getGreen(), 0);
-    assertEquals(red.getBlue(), 0);
-    assertEquals(red.getAlpha(), 255);
+    Color red = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+    assertEquals(red.getRed(), 1.0f);
+    assertEquals(red.getGreen(), 0.0f);
+    assertEquals(red.getBlue(), 0.0f);
+    assertEquals(red.getAlpha(), 1.0f);
   }
 
   @Test
   public void testGreen() {
-    Color green = new Color(0, 255, 0, 255);
-    assertEquals(green.getValue().intValue(), 16711935);
-    assertEquals(green.getRed(), 0);
-    assertEquals(green.getGreen(), 255);
-    assertEquals(green.getBlue(), 0);
-    assertEquals(green.getAlpha(), 255);
+    Color green = new Color(0.0f, 1.0f, 0.0f, 1.0f);
+    assertEquals(green.getRed(), 0.0f);
+    assertEquals(green.getGreen(), 1.0f);
+    assertEquals(green.getBlue(), 0.0f);
+    assertEquals(green.getAlpha(), 1.0f);
   }
 
   @Test
   public void testCyan() {
-    Color cyan = new Color(0, 255, 255, 255);
-    assertEquals(cyan.getValue().intValue(), 16777215);
-    assertEquals(cyan.getRed(), 0);
-    assertEquals(cyan.getGreen(), 255);
-    assertEquals(cyan.getBlue(), 255);
-    assertEquals(cyan.getAlpha(), 255);
+    Color cyan = new Color(0.0f, 1.0f, 1.0f, 1.0f);
+    assertEquals(cyan.getRed(), 0.0f);
+    assertEquals(cyan.getGreen(), 1.0f);
+    assertEquals(cyan.getBlue(), 1.0f);
+    assertEquals(cyan.getAlpha(), 1.0f);
   }
 
   @Test
   public void testMagenta() {
-    Color magenta = new Color(255, 0, 255, 255);
-    assertEquals(magenta.getValue().intValue(), -16711681);
-    assertEquals(magenta.getRed(), 255);
-    assertEquals(magenta.getGreen(), 0);
-    assertEquals(magenta.getBlue(), 255);
-    assertEquals(magenta.getAlpha(), 255);
+    Color magenta = new Color(1.0f, 0.0f, 1.0f, 1.0f);
+    assertEquals(magenta.getRed(), 1.0f);
+    assertEquals(magenta.getGreen(), 0.0f);
+    assertEquals(magenta.getBlue(), 1.0f);
+    assertEquals(magenta.getAlpha(), 1.0f);
   }
 
   @Test
   public void testYellow() {
+    Color yellow = new Color(1.0f, 1.0f, 0.0f, 1.0f);
+    assertEquals(yellow.getRed(), 1.0f);
+    assertEquals(yellow.getGreen(), 1.0f);
+    assertEquals(yellow.getBlue(), 0.0f);
+    assertEquals(yellow.getAlpha(), 1.0f);
+  }
+
+  @Test
+  public void testBlue8Bit() {
+    Color blue = new Color(0, 0, 255, 255);
+    assertEquals(blue.getRed8Bit(), 0);
+    assertEquals(blue.getGreen8Bit(), 0);
+    assertEquals(blue.getBlue8Bit(), 255);
+    assertEquals(blue.getAlpha8Bit(), 255);
+  }
+
+  @Test
+  public void testRed8Bit() {
+    Color red = new Color(255, 0, 0, 255);
+    assertEquals(red.getRed8Bit(), 255);
+    assertEquals(red.getGreen8Bit(), 0);
+    assertEquals(red.getBlue8Bit(), 0);
+    assertEquals(red.getAlpha8Bit(), 255);
+  }
+
+  @Test
+  public void testGreen8Bit() {
+    Color green = new Color(0, 255, 0, 255);
+    assertEquals(green.getRed8Bit(), 0);
+    assertEquals(green.getGreen8Bit(), 255);
+    assertEquals(green.getBlue8Bit(), 0);
+    assertEquals(green.getAlpha8Bit(), 255);
+  }
+
+  @Test
+  public void testCyan8Bit() {
+    Color cyan = new Color(0, 255, 255, 255);
+    assertEquals(cyan.getRed8Bit(), 0);
+    assertEquals(cyan.getGreen8Bit(), 255);
+    assertEquals(cyan.getBlue8Bit(), 255);
+    assertEquals(cyan.getAlpha8Bit(), 255);
+  }
+
+  @Test
+  public void testMagenta8Bit() {
+    Color magenta = new Color(255, 0, 255, 255);
+    assertEquals(magenta.getRed8Bit(), 255);
+    assertEquals(magenta.getGreen8Bit(), 0);
+    assertEquals(magenta.getBlue8Bit(), 255);
+    assertEquals(magenta.getAlpha8Bit(), 255);
+  }
+
+  @Test
+  public void testYellow8Bit() {
     Color yellow = new Color(255, 255, 0, 255);
-    assertEquals(yellow.getValue().intValue(), -65281);
-    assertEquals(yellow.getRed(), 255);
-    assertEquals(yellow.getGreen(), 255);
-    assertEquals(yellow.getBlue(), 0);
-    assertEquals(yellow.getAlpha(), 255);
+    assertEquals(yellow.getRed8Bit(), 255);
+    assertEquals(yellow.getGreen8Bit(), 255);
+    assertEquals(yellow.getBlue8Bit(), 0);
+    assertEquals(yellow.getAlpha8Bit(), 255);
+  }
+
+  @Test
+  public void testBlue16Bit() {
+    Color blue = new Color(0, 0, 255, 255);
+    assertEquals(blue.getRed16Bit(), 0);
+    assertEquals(blue.getGreen16Bit(), 0);
+    assertEquals(blue.getBlue16Bit(), 65535);
+    assertEquals(blue.getAlpha16Bit(), 65535);
+  }
+
+  @Test
+  public void testRed16Bit() {
+    Color red = new Color(255, 0, 0, 255);
+    assertEquals(red.getRed16Bit(), 65535);
+    assertEquals(red.getGreen16Bit(), 0);
+    assertEquals(red.getBlue16Bit(), 0);
+    assertEquals(red.getAlpha16Bit(), 65535);
+  }
+
+  @Test
+  public void testGreen16Bit() {
+    Color green = new Color(0, 255, 0, 255);
+    assertEquals(green.getRed16Bit(), 0);
+    assertEquals(green.getGreen16Bit(), 65535);
+    assertEquals(green.getBlue16Bit(), 0);
+    assertEquals(green.getAlpha16Bit(), 65535);
+  }
+
+  @Test
+  public void testCyan16Bit() {
+    Color cyan = new Color(0, 255, 255, 255);
+    assertEquals(cyan.getRed16Bit(), 0);
+    assertEquals(cyan.getGreen16Bit(), 65535);
+    assertEquals(cyan.getBlue16Bit(), 65535);
+    assertEquals(cyan.getAlpha16Bit(), 65535);
+  }
+
+  @Test
+  public void testMagenta16Bit() {
+    Color magenta = new Color(255, 0, 255, 255);
+    assertEquals(magenta.getRed16Bit(), 65535);
+    assertEquals(magenta.getGreen16Bit(), 0);
+    assertEquals(magenta.getBlue16Bit(), 65535);
+    assertEquals(magenta.getAlpha16Bit(), 65535);
+  }
+
+  @Test
+  public void testYellow16Bit() {
+    Color yellow = new Color(255, 255, 0, 255);
+    assertEquals(yellow.getRed16Bit(), 65535);
+    assertEquals(yellow.getGreen16Bit(), 65535);
+    assertEquals(yellow.getBlue16Bit(), 0);
+    assertEquals(yellow.getAlpha16Bit(), 65535);
+  }
+
+  // Test deserialisation.
+  @Test
+  public void testString1() {
+    Color test1 = Color.valueOf("0.0 1.0 0.25 0.5");
+    assertEquals(test1.getRed(), 0.0f);
+    assertEquals(test1.getGreen(), 1.0f);
+    assertEquals(test1.getBlue(), 0.25f);
+    assertEquals(test1.getAlpha(), 0.5f);
+  }
+
+  // Test range clamp.
+  @Test
+  public void testString2() {
+    Color test2 = Color.valueOf("12.4 -431.0 0.75 0.5");
+    assertEquals(test2.getRed(), 1.0f);
+    assertEquals(test2.getGreen(), 0.0f);
+    assertEquals(test2.getBlue(), 0.75f);
+    assertEquals(test2.getAlpha(), 0.5f);
+  }
+
+  // Test fail.
+  @Test
+  public void testString3() {
+    Color test2 = Color.valueOf("0.5 0.5 foo bar baz");
+    System.err.println("RGBA=" + test2.getRed() + " " + test2.getGreen() + " " + test2.getBlue() + " " + test2.getAlpha());
+    assertEquals(test2.getRed(), 0.5f);
+    assertEquals(test2.getGreen(), 0.5f);
+    assertEquals(test2.getBlue(), 1.0f);
+    assertEquals(test2.getAlpha(), 1.0f);
   }
 
 }
