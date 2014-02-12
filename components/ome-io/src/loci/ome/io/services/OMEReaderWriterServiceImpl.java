4b1fd8d5c73e982c0660d4f8535dbba2ba125c98
jmoore <josh@glencoesoftware.com>
2014-02-12 16:27:33 +0100

    ome-plugins: remove references to components
    Conflicts:
    	ant/toplevel.xml
    	scan-deps.pl

--- components/ome-io/src/loci/ome/io/services/OMEReaderWriterServiceImpl.java
+++ components/ome-io/src/loci/ome/io/services/OMEReaderWriterServiceImpl.java
@@ -9,15 +9,15 @@
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 2 of the 
+ * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public 
+ *
+ * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
  * #L%
@@ -28,11 +28,7 @@ package loci.ome.io.services;
 import loci.common.services.AbstractService;
 import loci.formats.IFormatReader;
 import loci.formats.IFormatWriter;
-import loci.ome.io.OMEReader;
-import loci.ome.io.OMEWriter;
 import loci.ome.io.OmeroReader;
-
-import org.openmicroscopy.ds.dto.Image;
 
 /**
  * <dl><dt><b>Source code:</b></dt>
@@ -47,11 +43,8 @@ public class OMEReaderWriterServiceImpl extends AbstractService
 
   public OMEReaderWriterServiceImpl() {
     // Just being thorough with these dependencies.
-    checkClassDependency(OMEReader.class);
-    checkClassDependency(OMEWriter.class);
     checkClassDependency(OmeroReader.class);
     checkClassDependency(omero.model.Image.class);
-    checkClassDependency(Image.class);
   }
 
   /* (non-Javadoc)
@@ -66,20 +59,6 @@ public class OMEReaderWriterServiceImpl extends AbstractService
    */
   public IFormatWriter newOMEROWriter() {
     throw new IllegalArgumentException("Unavailable OMERO writer.");
-  }
-
-  /* (non-Javadoc)
-   * @see loci.formats.OMEReaderWriterService#newOMEReader()
-   */
-  public IFormatReader newOMEReader() {
-    return new OMEReader();
-  }
-
-  /* (non-Javadoc)
-   * @see loci.formats.OMEReaderWriterService#newOMEWriter()
-   */
-  public IFormatWriter newOMEWriter() {
-    return new OMEWriter();
   }
 
 }
