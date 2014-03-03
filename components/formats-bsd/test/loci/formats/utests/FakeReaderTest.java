e170caec43789c9311357a91ae365622512b5867
Curtis Rueden <ctrueden@wisc.edu>
2014-03-03 13:06:36 -0600

    Update licenses
    This was done using:
        mvn license:update-project-license
        mvn license:update-file-header
    
    The main difference is the loss of the disclaimer at the bottom
    of each BSD-2-licensed file. For discussion and rationale, see:
        https://jira.codehaus.org/browse/MLICENSE-80
    
    There were also a few files missing the header, as well as a few missing
    LICENSE.txt files, both of which have been corrected by this update.

--- components/formats-bsd/test/loci/formats/utests/FakeReaderTest.java
+++ components/formats-bsd/test/loci/formats/utests/FakeReaderTest.java
@@ -1,6 +1,6 @@
 /*
  * #%L
- * Tests for OME Bio-Formats BSD-licensed readers and writers.
+ * BSD implementations of Bio-Formats readers and writers
  * %%
  * Copyright (C) 2005 - 2014 Open Microscopy Environment:
  *   - Board of Regents of the University of Wisconsin-Madison
@@ -9,13 +9,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *
+ * 
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -27,10 +27,6 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *
- * The views and conclusions contained in the software and documentation are
- * those of the authors and should not be interpreted as representing official
- * policies, either expressed or implied, of any organization.
  * #L%
  */
 
