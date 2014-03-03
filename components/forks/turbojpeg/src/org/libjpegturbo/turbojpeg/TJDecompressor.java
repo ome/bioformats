4d84cd6f3e17bf05851087c388f0a49c66d97008
Curtis Rueden <ctrueden@wisc.edu>
2014-03-03 13:06:36 -0600

    Update licenses
    This was done using:
        mvn license:update-project-license
        mvn license:update-file-header
    
    The main difference is the loss of the disclaimer at the bottom
    of each BSD-2-licensed file. For discussion and rationale, see:
        https://jira.codehaus.org/browse/MLICENSE-80
    
    There were also a few files missing the header, as well as a few missing
    LICENSE.txt files, both of which have been corrected by this update.

--- components/forks/turbojpeg/src/org/libjpegturbo/turbojpeg/TJDecompressor.java
+++ components/forks/turbojpeg/src/org/libjpegturbo/turbojpeg/TJDecompressor.java
@@ -1,4 +1,28 @@
 /*
+ * #%L
+ * Java bindings and pre-built binaries for libjpeg-turbo.
+ * %%
+ * Copyright (C) 2013 - 2014 Open Microscopy Environment:
+ *   - Board of Regents of the University of Wisconsin-Madison
+ *   - Glencoe Software, Inc.
+ *   - University of Dundee
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-2.0.html>.
+ * #L%
+ */
+/*
  * Copyright (C)2011 D. R. Commander.  All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
