cd0fb122bb4abd6a35d68f9db4a013a6cba15b0c
Brian Long <brianl@OSXLTZDV30.local>
2014-03-31 23:12:38 -0700

    Correct issues brought up by @melissalinkert
    See pull request for content. Most substantive change: V3DrawWriter
    class now takes endianness of file from metadata and writes the .v3draw
    file  with the same byte order as the input.

--- components/formats-bsd/src/loci/formats/out/V3DrawWriter.java
+++ components/formats-bsd/src/loci/formats/out/V3DrawWriter.java
@@ -1,8 +1,8 @@
 /*
  * #%L
- * OME SCIFIO package for reading and converting scientific file formats.
+ * OME Bio-Formats package for BSD-licensed readers and writers.
  * %%
- * Copyright (C) 2005 - 2013 Open Microscopy Environment:
+ * Copyright (C) 2005 - 2014 Open Microscopy Environment:
  *   - Board of Regents of the University of Wisconsin-Madison
  *   - Glencoe Software, Inc.
  *   - University of Dundee
@@ -33,7 +33,6 @@
  * policies, either expressed or implied, of any organization.
  * #L%
  */
-
 package loci.formats.out;
 
 import java.io.IOException;
@@ -45,170 +44,168 @@ import loci.formats.meta.MetadataRetrieve;
 import static loci.common.DataTools.unpackBytes;
 
 /**
-  */
+ * V3DrawWriter writes images in the .v3draw format for rapid I/O in <a
+ * href="http://vaa3d.org">Vaa3D</a>, an open-source 3D visualization and
+ * analysis toolkit.
+ *
+ * <dl><dt><b>Source code: (after merge) </b></dt>
+ * <dd><a
+ * href="http://trac.openmicroscopy.org.uk/ome/browser/bioformats.git/components/bio-formats/src/loci/formats/out/V3DrawWriter.java">Trac</a>,
+ * <a
+ * href="http://git.openmicroscopy.org/?p=bioformats.git;a=blob;f=components/bio-formats/src/loci/formats/out/V3DrawWriter.java;hb=HEAD">Gitweb</a></dd></dl>
+ */
 public class V3DrawWriter extends FormatWriter {
 
   // -- Fields --
+    private long pixelOffset = 43;
+    private long lastPlane = -1;
+    private RandomAccessOutputStream pixels;
 
-
-  private long pixelOffset = 43;
-  private long lastPlane = -1;
-  private RandomAccessOutputStream pixels;
-
-  private String outputOrder = "XYZCT";
+    private String outputOrder = "XYZCT";
 
   // -- Constructor --
+    public V3DrawWriter() {
+        super("Vaa3d", new String[]{"v3draw"});
 
-  public V3DrawWriter() {
-    super("Vaa3d", new String[] {"v3draw"});
-  
-  }
+    }
 
   // -- V3DRAWWriter API methods --
-
-  /**
-   * Set the order in which dimensions should be written to the file.
-   * Valid values are specified in the documentation for
-   * {@link loci.formats.IFormatReader#getDimensionOrder()}
-   *
-   * By default, the ordering is "XYZCT".
-   */
-  public void setOutputOrder(String outputOrder) {
-    this.outputOrder = outputOrder;
-  }
+    /**
+     * Set the order in which dimensions should be written to the file. Valid
+     * values are specified in the documentation for
+     * {@link loci.formats.IFormatReader#getDimensionOrder()}
+     *
+     * By default, the ordering is "XYZCT".
+     */
+    public void setOutputOrder(String outputOrder) {
+        this.outputOrder = outputOrder;
+    }
 
   // -- IFormatWriter API methods --
+    /**
+     * @see loci.formats.IFormatWriter#saveBytes(int, byte[], int, int, int,
+     * int)
+     */
+    public void saveBytes(int no, byte[] buf, int x, int y, int w, int h)
+            throws FormatException, IOException {
+        if (!isFullPlane(x, y, w, h)) {
+            throw new FormatException("V3DRawWriter does not support writing tiles");
+        }
 
-  /**
-   * @see loci.formats.IFormatWriter#saveBytes(int, byte[], int, int, int, int)
-   */
-  public void saveBytes(int no, byte[] buf, int x, int y, int w, int h)
-    throws FormatException, IOException
-  {    
         final String formatkey = "raw_image_stack_by_hpeng"; // for header  
         byte[] v2 = new byte[2];
         byte[] v4 = new byte[4];
 
         int[] sz = new int[4];  // new variable for clarity: vaa3d is in xyzct format
-        boolean little = true;
-  
-          checkParams(no, buf, x, y, w, h);
+
+        checkParams(no, buf, x, y, w, h);
       // .v3draw format doesn't know anything about x and y offsets, so they are ignored, asssuming
-      // each byte [] buf is a full width-height plane.
-     
-      
-      
-    if (pixels == null) {
-      pixels = new RandomAccessOutputStream(currentId);
-    }
+        // each byte [] buf is a full width-height plane.
 
-    MetadataRetrieve meta = getMetadataRetrieve();
+        if (pixels == null) {
+            pixels = new RandomAccessOutputStream(currentId);
+        }
+        String endianString = "L";
+        MetadataRetrieve meta = getMetadataRetrieve();
+        boolean bigendian =  meta.getPixelsBinDataBigEndian(series, 0);
+        if (!bigendian) {
+            endianString = "L";
+        }else{
+            endianString = "B";
+        }
+        int rgbChannels = getSamplesPerPixel();
 
-    int rgbChannels = getSamplesPerPixel();
+        String order = meta.getPixelsDimensionOrder(series).getValue();
+        int sizeZ = meta.getPixelsSizeZ(series).getValue().intValue();
+        int sizeC = meta.getChannelCount(series);
+        if (rgbChannels <= sizeC) {
+            sizeC /= rgbChannels;
+        }
+        int sizeT = meta.getPixelsSizeT(series).getValue().intValue();
+        int planes = sizeZ * sizeC * sizeT;
 
-    String order = meta.getPixelsDimensionOrder(series).getValue();
-    int sizeZ = meta.getPixelsSizeZ(series).getValue().intValue();
-    int sizeC = meta.getChannelCount(series);
-    if (rgbChannels <= sizeC) {
-      sizeC /= rgbChannels;
-    }
-    int sizeT = meta.getPixelsSizeT(series).getValue().intValue();
-    int planes = sizeZ * sizeC * sizeT;
+        int[] coords
+                = FormatTools.getZCTCoords(order, sizeZ, sizeC, sizeT, planes, no);
+        int realIndex
+                = FormatTools.getIndex(outputOrder, sizeZ, sizeC, sizeT, planes,
+                        coords[0], coords[1], coords[2]);
 
-    int[] coords =
-      FormatTools.getZCTCoords(order, sizeZ, sizeC, sizeT, planes, no);
-    int realIndex =
-      FormatTools.getIndex(outputOrder, sizeZ, sizeC, sizeT, planes,
-      coords[0], coords[1], coords[2]);
-
-    int sizeX = meta.getPixelsSizeX(series).getValue().intValue();
-    int sizeY = meta.getPixelsSizeY(series).getValue().intValue();
-    int pixelType =
-      FormatTools.pixelTypeFromString(meta.getPixelsType(series).toString());
-    int bytesPerPixel = FormatTools.getBytesPerPixel(pixelType);
-    long planeSize = sizeX * sizeY *  bytesPerPixel* rgbChannels;
+        int sizeX = meta.getPixelsSizeX(series).getValue().intValue();
+        int sizeY = meta.getPixelsSizeY(series).getValue().intValue();
+        int pixelType
+                = FormatTools.pixelTypeFromString(meta.getPixelsType(series).toString());
+        int bytesPerPixel = FormatTools.getBytesPerPixel(pixelType);
+        long planeSize = sizeX * sizeY * bytesPerPixel * rgbChannels;
 
 //FormatTools.get
-  
-    
-    			sz[0] = sizeX;
-			sz[1] = sizeY;
-			sz[2] = sizeZ*sizeT;// temporary aggregate for layer
-			sz[3] = sizeC*rgbChannels; // temp aggregate for color
-    if (!initialized[series][realIndex]) {
-        initialized[series][realIndex] = true;      
+        sz[0] = sizeX;
+        sz[1] = sizeY;
+        sz[2] = sizeZ * sizeT;// temporary aggregate for layer
+        sz[3] = sizeC * rgbChannels; // temp aggregate for color
+        if (!initialized[series][realIndex]) {
+            initialized[series][realIndex] = true;
+        }
+        try {
+//write the header if it's the first time through       
+            if (lastPlane == -1) {
+                pixels.write(formatkey.getBytes());             // write 	format key 
+                pixels.write(endianString.getBytes());                   //       	endianness.
+                unpackBytes(bytesPerPixel, v2, 0, 2, !bigendian);
+                pixels.write(v2);                                //       	unitSize 
+                for (int d : sz) {
+                    unpackBytes(d, v4, 0, 4, !bigendian);
+                    pixels.write(v4);
+                }        // and    	image dimensions into header 
+                pixels.write(buf);
+                LOGGER.info("*********   V3DrawWriter.java internal variables  *********");
+                LOGGER.info("bytesPerPixel = " + bytesPerPixel);
+                LOGGER.info("pixelType = " + pixelType);
+                LOGGER.info("rgbChannels =" + rgbChannels);
+                LOGGER.info("sizeC = " + sizeC);
+                LOGGER.info("sizeZ = " + sizeZ);
+                LOGGER.info("sizeT = " + sizeT);
+                LOGGER.info("endian= " + endianString);
+            } else {
+                pixels.seek(planeSize * realIndex + pixelOffset);
+                //write the rest of the plane 
+                pixels.write(buf);
+            }
+            lastPlane = realIndex;
+        } finally {
+            pixels.close();
+            pixels = null;
+        }
     }
 
-//write the header if it's the first time through   
-
-    
-    if (lastPlane == -1){
-    pixels.write(formatkey.getBytes());             // write 	format key 
-    pixels.write("L".getBytes());                   //       	endianness.
-    unpackBytes(bytesPerPixel, v2, 0, 2, little);		
-    pixels.write(v2);                                //       	unitSize 
-for (int d:sz){ 
-    unpackBytes(d, v4, 0, 4, little);		
-    pixels.write(v4); }        // and    	image dimensions into header 
-    pixels.write(buf);
-    
-    System.out.println("*********   V3DrawWriter.java internal variables  *********");
-
-    System.out.println("bytesPerPixel = "+bytesPerPixel);
-    System.out.println("pixelType = "+pixelType);
-    System.out.println("rgbChannels =" +rgbChannels);
-    System.out.println("sizeC = "+sizeC);
-    System.out.println("sizeZ = "+sizeZ);
-    System.out.println("sizeT = "+sizeT);
-    
-    
-    
+    /* @see loci.formats.IFormatWriter#canDoStacks() */
+    public boolean canDoStacks() {
+        return true;
     }
-    else{
-    /*
-    System.out.println("inside main loop");
-    System.out.println(planeSize);
-    System.out.println(realIndex);
-    System.out.println(pixelOffset);
-    System.out.println(planeSize*realIndex + pixelOffset);
-    */
-    pixels.seek(planeSize*realIndex + pixelOffset);
-    //write the rest of the plane 
-    pixels.write(buf);
-    } 
-    lastPlane = realIndex;
-    pixels.close();
-    pixels = null;
-  }
 
-  /* @see loci.formats.IFormatWriter#canDoStacks() */
-  public boolean canDoStacks() { return true; }
-
-  /* @see loci.formats.IFormatWriter#getPixelTypes(String) */
-  public int[] getPixelTypes(String codec) {
-    return new int[] { FormatTools.UINT8, 
-      FormatTools.UINT16,
-      FormatTools.FLOAT};
-  }
+    /* @see loci.formats.IFormatWriter#getPixelTypes(String) */
+    public int[] getPixelTypes(String codec) {
+        return new int[]{FormatTools.UINT8,
+            FormatTools.UINT16,
+            FormatTools.FLOAT};
+    }
 
   // -- IFormatHandler API methods --
 
-  /* @see loci.formats.IFormatHandler#setId(String) */
-  public void setId(String id) throws FormatException, IOException {
-    super.setId(id);
-  }
-
-  /* @see loci.formats.IFormatHandler#close() */
-  public void close() throws IOException {
-    super.close();
-    pixelOffset = 0;
-    lastPlane = -1;
-    if (pixels != null) {
-      pixels.close();
+    /* @see loci.formats.IFormatHandler#setId(String) */
+    public void setId(String id) throws FormatException, IOException {
+        super.setId(id);
     }
-    pixels = null;
-  }
+
+    /* @see loci.formats.IFormatHandler#close() */
+    public void close() throws IOException {
+        super.close();
+        pixelOffset = 43;
+        lastPlane = -1;
+        if (pixels != null) {
+            pixels.close();
+        }
+        pixels = null;
+    }
 
   // -- Helper methods --
-
 }
