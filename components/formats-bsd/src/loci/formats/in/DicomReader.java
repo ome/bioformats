d8c094fb4fccee2f720fcf5cb1637555896fdacc
Melissa Linkert <melissa@glencoesoftware.com>
2014-04-21 22:08:41 -0500

    DICOM: expand sequence reading to include known sequences
    See QA 9184.

--- components/formats-bsd/src/loci/formats/in/DicomReader.java
+++ components/formats-bsd/src/loci/formats/in/DicomReader.java
@@ -1045,7 +1045,9 @@ public class DicomReader extends FormatReader {
 
     // "Undefined" element length.
     // This is a sort of bracket that encloses a sequence of elements.
-    if (elementLength == -1) {
+    if (elementLength == -1 || (TYPES.containsKey(tag) &&
+      TYPES.get(tag).endsWith("Sequence")))
+    {
       elementLength = 0;
       inSequence = true;
     }
