1e280a32ea195174b95a244fe1f04bbe98f6e6ea
Roger Leigh <r.leigh@dundee.ac.uk>
2014-10-23 10:23:35 +0100

    cpp: bioformats: Add TIFFReader

---
+++ cpp/lib/ome/bioformats/in/TIFFReader.cpp
@@ -0,0 +1,203 @@
+/*
+ * #%L
+ * OME-BIOFORMATS C++ library for image IO.
+ * Copyright © 2006 - 2014 Open Microscopy Environment:
+ *   - Massachusetts Institute of Technology
+ *   - National Institutes of Health
+ *   - University of Dundee
+ *   - Board of Regents of the University of Wisconsin-Madison
+ *   - Glencoe Software, Inc.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+#include <algorithm>
+#include <cmath>
+
+#include <ome/bioformats/in/TIFFReader.h>
+#include <ome/bioformats/tiff/IFD.h>
+#include <ome/bioformats/tiff/TIFF.h>
+#include <ome/bioformats/tiff/Tags.h>
+#include <ome/bioformats/tiff/Field.h>
+
+using ome::bioformats::detail::ReaderProperties;
+using ome::bioformats::tiff::TIFF;
+using ome::bioformats::tiff::IFD;
+using ome::bioformats::tiff::tag_type;
+
+namespace ome
+{
+  namespace bioformats
+  {
+    namespace in
+    {
+
+      namespace
+      {
+
+        const char *suffixes[] = {"tif", "tiff", "tf2", "tf8", "btf"};
+        const char *companion_suffixes_array[] = {"txt", "xml"};
+
+        ReaderProperties
+        tiff_properties()
+        {
+          ReaderProperties p;
+
+          p.name = "TIFF";
+          p.description = "Tagged Image File Format";
+          p.suffixes = std::vector<std::string>(suffixes,
+                                                suffixes + (sizeof(suffixes) / sizeof(suffixes[0])));
+          p.compression_suffixes.push_back("gz");
+          p.metadata_levels.insert(MetadataOptions::METADATA_MINIMUM);
+          p.metadata_levels.insert(MetadataOptions::METADATA_NO_OVERLAYS);
+          p.metadata_levels.insert(MetadataOptions::METADATA_ALL);
+
+          return p;
+        }
+
+        const ReaderProperties props(tiff_properties());
+
+        std::vector<std::string> companion_suffixes(companion_suffixes_array,
+                                                    companion_suffixes_array + (sizeof(companion_suffixes_array) / sizeof(companion_suffixes_array[0])));
+
+      }
+
+      TIFFReader::TIFFReader():
+        MinimalTIFFReader(props)
+      {
+      }
+
+      TIFFReader::~TIFFReader()
+      {
+      }
+
+      void
+      TIFFReader::close(bool fileOnly)
+      {
+        ijmeta = boost::none;
+
+        MinimalTIFFReader::close(fileOnly);
+      }
+
+      void
+      TIFFReader::readIFDs()
+      {
+        std::shared_ptr<IFD> ifd0 = *(tiff->begin());
+
+        if (ifd0)
+          {
+            bool imagej_metadata = true;
+
+            try
+              {
+                tiff::ImageJMetadata ijmeta(*ifd0);
+
+                CoreMetadata ijm(tiff::makeCoreMetadata(*ifd0));
+
+                ijm.sizeZ = ijmeta.slices;
+                ijm.sizeT = ijmeta.frames;
+                ijm.sizeC = ijmeta.channels;
+
+                core.clear();
+                core.push_back(std::make_shared<CoreMetadata>(ijm));
+
+                dimension_size_type images = 0;
+                for (TIFF::const_iterator i = tiff->begin();
+                     i != tiff->end();
+                     ++i, ++images)
+                  {
+                    if (imagej_metadata)
+                      {
+                        // Verify metadata is consistent
+
+                        std::string desc;
+                        (*i)->getField(ome::bioformats::tiff::IMAGEDESCRIPTION).get(desc);
+                        std::map<std::string,std::string> imap(tiff::ImageJMetadata::parse_imagedescription(desc));
+
+                        if (imap != ijmeta.map)
+                          {
+                            std::cerr << "ImageJ TIFF metadata is inconsistent; treating as a plain TIFF";
+                            imagej_metadata = false;
+                            break;
+                          }
+                      }
+                  }
+
+                if (images != ijmeta.images)
+                  {
+                    std::cerr << "ImageJ TIFF metadata is inconsistent with TIFF image count; treating as a plain TIFF";
+                    imagej_metadata = false;
+                  }
+              }
+            catch (const std::exception& e)
+              {
+                // Catch all TIFF exceptions and parse failures.
+                imagej_metadata = false;
+                core.clear();
+              }
+
+            if (imagej_metadata)
+              this->ijmeta = ijmeta;
+          }
+
+        // If a plain TIFF, read metadata from IFDs.
+        if (!ijmeta)
+          MinimalTIFFReader::readIFDs();
+      }
+
+      void
+      TIFFReader::openBytes(dimension_size_type no,
+                            VariantPixelBuffer& buf) const
+      {
+        ::ome::bioformats::detail::FormatReader::openBytes(no, buf);
+      }
+
+      void
+      TIFFReader::openBytes(dimension_size_type no,
+                            VariantPixelBuffer& buf,
+                            dimension_size_type x,
+                            dimension_size_type y,
+                            dimension_size_type w,
+                            dimension_size_type h) const
+      {
+        assertId(currentId, true);
+
+        if (!!ijmeta)
+          {
+            const std::shared_ptr<IFD> ifd = tiff->getDirectoryByIndex(no);
+
+            ifd->readImage(buf, x, y, w, h);
+          }
+        else
+          {
+            MinimalTIFFReader::openBytes(no, buf, x, y, w, h);
+          }
+      }
+
+    }
+  }
+}
