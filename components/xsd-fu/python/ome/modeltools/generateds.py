fcbd4bb03489b42e012d397c96d06d74b1143532
jmoore <josh@glencoesoftware.com>
2014-12-03 09:36:34 +0100

    Use absolute_import instead of __import__

--- components/xsd-fu/python/ome/modeltools/generateds.py
+++ components/xsd-fu/python/ome/modeltools/generateds.py
@@ -1,10 +1,12 @@
+from __future__ import absolute_import
+
 import sys
 import keyword
 import logging
 
 # The generateDS package and our generateds module
 # collide on case-insensitive file systems.
-generateDS = __import__("generateDS.generateDS")
+import generateDS.generateDS
 XschemaHandler = generateDS.generateDS.XschemaHandler
 set_type_constants = generateDS.generateDS.set_type_constants
 
