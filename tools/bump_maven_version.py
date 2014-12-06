51f0b15c491c12312c8b5803424b8ce9b876ee46
Sebastien Besson <seb.besson@gmail.com>
2014-12-06 14:29:53 +0000

    Add substitution for release.version

--- tools/bump_maven_version.py
+++ tools/bump_maven_version.py
@@ -2,20 +2,24 @@
 # Script for updating copyright headers across the code
 
 import glob
-import os
 import re
 import sys
 
 version = sys.argv[1]
-pattern = re.compile(r"(.*<groupId>ome</groupId>\n.*\n.*<version>)"
-                     ".*(</version>)")
+
+# Define regular expression objects
+pattern1 = r"(.*<groupId>ome</groupId>\n.*\n.*<version>).*(</version>)"
+regexp1 = re.compile(pattern1)
+pattern2 = r"(<release.version>).*(</release.version>)"
+regexp2 = re.compile(pattern2)
 
 for pomfile in (glob.glob("pom.xml") + glob.glob("*/*/pom.xml") +
                 glob.glob("*/*/*/pom.xml")):
     with open(pomfile, "r") as infile:
-        instr = infile.read()
+        infile_str = infile.read()
+        outfile_str = regexp1.sub(r"\g<1>%s\g<2>" % version, infile_str)
+        outfile_str = regexp2.sub(r"\g<1>%s\g<2>" % version, outfile_str)
         with open(pomfile, "w") as output:
-            output.write(pattern.sub(r"\g<1>%s\g<2>" % version, instr))
+            output.write(outfile_str)
             output.close()
         infile.close()
-    
