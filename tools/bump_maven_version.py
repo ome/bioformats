19007735af28a304cfd155f6adfc3e3df215bf96
Sebastien Besson <seb.besson@gmail.com>
2014-12-06 14:33:06 +0000

    Add logic to replace STABLE_VERSION

--- tools/bump_maven_version.py
+++ tools/bump_maven_version.py
@@ -23,3 +23,15 @@ for pomfile in (glob.glob("pom.xml") + glob.glob("*/*/pom.xml") +
             output.write(outfile_str)
             output.close()
         infile.close()
+
+pattern3 = r"(STABLE_VERSION = \").*(\";)"
+regexp3 = re.compile(pattern3)
+
+upgradecheck = "components/formats-bsd/src/loci/formats/UpgradeChecker.java"
+with open(upgradecheck, "r") as infile:
+    infile_str = infile.read()
+    outfile_str = regexp3.sub(r"\g<1>%s\g<2>" % version, infile_str)
+    with open(upgradecheck, "w") as output:
+        output.write(outfile_str)
+        output.close()
+    infile.close()
