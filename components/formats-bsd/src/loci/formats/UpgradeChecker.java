e9771abfdb56db2584ee451bdfbc5259d962448b
Melissa Linkert <melissa@glencoesoftware.com>
2015-01-28 14:56:59 -0600

    Update POM version numbers to 5.0.7

--- components/formats-bsd/src/loci/formats/UpgradeChecker.java
+++ components/formats-bsd/src/loci/formats/UpgradeChecker.java
@@ -67,7 +67,7 @@ public class UpgradeChecker {
   // -- Constants --
 
   /** Version number of the latest stable release. */
-  public static final String STABLE_VERSION = "5.0.6";
+  public static final String STABLE_VERSION = "5.0.7";
 
   /** Location of the OME continuous integration server. */
   public static final String CI_SERVER = "http://ci.openmicroscopy.org";
