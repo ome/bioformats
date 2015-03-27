b4f2fcefd23f8a88b0a3c30fe7a946834d8c441b
Sebastien Besson <seb.besson@gmail.com>
2015-03-27 15:52:37 +0000

    Minor improvements

--- components/formats-gpl/test/matlab/TestMemoizer.m
+++ components/formats-gpl/test/matlab/TestMemoizer.m
@@ -25,19 +25,19 @@
 % 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 classdef TestMemoizer < ReaderTest
-    
+
     properties
         filepath
     end
-    
+
     methods
         function self = TestMemoizer(name)
             self = self@ReaderTest(name);
         end
-        
+
         function setUp(self)
             setUp@ReaderTest(self);
-            
+
             % Create fake file for testing
             mkdir(self.tmpdir);
             self.filepath = fullfile(self.tmpdir, 'test.fake');
@@ -48,50 +48,50 @@ classdef TestMemoizer < ReaderTest
             if exist(self.tmpdir, 'dir') == 7, rmdir(self.tmpdir, 's'); end
             tearDown@ReaderTest(self);
         end
- 
-        function testMinimumElapsed(self)            
+
+        function testMinimumElapsed(self)
             % Create memoizer reader with large minimum initialization
             % time
             self.reader = loci.formats.Memoizer(bfGetReader(), 1e10);
             self.reader.setId(self.filepath);
-            
+
             % Check reader is not saved to memo file
             assertFalse(self.reader.isLoadedFromMemo());
             assertFalse(self.reader.isSavedToMemo());
             self.reader.close();
         end
-        
+
         function testInPlaceCaching(self)
-            
+
             % Create memoizer reader and initializes file
             self.reader = loci.formats.Memoizer(bfGetReader(), 0);
             self.reader.setId(self.filepath);
-            
+
             % Check reader has been saved to memo file
             assertFalse(self.reader.isLoadedFromMemo());
             assertTrue(self.reader.isSavedToMemo());
             self.reader.close();
         end
-        
+
         function testReinitialization(self)
             % Create Memoizer reader
             self.reader = loci.formats.Memoizer(bfGetReader(), 0);
             self.reader.setId(self.filepath);
-            
-            % Re-initialize the existingreader
+
+            % Re-initialize the existing reader
             self.reader.close();
             self.reader.setId(self.filepath);
-            
+
             % Check reader has been loaded from memo file
             assertTrue(self.reader.isLoadedFromMemo());
             assertFalse(self.reader.isSavedToMemo());
         end
-        
+
         function testNewReader(self)
             % Create memoizer reader
             self.reader = loci.formats.Memoizer(bfGetReader(), 0);
             self.reader.setId(self.filepath);
-            
+
             % Construct new memoizer
             reader2 = loci.formats.Memoizer(bfGetReader(), 0);
             reader2.setId(self.filepath);
@@ -101,12 +101,12 @@ classdef TestMemoizer < ReaderTest
             self.reader.close()
             clear reader2
         end
-        
+
         function testMultiThreaded(self)
             % Create memoized reader
             self.reader = loci.formats.Memoizer(bfGetReader(), 0);
             self.reader.setId(self.filepath);
-            
+
             localpath = self.filepath;
             for i = 1 : 4
                 r = loci.formats.Memoizer(bfGetReader(), 0);
