7095be0f070a06fd6dc9bab7c2372becdbbcda5f
Roger Leigh <r.leigh@dundee.ac.uk>
2015-04-02 15:40:06 +0100

    cmake: Move sphinx build rules to docs/sphinx

--- cpp/cmake/Sphinx.cmake
+++ cpp/cmake/Sphinx.cmake
@@ -67,114 +67,38 @@ endif()
 option(sphinx-pdf "Enable sphinx PDF documentation" ${SPHINX_PDF_DEFAULT})
 set(BUILD_SPHINX_PDF ${sphinx-pdf})
 
-if (BUILD_SPHINX AND SPHINX_BUILD)
-  message(STATUS "Checking manual page dependencies")
-
-  # Create build directory and conf.py
-  set(sphinx_srcdir "${PROJECT_SOURCE_DIR}/docs/sphinx")
-  set(sphinx_builddir "${PROJECT_BINARY_DIR}/docs/sphinx")
-  file(MAKE_DIRECTORY "${sphinx_builddir}")
-  configure_file("${sphinx_srcdir}/conf.py.in"
-                 "${sphinx_builddir}/conf.py")
-
-  execute_process(COMMAND python -B ${CMAKE_CURRENT_LIST_DIR}/list-manpages.py
-                          "${sphinx_builddir}"
-                          "${sphinx_srcdir}"
-                          "${PROJECT_BINARY_DIR}/cpp/man"
+function(sphinx_manpages srcdir confdir mandir manvar)
+  execute_process(COMMAND python -B ${PROJECT_SOURCE_DIR}/cpp/cmake/list-manpages.py
+                          "${confdir}" "${srcdir}" "${mandir}"
                   RESULT_VARIABLE sphinx_man_fail
-                  OUTPUT_VARIABLE MAN_PAGES
-                  ERROR_FILE docs/sphinx/sphinx-man.log)
+                  OUTPUT_VARIABLE MAN_PAGES)
+  if (sphinx_man_fail)
+    message(WARNING "Failed to get generated sphinx manual pages from ${confdir}")
+  endif()
   string(REPLACE "\n" ";" MAN_PAGES "${MAN_PAGES}")
-  execute_process(COMMAND python -B ${CMAKE_CURRENT_LIST_DIR}/list-manpage-dependencies.py
-                          "${sphinx_builddir}"
-                          "${sphinx_srcdir}"
-                  RESULT_VARIABLE sphinx_man_dep_fail
-                  OUTPUT_VARIABLE MAN_PAGE_DEPENDENCIES
-                  ERROR_FILE docs/sphinx/sphinx-man-dep.log)
-  string(REPLACE "\n" ";" MAN_PAGE_DEPENDENCIES "${MAN_PAGE_DEPENDENCIES}")
+  set(${manvar} "${MAN_PAGES}" PARENT_SCOPE)
+endfunction(sphinx_manpages)
 
-  file(GLOB_RECURSE CRUDE_SPHINX_DEPENDENCIES "${sphinx_srcdir}/*.txt")
+function(sphinx_manpage_dependencies srcdir confdir depvar)
+  execute_process(COMMAND python -B ${PROJECT_SOURCE_DIR}/cpp/cmake/list-manpage-dependencies.py
+                          "${confdir}"
+                          "${srcdir}"
+                  RESULT_VARIABLE sphinx_dep_fail
+                  OUTPUT_VARIABLE SPHINX_MAN_DEPENDENCIES)
+  if (sphinx_dep_fail)
+    message(WARNING "Failed to get Sphinx dependencies from ${confdir}")
+  endif()
+  string(REPLACE "\n" ";" SPHINX_MAN_DEPENDENCIES "${SPHINX_MAN_DEPENDENCIES}")
+  set(${depvar} "${SPHINX_MAN_DEPENDENCIES}" PARENT_SCOPE)
+endfunction(sphinx_manpage_dependencies)
+
+function(sphinx_dependencies srcdir depvar)
+  file(GLOB_RECURSE CRUDE_SPHINX_DEPENDENCIES "${srcdir}/*.txt")
   foreach(file ${CRUDE_SPHINX_DEPENDENCIES})
     string(FIND "${file}" "_build" FILE_MATCH)
     if(FILE_MATCH EQUAL -1)
       list(APPEND SPHINX_DEPENDENCIES "${file}")
     endif()
   endforeach()
-
-  # Generate and install man pages
-
-  add_custom_command(OUTPUT ${MAN_PAGES}
-                     COMMAND ${CMAKE_COMMAND} -E make_directory "${sphinx_builddir}/cache"
-                     COMMAND ${CMAKE_COMMAND} -E make_directory "${PROJECT_BINARY_DIR}/cpp/man"
-                     COMMAND ${SPHINX_BUILD}
-                             -D "release=${OME_VERSION_SHORT}"
-                             -D "version=${OME_VERSION_MAJOR}.${OME_VERSION_MINOR}"
-                             -c "${sphinx_builddir}"
-                             -d "${sphinx_builddir}/cache"
-                             -b man
-                             "${sphinx_srcdir}" "${PROJECT_BINARY_DIR}/cpp/man"
-                     WORKING_DIRECTORY "${sphinx_srcdir}"
-                     DEPENDS ${MAN_PAGE_DEPENDENCIES})
-
-  add_custom_target(man ALL DEPENDS ${MAN_PAGES})
-
-  foreach (man ${MAN_PAGES})
-    string(REGEX REPLACE ".*(.)\$" "\\1" man_section "${man}")
-    install(FILES "${man}"
-            DESTINATION "${CMAKE_INSTALL_FULL_MANDIR}/man${man_section}")
-  endforeach()
-
-  # Generate and install HTML manual
-
-  add_custom_command(OUTPUT "${sphinx_builddir}/html/index.html"
-                     COMMAND ${CMAKE_COMMAND} -E make_directory "${sphinx_builddir}/cache"
-                     COMMAND ${CMAKE_COMMAND} -E make_directory "${sphinx_builddir}/html"
-                     COMMAND ${SPHINX_BUILD}
-                             -D "release=${OME_VERSION_SHORT}"
-                             -D "version=${OME_VERSION_MAJOR}.${OME_VERSION_MINOR}"
-                             -c "${sphinx_builddir}"
-                             -d "${sphinx_builddir}/cache"
-                             -b html
-                             "${sphinx_srcdir}" "${sphinx_builddir}/html"
-                     WORKING_DIRECTORY "${sphinx_srcdir}"
-                     DEPENDS ${SPHINX_DEPENDENCIES})
-
-  add_custom_target(doc-html ALL DEPENDS "${sphinx_builddir}/html/index.html")
-
-  install(DIRECTORY "${sphinx_builddir}/html"
-          DESTINATION "${CMAKE_INSTALL_FULL_DOCDIR}/user")
-
-  # Generate and install PDF manual
-
-  if (BUILD_SPHINX_PDF AND XELATEX AND MAKEINDEX)
-    add_custom_command(OUTPUT "${sphinx_builddir}/latex/Bio-Formats.tex" "${sphinx_builddir}/latex/preamble.tex"
-                       COMMAND ${CMAKE_COMMAND} -E make_directory "${sphinx_builddir}/cache"
-                       COMMAND ${CMAKE_COMMAND} -E make_directory "${sphinx_builddir}/latex"
-                       COMMAND ${SPHINX_BUILD}
-                               -D "release=${OME_VERSION_SHORT}"
-                               -D "version=${OME_VERSION_MAJOR}.${OME_VERSION_MINOR}"
-                               -c "${sphinx_builddir}"
-                               -d "${sphinx_builddir}/cache"
-                               -b latex
-                               "${sphinx_srcdir}" "${sphinx_builddir}/latex"
-                       COMMAND ${CMAKE_COMMAND} -E copy "preamble.tex" "${sphinx_builddir}/latex"
-                       WORKING_DIRECTORY "${sphinx_srcdir}"
-                       DEPENDS ${SPHINX_DEPENDENCIES})
-    add_custom_command(OUTPUT "${sphinx_builddir}/latex/Bio-Formats.pdf"
-                       COMMAND ${XELATEX} "Bio-Formats.tex"
-                       COMMAND ${XELATEX} "Bio-Formats.tex"
-                       COMMAND ${MAKEINDEX} -s python.ist "Bio-Formats.idx"
-                       COMMAND ${XELATEX} "Bio-Formats.tex"
-                       COMMAND ${XELATEX} "Bio-Formats.tex"
-                       COMMAND ${XELATEX} "Bio-Formats.tex"
-                       WORKING_DIRECTORY "${sphinx_builddir}/latex"
-                       DEPENDS "${sphinx_builddir}/latex/Bio-Formats.tex" "${sphinx_builddir}/latex/preamble.tex")
-
-    add_custom_target(doc-pdf ALL DEPENDS "${sphinx_builddir}/latex/Bio-Formats.pdf")
-
-    install(FILES "${sphinx_builddir}/latex/Bio-Formats.pdf"
-            DESTINATION "${CMAKE_INSTALL_FULL_DOCDIR}/user")
-  endif (BUILD_SPHINX_PDF AND XELATEX AND MAKEINDEX)
-else()
-  message(WARNING "Manual pages and HTML manual will not be generated or installed")
-endif()
+  set(${depvar} "${SPHINX_DEPENDENCIES}" PARENT_SCOPE)
+endfunction(sphinx_dependencies)
