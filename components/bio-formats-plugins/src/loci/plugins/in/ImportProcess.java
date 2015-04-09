ee18e2e18569dfc4c5b5518070a7346a9c03631c
Balaji Ramalingam <b.ramalingam@dundee.ac.uk>
2015-04-09 15:18:41 +0100

    setMetadataLevel to no_overlays when display roi's is not selected

--- components/bio-formats-plugins/src/loci/plugins/in/ImportProcess.java
+++ components/bio-formats-plugins/src/loci/plugins/in/ImportProcess.java
@@ -55,6 +55,9 @@ import loci.formats.IFormatReader;
 import loci.formats.ImageReader;
 import loci.formats.MinMaxCalculator;
 import loci.formats.TileStitcher;
+import loci.formats.in.DefaultMetadataOptions;
+import loci.formats.in.MetadataLevel;
+import loci.formats.in.MetadataOptions;
 import loci.formats.meta.IMetadata;
 import loci.formats.services.OMEXMLService;
 import loci.plugins.BF;
@@ -530,6 +533,15 @@ public class ImportProcess implements StatusReporter {
     }
     r = virtualReader = new VirtualReader(r);
     reader = new ImageProcessorReader(r);
+    if(options != null && !options.showROIs()){
+        MetadataOptions mo = reader.getMetadataOptions();
+        if(mo == null){
+            mo = new DefaultMetadataOptions();
+        }else{
+            mo.setMetadataLevel(MetadataLevel.NO_OVERLAYS);
+        }
+        reader.setMetadataOptions(mo);
+    }
     setId();
 
     computeSeriesLabels(reader);
