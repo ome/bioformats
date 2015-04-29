6eb3eda9eef0e5290d4da3c985d10d58991ddebd
Melissa Linkert <melissa@glencoesoftware.com>
2015-04-29 14:50:01 -0500

    Reduce unknown box log level
    This is often a valid state for the parser, so WARN is not entirely
    appropriate.

--- components/formats-bsd/src/loci/formats/in/JPEG2000MetadataParser.java
+++ components/formats-bsd/src/loci/formats/in/JPEG2000MetadataParser.java
@@ -177,7 +177,7 @@ public class JPEG2000MetadataParser {
         length -= 8;
       }
       if (boxType == null) {
-        LOGGER.warn("Unknown JPEG 2000 box 0x{} at {}",
+        LOGGER.info("Unknown JPEG 2000 box 0x{} at {}",
             Integer.toHexString(boxCode), pos);
         if (pos == originalPos) {
           in.seek(originalPos);
