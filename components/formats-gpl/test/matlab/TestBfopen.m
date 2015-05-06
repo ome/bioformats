f96613b2b84f50b4cee4f67c16cfca5d5b2e8197
Balaji Ramalingam <b.ramalingam@dundee.ac.uk>
2015-05-06 14:03:29 +0100

    bfopen : remove a wrong test

--- components/formats-gpl/test/matlab/TestBfopen.m
+++ components/formats-gpl/test/matlab/TestBfopen.m
@@ -2,6 +2,7 @@
 %
 % Require MATLAB xUnit Test Framework to be installed
 % http://www.mathworks.com/matlabcentral/fileexchange/22846-matlab-xunit-test-framework
+% https://github.com/psexton/matlab-xunit (GitHub source code)
 
 % OME Bio-Formats package for reading and converting biological file formats.
 %
@@ -129,16 +130,7 @@ classdef TestBfopen < ReaderTest
             
             assertEqual(self.bpp,bpp);
             
-            seriesMetadata = self.reader.getSeriesMetadata();
-            globalMetadata = self.reader.getGlobalMetadata();
-
-            metadata1 = loci.formats.MetadataTools.merge(globalMetadata, seriesMetadata, 'Global ');
-            metadata2 = javaMethod('merge', 'loci.formats.MetadataTools', ...
-                globalMetadata, seriesMetadata, 'Global ');
-            
-            assertEqual(metadata1,metadata2);
-            
         end
-        
+               
     end
 end
