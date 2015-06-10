35e0f7bc71abe4e69368b8adec9b69d1fa0c5a3c
jmoore <josh@glencoesoftware.com>
2015-06-10 15:40:52 +0200

    FakeReader: add support for `[GlobalMetadata]`
    In order to round trip test the import of fake files,
    having a simple way of adding arbitrary fake global
    metadata to the reader is necessary. This currently adds
    support to fake.ini files for parsing the `[GlobalMetadata]`
    section into calls to FormatReader.addGlobalMetadata. Later
    work could add series-specific metadata as well.

--- components/formats-bsd/src/loci/formats/in/FakeReader.java
+++ components/formats-bsd/src/loci/formats/in/FakeReader.java
@@ -467,6 +467,13 @@ public class FakeReader extends FormatReader {
         }
       }
 
+      table = list.getTable(list.getHeaders().get(0));
+      if (table != null) {
+        for (Map.Entry<String, String> entry : table.entrySet()) {
+          addGlobalMeta(entry.getKey(), entry.getValue());
+        }
+      }
+
       String[] newTokArr = newTokens.toArray(new String[0]);
       String[] oldTokArr = tokens;
       tokens = new String[newTokArr.length + oldTokArr.length];
