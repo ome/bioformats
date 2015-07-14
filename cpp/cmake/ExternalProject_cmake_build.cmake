8e3c2f615af0c83804668396c3ef3dfcb0b3848b
Roger Leigh <rleigh@codelibre.net>
2015-07-14 11:42:56 +0000

    cmake: External bioformats: Use generic cmake build
    If a manual configure step is configured, the build step defaults
    to "make", so add manual build step to work around this.

---
+++ cpp/cmake/ExternalProject_cmake_build.cmake
@@ -0,0 +1,22 @@
+# Generic cmake installation
+include("${EP_SCRIPT_CONFIG}")
+include("${CMAKE_CURRENT_LIST_DIR}/ExternalProjectEnvironment.cmake")
+
+if(CMAKE_GENERATOR MATCHES "Unix Makefiles")
+
+  execute_process(COMMAND ${CMAKE_MAKE_PROGRAM}
+                  WORKING_DIRECTORY "${EP_BUILD_DIR}"
+                  RESULT_VARIABLE install_result)
+
+else()
+
+  execute_process(COMMAND "${CMAKE_COMMAND}" --build .
+                                             --config "${CONFIG}"
+                  WORKING_DIRECTORY "${EP_BUILD_DIR}"
+                  RESULT_VARIABLE install_result)
+
+endif(WIN32)
+
+if(install_result)
+  message(FATAL_ERROR "cmake: Install failed")
+endif()
