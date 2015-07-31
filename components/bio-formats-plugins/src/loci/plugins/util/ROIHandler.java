7fea6661a1af1ea039b8ebf8f9d1ca9c7a4826d6
Jean-Marie Burel <j.burel@dundee.ac.uk>
2015-07-31 15:06:59 +0100

    Check if value == 1.

--- components/bio-formats-plugins/src/loci/plugins/util/ROIHandler.java
+++ components/bio-formats-plugins/src/loci/plugins/util/ROIHandler.java
@@ -399,13 +399,13 @@ public class ROIHandler {
             int z = ijRoi.getZPosition();
             int t = ijRoi.getTPosition();
             //set the position to the correct ome-index
-            if (c > 1) {
+            if (c >= 1) {
                 c = c-1;
             }
-            if (t > 1) {
+            if (t >= 1) {
                 t = t-1;
             }
-            if (z > 1) {
+            if (z >= 1) {
                 z = z-1;
             }
             ijRoi.setPosition(c, z, t);
