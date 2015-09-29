779ac4b7b46f2c8c1af344513c45a10916274545
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2015-09-29 09:08:40 +0100

    XML code should be using the newest schema

--- components/specification/src/ome/specification/XMLWriter.java
+++ components/specification/src/ome/specification/XMLWriter.java
@@ -143,15 +143,15 @@ public class XMLWriter
 
 	/** The schemas. */
 	private static final String[] SCHEMAS = {
-	    "http://www.openmicroscopy.org/Schemas/OME/2015-01/ome.xsd"};
+	    "http://www.openmicroscopy.org/Schemas/OME/2016-DEV0/ome.xsd"};
 
 	/** The XML namespace. */
 	private static final String XML_NS =
-		"http://www.openmicroscopy.org/Schemas/OME/2015-01";
+		"http://www.openmicroscopy.org/Schemas/OME/2016-DEV0";
 
 	/** The XML namespace. */
 	private static final String BIN_NS =
-		"http://www.openmicroscopy.org/Schemas/BinaryFile/2015-01";
+		"http://www.openmicroscopy.org/Schemas/BinaryFile/2016-DEV0";
 
 	/** The XSI namespace. */
 	private static final String XSI_NS =
@@ -159,7 +159,7 @@ public class XMLWriter
 
 	/** The schema location. */
 	private static final String SCHEMA_LOCATION =
-		"http://www.openmicroscopy.org/Schemas/OME/2015-01/ome.xsd";
+		"http://www.openmicroscopy.org/Schemas/OME/2016-DEV0/ome.xsd";
 
 	/** A default plane. */
 	private static final String PLANE =
