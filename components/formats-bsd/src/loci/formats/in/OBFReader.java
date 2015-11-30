bc5ca92d0d3126d67f430cd3251344b76a761a19
Melissa Linkert <melissa@glencoesoftware.com>
2015-11-30 12:26:24 -0600

    OBF: make sure minimal MetadataStore methods are always called

--- components/formats-bsd/src/loci/formats/in/OBFReader.java
+++ components/formats-bsd/src/loci/formats/in/OBFReader.java
@@ -203,6 +203,7 @@ public class OBFReader extends FormatReader
         if (service.validateOMEXML(ome_xml)) {
           service.convertMetadata( ome_xml, ome ) ;
           parsedXML = true;
+          MetadataTools.populatePixels(ome, this, false, false);
         }
       }
       catch (DependencyException exception) 
@@ -221,7 +222,6 @@ public class OBFReader extends FormatReader
     if (fileVersion <= 1 || !parsedXML)
     {
       MetadataTools.populatePixels(ome, this);
-
       for (int series = 0; series != core.size(); ++ series)
       {
         CoreMetadata obf = core.get(series);
