0028ae3e72946800bf268d78fbfd564503cf68cf
Sebastien Besson <seb.besson@gmail.com>
2016-03-04 17:59:56 +0000

    Modify bfInitLogging to use loci.common.DebugTools.isEnabled()

--- components/formats-gpl/matlab/bfInitLogging.m
+++ components/formats-gpl/matlab/bfInitLogging.m
@@ -1,35 +1,51 @@
 function bfInitLogging(varargin)
-% BFINITLOGGING initializes Bio-Formats and its logging level
+% BFINITLOGGING initializes the Bio-Formats logging system
 %
-%   bfInitLogging() autoloads Bio-Formats is in the Java path and checks
-%   whether log4j is initialized. If not, it initializes logging at the
-%   WARN level.
+%   bfInitLogging() intializes the logging system at WARN level by default.
+%   It allows
 %
-%   bfInitLogging(level) sets the logging to the input level.
+%   bfInitLogging(level) sets the logging level to use when initializing
+%   the logging system
 %
 % Examples
 %
 %    bfInitLogging();
 %    bfInitLogging('DEBUG');
-%
-% Sebastien Besson, Nov 2014
 
-% Autoload Bio-Formats Java path
+% OME Bio-Formats package for reading and converting biological file formats.
+%
+% Copyright (C) 2016 Open Microscopy Environment:
+%   - Board of Regents of the University of Wisconsin-Madison
+%   - Glencoe Software, Inc.
+%   - University of Dundee
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as
+% published by the Free Software Foundation, either version 2 of the
+% License, or (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License along
+% with this program; if not, write to the Free Software Foundation, Inc.,
+% 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+% Check Bio-Formats is set in the Java class path
 bfCheckJavaPath();
 
-% Return if log4j is alreadyinitialied
-import org.apache.log4j.Logger;
-root = Logger.getRootLogger();
-appenders = root.getAllAppenders();
-if appenders.hasMoreElements(), return; end
+% Return if the logging system is already initialized
+if javaMethod('isEnabled', 'loci.common.DebugTools'), return; end
 
 % Input check
 import org.apache.log4j.Level;
 levels = Level.getAllPossiblePriorities();
 levels = arrayfun(@(x) char(x.toString()), levels, 'Unif', false);
-ip=inputParser;
+ip = inputParser;
 ip.addOptional('level', 'WARN', @(x) ismember(x, levels));
 ip.parse(varargin{:});
 
-% Configure log4j and set debug level
-loci.common.DebugTools.enableLogging(ip.Results.level);
+% Set logging level
+javaMethod('enableLogging', 'loci.common.DebugTools', ip.Results.level)
