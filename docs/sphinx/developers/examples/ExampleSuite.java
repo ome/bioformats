2588033e84f6d2cf14fed37a5de1c2759921f6f4
Sebastien Besson <seb.besson@gmail.com>
2016-08-25 20:46:16 +0100

    Add ExampleSuite class executing all examples of usage of Bio-Formats
    Use class.getResource() to access resources relative to the main class

---
+++ docs/sphinx/developers/examples/ExampleSuite.java
@@ -0,0 +1,61 @@
+/*
+ * #%L
+ * OME Bio-Formats package for reading and converting biological file formats.
+ * %%
+ * Copyright (C) 2016 Open Microscopy Environment:
+ *   - Board of Regents of the University of Wisconsin-Madison
+ *   - Glencoe Software, Inc.
+ *   - University of Dundee
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+import java.net.URL;
+import java.io.File;
+
+
+public class ExampleSuite {
+
+  /**
+   * Execute a series of examples using the test files
+   *
+   * $ java ExampleSuite
+   */
+  public static void main(String[] args) throws Exception {
+
+    // Retrieve local test files
+    URL resource =  ExampleSuite.class.getResource("test.fake");
+    File inputFile = new File(resource.toURI());
+    File parentDir = inputFile.getParentFile();
+    File convertedFile = new File(parentDir, "converted.ome.tiff");
+    File exportFile = new File(parentDir, "export.ome.tiff");
+    File exportSPWFile = new File(parentDir, "exportSPW.ome.tiff");
+
+    // Execute examples
+    ReadPhysicalSize.main(new String[] {inputFile.getAbsolutePath()});
+    FileConvert.main(new String[] {
+      inputFile.getAbsolutePath(), convertedFile.getAbsolutePath()});
+    FileExport.main(new String[] {exportFile.getAbsolutePath()});
+    FileExport.main(new String[] {exportSPWFile.getAbsolutePath()});
+  }
+}
