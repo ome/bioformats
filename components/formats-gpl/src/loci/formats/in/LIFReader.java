eb5733f9255eb018f8d7ec1384b934ab53f6c3a7
Michael Goelzer <michael.goelzer@leica-microsystems.com>
2016-10-19 09:30:12 +0200

    Support current timestamp format
    added support for timestamps of lif files created with LAS AF 3.1 or
    newer

--- components/formats-gpl/src/loci/formats/in/LIFReader.java
+++ components/formats-gpl/src/loci/formats/in/LIFReader.java
@@ -1571,42 +1571,75 @@ public class LIFReader extends FormatReader {
   private void translateTimestamps(Element imageNode, int image)
     throws FormatException
   {
-    NodeList timestampNodes = getNodes(imageNode, "TimeStamp");
-    if (timestampNodes == null) return;
+    NodeList timeStampLists = getNodes(imageNode, "TimeStampList");
+    if (timeStampLists == null) return;
 
+    Element timeStampList = (Element)timeStampLists.item(0);
     timestamps[image] = new double[getImageCount()];
-
-    if (timestampNodes != null) {
-      for (int stamp=0; stamp<timestampNodes.getLength(); stamp++) {
+    
+    // probe if timestamps are saved in the format of LAS AF 3.1 or newer
+    String numberOfTimeStamps = timeStampList.getAttribute("NumberOfTimeStamps");
+    if (numberOfTimeStamps != null && !numberOfTimeStamps.isEmpty()) {
+      // LAS AF 3.1 (or newer) timestamps are available
+      String timeStampsRaw = timeStampList.getTextContent();
+      List<String> timeStamps = Arrays.asList(timeStampsRaw.split(" "));
+      for (int stamp = 0; stamp < timeStamps.size(); stamp++) {
         if (stamp < getImageCount()) {
-          Element timestamp = (Element) timestampNodes.item(stamp);
-          String stampHigh = timestamp.getAttribute("HighInteger");
-          String stampLow = timestamp.getAttribute("LowInteger");
-          long high =
-            stampHigh == null || stampHigh.trim().isEmpty() ? 0 :
-                Long.parseLong(stampHigh.trim());
-          long low =
-            stampLow == null || stampHigh.trim().isEmpty() ? 0 :
-                Long.parseLong(stampLow.trim());
-
-          long ms = DateTools.getMillisFromTicks(high, low);
-
-          timestamps[image][stamp] = ms / 1000.0;
+          String timestamp = timeStamps.get(stamp);
+          timestamps[image][stamp] = translateSingleTimestamp(timestamp);
         }
       }
     }
+    else {
+      // probe if timestamps are saved in the format of LAS AF 3.0 or older
+      NodeList timestampNodes = getNodes(imageNode, "TimeStamp");
+      if (timestampNodes != null) {
+        // LAS AF 3.0 (or older) timestamps are available
+        for (int stamp = 0; stamp < timestampNodes.getLength(); stamp++) {
+          if (stamp < getImageCount()) {
+            Element timestamp = (Element) timestampNodes.item(stamp);
+            timestamps[image][stamp] = translateSingleTimestamp(timestamp);
+          }
+        }
+      }
+      else {
+        return;
+      }
+    }
+    
     acquiredDate[image] = timestamps[image][0];
+  }
 
-    NodeList relTimestampNodes = getNodes(imageNode, "RelTimeStamp");
-    if (relTimestampNodes != null) {
-      for (int stamp=0; stamp<relTimestampNodes.getLength(); stamp++) {
-        if (stamp < getImageCount()) {
-          Element timestamp = (Element) relTimestampNodes.item(stamp);
-          timestamps[image][stamp] =
-            new Double(timestamp.getAttribute("Time"));
-        }
-      }
-    }
+  private double translateSingleTimestamp(String timestamp) {
+    timestamp = timestamp.trim();
+    int stampLowStart = Math.max(0, timestamp.length() - 8);
+    int stampHighEnd = Math.max(0, stampLowStart - 1);
+    String stampHigh = timestamp.substring(0, stampHighEnd);
+    String stampLow = timestamp.substring(stampLowStart, timestamp.length());
+    long high
+            = stampHigh == null || stampHigh.trim().isEmpty() ? 0
+                    : Long.parseLong(stampHigh.trim(), 16);
+    long low
+            = stampLow == null || stampLow.trim().isEmpty() ? 0
+                    : Long.parseLong(stampLow.trim(), 16);
+    long milliseconds = DateTools.getMillisFromTicks(high, low);
+    double seconds = (double)milliseconds / 1000;
+    return seconds;
+  }
+  
+  private double translateSingleTimestamp(Element timestamp) {
+    String stampHigh = timestamp.getAttribute("HighInteger");
+    String stampLow = timestamp.getAttribute("LowInteger");
+    long high
+            = stampHigh == null || stampHigh.trim().isEmpty() ? 0
+                    : Long.parseLong(stampHigh.trim());
+    long low
+            = stampLow == null || stampLow.trim().isEmpty() ? 0
+                    : Long.parseLong(stampLow.trim());
+
+    long milliseconds = DateTools.getMillisFromTicks(high, low);
+    double seconds = (double)milliseconds / 1000;
+    return seconds;
   }
 
   private void translateFilterSettings(Element imageNode, int image)
