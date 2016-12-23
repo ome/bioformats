a62d5aea11a08e1b5b98429bca643f3e38806a35
Sebastien Besson <seb.besson@gmail.com>
2016-12-23 14:19:06 +0000

    dd minimal framework for testing the command line tools via TestNG
    This commit declares org.testng as a dependency of the bio-formats-tools module
    like the other components and provides a first test class for ImageConverter.
    As command-line tools classes have private constructors and use System.exit(),
    the security manager is overriden at set up and restored at teardown while
    individual methods invoke ImageConverter.main() directly.

---
+++ components/bio-formats-tools/test/loci/formats/tools/ImageConverterTest.java
@@ -0,0 +1,128 @@
+/*
+ * #%L
+ * Bio-Formats command line tools for reading and converting files
+ * %%
+ * Copyright (C) 2017 Open Microscopy Environment:
+ *   - Board of Regents of the University of Wisconsin-Madison
+ *   - Glencoe Software, Inc.
+ *   - University of Dundee
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package loci.formats.tools;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.Permission;
+
+import loci.formats.IFormatReader;
+import loci.formats.ImageReader;
+import loci.formats.ImageWriter;
+import loci.formats.FormatException;
+import loci.formats.tools.ImageConverter;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Tests the functionality of ImageConverter
+ */
+public class ImageConverterTest {
+
+  private File outFile;
+
+  protected static class ExitException extends SecurityException {
+    public final int status;
+    public ExitException(int status) {
+      this.status = status;
+    }
+  }
+
+  private static class NoExitSecurityManager extends SecurityManager {
+    @Override
+    public void checkPermission(Permission perm) {}
+
+    @Override
+    public void checkPermission(Permission perm, Object context) {}
+
+    @Override
+    public void checkExit(int status) {
+      super.checkExit(status);
+      throw new ExitException(status);
+    }
+  }
+
+  @BeforeMethod
+  public void setUp() {
+    System.setSecurityManager(new NoExitSecurityManager());
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    System.setSecurityManager(null);
+  }
+
+  @DataProvider(name = "suffixes")
+  public Object[][] createSuffixes() {
+    return new Object[][] {{".ome.tiff"}, {".tif"}, {".ics"}};
+  }
+
+  public void checkImage() throws FormatException, IOException {
+    IFormatReader r = new ImageReader();
+    r.setId(outFile.getAbsolutePath());
+    assertEquals(r.getSizeX(), 512);
+    r.close();
+  }
+
+  @Test(dataProvider = "suffixes")
+  public void testDefault(String suffix) throws FormatException, IOException {
+    outFile = File.createTempFile("test", suffix);
+    outFile.delete();
+    String[] args = {"test.fake", outFile.getAbsolutePath()};
+    try {
+      ImageConverter.main(args);
+    } catch (ExitException e) {
+      outFile.deleteOnExit();
+      assertEquals(e.status, 0);
+      checkImage();
+    }
+  }
+
+  @Test(dataProvider = "suffixes")
+  public void testOverwrite(String suffix) throws FormatException, IOException {
+    outFile = File.createTempFile("test", suffix);
+    String[] args = {"-overwrite", "test.fake", outFile.getAbsolutePath()};
+    try {
+      ImageConverter.main(args);
+    } catch (ExitException e) {
+      outFile.deleteOnExit();
+      assertEquals(e.status, 0);
+      checkImage();
+    }
+  }
+}
