94f26289c16302072d3d91f10b9359b3579a2721
Melissa Linkert <melissa@glencoesoftware.com>
2017-05-15 13:17:28 -0500

    TileJPEGReader: use the TurboJPEG service for all image reading
    JPEGTileDecoder is now only used for determining image dimensions, as
    the service does not provide that information.

--- components/formats-bsd/src/loci/formats/in/TileJPEGReader.java
+++ components/formats-bsd/src/loci/formats/in/TileJPEGReader.java
@@ -47,7 +47,7 @@ import loci.formats.services.JPEGTurboService;
 import loci.formats.services.JPEGTurboServiceImpl;
 
 /**
- * Reader for decoding JPEG images using java.awt.Toolkit.
+ * Reader for decoding JPEG images using java.awt.Toolkit and TurboJPEG.
  * This reader is useful for reading very large JPEG images, as it supports
  * tile-based access.
  *
@@ -58,8 +58,7 @@ public class TileJPEGReader extends FormatReader {
 
   // -- Fields --
 
-  private JPEGTileDecoder decoder;
-  private JPEGTurboService service;
+  private transient JPEGTurboService service;
 
   // -- Constructor --
 
@@ -82,18 +81,6 @@ public class TileJPEGReader extends FormatReader {
     if (service != null) {
       service.getTile(buf, x, y, w, h);
     }
-    else {
-      int c = getRGBChannelCount();
-
-      for (int ty=y; ty<y+h; ty++) {
-        byte[] scanline = decoder.getScanline(ty);
-        if (scanline == null) {
-          decoder.initialize(currentId, 0);
-          scanline = decoder.getScanline(ty);
-        }
-        System.arraycopy(scanline, c * x, buf, (ty - y) * c * w, c * w);
-      }
-    }
 
     return buf;
   }
@@ -103,10 +90,6 @@ public class TileJPEGReader extends FormatReader {
   public void close(boolean fileOnly) throws IOException {
     super.close(fileOnly);
     if (!fileOnly) {
-      if (decoder != null) {
-        decoder.close();
-      }
-      decoder = null;
       if (service != null) {
         service.close();
       }
@@ -122,7 +105,7 @@ public class TileJPEGReader extends FormatReader {
     super.initFile(id);
 
     in = new RandomAccessInputStream(id);
-    decoder = new JPEGTileDecoder();
+    JPEGTileDecoder decoder = new JPEGTileDecoder();
     decoder.initialize(in, 0, 1, 0);
 
     CoreMetadata m = core.get(0);
@@ -134,22 +117,12 @@ public class TileJPEGReader extends FormatReader {
     m.sizeY = decoder.getHeight();
     m.sizeZ = 1;
     m.sizeT = 1;
-    try {
-      m.sizeC = decoder.getScanline(0).length / getSizeX();
-    }
-    catch (Exception e) {
-      decoder.close();
-      in = new RandomAccessInputStream(id);
-      in.seek(0);
-      service = new JPEGTurboServiceImpl();
-      try {
-        service.initialize(in, m.sizeX, m.sizeY);
-      }
-      catch (ServiceException se) {
-        throw new FormatException("Could not initialize JPEG service", se);
-      }
-      m.sizeC = 3;
-    }
+
+    decoder.close();
+    decoder = null;
+    reopenFile();
+
+    m.sizeC = 3;
     m.rgb = getSizeC() > 1;
     m.imageCount = 1;
     m.pixelType = FormatTools.UINT8;
@@ -159,6 +132,22 @@ public class TileJPEGReader extends FormatReader {
 
     MetadataStore store = makeFilterMetadata();
     MetadataTools.populatePixels(store, this);
+  }
+
+  @Override
+  public void reopenFile() throws IOException {
+    if (in != null) {
+      in.close();
+    }
+    in = new RandomAccessInputStream(currentId);
+    in.seek(0);
+    service = new JPEGTurboServiceImpl();
+    try {
+      service.initialize(in, getSizeX(), getSizeY());
+    }
+    catch (ServiceException se) {
+      throw new IOException("Could not initialize JPEG service", se);
+    }
   }
 
 }
