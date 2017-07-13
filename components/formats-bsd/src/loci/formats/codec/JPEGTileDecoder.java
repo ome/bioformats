25688cefee967734b9b092efd74696637c9165e9
Roger Leigh <rleigh@dundee.ac.uk>
2017-07-13 14:05:25 +0100

    formats-bsd: JPEGTileDecoder: Delegate to ome.codecs.JPEGTileDecoder

--- components/formats-bsd/src/loci/formats/codec/JPEGTileDecoder.java
+++ components/formats-bsd/src/loci/formats/codec/JPEGTileDecoder.java
@@ -32,7 +32,60 @@
 
 package loci.formats.codec;
 
+import loci.common.RandomAccessInputStream;
+
 /**
  */
-public class JPEGTileDecoder extends ome.codecs.JPEGTileDecoder {
+public class JPEGTileDecoder {
+  private ome.codecs.JPEGTileDecoder decoder;
+
+  public JPEGTileDecoder() {
+    decoder = new ome.codecs.JPEGTileDecoder();
+  }
+
+  // -- JPEGTileDecoder API methods --
+
+  public void initialize(String id, int imageWidth) {
+    this.decoder.initialize(id, imageWidth);
+  }
+
+  public void initialize(RandomAccessInputStream in, int imageWidth) {
+    this.decoder.initialize(in, imageWidth);
+  }
+
+  public void initialize(RandomAccessInputStream in, int y, int h,
+    int imageWidth)
+  {
+    this.decoder.initialize(in, y, h, imageWidth);
+  }
+
+  /**
+    * Pre-process the stream to make sure that the
+    * image width and height are non-zero.  Returns an array containing
+    * the image width and height.
+    */
+  public int[] preprocess(RandomAccessInputStream in) {
+    return this.decoder.preprocess(in);
+  }
+
+  public byte[] getScanline(int y) {
+    return this.decoder.getScanline(y);
+  }
+
+  public int getWidth() {
+    return this.decoder.getWidth();
+  }
+
+  public int getHeight() {
+    return this.decoder.getHeight();
+  }
+
+  public void close() {
+    this.decoder.close();
+  }
+
+  ome.codecs.JPEGTileDecoder getWrapped() {
+    return this.decoder;
+  }
+
 }
