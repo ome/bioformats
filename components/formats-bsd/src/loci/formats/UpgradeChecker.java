15ef8173db1f83a47f2d020d46ebe143bf982911
jmoore <josh@glencoesoftware.com>
2017-07-13 15:24:00 +0200

    Drop SNAPSHOT

--- components/formats-bsd/src/loci/formats/UpgradeChecker.java
+++ components/formats-bsd/src/loci/formats/UpgradeChecker.java
@@ -64,7 +64,7 @@ public class UpgradeChecker {
   // -- Constants --
 
   /** Version number of the latest stable release. */
-  public static final String STABLE_VERSION = "0.4.0";
+  public static final String STABLE_VERSION = "0.4.1";
 
   /** Location of the OME continuous integration server. */
   public static final String CI_SERVER = "http://ci.openmicroscopy.org";
