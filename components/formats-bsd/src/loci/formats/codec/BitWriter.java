31654a4598db262ce4ff2f33991ecac5b94b3664
Roger Leigh <rleigh@dundee.ac.uk>
2017-07-13 13:54:30 +0100

    formats-bsd: BitWriter: Delegate to ome.codecs.BitWriter

--- components/formats-bsd/src/loci/formats/codec/BitWriter.java
+++ components/formats-bsd/src/loci/formats/codec/BitWriter.java
@@ -38,14 +38,52 @@ package loci.formats.codec;
  * @author Curtis Rueden ctrueden at wisc.edu
  * @deprecated Use loci.common.RandomAccessOutputStream instead
  */
-public class BitWriter extends ome.codecs.BitWriter{
+public class BitWriter {
+  private ome.codecs.BitWriter writer;
+
+  // -- Constructors --
+
   /** Constructs a new bit writer. */
   public BitWriter() {
-    super();
+    writer = new ome.codecs.BitWriter();
   }
 
   /** Constructs a new bit writer with the given initial buffer size. */
   public BitWriter(int size) {
-    super(size);
+    writer = new ome.codecs.BitWriter(size);
   }
+
+  // -- BitWriter API methods --
+
+  /** Writes the given value using the given number of bits. */
+  public void write(int value, int numBits) {
+    this.writer.write(value, numBits);
+  }
+
+  /**
+   * Writes the bits represented by a bit string to the buffer.
+   *
+   * @throws IllegalArgumentException If any characters other than
+   *   '0' and '1' appear in the string.
+   */
+  public void write(String bitString) {
+    this.writer.write(bitString);
+  }
+
+  /** Gets an array containing all bits written thus far. */
+  public byte[] toByteArray() {
+    return this.writer.toByteArray();
+  }
+
+  ome.codecs.BitWriter getWrapped() {
+    return this.writer;
+  }
+
+  // -- Main method --
+
+  /** Tests the BitWriter class. */
+  public static void main(String[] args) {
+    ome.codecs.BitWriter.main(args);
+  }
+
 }
