2542475ab7238cbed8d366a944fd79ee1ed022a1
Roger Leigh <rleigh@dundee.ac.uk>
2018-02-28 11:16:48 +0000

    Remove package bundles

--- tools/test-build.bat
+++ tools/test-build.bat
@@ -21,7 +21,4 @@ if [%build%] == [ant] (
   ant clean compile-tests || exit /b 1
   ant clean compile-turbojpeg || exit /b 1
   ant clean utils || exit /b 1
-  REM Do not clean here so that we can potentially archive both
-  REM docs and java archives.
-  ant tools dist-bftools || exit /b 1
 )
