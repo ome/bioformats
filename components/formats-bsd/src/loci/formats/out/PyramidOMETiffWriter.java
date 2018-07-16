cee1a0aca1ec520c93f11a7c7c146d1a735ce409
Melissa Linkert <melissa@glencoesoftware.com>
2018-07-16 13:04:19 -0500

    Add empty PyramidOMETiffWriter
    Only type detection works at the moment.  PyramidOMETiffWriter is used
    when an OME-TIFF suffix is present and an IPyramidStore with multiple
    resolutions was passed to setMetadataRetrieve(...).
    PyramidOMETiffWriter takes precedence over OMETiffWriter.

---
+++ components/formats-bsd/src/loci/formats/out/PyramidOMETiffWriter.java
@@ -0,0 +1,91 @@
+/*
+ * #%L
+ * BSD implementations of Bio-Formats readers and writers
+ * %%
+ * Copyright (C) 2018 Open Microscopy Environment:
+ *   - Board of Regents of the University of Wisconsin-Madison
+ *   - Glencoe Software, Inc.
+ *   - University of Dundee
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package loci.formats.out;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import ome.xml.meta.OMEXMLMetadataRoot;
+import ome.xml.model.primitives.NonNegativeInteger;
+import ome.xml.model.primitives.PositiveInteger;
+
+import loci.common.Location;
+import loci.common.Constants;
+import loci.common.RandomAccessInputStream;
+import loci.common.RandomAccessOutputStream;
+import loci.common.services.DependencyException;
+import loci.common.services.ServiceException;
+import loci.common.services.ServiceFactory;
+import loci.common.xml.XMLTools;
+import loci.formats.FormatException;
+import loci.formats.FormatTools;
+import loci.formats.meta.IPyramidStore;
+import loci.formats.meta.MetadataRetrieve;
+import loci.formats.ome.OMEXMLMetadata;
+import loci.formats.services.OMEXMLService;
+import loci.formats.tiff.IFD;
+import loci.formats.tiff.TiffSaver;
+import loci.formats.in.MetadataOptions;
+import loci.formats.in.DynamicMetadataOptions;
+
+/**
+ * PyramidOMETiffWriter is the file format writer for pyramid OME-TIFF files.
+ */
+public class PyramidOMETiffWriter extends OMETiffWriter {
+
+  // -- Constructor --
+
+  // -- IFormatHandler API methods --
+
+  /* @see IFormatHandler#isThisType(String) */
+  @Override
+  public boolean isThisType(String name) {
+    if (!super.isThisType(name)) {
+      return false;
+    }
+    MetadataRetrieve r = getMetadataRetrieve();
+    if (!(r instanceof IPyramidStore)) {
+      return false;
+    }
+    return ((IPyramidStore) r).getResolutionCount(0) > 1;
+  }
+
+  // -- IFormatWriter API methods --
+
+
+}
