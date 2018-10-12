98b43cdc37926de78d0ce020a1b3dc2cdf6e4551
Melissa Linkert <melissa@glencoesoftware.com>
2018-10-12 09:39:06 -0500

    Progress on tile stitching for the full resolution image
    Not done yet, as the overlaps are still not quite right.  This is closer
    than the previous version with no overlap calculation though.

--- components/formats-gpl/src/loci/formats/in/VentanaReader.java
+++ components/formats-gpl/src/loci/formats/in/VentanaReader.java
@@ -31,6 +31,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,6 +40,8 @@ import loci.common.Constants;
 import loci.common.DataTools;
 import loci.common.DateTools;
 import loci.common.RandomAccessInputStream;
+import loci.common.Region;
+import loci.common.xml.XMLTools;
 import loci.formats.CoreMetadata;
 import loci.formats.FormatException;
 import loci.formats.FormatTools;
@@ -53,6 +56,12 @@ import ome.xml.model.primitives.Timestamp;
 
 import ome.units.quantity.Length;
 import ome.units.UNITS;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  * VentanaReader is the file format reader for Ventana .bif files.
@@ -78,6 +87,9 @@ public class VentanaReader extends BaseTiffReader {
   // -- Fields --
 
   private Double magnification = null;
+  private List<AreaOfInterest> areas = new ArrayList<AreaOfInterest>();
+  private int tileWidth, tileHeight;
+  private TIFFTile[] tiles;
 
   // -- Constructor --
 
@@ -147,8 +159,66 @@ public class VentanaReader extends BaseTiffReader {
     if (tiffParser == null) {
       initTiffParser();
     }
-    int ifd = getIFDIndex(getCoreIndex(), no);
-    tiffParser.getSamples(ifds.get(ifd), buf, x, y, w, h);
+    Arrays.fill(buf, (byte) 0);
+    IFD ifd = ifds.get(getIFDIndex(getCoreIndex(), no));
+    if (getCoreIndex() >= core.get(0).resolutionCount) {
+      tiffParser.getSamples(ifd, buf, x, y, w, h);
+      return buf;
+    }
+
+    Region imageBox = new Region(x, y, w, h);
+
+    int bpp = FormatTools.getBytesPerPixel(getPixelType());
+    int pixel = bpp * getRGBChannelCount();
+    int outputRowLen = w * bpp;
+    boolean interleaved = isInterleaved();
+    if (interleaved) {
+      outputRowLen *= getRGBChannelCount();
+    }
+    byte[] tilePixels = new byte[tileWidth * tileHeight * pixel];
+    for (TIFFTile tile : tiles) {
+      Region tileBox = new Region(tile.realX, tile.realY, tileWidth, tileHeight);
+
+      if (tileBox.intersects(imageBox)) {
+        tiffParser.getSamples(ifd, tilePixels, tile.baseX, tile.baseY, tileWidth, tileHeight);
+
+        Region intersection = tileBox.intersection(imageBox);
+        int intersectionX = 0;
+
+        if (tileBox.x < imageBox.x) {
+          intersectionX = imageBox.x - tileBox.x;
+        }
+
+        int rowLen = (int) Math.min(intersection.width, tileWidth) * bpp;
+        if (interleaved) {
+          rowLen *= getRGBChannelCount();
+        }
+
+        int count = interleaved ? 1 : getRGBChannelCount();
+        for (int c=0; c<count; c++) {
+          for (int row=0; row<intersection.height; row++) {
+            int realRow = row + intersection.y - tileBox.y;
+            int inputOffset = bpp * (realRow * tileWidth + intersection.x - tileBox.x);
+            if (interleaved) {
+              inputOffset *= getRGBChannelCount();
+            }
+            else {
+              inputOffset += (c * tileWidth * tileHeight * bpp);
+            }
+            int outputOffset = bpp * (intersection.x - x) + outputRowLen * (row + intersection.y - y);
+            if (interleaved) {
+              outputOffset *= getRGBChannelCount();
+            }
+            else {
+              outputOffset += (c * w * h * bpp);
+            }
+            int copy = (int) Math.min(rowLen, buf.length - outputOffset);
+            System.arraycopy(tilePixels, inputOffset, buf, outputOffset, copy);
+          }
+        }
+      }
+    }
+
     return buf;
   }
 
@@ -164,6 +234,10 @@ public class VentanaReader extends BaseTiffReader {
     super.close(fileOnly);
     if (!fileOnly) {
       magnification = null;
+      areas.clear();
+      tileWidth = 0;
+      tileHeight = 0;
+      tiles = null;
     }
   }
 
@@ -237,6 +311,9 @@ public class VentanaReader extends BaseTiffReader {
       }
       String xml = ifds.get(index).getIFDTextValue(XML_TAG);
       LOGGER.debug("XMP tag for series #{} = {}", i, xml);
+      if (xml != null && resolutionCount == 1) {
+        parseXML(xml);
+      }
     }
     setSeries(0);
 
@@ -249,7 +326,8 @@ public class VentanaReader extends BaseTiffReader {
       ms.sizeT = 1;
       ms.imageCount = ms.sizeZ * ms.sizeT;
 
-      IFD ifd = ifds.get(getIFDIndex(s, 0));
+      int ifdIndex = getIFDIndex(s, 0);
+      IFD ifd = ifds.get(ifdIndex);
       PhotoInterp p = ifd.getPhotometricInterpretation();
       int samples = ifd.getSamplesPerPixel();
       ms.rgb = samples > 1 || p == PhotoInterp.RGB;
@@ -266,7 +344,84 @@ public class VentanaReader extends BaseTiffReader {
       ms.falseColor = false;
       ms.dimensionOrder = "XYCZT";
       ms.thumbnail = s != 0;
+
+      if (s == 0) {
+        tileWidth = (int) ifd.getTileWidth();
+        tileHeight = (int) ifd.getTileLength();
+        long[] offsets = ifd.getStripOffsets();
+        int x = 0, y = 0;
+        tiles = new TIFFTile[offsets.length];
+        for (int i=0; i<offsets.length; i++) {
+          tiles[i] = new TIFFTile();
+          tiles[i].offset = offsets[i];
+          tiles[i].ifd = ifdIndex;
+          tiles[i].realX = -1 * tileWidth;
+          tiles[i].realY = -1 * tileHeight;
+          tiles[i].baseX = tileWidth * (x++);
+          tiles[i].baseY = tileHeight * y;
+          if (x * tileWidth >= ms.sizeX) {
+            y++;
+            x = 0;
+          }
+        }
+      }
     }
+
+    // now process TIFF tiles and overlap data to get the real coordinates for each tile
+    int tileCols = core.get(0).sizeX / tileWidth;
+    for (AreaOfInterest area : areas) {
+      int tileRow = area.yOrigin / tileHeight;
+      int tileCol = area.xOrigin / tileWidth;
+
+      for (int row=0; row<area.tileRows; row++) {
+        for (int col=0; col<area.tileColumns; col++) {
+          int index = (tileRow + row) * tileCols + (tileCol + col);
+          tiles[index].realX = tiles[index].baseX;
+          tiles[index].realY = tiles[index].baseY;
+        }
+      }
+
+      // largest tile index is in upper right corner
+      // smallest tile index is in lower left corner (snake ordering)
+
+      for (Overlap overlap : area.overlaps) {
+        int thisRow = getTileRow(overlap.a, area.tileRows, area.tileColumns);
+        int thisCol = getTileColumn(overlap.a, area.tileRows, area.tileColumns);
+        if (overlap.direction.equals("RIGHT")) {
+          for (int col=0; col<=thisCol; col++) {
+            int currentIndex = (tileRow + thisRow) * tileCols + (tileCol + col);
+            tiles[currentIndex].realX += overlap.x;
+          }
+          int currentIndex = (tileRow + thisRow) * tileCols + (tileCol + thisCol);
+          tiles[currentIndex].realY += overlap.y;
+        }
+        else if (overlap.direction.equals("UP")) {
+          for (int row=thisRow; row<area.tileRows; row++) {
+            int currentIndex = (tileRow + row) * tileCols + (tileCol + thisCol);
+            tiles[currentIndex].realY -= overlap.y;
+          }
+        }
+        else {
+          throw new FormatException("Unsupported overlap direction: " + overlap.direction);
+        }
+      }
+    }
+  }
+
+  private int getTileRow(int index, int rows, int cols) {
+    int row = (int) Math.floor((double) index / cols);
+    return rows - row - 1;
+  }
+
+  private int getTileColumn(int index, int rows, int cols) {
+    int row = (int) Math.floor((double) index / cols);
+    // even numbered rows increase from left to right
+    // odd numbered rows decrease from left to right
+    int col = index - (row * cols);
+    if (row % 2 == 1) {
+      return cols - col - 1;
+    }
+    return col;
   }
 
   /* @see loci.formats.BaseTiffReader#initMetadataStore() */
@@ -297,6 +452,113 @@ public class VentanaReader extends BaseTiffReader {
       return extra + (coreIndex * core.get(0).imageCount) + no;
     }
     return coreIndex - (core.size() - extra);
+  }
+
+  private void parseXML(String xml) throws IOException {
+    Element root = null;
+    try {
+      root = XMLTools.parseDOM(xml).getDocumentElement();
+    }
+    catch (ParserConfigurationException | SAXException e) {
+      throw new IOException(e);
+    }
+    Element slideStitchInfo = (Element) root.getElementsByTagName("SlideStitchInfo").item(0);
+    Element aoiOrigins = (Element) root.getElementsByTagName("AoiOrigin").item(0);
+
+    NodeList imageInfos = slideStitchInfo.getElementsByTagName("ImageInfo");
+    for (int i=0; i<imageInfos.getLength(); i++) {
+      Element imageInfo = (Element) imageInfos.item(i);
+
+      if (imageInfo.getAttribute("AOIScanned").equals("0")) {
+        continue;
+      }
+      AreaOfInterest aoi = new AreaOfInterest();
+      aoi.index = Integer.parseInt(imageInfo.getAttribute("AOIIndex"));
+      aoi.tileRows = Integer.parseInt(imageInfo.getAttribute("NumRows"));
+      aoi.tileColumns = Integer.parseInt(imageInfo.getAttribute("NumCols"));
+
+      NodeList joints = imageInfo.getElementsByTagName("TileJointInfo");
+      for (int j=0; j<joints.getLength(); j++) {
+        Element joint = (Element) joints.item(j);
+        if (joint.getAttribute("FlagJoined").equals("1")) {
+          Overlap overlap = new Overlap();
+          overlap.a = Integer.parseInt(joint.getAttribute("Tile1")) - 1;
+          overlap.b = Integer.parseInt(joint.getAttribute("Tile2")) - 1;
+          overlap.x = DataTools.parseDouble(joint.getAttribute("OverlapX")).intValue();
+          overlap.y = DataTools.parseDouble(joint.getAttribute("OverlapY")).intValue();
+          overlap.direction = joint.getAttribute("Direction");
+          aoi.overlaps.add(overlap);
+        }
+      }
+      areas.add(aoi);
+    }
+
+    NodeList aois = aoiOrigins.getChildNodes();
+    for (int i=0; i<aois.getLength(); i++) {
+      String name = aois.item(i).getNodeName();
+      if (!name.startsWith("AOI")) {
+        continue;
+      }
+      Element aoi = (Element) aois.item(i);
+      int thisIndex = Integer.parseInt(name.replace("AOI", ""));
+      for (AreaOfInterest a : areas) {
+        if (a.index == thisIndex) {
+          a.xOrigin = Integer.parseInt(aoi.getAttribute("OriginX"));
+          a.yOrigin = Integer.parseInt(aoi.getAttribute("OriginY"));
+        }
+      }
+    }
+  }
+
+
+  class AreaOfInterest {
+    // origin in pixels
+    public int xOrigin;
+    public int yOrigin;
+    public int index;
+    public int tileRows;
+    public int tileColumns;
+    public List<Overlap> overlaps = new ArrayList<Overlap>();
+
+    @Override
+    public String toString() {
+      return "AOI #" + index + ", X=" + xOrigin + ", Y=" + yOrigin +
+        ", rows=" + tileRows + ", cols=" + tileColumns +
+        ", overlaps=" + overlaps.size();
+    }
+  }
+
+  class Overlap implements Comparable<Overlap> {
+    public int a;
+    public int b;
+    public int x;
+    public int y;
+    public String direction;
+
+    public int compareTo(Overlap o) {
+      if (a != o.a) {
+        return a - o.a;
+      }
+      return b - o.b;
+    }
+
+    @Override
+    public String toString() {
+      return a + " => " + b + ", overlap = {" + x + ", " + y +
+        "}, direction = " + direction;
+    }
+  }
+
+  class TIFFTile {
+    public int ifd;
+    public long offset;
+    // these are the XY coordinates if we were placing the tile
+    // as in standard TIFF, with no overlaps
+    public int baseX;
+    public int baseY;
+    // these are the XY coordinates after overlap calculation
+    public int realX;
+    public int realY;
   }
 
 }
