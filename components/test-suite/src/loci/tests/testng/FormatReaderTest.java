ffd84d74c944ddecc7595817143109769613c7fa
Melissa Linkert <melissa@glencoesoftware.com>
2018-10-24 20:47:06 -0500

    Update testIsThisType and testSaneUsedFiles to pass for Operetta metadata files

--- components/test-suite/src/loci/tests/testng/FormatReaderTest.java
+++ components/test-suite/src/loci/tests/testng/FormatReaderTest.java
@@ -1646,6 +1646,12 @@ public class FormatReaderTest {
             }
           }
 
+          // extra metadata files in Harmony/Operetta datasets
+          // cannot be used for type detection
+          if (reader.getFormat().equals("PerkinElmer Operetta")) {
+            continue;
+          }
+
           // Volocity datasets can only be detected with the .mvd2 file
           if (file.toLowerCase().endsWith(".mvd2") &&
             !base[i].toLowerCase().endsWith(".mvd2"))
@@ -2435,6 +2441,11 @@ public class FormatReaderTest {
 
             // Inveon only reliably detected from header file
             if (!result && r instanceof InveonReader) {
+              continue;
+            }
+
+            // Operetta only reliably detects from Index.*.xml
+            if (!result && r instanceof OperettaReader) {
               continue;
             }
 
