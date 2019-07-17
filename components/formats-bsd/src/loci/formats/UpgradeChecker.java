961442c8aab1fe233ad06f6f0c673cf18c51fcb4
Snoopy Crime Cop <snoopycrimecop@gmail.com>
2019-07-17 18:14:47 +0100

    Update POM version numbers to 6.2.0
    Repository: openmicroscopy/bioformats
    Already up-to-date.
    
    Generated by BIOFORMATS-DEV-latest-version-bump#107 (https://ci.openmicroscopy.org/job/BIOFORMATS-DEV-latest-version-bump/107/)

--- components/formats-bsd/src/loci/formats/UpgradeChecker.java
+++ components/formats-bsd/src/loci/formats/UpgradeChecker.java
@@ -64,7 +64,7 @@ public class UpgradeChecker {
   // -- Constants --
 
   /** Version number of the latest stable release. */
-  public static final String STABLE_VERSION = "6.1.1";
+  public static final String STABLE_VERSION = "6.2.0";
 
   /** Location of the OME continuous integration server. */
   public static final String CI_SERVER = "http://ci.openmicroscopy.org";
