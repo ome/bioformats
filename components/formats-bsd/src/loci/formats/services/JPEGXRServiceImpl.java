2381e5a9be04fc59b0c8b789e98ced8ab611b105
Melissa Linkert <melissa@glencoesoftware.com>
2020-03-03 11:58:28 -0600

    JPEG-XR: rework BGR correction
    Revert to using Decode.decodeFirstFrame(...) instead of constructing a new Decode object
    to get the raw pixels and add metadata parsing to get the pixel type directly.

--- components/formats-bsd/src/loci/formats/services/JPEGXRServiceImpl.java
+++ components/formats-bsd/src/loci/formats/services/JPEGXRServiceImpl.java
@@ -32,10 +32,13 @@
 
 package loci.formats.services;
 
-import java.nio.ByteBuffer;
+import java.io.IOException;
 
+import loci.common.RandomAccessInputStream;
 import loci.common.services.AbstractService;
 import loci.formats.FormatException;
+import loci.formats.tiff.IFD;
+import loci.formats.tiff.TiffParser;
 
 import ome.jxrlib.Decode;
 
@@ -46,6 +49,15 @@ import org.slf4j.LoggerFactory;
  * Interface defining methods for working with JPEG-XR data
  */
 public class JPEGXRServiceImpl extends AbstractService implements JPEGXRService {
+
+  // see table A.4 in ITU-T T.832
+  private static final int PIXEL_FORMAT_TAG = 0xbc01;
+
+  // see table A.6 in ITU-T T.832
+  private static final short BGR_24 = 0x0c;
+  private static final short BGR_32 = 0x0e;
+  private static final short BGRA_32 = 0x0f;
+  private static final short PBGRA_32 = 0x10;
 
   private static final Logger LOGGER =
     LoggerFactory.getLogger(JPEGXRServiceImpl.class);
@@ -60,30 +72,47 @@ public class JPEGXRServiceImpl extends AbstractService implements JPEGXRService 
   public byte[] decompress(byte[] compressed) throws FormatException {
       LOGGER.trace("begin tile decode; compressed size = {}", compressed.length);
       try {
-        Decode decode = new Decode(compressed);
-        boolean bgr = decode.isBGR();
-        int bpp = (int) decode.getBytesPerPixel();
-        int pixels = (int) (decode.getWidth() * decode.getHeight());
-        ByteBuffer output = ByteBuffer.allocateDirect(pixels * bpp);
-        decode.toBytes(output);
+        byte[] raw = Decode.decodeFirstFrame(compressed, 0, compressed.length);
+        short[] format = getPixelFormat(compressed);
 
-        if (bgr) {
+        if (isBGR(format)) {
+          int bpp = getBGRComponents(format);
           // only happens with 8 bits per channel,
           // 3 (BGR) or 4 (BGRA) channel data
-          for (int p=0; p<output.capacity(); p+=bpp) {
-            byte tmp = output.get(p);
-            output.put(p, output.get(p + 2));
-            output.put(p + 2, tmp);
+          for (int p=0; p<raw.length; p+=bpp) {
+            byte tmp = raw[p];
+            raw[p] = raw[p + 2];
+            raw[p + 2] = tmp;
           }
         }
-        byte[] raw = new byte[output.capacity()];
-        output.get(raw);
         return raw;
       }
       // really only want to catch ome.jxrlib.FormatError, but that doesn't compile
       catch (Exception e) {
         throw new FormatException(e);
       }
+  }
+
+  private short[] getPixelFormat(byte[] stream) throws FormatException, IOException {
+    try (RandomAccessInputStream s = new RandomAccessInputStream(stream)) {
+      s.order(true);
+      s.seek(4);
+      long ifdPointer = s.readInt();
+      TiffParser p = new TiffParser(s);
+      IFD ifd = p.getIFD(ifdPointer);
+      return ifd.getIFDShortArray(PIXEL_FORMAT_TAG);
+    }
+  }
+
+  private boolean isBGR(short[] format) {
+    short lastByte = format[format.length - 1];
+    return lastByte == BGR_24 || lastByte == BGR_32 ||
+      lastByte == BGRA_32 || lastByte == PBGRA_32;
+  }
+
+  private int getBGRComponents(short[] format) {
+    short lastByte = format[format.length - 1];
+    return lastByte == BGR_24 || lastByte == BGR_32 ? 3 : 4;
   }
 
 }
