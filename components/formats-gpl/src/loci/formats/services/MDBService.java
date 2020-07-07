560f42a2c47018568e6a2fe32aadeb258ee9626a
Melissa Linkert <melissa@glencoesoftware.com>
2020-07-06 20:09:04 -0500

    MDB service: add API to retrieve a single table

--- components/formats-gpl/src/loci/formats/services/MDBService.java
+++ components/formats-gpl/src/loci/formats/services/MDBService.java
@@ -58,6 +58,21 @@ public interface MDBService extends Service {
    */
   public Vector<Vector<String[]>> parseDatabase() throws IOException;
 
+  /**
+   * Read named table from a pre-initialized .mdb files.
+   * Each String[] in a table represents a row, and each element of the String[]
+   * represents the value of a particular column within the row.
+   *
+   * The first row in the table contains the names for each column.
+   *
+   * {@link #initialize(String)} must be called before calling parseTable.
+   *
+   * @param name table name
+   * @return table data or null if the named table does not exist
+   * @throws IOException if there is a problem reading the table data
+   */
+  public Vector<String[]> parseTable(String name) throws IOException;
+
   /** Close the currently initialized file. */
   public void close();
 
