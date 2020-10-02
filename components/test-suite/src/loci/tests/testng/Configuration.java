e90acef1fd21e4e08d14508653531399daa07a09
Sebastien Besson <seb.besson@gmail.com>
2020-10-02 21:51:35 +0100

    Fix import

--- components/test-suite/src/loci/tests/testng/Configuration.java
+++ components/test-suite/src/loci/tests/testng/Configuration.java
@@ -42,6 +42,7 @@ import loci.formats.FormatException;
 import loci.formats.FormatTools;
 import loci.formats.IFormatReader;
 import loci.formats.ImageReader;
+import loci.formats.in.DefaultMetadataOptions;
 import loci.formats.ReaderWrapper;
 import loci.formats.meta.IMetadata;
 
