6a75a0d92863fb7b75be27f937d623b7fd1e04c7
David Gault <d.gault@dundee.ac.uk>
2022-02-16 15:40:00 +0000

    OBFReader: Add Java version specific comments
    Add an additional comment outlining expectations of different Java versions when a DataFormatException exception occurs for older OBF files

--- components/formats-bsd/src/loci/formats/in/OBFReader.java
+++ components/formats-bsd/src/loci/formats/in/OBFReader.java
@@ -748,6 +748,11 @@ public class OBFReader extends FormatReader {
           remainingBytes -= decompressedBytes;
         }
         catch (DataFormatException exception) {
+          /* For older OBF files a zlib error may occur after all bytes have been decompressed.
+           In this scenario we will attempt to ignore the error if no bytes remain and output a warning.
+           This will only work for Java versions 11 and upwards, prior versions will still throw a FormatException.
+           See https://github.com/openjdk/jdk/commit/883d41fefc2b5da40b159b24e7387f3bdbd22a5a for more details */
+
           long bytesWrittenAfter = inflater.getBytesWritten();
 
           long decompressedBytes = bytesWrittenAfter - bytesWrittenBefore;
@@ -756,7 +761,8 @@ public class OBFReader extends FormatReader {
           remainingBytes -= decompressedBytes;
 
           if(remainingBytes != 0) {
-            throw new FormatException(exception.getMessage());
+            throw new FormatException(
+                "Error while deflating stream, upgrading to Java 11 or later may help", exception);
           } else {
             LOGGER.warn("Error past the end of deflated stream", exception);
           }
