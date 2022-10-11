26e83eb46d399859fe36cc2491a34a91b6b2c0b0
David Gault <d.gault@dundee.ac.uk>
2022-10-11 16:26:56 +0100

    Replace Zstd codec implementation with WrappedCodec

--- components/formats-bsd/src/loci/formats/codec/ZstdCodec.java
+++ components/formats-bsd/src/loci/formats/codec/ZstdCodec.java
@@ -1,59 +1,39 @@
+/*
+ * #%L
+ * BSD implementations of Bio-Formats readers and writers
+ * %%
+ * Copyright (C) 2005 - 2022 Open Microscopy Environment:
+ *   - Board of Regents of the University of Wisconsin-Madison
+ *   - Glencoe Software, Inc.
+ *   - University of Dundee
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
 package loci.formats.codec;
 
-import loci.common.RandomAccessInputStream;
-import loci.formats.FormatException;
-import java.io.EOFException;
-import java.io.IOException;
-import io.airlift.compress.zstd.ZstdDecompressor;
-import loci.formats.UnsupportedCompressionException;
-
-/**
- * This class implements Zstandard decompression.
- *
- * @author Wim Pomp w.pomp at nki.nl
- */
-public class ZstdCodec extends BaseCodec {
-
-    @Override
-    public byte[] compress(byte[] data, CodecOptions options)
-        throws FormatException
-    {
-        if (data == null || data.length == 0)
-            throw new IllegalArgumentException("No data to compress");
-        // TODO: Add compression support.
-        throw new UnsupportedCompressionException("Zstandard Compression not currently supported.");
-    }
-
-    @Override
-    public byte[] decompress(RandomAccessInputStream in, CodecOptions options)
-        throws FormatException, IOException
-    {
-        ByteVector bytes = new ByteVector();
-        byte[] buf = new byte[8192];
-        int r;
-        // read until eof reached
-        try {
-            while ((r = in.read(buf, 0, buf.length)) > 0) bytes.add(buf, 0, r);
-        }
-        catch (EOFException ignored) { }
-
-        byte[] data = bytes.toByteArray();
-        return decompress(data);
-    }
-    
-    @Override
-    public byte[] decompress(byte[] data)
-        throws FormatException
-    {
-        return decompress(data, 0, data.length);
-    }
-    
-    public byte[] decompress(byte[] data, int inputOffset, int length)
-        throws FormatException
-    {
-        ZstdDecompressor decompressor = new ZstdDecompressor();
-        byte[] output = new byte[(int) ZstdDecompressor.getDecompressedSize(data, inputOffset, length)];
-        decompressor.decompress(data, inputOffset, length, output, 0, output.length);
-        return output;
-    }
+public class ZstdCodec extends WrappedCodec {
+  public ZstdCodec() {
+    super(new ome.codecs.ZstdCodec());
+  }
 }
