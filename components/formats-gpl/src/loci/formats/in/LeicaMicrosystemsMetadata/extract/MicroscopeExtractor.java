73be214df12aa7a358b01142c437a2b96edcec0e
XLEFReaderForBioformats <constanze.wendlandt@leica-microsystems.com>
2024-03-04 14:29:22 +0100

    adding immersion and correction lookup functions from objective name string (which is not as formatted as assumed)

--- components/formats-gpl/src/loci/formats/in/LeicaMicrosystemsMetadata/extract/MicroscopeExtractor.java
+++ components/formats-gpl/src/loci/formats/in/LeicaMicrosystemsMetadata/extract/MicroscopeExtractor.java
@@ -92,8 +92,7 @@ public class MicroscopeExtractor {
     Objective objective = new Objective();
     // e.g. "HC PL APO    20x/0.70 DRY" or "HCX PL FLUOTAR    40x/0.75 DRY"
     objective.model = setting.getAttribute("ObjectiveName");
-    String[] objValues = objective.model.split(" +");
-    objective.correction = objValues.length > 2 ? objValues[2] : "";
+    objective.setCorrectionFromObjectiveName(objective.model);
     String naS = setting.getAttribute("NumericalAperture");
     objective.numericalAperture = Extractor.parseDouble(naS);
     objective.objectiveNumber = setting.getAttribute("ObjectiveNumber");
--- components/formats-gpl/src/loci/formats/in/LeicaMicrosystemsMetadata/model/Objective.java
+++ components/formats-gpl/src/loci/formats/in/LeicaMicrosystemsMetadata/model/Objective.java
@@ -1,5 +1,9 @@
 package loci.formats.in.LeicaMicrosystemsMetadata.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Objective {
   public String model;
   public double numericalAperture;
@@ -8,4 +12,65 @@ public class Objective {
   public String immersion;
   public double refractionIndex;
   public String correction;
+
+  /**
+   * Tries to find correction value in objective name string
+   * @param objectiveName e.g. "HC PL APO    20x/0.70 DRY" or "HCX PL FLUOTAR    40x/0.75 DRY"
+   *  or "HCX PL APO lambda blue  63.0x1.40 OIL  UV" or "HCX APO L  20.0x1.00 WATER "
+   */
+  public void setCorrectionFromObjectiveName(String objectiveName){
+    String[] objValues = objectiveName.split(" +");
+
+    List<String> corrections = new ArrayList<String>(
+    Arrays.asList("PlanApo",
+    "PlanFluor",
+    "SuperFluor",
+    "VioletCorrected", 	
+    "Achro",
+    "Achromat", 	
+    "Fluor",	
+    "Fl",
+    "Fluar",
+    "Neofluar",	
+    "Fluotar", 	
+    "Apo",
+    "PlanNeofluar",
+    "UV"));
+
+    for (String value : objValues){
+      for (String correction : corrections){
+        if (correction.equalsIgnoreCase(value)){
+          this.correction = value;
+          return;
+        }
+      }
+    }
+  }
+
+  /**
+   * Tries to find immersion value in objective name string
+   * @param objectiveName e.g. "HC PL APO    20x/0.70 DRY" or "HCX PL FLUOTAR    40x/0.75 DRY"
+   *  or "HCX PL APO lambda blue  63.0x1.40 OIL  UV" or "HCX APO L  20.0x1.00 WATER "
+   */
+  public void setImmersionFromObjectiveName(String objectiveName){
+    String[] objValues = objectiveName.split(" +");
+    
+    List<String> immersions = new ArrayList<String>(
+      Arrays.asList("Oil",
+      "Water",
+      "WaterDipping",
+      "Air",
+      "Dry",
+      "Multi",
+      "Glycerol"));
+
+      for (String value : objValues){
+        for (String immersion : immersions){
+          if (immersion.equalsIgnoreCase(value)){
+            this.immersion = value;
+            return;
+          }
+        }
+      }
+  }
 }
