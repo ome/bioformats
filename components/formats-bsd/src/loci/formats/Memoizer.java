547935e72e0eb19f29a95e9d74ee60cd5cf3a1ec
Kilian Paul <81327374+Darlokt@users.noreply.github.com>
2026-08-02 22:36:20 +0200

    Revert internal double increase for testing

--- components/formats-bsd/src/loci/formats/Memoizer.java
+++ components/formats-bsd/src/loci/formats/Memoizer.java
@@ -520,7 +520,7 @@ public class Memoizer extends ReaderWrapper {
    * cached items. This should happen when the order and type of objects stored
    * in the memo file changes.
    */
-  public static final Integer VERSION = 6;
+  public static final Integer VERSION = 5;
 
   /**
    * Default value for {@link #minimumElapsed} if none is provided in the
